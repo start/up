91646da14bbf1631c4a931af00f0021940ba8d37
Daniel Miller <daniel@wants.email>
2015-12-28 07:05:10 -0600

    Add helper to remove repetitiveness from tests

--- built/Test/ast.js
+++ built/Test/ast.js
@@ -7,6 +7,9 @@ var StressNode_1 = require('../SyntaxNodes/StressNode');
 var InlineCodeNode_1 = require('../SyntaxNodes/InlineCodeNode');
 var RevisionInsertionNode_1 = require('../SyntaxNodes/RevisionInsertionNode');
 var RevisionDeletionNode_1 = require('../SyntaxNodes/RevisionDeletionNode');
+function insideDocument(syntaxNodes) {
+    return new DocumentNode_1.DocumentNode(syntaxNodes);
+}
 describe('No text', function () {
     it('creates only a document node', function () {
         chai_1.expect(Up.ast('')).to.be.eql(new DocumentNode_1.DocumentNode());
@@ -14,41 +17,41 @@ describe('No text', function () {
 });
 describe('Text', function () {
     it('is put inside a plain text node', function () {
-        chai_1.expect(Up.ast('Hello, world!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, world!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, world!')
         ]));
     });
 });
 describe('A backslash', function () {
     it('causes the following character to be treated as plain text', function () {
-        chai_1.expect(Up.ast('Hello, \\world!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, \\world!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, world!')
         ]));
     });
     it('causes the following backslash to be treated as plain text', function () {
-        chai_1.expect(Up.ast('Hello, \\\\!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, \\\\!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, \\!')
         ]));
     });
     it('disables any special meaning of the following character', function () {
-        chai_1.expect(Up.ast('Hello, \\*world\\*!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, \\*world\\*!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, *world*!')
         ]));
     });
     it('causes only the following character to be treated as plain text', function () {
-        chai_1.expect(Up.ast('Hello, \\\\, meet \\\\!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, \\\\, meet \\\\!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, \\, meet \\!')
         ]));
     });
     it('is ignored if it is the final character', function () {
-        chai_1.expect(Up.ast('Hello, \\')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, \\')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, ')
         ]));
     });
 });
 describe('Text surrounded by asterisks', function () {
     it('is put inside an emphasis node', function () {
-        chai_1.expect(Up.ast('Hello, *world*!!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, *world*!!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new EmphasisNode_1.EmphasisNode([
                 new PlainTextNode_1.PlainTextNode('world')
@@ -57,7 +60,7 @@ describe('Text surrounded by asterisks', function () {
         ]));
     });
     it('is evaluated for other conventions', function () {
-        chai_1.expect(Up.ast('Hello, *`world`*!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, *`world`*!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new EmphasisNode_1.EmphasisNode([
                 new InlineCodeNode_1.InlineCodeNode([
@@ -68,7 +71,7 @@ describe('Text surrounded by asterisks', function () {
         ]));
     });
     it('can even hold stressed text', function () {
-        chai_1.expect(Up.ast('Hello, *my **little** world*!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, *my **little** world*!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new EmphasisNode_1.EmphasisNode([
                 new PlainTextNode_1.PlainTextNode('my '),
@@ -81,7 +84,7 @@ describe('Text surrounded by asterisks', function () {
         ]));
     });
     it('can be indirectly nested inside another emphasis node', function () {
-        chai_1.expect(Up.ast('Hello, *my **very, *very* little** world*!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, *my **very, *very* little** world*!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new EmphasisNode_1.EmphasisNode([
                 new PlainTextNode_1.PlainTextNode('my '),
@@ -100,7 +103,7 @@ describe('Text surrounded by asterisks', function () {
 });
 describe('Text surrounded by backticks', function () {
     it('is put inside an inline code node', function () {
-        chai_1.expect(Up.ast('Hello, `["w", "o", "r", "l", "d"].join("")`!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, `["w", "o", "r", "l", "d"].join("")`!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new InlineCodeNode_1.InlineCodeNode([
                 new PlainTextNode_1.PlainTextNode('["w", "o", "r", "l", "d"].join("")')
@@ -109,7 +112,7 @@ describe('Text surrounded by backticks', function () {
         ]));
     });
     it('is not evaluated for other conventions', function () {
-        chai_1.expect(Up.ast('Hello, `*world*`!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, `*world*`!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new InlineCodeNode_1.InlineCodeNode([
                 new PlainTextNode_1.PlainTextNode('*world*')
@@ -120,7 +123,7 @@ describe('Text surrounded by backticks', function () {
 });
 describe('Text surrounded by 2 asterisks', function () {
     it('is put inside a stress node', function () {
-        chai_1.expect(Up.ast('Hello, **world**!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, **world**!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new StressNode_1.StressNode([
                 new PlainTextNode_1.PlainTextNode('world')
@@ -129,7 +132,7 @@ describe('Text surrounded by 2 asterisks', function () {
         ]));
     });
     it('can even hold emphasized text', function () {
-        chai_1.expect(Up.ast('Hello, **my *little* world**!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, **my *little* world**!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new StressNode_1.StressNode([
                 new PlainTextNode_1.PlainTextNode('my '),
@@ -144,7 +147,7 @@ describe('Text surrounded by 2 asterisks', function () {
 });
 describe('Text starting with 3 asterisks', function () {
     it('can have its emphasis node closed first', function () {
-        chai_1.expect(Up.ast('Hello, ***my* world**!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, ***my* world**!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new StressNode_1.StressNode([
                 new EmphasisNode_1.EmphasisNode([
@@ -156,7 +159,7 @@ describe('Text starting with 3 asterisks', function () {
         ]));
     });
     it('can have its stress node closed first', function () {
-        chai_1.expect(Up.ast('Hello, ***my** world*!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, ***my** world*!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, '),
             new EmphasisNode_1.EmphasisNode([
                 new StressNode_1.StressNode([
@@ -170,14 +173,14 @@ describe('Text starting with 3 asterisks', function () {
 });
 describe('An unmatched asterisk', function () {
     it('does not create an emphasis node', function () {
-        chai_1.expect(Up.ast('Hello, *world!')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('Hello, *world!')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('Hello, *world!')
         ]));
     });
 });
 describe('Text surrounded by 2 plus signs', function () {
     it('is put inside a revision insertion node', function () {
-        chai_1.expect(Up.ast('I like ++to brush++ my teeth')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('I like ++to brush++ my teeth')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('I like '),
             new RevisionInsertionNode_1.RevisionInsertionNode([
                 new PlainTextNode_1.PlainTextNode('to brush')
@@ -186,7 +189,7 @@ describe('Text surrounded by 2 plus signs', function () {
         ]));
     });
     it('is evaluated for other conventions', function () {
-        chai_1.expect(Up.ast('I like ++to *regularly* brush++ my teeth')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('I like ++to *regularly* brush++ my teeth')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('I like '),
             new RevisionInsertionNode_1.RevisionInsertionNode([
                 new PlainTextNode_1.PlainTextNode('to '),
@@ -201,7 +204,7 @@ describe('Text surrounded by 2 plus signs', function () {
 });
 describe('Text surrounded by 2 tildes', function () {
     it('is put inside a revision deletion node', function () {
-        chai_1.expect(Up.ast('I like ~~certain types of~~ pizza')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('I like ~~certain types of~~ pizza')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('I like '),
             new RevisionDeletionNode_1.RevisionDeletionNode([
                 new PlainTextNode_1.PlainTextNode('certain types of')
@@ -210,7 +213,7 @@ describe('Text surrounded by 2 tildes', function () {
         ]));
     });
     it('is evaluated for other conventions', function () {
-        chai_1.expect(Up.ast('I like ~~certain *types* of~~ pizza')).to.be.eql(new DocumentNode_1.DocumentNode([
+        chai_1.expect(Up.ast('I like ~~certain *types* of~~ pizza')).to.be.eql(insideDocument([
             new PlainTextNode_1.PlainTextNode('I like '),
             new RevisionDeletionNode_1.RevisionDeletionNode([
                 new PlainTextNode_1.PlainTextNode('certain '),
--- src/Test/ast.ts
+++ src/Test/ast.ts
@@ -3,6 +3,7 @@
 
 import { expect } from 'chai'
 import * as Up from '../index'
+import { SyntaxNode } from '../SyntaxNodes/SyntaxNode'
 import { DocumentNode } from '../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../SyntaxNodes/EmphasisNode'
@@ -10,6 +11,10 @@ import { StressNode } from '../SyntaxNodes/StressNode'
 import { InlineCodeNode } from '../SyntaxNodes/InlineCodeNode'
 import { RevisionInsertionNode } from '../SyntaxNodes/RevisionInsertionNode'
 import { RevisionDeletionNode } from '../SyntaxNodes/RevisionDeletionNode'
+
+function insideDocument(syntaxNodes: SyntaxNode[]): DocumentNode {
+  return new DocumentNode(syntaxNodes);
+}
 
 describe('No text', function() {
   it('creates only a document node', function() {
@@ -20,7 +25,7 @@ describe('No text', function() {
 describe('Text', function() {
   it('is put inside a plain text node', function() {
     expect(Up.ast('Hello, world!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, world!')
       ]))
   })
@@ -29,31 +34,31 @@ describe('Text', function() {
 describe('A backslash', function() {
   it('causes the following character to be treated as plain text', function() {
     expect(Up.ast('Hello, \\world!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, world!')
       ]))
   })
   it('causes the following backslash to be treated as plain text', function() {
     expect(Up.ast('Hello, \\\\!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, \\!')
       ]))
   })
   it('disables any special meaning of the following character', function() {
     expect(Up.ast('Hello, \\*world\\*!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, *world*!')
       ]))
   })
   it('causes only the following character to be treated as plain text', function() {
     expect(Up.ast('Hello, \\\\, meet \\\\!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, \\, meet \\!')
       ]))
   })
   it('is ignored if it is the final character', function() {
     expect(Up.ast('Hello, \\')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, ')
       ]))
   })
@@ -62,7 +67,7 @@ describe('A backslash', function() {
 describe('Text surrounded by asterisks', function() {
   it('is put inside an emphasis node', function() {
     expect(Up.ast('Hello, *world*!!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new EmphasisNode([
           new PlainTextNode('world')
@@ -73,7 +78,7 @@ describe('Text surrounded by asterisks', function() {
   
   it('is evaluated for other conventions', function() {
     expect(Up.ast('Hello, *`world`*!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new EmphasisNode([
           new InlineCodeNode([
@@ -86,7 +91,7 @@ describe('Text surrounded by asterisks', function() {
   
   it('can even hold stressed text', function() {
     expect(Up.ast('Hello, *my **little** world*!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new EmphasisNode([
           new PlainTextNode('my '),
@@ -101,7 +106,7 @@ describe('Text surrounded by asterisks', function() {
   
   it('can be indirectly nested inside another emphasis node', function() {
     expect(Up.ast('Hello, *my **very, *very* little** world*!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new EmphasisNode([
           new PlainTextNode('my '),
@@ -122,7 +127,7 @@ describe('Text surrounded by asterisks', function() {
 describe('Text surrounded by backticks', function() {
   it('is put inside an inline code node', function() {
     expect(Up.ast('Hello, `["w", "o", "r", "l", "d"].join("")`!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new InlineCodeNode([
           new PlainTextNode('["w", "o", "r", "l", "d"].join("")')
@@ -133,7 +138,7 @@ describe('Text surrounded by backticks', function() {
   
   it('is not evaluated for other conventions', function() {
     expect(Up.ast('Hello, `*world*`!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new InlineCodeNode([
           new PlainTextNode('*world*')
@@ -146,7 +151,7 @@ describe('Text surrounded by backticks', function() {
 describe('Text surrounded by 2 asterisks', function() {
   it('is put inside a stress node', function() {
     expect(Up.ast('Hello, **world**!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new StressNode([
           new PlainTextNode('world')
@@ -157,7 +162,7 @@ describe('Text surrounded by 2 asterisks', function() {
   
   it('can even hold emphasized text', function() {
     expect(Up.ast('Hello, **my *little* world**!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new StressNode([
           new PlainTextNode('my '),
@@ -174,7 +179,7 @@ describe('Text surrounded by 2 asterisks', function() {
 describe('Text starting with 3 asterisks', function() {
   it('can have its emphasis node closed first', function() {
     expect(Up.ast('Hello, ***my* world**!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new StressNode([
           new EmphasisNode([
@@ -188,7 +193,7 @@ describe('Text starting with 3 asterisks', function() {
   
   it('can have its stress node closed first', function() {
     expect(Up.ast('Hello, ***my** world*!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, '),
         new EmphasisNode([
           new StressNode([
@@ -204,7 +209,7 @@ describe('Text starting with 3 asterisks', function() {
 describe('An unmatched asterisk', function() {
   it('does not create an emphasis node', function() {
     expect(Up.ast('Hello, *world!')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('Hello, *world!')
       ]))
   })
@@ -214,7 +219,7 @@ describe('An unmatched asterisk', function() {
 describe('Text surrounded by 2 plus signs', function() {
   it('is put inside a revision insertion node', function() {
     expect(Up.ast('I like ++to brush++ my teeth')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('I like '),
         new RevisionInsertionNode([
           new PlainTextNode('to brush')
@@ -225,7 +230,7 @@ describe('Text surrounded by 2 plus signs', function() {
   
   it('is evaluated for other conventions', function() {
     expect(Up.ast('I like ++to *regularly* brush++ my teeth')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('I like '),
         new RevisionInsertionNode([
           new PlainTextNode('to '),
@@ -243,7 +248,7 @@ describe('Text surrounded by 2 plus signs', function() {
 describe('Text surrounded by 2 tildes', function() {
   it('is put inside a revision deletion node', function() {
     expect(Up.ast('I like ~~certain types of~~ pizza')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('I like '),
         new RevisionDeletionNode([
           new PlainTextNode('certain types of')
@@ -254,7 +259,7 @@ describe('Text surrounded by 2 tildes', function() {
   
   it('is evaluated for other conventions', function() {
     expect(Up.ast('I like ~~certain *types* of~~ pizza')).to.be.eql(
-      new DocumentNode([
+      insideDocument([
         new PlainTextNode('I like '),
         new RevisionDeletionNode([
           new PlainTextNode('certain '),
