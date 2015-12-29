23d5763963e6f5b7f9550036cec5d1d9d29255d5
Daniel Miller <daniel@wants.email>
2015-12-29 11:01:13 -0600

    Remove built directory from repo

--- built/Parsing/ParentNodeClosureStatus.js
+++
@@ -1,5 +0,0 @@
-(function (ParentNodeClosureStatus) {
-    ParentNodeClosureStatus[ParentNodeClosureStatus["Closed"] = 0] = "Closed";
-    ParentNodeClosureStatus[ParentNodeClosureStatus["OpenAndMustBeClosed"] = 1] = "OpenAndMustBeClosed";
-})(exports.ParentNodeClosureStatus || (exports.ParentNodeClosureStatus = {}));
-var ParentNodeClosureStatus = exports.ParentNodeClosureStatus;
--- built/index.js
+++
@@ -1,5 +0,0 @@
-var Parser_1 = require('./Parsing/Parser');
-function ast(text) {
-    return new Parser_1.Parser(text).documentNode;
-}
-exports.ast = ast;
