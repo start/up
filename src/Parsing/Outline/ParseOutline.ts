15d73a6254417352df44110a59b5778be792f839
Daniel Miller <daniel@wants.email>
2016-01-30 23:01:05 -0600

    Pass outline test; fail backslash tests

--- src/Parsing/Outline/ParseOutline.ts
+++ src/Parsing/Outline/ParseOutline.ts
@@ -4,17 +4,21 @@ import { parseInline } from '../Inline/ParseInline'
 
 import { SyntaxNode } from '../../SyntaxNodes/SyntaxNode'
 
+import { TextConsumer } from '../../TextConsumption/TextConsumer'
+
 export function parseOutline(text: string): ParseResult {
   let nodes: SyntaxNode[] = []
 
-  const lines = text.split('\n')
-
-  for (const line of lines) {
-    if (line) {
-      const paragraphNode = new ParagraphNode()
-      paragraphNode.addChildren(parseInline(line, paragraphNode).nodes)
-      nodes.push(paragraphNode)
-    }
+  const consumer = new TextConsumer(text)
+  
+  while (!consumer.done()) {
+    consumer.consumeLine((line, remaining, skip, reject) => {
+      if (/\S/.test(line)) {
+        const paragraphNode = new ParagraphNode()
+        paragraphNode.addChildren(parseInline(line, paragraphNode).nodes)
+        nodes.push(paragraphNode)
+      }
+    })
   }
 
   return new ParseResult(nodes, text.length)
