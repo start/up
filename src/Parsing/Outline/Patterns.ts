c05d54959aa04562f163bed231ba34c53eb2b83f
Daniel Miller <daniel@wants.email>
2016-02-11 08:38:22 -0600

    Begin adding more patterns

--- src/Parsing/Outline/Patterns.ts
+++ src/Parsing/Outline/Patterns.ts
@@ -1,5 +1,20 @@
-const BLANK_LINE = /^\s*$/
+
+const group = (pattern: string) => `(?:${pattern})`
+
+const optional = (pattern: string) => pattern + '?'
+
+const all = (pattern: string) => pattern + '*'
+
+const atLeast = (pattern: string, count: number) => pattern + `{${atLeast},}`
+
+const either = (...patterns: string[]) => group(patterns.join('|'))
+
+const lineOf = (pattern: string) => `^` + pattern + '$'
+
+const space = '[^\\S\n]'
+
+const BLANK_LINE = new RegExp(lineOf(all(space)))
 
 export {
-  BLANK_LINE
+   BLANK_LINE
 }
