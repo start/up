0e7191a08e09fb0b3f225fb0f13f71e6ed5a087e
Daniel Miller <daniel@wants.email>
2016-02-11 14:05:46 -0600

    Add failing heading test

---
+++ src/Test/Ast/Outline.ts
@@ -0,0 +1,38 @@
+/// <reference path="../../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../../typings/chai/chai.d.ts" />
+
+import { expect } from 'chai'
+import * as Up from '../../index'
+import { SyntaxNode } from '../../SyntaxNodes/SyntaxNode'
+import { LinkNode } from '../../SyntaxNodes/LinkNode'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../SyntaxNodes/StressNode'
+import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
+import { RevisionInsertionNode } from '../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../SyntaxNodes/RevisionDeletionNode'
+import { SpoilerNode } from '../../SyntaxNodes/SpoilerNode'
+import { InlineAsideNode } from '../../SyntaxNodes/InlineAsideNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
+import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+
+
+function insideDocument(syntaxNodes: SyntaxNode[]): DocumentNode {
+  return new DocumentNode(syntaxNodes);
+}
+
+
+describe('A non-blank line underlined by number signs', function() {
+  it('produces a level-1 heading node', function() {
+    const text =
+`Hello, world!
+######`
+    expect(Up.ast(text)).to.be.eql(
+      insideDocument([
+        new HeadingNode([new PlainTextNode('Hello, world!')], 1),
+      ]))
+  })
+})
+
