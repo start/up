d20e92b94c0766fecad50a4bfc68fb9d4b51ac53
Daniel Miller <daniel@wants.email>
2016-02-11 12:25:42 -0600

    Clean up parseCode

--- src/Parsing/Inline/ParseCode.ts
+++ src/Parsing/Inline/ParseCode.ts
@@ -7,14 +7,10 @@ import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
 
 export function parseCode(text: string, parseArgs: ParseArgs, onParse: OnParse) {
   const consumer = new TextConsumer(text)
-  const delimiter = '`'
-
-  if (!consumer.consumeIf(delimiter)) {
-    return false
-  }
   
-  return consumer.consumeUpTo(delimiter, (code, totalCountCharsAdvanced) => {
-    const node = new InlineCodeNode([new PlainTextNode(code)])
-    onParse([node], totalCountCharsAdvanced, parseArgs.parentNode)
-  })
+  return consumer.consumeIf('`')
+    && consumer.consumeUpTo('`', (code, totalCountCharsAdvanced) => {
+      const node = new InlineCodeNode([new PlainTextNode(code)])
+      onParse([node], totalCountCharsAdvanced, parseArgs.parentNode)
+    })
 }
