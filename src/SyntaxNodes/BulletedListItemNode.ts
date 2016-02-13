a6889b5f9279c81c09b7da6d665cf0d08ac660e1
Daniel Miller <daniel@wants.email>
2016-02-13 17:04:40 -0600

    Add bulleted list and list item nodes

---
+++ src/SyntaxNodes/BulletedListItemNode.ts
@@ -0,0 +1,5 @@
+import { RichSyntaxNode } from './RichSyntaxNode'
+
+export class BulletedListItemNode extends RichSyntaxNode {
+  private BULLETED_LIST_ITEM: any = null
+}
---
+++ src/SyntaxNodes/BulletedListNode.ts
@@ -0,0 +1,5 @@
+import { RichSyntaxNode } from './RichSyntaxNode'
+
+export class BulletedListNode extends RichSyntaxNode {
+  private BULLETED_LIST: any = null
+}
