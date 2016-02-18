67d677b426d97964cd20b4825235b25f1e102c01
Daniel Miller <daniel@wants.email>
2016-02-18 00:56:45 -0600

    Pass consecutive streak test

--- src/Parsing/Outline/GetHeadingParser.ts
+++ src/Parsing/Outline/GetHeadingParser.ts
@@ -1,12 +1,23 @@
 import { TextConsumer } from '../../TextConsumption/TextConsumer'
 import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
 import { ParseArgs, OnParse, Parser } from '../Parser'
-import { NON_BLANK } from './Patterns'
+import { streakOf, dottedStreakOf, either, NON_BLANK } from './Patterns'
 import { parseInline } from '../Inline/ParseInline'
 
 const NON_BLANK_LINE_PATTERN = new RegExp(
   NON_BLANK
-) 
+)
+
+const STREAK_PATTERN = new RegExp(
+  either(
+    streakOf('-'),
+    streakOf('='),
+    streakOf('#'),
+    dottedStreakOf('-'),
+    dottedStreakOf('='),
+    dottedStreakOf('#')
+  )
+)
 
 // Underlined text is treated as a heading. Headings can have an optional overline, too.
 export function getHeadingParser(underlinePattern: string, level: number): Parser {
@@ -15,17 +26,29 @@ export function getHeadingParser(underlinePattern: string, level: number): Parse
   return function parseHeading(text: string, parseArgs: ParseArgs, onParse: OnParse): boolean {
     const consumer = new TextConsumer(text)
 
-    // Parse optional overline
+    // First, let's consume the optional overline
     consumer.consumeLineIf(underlineOrOverline)
 
     let content: string
 
+    // Now, let's consume the content and the required underline. The content itself must
+    // not be a streak. Why? Take a look:
+    //
+    // -----------------------
+    // -----------------------
+    //
+    // And...
+    //
+    // =======================
+    // # # # # # # # # # # # #
+    //
+    // The author almost certainly intended those lines to serve as a section separator,
+    // not as a heading.
     const hasContentAndUnderline =
-      consumer.consumeLineIf(NON_BLANK_LINE_PATTERN,
-        (line) => {
-          content = line
-        })
+      consumer.consumeLineIf(NON_BLANK_LINE_PATTERN, (line) => content = line)
+      && !STREAK_PATTERN.test(content)
       && consumer.consumeLineIf(underlineOrOverline)
+      
 
     return (
       hasContentAndUnderline
