65ed414de6c097e4662ab190ef7a100063aae5a1
Daniel Miller <daniel@wants.email>
2016-02-20 08:36:16 -0600

    Fix check for matching overline and underline

--- src/Parsing/Outline/ParseHeading.ts
+++ src/Parsing/Outline/ParseHeading.ts
@@ -16,38 +16,39 @@ const STREAK_PATTERN = new RegExp(
 export function parseHeading(text: string, parseArgs: ParseArgs, onParse: OnParse): boolean {
   const consumer = new TextConsumer(text)
 
-
   // First, let's consume the optional overline.
   let overlineChars: string
   consumer.consumeLineIf(STREAK_PATTERN, (overline) => overlineChars = getDistinctStreakChars(overline))
   
   // Next, let's consume the content.
   let content: string
-  consumer.consumeLineIf(NON_BLANK_PATTERN, (contentLine) => content = contentLine)
-    
+  if (!consumer.consumeLineIf(NON_BLANK_PATTERN, (contentLine) => content = contentLine)) {
+    return false
+  }
+   
   // The content must not be a streak! Why not? Take a look:
   //
-  // #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#
-  // ===============================================
-  // #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#
+  // #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
+  // ===***===***===***===***===***===***===***===*
+  // #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~
   //
   // The author almost certainly intended those lines to serve as a section separator,
   // not as a heading.
-  if (!content || STREAK_PATTERN.test(content)) {
+  //
+  // This wouldn't be necessary if we could simply parse separator streaks before headings, but we can't.
+  // That's because the separator streak parser would also consume a heading's overline.
+  if (STREAK_PATTERN.test(content)) {
     return false
   }
   
-  // Finally, we consume the underline
+  // Finally, we consume the underline.
   let underlineChars: string
-  consumer.consumeLineIf(STREAK_PATTERN,
-    (underline) => underlineChars = getDistinctStreakChars(underline))
-
-  if (!underlineChars) {
+  if (!consumer.consumeLineIf(STREAK_PATTERN, (underline) => underlineChars = getDistinctStreakChars(underline))) {
     return false
   }
       
-  //  If there was an overline, it must consist of the same chars as the underline.
-  if (!overlineChars || (overlineChars === underlineChars)) {
+  // If there is an overline, it must consist of the same chars as the underline.
+  if (overlineChars && (overlineChars !== underlineChars)) {
     return false
   }
 
--- src/Parsing/Outline/ParseSectionSeparatorStreak.ts
+++ src/Parsing/Outline/ParseSectionSeparatorStreak.ts
@@ -7,9 +7,7 @@ const STREAK_PATTERN = new RegExp(
   STREAK
 )
 
-// A line consisting solely of a streak of '-', '=', or '#' characters indicates separation between
-// sections. The streak must not be immediately followed by a non-blank line, and it can be surrounded
-// by any number of blank lines.
+// A horizontal streak of characters indicates separation between sections.
 export function parseSectionSeparatorStreak(text: string, parseArgs: ParseArgs, onParse: OnParse): boolean {
   const consumer = new TextConsumer(text)
 
