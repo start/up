ebd760277f191ae8be4877962c4f12d81a6b225e
Daniel Miller <daniel@wants.email>
2016-02-22 07:59:30 -0600

    Refactor parseLink

--- src/Parsing/Inline/ParseLink.ts
+++ src/Parsing/Inline/ParseLink.ts
@@ -1,32 +1,32 @@
 import { parseInline } from './ParseInline'
 import { TextConsumer } from '../TextConsumer'
 import { applyBackslashEscaping } from '../TextHelpers'
-import { RichSyntaxNode } from '../../SyntaxNodes/RichSyntaxNode'
 import { LinkNode } from '../../SyntaxNodes/LinkNode'
 import { ParseContextArgs, OnParse } from '../Parser'
 
-// Todo: Handle parent node's inline terminator?
+// TODO: Handle parent node's inline terminator?
 
 export function parseLink(text: string, parseArgs: ParseContextArgs, onParse: OnParse): boolean {
-  const consumer = new TextConsumer(text)
-
   // Links cannot be nested within other links
   if (parseArgs.parentNode.orAnyAncestor(ancestor => ancestor instanceof LinkNode)) {
     return false
   }
-
+  
+  const consumer = new TextConsumer(text)
   const linkNode = new LinkNode(parseArgs.parentNode)
 
-  const didParseOpeningBracketAndContent =
+  return (
+    // Parse the opening bracket
     consumer.consumeIfMatches('[')
+    
+    // Parse the link's content and the URL arrow
     && parseInline(consumer.remainingText(), { parentNode: linkNode, inlineTerminator: ' -> ' },
       (nodes, countChars) => {
         consumer.skip(countChars)
         linkNode.addChildren(nodes)
       })
-
-  return (
-    didParseOpeningBracketAndContent
+    
+    // Parse the URL and the closing bracket
     && consumer.consumeUpTo({
       needle: ']',
       then: (url) => {
