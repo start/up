6ed2ef1326cd8c2f287f1e4999b735c4af26b56e
Daniel Miller <daniel@wants.email>
2016-03-03 07:47:36 -0600

    Add ordinal to ordered list items

--- src/SyntaxNodes/OrderedListItemNode.ts
+++ src/SyntaxNodes/OrderedListItemNode.ts
@@ -1,5 +1,10 @@
-import { RichSyntaxNode } from './RichSyntaxNode'
+import { SyntaxNode } from '../SyntaxNodes/SyntaxNode'
+import { RichSyntaxNode } from '../SyntaxNodes/RichSyntaxNode'
 
 export class OrderedListItemNode extends RichSyntaxNode {
+  constructor(parentOrChildren?: RichSyntaxNode | SyntaxNode[], public ordinal?: number) {
+    super(parentOrChildren)
+  }
+  
   private NUMBERED_LIST_ITEM: any = null
 }
