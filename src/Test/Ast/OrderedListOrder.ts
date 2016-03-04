aecca6154061ef9186ea8b7c455e6c53308d674c
Daniel Miller <daniel@wants.email>
2016-03-04 13:57:01 -0600

    Add ordered list test

---
+++ src/Test/Ast/OrderedListOrder.ts
@@ -0,0 +1,40 @@
+/// <reference path="../../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../../typings/chai/chai.d.ts" />
+
+import { expect } from 'chai'
+import * as Up from '../../index'
+import { SyntaxNode } from '../../SyntaxNodes/SyntaxNode'
+import { LinkNode } from '../../SyntaxNodes/LinkNode'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../SyntaxNodes/StressNode'
+import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
+import { RevisionInsertionNode } from '../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../SyntaxNodes/RevisionDeletionNode'
+import { SpoilerNode } from '../../SyntaxNodes/SpoilerNode'
+import { InlineAsideNode } from '../../SyntaxNodes/InlineAsideNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
+import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+import { LineBlockNode } from '../../SyntaxNodes/LineBlockNode'
+import { LineNode } from '../../SyntaxNodes/LineNode'
+import { ListOrder, OrderedListNode } from '../../SyntaxNodes/OrderedListNode'
+import { OrderedListItemNode } from '../../SyntaxNodes/OrderedListItemNode'
+import { CodeBlockNode } from '../../SyntaxNodes/CodeBlockNode'
+
+function listOrder(text: string): number {
+  const list = <OrderedListNode>Up.ast(text).children[0]
+  return list.order()
+}
+
+describe('An ordered list with non-numeral bullets', () => {
+  it('is automatically in ascending order', () => {
+    const text =
+      `
+# Hello, world!
+#. Goodbye, world!
+#) Goodbye, world!`
+    expect(listOrder(text)).to.be.eql(ListOrder.Ascending)
+  })
+})
