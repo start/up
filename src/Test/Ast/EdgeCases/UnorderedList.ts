c44993b75169484a98ae864675c5528f6d051e46
Daniel Miller <daniel@wants.email>
2016-03-05 10:24:40 -0600

    Add failing unordered list test

--- src/Test/Ast/EdgeCases/UnorderedList.ts
+++ src/Test/Ast/EdgeCases/UnorderedList.ts
@@ -21,6 +21,8 @@ import { OrderedListNode } from '../../../SyntaxNodes/OrderedListNode'
 import { OrderedListItemNode } from '../../../SyntaxNodes/OrderedListItemNode'
 import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
 import { HeadingNode } from '../../../SyntaxNodes/HeadingNode'
+import { LineBlockNode } from '../../../SyntaxNodes/LineBlockNode'
+import { LineNode } from '../../../SyntaxNodes/LineNode'
 
 
 describe('An unordered list with a single item', () => {
@@ -41,5 +43,49 @@ describe('An unordered list with a single item', () => {
         ]),
         new SectionSeparatorNode()
       ]))
+  })
+})
+
+describe('An unordered list', () => {
+  it('can be sandwched by line blocks', () => {
+    const text = `
+Roses are red
+Violets are blue
+- Kansas
+- Nebraska
+Lyrics have lines
+And addresses do, too`
+    expect(Up.ast(text)).to.be.eql(
+      new DocumentNode([
+        new LineBlockNode([
+          new LineNode([
+            new PlainTextNode('Roses are red')
+          ]),
+          new LineNode([
+            new PlainTextNode('Violets are blue')
+          ])
+        ]),
+        new UnorderedListNode([
+          new UnorderedListItemNode([
+            new ParagraphNode([
+              new PlainTextNode('Kansas')
+            ])
+          ]),
+          new UnorderedListItemNode([
+            new ParagraphNode([
+              new PlainTextNode('Nebraska')
+            ])
+          ])
+        ]),
+        new LineBlockNode([
+          new LineNode([
+            new PlainTextNode('Roses are red')
+          ]),
+          new LineNode([
+            new PlainTextNode('Violets are blue')
+          ])
+        ]),
+      ])
+    )
   })
 })
