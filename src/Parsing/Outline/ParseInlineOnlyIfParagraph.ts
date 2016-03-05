e5261df82d291449d95de3d9de6a914e2bae4bc9
Daniel Miller <daniel@wants.email>
2016-03-05 14:17:55 -0600

    Add helper for excluding fancy outline lines

---
+++ src/Parsing/Outline/ParseInlineOnlyIfParagraph.ts
@@ -0,0 +1,39 @@
+import { TextConsumer } from '../TextConsumer'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { SyntaxNode } from '../../SyntaxNodes/SyntaxNode'
+import { getOutlineNodes } from './GetOutlineNodes'
+import { NON_BLANK } from './Patterns'
+import { OutlineParser, OutlineParserArgs, } from './OutlineParser'
+
+interface ParseInlineOnlyIfParagraphArgs {
+  text: string,
+  then: OnSuccess
+}
+
+interface OnSuccess {
+  (resultNodes: SyntaxNode[]): void
+}
+
+const NON_BLANK_PATTERN = new RegExp(
+  NON_BLANK
+)
+
+// `parseInlineOnlyIfParagraph` only succeeds if the text would otherwise be parsed as a plain paragraph.
+//
+// The following three inputs would fail: 
+//
+// 1) Buy milk
+//
+// =-=-=-=-=-=
+//
+// * Drink milk
+export function parseInlineOnlyIfParagraph(args: ParseInlineOnlyIfParagraphArgs): boolean {
+  const outlineNodeFromContent = getOutlineNodes(args.text)[0]
+
+  if (outlineNodeFromContent instanceof ParagraphNode) {
+    args.then(outlineNodeFromContent.children)
+    return true
+  }
+
+  return false
+}
