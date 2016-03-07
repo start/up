15766ea10a797119cff04c1e840376e15321a6c4
Daniel Miller <daniel@wants.email>
2016-03-07 09:44:54 -0600

    Clarify comment

--- src/SyntaxNodes/OrderedListItemNode.ts
+++ src/SyntaxNodes/OrderedListItemNode.ts
@@ -3,7 +3,7 @@ import { RichSyntaxNode } from '../SyntaxNodes/RichSyntaxNode'
 
 export class OrderedListItemNode extends RichSyntaxNode {
   // During parsing, `ordinal` can be either `null` or a number. Defaulting `ordinal` to `null`
-  // allows our unit tests to be cleaner.  
+  // rather than `undefined` allows our unit tests to be cleaner.
   constructor(children?: SyntaxNode[], public ordinal: number = null) {
     super(children)
   }
