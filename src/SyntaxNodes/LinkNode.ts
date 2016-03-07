6c651231793103ab90963c5b445160dce8e01712
Daniel Miller <daniel@wants.email>
2016-03-07 13:23:03 -0600

    Pass all tests

--- src/SyntaxNodes/LinkNode.ts
+++ src/SyntaxNodes/LinkNode.ts
@@ -2,7 +2,7 @@ import { RichInlineSyntaxNode } from './RichInlineSyntaxNode'
 import { InlineSyntaxNode } from './InlineSyntaxNode'
 
 export class LinkNode extends RichInlineSyntaxNode {
-  constructor(public children?: InlineSyntaxNode[], public url: string = '') {
+  constructor(public children: InlineSyntaxNode[] = [], public url: string = '') {
     super(children)
   }
   
