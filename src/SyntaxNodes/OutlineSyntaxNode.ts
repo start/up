40c03e5c12223b199f69f5dcccce8d3cf92d7d28
Daniel Miller <daniel@wants.email>
2016-03-07 10:42:33 -0600

    Add outline syntax node class

---
+++ src/SyntaxNodes/OutlineSyntaxNode.ts
@@ -0,0 +1,7 @@
+import { SyntaxNode } from '../SyntaxNodes/SyntaxNode'
+import { PlainTextNode } from '../SyntaxNodes/PlainTextNode'
+import { RichSyntaxNode } from '../SyntaxNodes/RichSyntaxNode'
+
+export abstract class OutlineSyntaxNode extends SyntaxNode {
+  private OUTLINE_SYNTAX_NODE: any = null
+}
