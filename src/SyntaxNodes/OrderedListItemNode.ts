17aa76ae91b2a3889bf543eb4ad9f9cc555f14a0
Daniel Miller <daniel@wants.email>
2016-03-07 18:36:55 -0600

    Make OrderedListItemNode not extend SyntaxNode

--- src/SyntaxNodes/OrderedListItemNode.ts
+++ src/SyntaxNodes/OrderedListItemNode.ts
@@ -1,12 +1,10 @@
-import { SyntaxNode } from '../SyntaxNodes/SyntaxNode'
+
 import { OutlineSyntaxNode } from '../SyntaxNodes/OutlineSyntaxNode'
 
-export class OrderedListItemNode extends SyntaxNode {
+export class OrderedListItemNode  {
   // During parsing, `ordinal` can be either `null` or a number. Defaulting `ordinal` to `null`
   // rather than `undefined` allows our unit tests to be cleaner.
-  constructor(public children?: OutlineSyntaxNode[], public ordinal: number = null) {
-    super()
-  }
+  constructor(public children?: OutlineSyntaxNode[], public ordinal: number = null) { }
   
   private ORDERED_LIST_ITEM: any = null
 }
