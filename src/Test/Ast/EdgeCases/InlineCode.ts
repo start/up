a1ed77c2e3893c928ef9fe170efc32ad22fc7222
Daniel Miller <daniel@wants.email>
2016-03-09 11:50:10 -0600

    Remove duplicate insideDocumentAndParagraph helper

--- src/Test/Ast/EdgeCases/InlineCode.ts
+++ src/Test/Ast/EdgeCases/InlineCode.ts
@@ -3,26 +3,11 @@
 
 import { expect } from 'chai'
 import * as Up from '../../../index'
-import { SyntaxNode } from '../../../SyntaxNodes/SyntaxNode'
-import { LinkNode } from '../../../SyntaxNodes/LinkNode'
-import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { insideDocumentAndParagraph } from '../Helpers'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
-import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
-import { StressNode } from '../../../SyntaxNodes/StressNode'
 import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
-import { InlineAsideNode } from '../../../SyntaxNodes/InlineAsideNode'
 import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
-import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
 
-
-function insideDocumentAndParagraph(syntaxNodes: SyntaxNode[]): DocumentNode {
-  return new DocumentNode([
-    new ParagraphNode(syntaxNodes)
-  ])
-}
 
 describe('Inline code', () => {
   it('can be the last convention in a paragraph', () => {
