f759603a2b869aa7faf1559b11f10ae3355b838f
Daniel Miller <daniel@wants.email>
2016-03-19 04:58:52 -0500

    Add failing inline overlapping test

---
+++ src/Test/Ast/EdgeCases/InlineOverlapping.ts
@@ -0,0 +1,39 @@
+/// <reference path="../../../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../../../typings/chai/chai.d.ts" />
+
+import { expect } from 'chai'
+import * as Up from '../../../index'
+import { insideDocumentAndParagraph } from '../Helpers'
+import { SyntaxNode } from '../../../SyntaxNodes/SyntaxNode'
+import { LinkNode } from '../../../SyntaxNodes/LinkNode'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../../SyntaxNodes/StressNode'
+import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
+import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
+import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
+import { InlineAsideNode } from '../../../SyntaxNodes/InlineAsideNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
+import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
+
+
+describe('Overlapped stressed and deleted text', () => {
+  it('produces a stress node, a nested revision deletion node, then a non-nested revision deletion node', () => {
+    expect(Up.ast('I **love ~~drinking** whole~~ milk.')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('I '),
+        new StressNode([
+          new PlainTextNode('love '),
+          new RevisionDeletionNode([
+            new PlainTextNode('drinking')
+          ])
+        ]),
+        new RevisionDeletionNode([
+          new PlainTextNode(' whole')
+        ]),
+        new PlainTextNode(' milk.')
+      ]))
+  })
+})
