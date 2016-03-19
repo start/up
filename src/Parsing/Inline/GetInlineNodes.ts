df6b8e36c371a4df94de0a19ebfb9fc88cd41ebe
Daniel Miller <daniel@wants.email>
2016-03-19 17:06:55 -0500

    Begin adding tokenization

--- src/Parsing/Inline/GetInlineNodes.ts
+++ src/Parsing/Inline/GetInlineNodes.ts
@@ -1,6 +1,34 @@
 import { InlineSyntaxNode } from '../../SyntaxNodes/InlineSyntaxNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { TextConsumer } from '../TextConsumer'
+
+enum TokenMeaning {
+  Text,
+  EmphasisStart,
+  EmphasisEnd  
+}
+
+
+class Token {
+  constructor(public meaning: TokenMeaning, public index: number, public value: string) { }
+}
+
 
 export function getInlineNodes(text: string): InlineSyntaxNode[] {
   return [new PlainTextNode(text)]
+}
+
+
+function tokenize(text: string): Token[] {
+  const consumer = new TextConsumer(text)
+  const tokens: Token[] = []
+  
+  return tokens
+}
+
+
+function parse(tokens: Token[]): InlineSyntaxNode[] {
+  const nodes: InlineSyntaxNode[] = []
+  
+  return nodes
 }
