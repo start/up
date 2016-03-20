a4635dc3ff2d26b9610c37329c955bb4fe37bb16
Daniel Miller <daniel@wants.email>
2016-03-19 19:08:58 -0500

    Add barebones tokenizer

--- src/Parsing/Inline/GetInlineNodes.ts
+++ src/Parsing/Inline/GetInlineNodes.ts
@@ -1,6 +1,7 @@
 import { InlineSyntaxNode } from '../../SyntaxNodes/InlineSyntaxNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { TextConsumer } from '../TextConsumer'
+import { last } from '../CollectionHelpers'
 
 enum TokenMeaning {
   Text,
@@ -23,7 +24,28 @@ function tokenize(text: string): Token[] {
   const consumer = new TextConsumer(text)
   const tokens: Token[] = []
   
-  return tokens
+  while (!consumer.done()) {
+    const index = consumer.lengthConsumed()
+    
+    tokens.push(new Token(TokenMeaning.Text, index, consumer.escapedCurrentChar()))
+    
+    consumer.moveNext()
+  }
+  
+  return withMergedConsecutiveTextTokens(tokens)
+}
+
+function withMergedConsecutiveTextTokens(tokens: Token[]): Token[] {
+  return tokens.reduce((cleaned, current) => {
+      const lastToken = last(cleaned)
+      
+      if (lastToken && (lastToken.meaning === TokenMeaning.Text) && (current.meaning === TokenMeaning.Text)) {
+        lastToken.value += current.value
+        return cleaned
+      }
+      
+      return cleaned.concat([current])
+    }, [])
 }
 
 
