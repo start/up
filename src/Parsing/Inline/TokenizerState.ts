d7e19af60224112b58ace18937eb3778a4896d67
Daniel Miller <daniel@wants.email>
2016-03-22 21:39:33 -0500

    Fix formatting in tokenizer state file

--- src/Parsing/Inline/TokenizerState.ts
+++ src/Parsing/Inline/TokenizerState.ts
@@ -3,26 +3,26 @@ import { TextConsumer } from '../TextConsumer'
 import { Token } from './Token'
 
 interface Args {
-    consumer?: TextConsumer,
-     tokens?: Token[],
-     sandwichTrackers?: RichSandwichTracker[]
+  consumer?: TextConsumer,
+  tokens?: Token[],
+  sandwichTrackers?: RichSandwichTracker[]
 }
 
 export class TokenizerState {
   public consumer: TextConsumer
-    public tokens: Token[]
-    public sandwichTrackers: RichSandwichTracker[]
-    
+  public tokens: Token[]
+  public sandwichTrackers: RichSandwichTracker[]
+
   constructor(args?: Args) {
     if (!args) {
       return
     }
-    
+
     this.consumer = args.consumer
     this.tokens = args.tokens
     this.sandwichTrackers = args.sandwichTrackers
   }
-  
+
   clone(): TokenizerState {
     return new TokenizerState({
       consumer: this.consumer.clone(),
