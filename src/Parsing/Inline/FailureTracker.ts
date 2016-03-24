dde97db82bd605bff07073382f39b6a15f70e321
Daniel Miller <daniel@wants.email>
2016-03-24 03:30:58 -0500

    Put back failure tracker

---
+++ src/Parsing/Inline/FailureTracker.ts
@@ -0,0 +1,34 @@
+import { RichSandwich } from './RichSandwich'
+import { Token, TokenMeaning } from './Token'
+
+export class FailureTracker {
+  failures: TokenMeaning[][] = []
+  
+  registerSandwichFailure(sandwich: RichSandwich, index: number) {
+    this.registerFailure(sandwich.meaningStart, index)
+  }
+    
+  wasSandwichAlreadyTried(sandwich: RichSandwich, index: number): boolean {
+    return this.hasFailed(sandwich.meaningStart, index)
+  }
+  
+  private registerFailure(meaningStart: TokenMeaning, index: number) {
+    if (this.hasNoFailuresAtIndex(index)) {
+      this.failures[index] = [meaningStart]
+    } else {
+      this.failures[index].push(meaningStart)
+    }
+  }
+  
+  private hasFailed(meaningStart: TokenMeaning, index: number): boolean {
+    if (this.hasNoFailuresAtIndex(index)) {
+      return false
+    }
+    
+    return -1 !== this.failures[index].indexOf(meaningStart)
+  }
+  
+  private hasNoFailuresAtIndex(index: number): boolean {
+    return !this.failures[index]
+  }
+}
