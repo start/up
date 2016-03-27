cf8f573ca9eedefdc7847eb31277d394e91fc354
Daniel Miller <daniel@wants.email>
2016-03-27 03:51:59 -0500

    Add method for adding tokens

--- src/Parsing/Inline/Tokenize.ts
+++ src/Parsing/Inline/Tokenize.ts
@@ -16,6 +16,158 @@ export function tokenize(text: string): Token[] {
 
 const LINK_CONVENTIONS = [TokenMeaning.LinkStart, TokenMeaning.LinkUrlAndLinkEnd]
 
+class Tokenizer {
+  public tokens: Token[] = []
+  private failureTracker = new FailureTracker()
+  private consumer: TextConsumer
+
+  constructor(text: string) {
+    this.consumer = new TextConsumer(text)
+
+    while (true) {
+      if (this.consumer.done()) {
+        if (this.backtrackIfAnyConventionsAreUnclosed()) {
+          continue
+        }
+
+        break
+      }
+      
+      const wasAnythingDiscovered = (
+        this.handleInlineCode()
+        || this.handleSandwiches()
+        || this.handleLink()
+      )
+      
+      if (wasAnythingDiscovered) {
+        continue
+      }
+
+      this.treatCurrentCharAsPlainText()
+      this.consumer.moveNext()
+    }
+  }
+
+  backtrackIfAnyConventionsAreUnclosed(): boolean {
+    for (let i = 0; i < this.tokens.length; i++) {
+      if (isTokenUnmatched(i, this.tokens)) {
+        this.backtrack(i)
+        return true
+      }
+    }
+    
+    return false
+  }
+
+  handleInlineCode(): boolean {
+    return this.consumer.consume({
+      from: '`',
+      upTo: '`',
+      then: code => this.addToken(TokenMeaning.InlineCode, applyBackslashEscaping(code))
+    })
+  }
+
+  handleSandwiches(): boolean {
+    const textIndex = this.consumer.lengthConsumed()
+
+    for (const sandwich of RICH_SANDWICHES) {
+      if (isInsideSandwich(sandwich, this.tokens) && this.consumer.consumeIfMatches(sandwich.end)) {
+        this.addToken(sandwich.meaningEnd)
+        return true
+      }
+
+      const sandwichStart = sandwich.start
+
+      const foundStartToken = (
+        !this.failureTracker.wasSandwichAlreadyTried(sandwich, textIndex)
+        && this.consumer.consumeIfMatches(sandwichStart)
+      )
+
+      if (foundStartToken) {
+        this.addToken(sandwich.meaningStart, this.consumer.asBeforeMatch(sandwichStart.length))
+        return true
+      }
+    }
+  }
+
+
+  handleLink(): boolean {
+    const textIndex = this.consumer.lengthConsumed()
+
+    if (this.failureTracker.hasFailed(TokenMeaning.LinkStart, textIndex)) {
+      return false
+    }
+
+    if (!isInsideLink(this.tokens)) {
+      // If we're not inside a link, that means we can potentially start one. Let's see whether we should...
+      const LINK_START = '['
+
+      if (this.consumer.consumeIfMatches(LINK_START)) {
+         this.addToken(TokenMeaning.LinkStart, this.consumer.asBeforeMatch(LINK_START.length))
+        return true
+      }
+
+      // ... Nope. We didn't find anything. Since we're not inside a link, let's bail.
+      return false
+    }
+
+    // We're insied a link! Are we looking at the URL arrow?
+    if (this.consumer.consumeIfMatches(' -> ')) {
+
+      // Great! We found the URL arrow. Let's try to find the closing bracket.
+      const didFindLinkEnd = this.consumer.consume({
+        upTo: ']',
+        then: url => this.addToken(TokenMeaning.LinkUrlAndLinkEnd, applyBackslashEscaping(url))
+      })
+
+      if (!didFindLinkEnd) {
+        // No, the closing bracket is nowehere to be found. This wasn't a link. Oops!
+        this.undoLatest(TokenMeaning.LinkStart)
+      }
+
+      return true
+    }
+
+    // We haven't found the URL arrow yet, which means we're still tokenizing link's contents.
+    //
+    // If we find a closing brace before finding any URL arrow, that means we're actually looking at regular
+    // bracketed text.
+    if (this.consumer.consumeIfMatches(']')) {
+      this.undoLatest(TokenMeaning.LinkStart)
+      return true
+    }
+
+    return false
+  }
+  
+  addToken(meaning: TokenMeaning, valueOrConsumerBefore?: string|TextConsumer): void {
+    this.tokens.push(new Token(meaning, valueOrConsumerBefore))
+  }
+
+  treatCurrentCharAsPlainText(): void {
+    const currentChar = this.consumer.escapedCurrentChar()
+    const lastToken = last(this.tokens)
+
+    if (lastToken && (lastToken.meaning === TokenMeaning.PlainText)) {
+      lastToken.value += currentChar
+    } else {
+      this.tokens.push(new Token(TokenMeaning.PlainText, currentChar))
+    }
+  }
+
+  undoLatest(meaning: TokenMeaning): void {
+    this.backtrack(indexOfLastToken(meaning, this.tokens))
+  }
+
+  backtrack(indexOfEarliestTokenToUndo: number): void {
+    const token = this.tokens[indexOfEarliestTokenToUndo]
+
+    this.failureTracker.registerFailure(token.meaning, token.textIndex())
+    this.consumer = token.consumerBefore
+    this.tokens.splice(indexOfEarliestTokenToUndo)
+  }
+}
+
 function isTokenUnmatched(index: number, tokens: Token[]): boolean {
   const token = tokens[index]
 
@@ -89,7 +241,7 @@ function isInsideConvention(conventionMeanings: TokenMeaning[], tokens: Token[])
 function isConventionAtIndexUnclosed(conventionMeanings: TokenMeaning[], index: number, tokens: Token[]): boolean {
   const meaningCounts: number[] = new Array(conventionMeanings.length)
 
-  // We haven't seen any of the convention's meanings so far...
+  // We haven't found any of the convention's meanings so far...
   for (let i = 0; i < conventionMeanings.length; i++) {
     meaningCounts[i] = 0
   }
@@ -127,155 +279,4 @@ function indexOfLastToken(meaning: TokenMeaning, tokens: Token[]): number {
   }
 
   throw new Error('Token not found')
-}
-
-class Tokenizer {
-  public tokens: Token[] = []
-  private failureTracker = new FailureTracker()
-  private consumer: TextConsumer
-
-  constructor(text: string) {
-    this.consumer = new TextConsumer(text)
-
-    while (true) {
-      if (this.consumer.done()) {
-        if (this.backtrackIfAnyConventionsAreUnclosed()) {
-          continue
-        }
-
-        break
-      }
-      
-      const wasAnythingDiscovered = (
-        this.handleInlineCode()
-        || this.handleSandwiches()
-        || this.handleLink()
-      )
-      
-      if (wasAnythingDiscovered) {
-        continue
-      }
-
-      this.treatCurrentCharAsPlainText()
-      this.consumer.moveNext()
-    }
-  }
-
-  backtrackIfAnyConventionsAreUnclosed(): boolean {
-    for (let i = 0; i < this.tokens.length; i++) {
-      if (isTokenUnmatched(i, this.tokens)) {
-        this.backtrack(i)
-        return true
-      }
-    }
-    
-    return false
-  }
-
-  handleInlineCode(): boolean {
-    return this.consumer.consume({
-      from: '`',
-      upTo: '`',
-      then: (rawText) => {
-        const code = applyBackslashEscaping(rawText)
-        this.tokens.push(new Token(TokenMeaning.InlineCode, code))
-      }
-    })
-  }
-
-  handleSandwiches(): boolean {
-    const textIndex = this.consumer.lengthConsumed()
-
-    for (const sandwich of RICH_SANDWICHES) {
-      if (isInsideSandwich(sandwich, this.tokens) && this.consumer.consumeIfMatches(sandwich.end)) {
-        this.tokens.push(new Token(sandwich.meaningEnd))
-        return true
-      }
-
-      const sandwichStart = sandwich.start
-
-      const foundStartToken = (
-        !this.failureTracker.wasSandwichAlreadyTried(sandwich, textIndex)
-        && this.consumer.consumeIfMatches(sandwichStart)
-      )
-
-      if (foundStartToken) {
-        this.tokens.push(new Token(sandwich.meaningStart, this.consumer.asBeforeMatch(sandwichStart.length)))
-        return true
-      }
-    }
-  }
-
-
-  handleLink(): boolean {
-    const textIndex = this.consumer.lengthConsumed()
-
-    if (this.failureTracker.hasFailed(TokenMeaning.LinkStart, textIndex)) {
-      return false
-    }
-
-    if (!isInsideLink(this.tokens)) {
-      // If we're not inside a link, that means we can potentially start one. Let's see whether we should...
-      const LINK_START = '['
-
-      if (this.consumer.consumeIfMatches(LINK_START)) {
-        this.tokens.push(new Token(TokenMeaning.LinkStart, this.consumer.asBeforeMatch(LINK_START.length)))
-        return true
-      }
-
-      // ... Nope. We didn't find anything. Since we're not inside a link, let's bail.
-      return false
-    }
-
-    // We're insied a link! Are we looking at the URL arrow?
-    if (this.consumer.consumeIfMatches(' -> ')) {
-
-      // Great! We found the URL arrow. Let's try to find the closing bracket.
-      const didFindLinkEnd = this.consumer.consume({
-        upTo: ']',
-        then: url => this.tokens.push(new Token(TokenMeaning.LinkUrlAndLinkEnd, applyBackslashEscaping(url)))
-      })
-
-      if (!didFindLinkEnd) {
-        // No, the closing bracket is nowehere to be found. This wasn't a link. Oops!
-        this.undoLatest(TokenMeaning.LinkStart)
-      }
-
-      return true
-    }
-
-    // We haven't found the URL arrow yet, which means we're still tokenizing link's contents.
-    //
-    // If we find a closing brace before finding any URL arrow, that means we're actually looking at regular
-    // bracketed text.
-    if (this.consumer.consumeIfMatches(']')) {
-      this.undoLatest(TokenMeaning.LinkStart)
-      return true
-    }
-
-    return false
-  }
-
-  treatCurrentCharAsPlainText(): void {
-    const currentChar = this.consumer.escapedCurrentChar()
-    const lastToken = last(this.tokens)
-
-    if (lastToken && (lastToken.meaning === TokenMeaning.PlainText)) {
-      lastToken.value += currentChar
-    } else {
-      this.tokens.push(new Token(TokenMeaning.PlainText, currentChar))
-    }
-  }
-
-  undoLatest(meaning: TokenMeaning): void {
-    this.backtrack(indexOfLastToken(meaning, this.tokens))
-  }
-
-  backtrack(indexOfEarliestTokenToUndo: number): void {
-    const token = this.tokens[indexOfEarliestTokenToUndo]
-
-    this.failureTracker.registerFailure(token.meaning, token.textIndex())
-    this.consumer = token.consumerBefore
-    this.tokens.splice(indexOfEarliestTokenToUndo)
-  }
 }
