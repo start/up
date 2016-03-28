446998158d6888a17b7a80faa88aef6a841bd8ef
Daniel Miller <daniel@wants.email>
2016-03-27 21:36:19 -0500

    Add convention class

---
+++ src/Parsing/Inline/Convention.ts
@@ -0,0 +1,19 @@
+import { TokenMeaning } from './Token'
+import { RichInlineSyntaxNodeType } from '../../SyntaxNodes/RichInlineSyntaxNode'
+import { last } from '../CollectionHelpers'
+
+class Convention {
+  public meanings: TokenMeaning[]
+  
+  constructor(public NodeType: RichInlineSyntaxNodeType, ...meanings: TokenMeaning[]) {
+    this.meanings = meanings
+  }
+  
+  startMeaning(): TokenMeaning {
+    return this.meanings[0]
+  }
+  
+  endMeaning(): TokenMeaning {
+    return last(this.meanings)
+  }
+}
