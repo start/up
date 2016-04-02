493358acaf7b549390adc8c0a8f9a51aa124a9b0
Daniel Miller <daniel@wants.email>
2016-04-02 02:25:31 -0500

    Add passing backslash test

---
+++ src/Test/Ast/EdgeCases/Backslash.ts
@@ -0,0 +1,29 @@
+/// <reference path="../../../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../../../typings/chai/chai.d.ts" />
+
+import { expect } from 'chai'
+import * as Up from '../../../index'
+import { insideDocumentAndParagraph } from '../Helpers'
+import { SyntaxNode } from '../../../SyntaxNodes/SyntaxNode'
+import { LinkNode } from '../../../SyntaxNodes/LinkNode'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../../SyntaxNodes/StressNode'
+import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
+import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
+import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
+import { InlineAsideNode } from '../../../SyntaxNodes/InlineAsideNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
+import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
+
+
+describe('4 consecutive backslashes', () => {
+  it('produce plain text consisting of 2 consecutive backslashes', () => {
+    expect(Up.ast('\\\\\\\\')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('\\\\')
+      ]))
+  })
+})
