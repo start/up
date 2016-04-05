a3b47cd87e757cc12823289255fe1f76c380451b
Daniel Miller <daniel@wants.email>
2016-04-05 00:32:04 -0500

    Continue implementing shouting

--- src/Parsing/Inline/ApplyShouting.ts
+++ src/Parsing/Inline/ApplyShouting.ts
@@ -1,54 +1,71 @@
 import { Token, TokenMeaning } from './Token'
 import { STRESS, EMPHASIS, SHOUTING_EMPHASIS, SHOUTING_STRESS } from './Sandwiches'
 
-class TokenAndIndex {
-  constructor(public token: Token, public index: number) { }
+class TokenAndOriginalIndex {
+  constructor(public token: Token, public originalIndex: number) { }
 }
 
 // This method examines the shouting placeholder tokens, determines the author's intent, and generates actual
 // stress/emphasis tokens to reflect that intent.
+//
+// Stress and emphasis can only be started by asterisks immediately followed by non-whitespace, and both can
+// only be ended by asterisks immediadly following whitespace:
+//
+// This is *emphasized*
+// This is empha*size*d, too
+//
+// Inline code always counts as non-whitespace, but other token types are completely ignored. It doesn't
+// matter if they get in between the asterisks and any non-whitespace:
+//
+// *++This is emphasized++*
+
 export function applyShouting(tokens: Token[]): Token[] {
   return new ShoutingApplier(tokens).tokens
 }
 
+const RELEVANT_TOKEN_MEANINGS = [
+  TokenMeaning.PlainText,
+  TokenMeaning.ShoutingPlaceholder,
+  TokenMeaning.InlineCode
+]
+
 class ShoutingApplier {
-  constructor(public tokens: Token[]) {
-    const relevantTokens =
+  public relevantTokensAndOriginalIndexes: TokenAndOriginalIndex[]
+  public tokens: Token[]
+
+  constructor(tokens: Token[]) {
+    this.relevantTokensAndOriginalIndexes =
       tokens
-        .filter(token =>
-          token.meaning === TokenMeaning.PlainText || token.meaning === TokenMeaning.ShoutingPlaceholder)
-        .map((token, index) =>
-          new TokenAndIndex(token, index))
-  }
+        .filter(token => RELEVANT_TOKEN_MEANINGS.indexOf(token.meaning) !== -1)
+        .map((token, index) => new TokenAndOriginalIndex(token, index))
 
-  // For the purposes of shouting, the beginning and end of the string both count as whitespace. 
-  surroundedByWhitespace(index: number) {
-    return this.preceededByWhitespace(index) && this.followedByWhitespace(index)
-  }
+    let isEmphasized = false
+    let isStressed = false
 
-  followedByWhitespace(index: number): boolean {
-    if (index >= this.tokens.length) {
-      return true
+    for (let i = 0; i < this.relevantTokensAndOriginalIndexes.length; i++) {
+      const canStartConvention = this.doesTokenPreceedNonWhitespace(i)
+      const canEndConvention = this.doesTokenFollowNonWhitespace(i)
     }
 
-    return this.isTokenTextAndDoesTextMatchPattern(/^\s/, index + 1)
+    this.tokens = tokens
   }
 
-  preceededByWhitespace(index: number): boolean {
-    if (index < 0) {
-      return true
+  doesTokenPreceedNonWhitespace(index: number): boolean {
+    return !this.doesTokenMatchWhitespacePattern(index + 1, /^\s/)
+  }
+
+  doesTokenFollowNonWhitespace(index: number): boolean {
+    return !this.doesTokenMatchWhitespacePattern(index - 1, /\s$/)
+  }
+
+  doesTokenMatchWhitespacePattern(index: number, pattern: RegExp): boolean {
+    if ((index < 0) || (index >= this.relevantTokensAndOriginalIndexes.length)) {
+      return false
     }
 
-    return this.isTokenTextAndDoesTextMatchPattern(/\s$/, index)
-  }
+    const {meaning, value} = this.relevantTokensAndOriginalIndexes[index].token
 
-  isTokenTextAndDoesTextMatchPattern(pattern: RegExp, index: number): boolean {
-    const {meaning, value} = this.tokens[index]
-
-    return (
-      meaning === TokenMeaning.PlainText
-      && pattern.test(value)
-    )
+    return (meaning === TokenMeaning.PlainText) && pattern.test(value)
   }
 }
 
