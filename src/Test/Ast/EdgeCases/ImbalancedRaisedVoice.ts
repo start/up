c1ea3e24eb482e8165002dc29323a69edbe52911
Daniel Miller <daniel@wants.email>
2016-04-11 12:24:44 -0500

    Add passing raised voice test

--- src/Test/Ast/EdgeCases/ImbalancedRaisedVoice.ts
+++ src/Test/Ast/EdgeCases/ImbalancedRaisedVoice.ts
@@ -73,3 +73,17 @@ describe('Text surrounded by 3 asterisks to its left and 2 asterisks to its righ
       ]))
   })
 })
+
+
+describe('Text surrounded by 1 asterisk to its left and 3 asterisks to its right', () => {
+  it('is emphasized, and the 2 extra asterisks on the right do not appear in the final document as plain text', () => {
+    expect(Up.ast('Xamarin is now *free***!')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('Xamarin is now '),
+        new EmphasisNode([
+          new PlainTextNode('free'),
+        ]),
+        new PlainTextNode('!')
+      ]))
+  })
+})
