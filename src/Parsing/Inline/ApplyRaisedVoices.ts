f55537be80b1def2ad8f3df837bd9bc0f5da8730
Daniel Miller <daniel@wants.email>
2016-04-12 01:33:46 -0500

    Continue implementing applyRaisedVoices

--- src/Parsing/Inline/ApplyRaisedVoices.ts
+++ src/Parsing/Inline/ApplyRaisedVoices.ts
@@ -17,18 +17,44 @@ const POTENTIAL_RAISED_VOICE_TOKEN_MEANINGS = [
   ]
   
 export function applyRaisedVoices(tokens: Token[]): Token[] {
-  const intents =
-    tokens
-      .filter(token => -1 !== POTENTIAL_RAISED_VOICE_TOKEN_MEANINGS.indexOf(token.meaning))
-      .map(token => new RaisedVoiceTokenIntent(token.meaning))
-    
-  return tokens.map(token =>
-    (POTENTIAL_RAISED_VOICE_TOKEN_MEANINGS.indexOf(token.meaning) !== -1)
-      ? new Token(TokenMeaning.PlainText, token.value)
-      : token
-  )
+  const intentions = getEmptyIntentions(tokens)
+  return applyIntentions(intentions, tokens)
 }
 
-class RaisedVoiceTokenIntent {
-  constructor(public meaning: TokenMeaning) { }
+function getEmptyIntentions(tokens: Token[]): RaisedVoiceTokenIntention[] {
+    const intents: RaisedVoiceTokenIntention[] = []
+  
+  for (let tokenIndex = 0; tokenIndex < tokens.length; tokenIndex++) {
+    const token = tokens[tokenIndex]
+    const tokenMeaning = tokens[tokenIndex].meaning
+    
+    if (-1 !== POTENTIAL_RAISED_VOICE_TOKEN_MEANINGS.indexOf(token.meaning)) {
+      intents.push(new RaisedVoiceTokenIntention(tokenIndex, token))
+    }
+  }
+  
+  return intents
+}
+
+function applyIntentions(intentions: RaisedVoiceTokenIntention[], tokens: Token[]): Token[] {
+  // We could probably be naughty and modify the `tokens` collection directly.
+  const resultTokens = tokens.slice()
+  
+  for (const intention of intentions.sort(compareIntentionsDecending)) {
+    tokens.splice(intention.originalTokenIndex, 1, ...intention.finalTokens())
+  }
+  
+  return resultTokens
+}
+
+class RaisedVoiceTokenIntention {
+  constructor(public originalTokenIndex: number, private originalToken: Token) { }
+  
+  finalTokens(): Token[] {
+    return [new Token(TokenMeaning.PlainText, this.originalToken.value)]
+  }
+}
+
+function compareIntentionsDecending(a: RaisedVoiceTokenIntention, b: RaisedVoiceTokenIntention): number {
+  return b.originalTokenIndex - a.originalTokenIndex
 }
