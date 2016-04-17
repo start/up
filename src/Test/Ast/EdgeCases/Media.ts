913eebc81278508c5b8962f6f51a43ab743409b3
Daniel Miller <daniel@wants.email>
2016-04-17 12:10:47 -0500

    Add passing media convention

--- src/Test/Ast/EdgeCases/Media.ts
+++ src/Test/Ast/EdgeCases/Media.ts
@@ -28,3 +28,20 @@ describe('A line consisting solely of media conventions', () => {
 })
 
 
+describe('A paragraph followed by a line consisting solely of media conventions', () => {
+  it('does not produce a line block node', () => {
+    const text = `
+You'll never believe this fake evidence!
+[-_-: ghostly howling -> http://example.com/ghosts.ogg][o_o: haunted house -> http://example.com/hauntedhouse.svg][o_-: ghosts eating luggage -> http://example.com/poltergeists.webm]`
+    
+    expect(Up.ast(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode("You'll never believe this fake evidence!")
+        ]),
+        new AudioNode('ghostly howling', 'http://example.com/ghosts.ogg'),
+        new ImageNode('haunted house', 'http://example.com/hauntedhouse.svg'),
+        new VideoNode('ghosts eating luggage', 'http://example.com/poltergeists.webm')
+      ]))
+  })
+})
