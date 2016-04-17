a986f5be2e59ae3634fd86ade93d2da9d0a86fe0
Daniel Miller <daniel@wants.email>
2016-04-17 06:53:53 -0500

    Add failing audio test

--- src/Test/Ast/EdgeCases/Audio.ts
+++ src/Test/Ast/EdgeCases/Audio.ts
@@ -4,6 +4,8 @@
 import { expect } from 'chai'
 import * as Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 import { AudioNode } from '../../../SyntaxNodes/AudioNode'
 import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 
@@ -32,5 +34,21 @@ describe('Audio with a blank URL', () => {
   it('is not included in the document', () => {
     expect(Up.ast('[-_-: ghostly howling -> \t   ]')).to.be.eql(
       new DocumentNode([]))
+  })
+})
+
+
+describe('A paragraph directly followed by audio on its own line', () => {
+  it('produces a pagraph node followed by an audio node, not a line block', () => {
+    const text = `
+Do not pour the spiders into your sister's cereal.
+[-_-: six seconds of screaming -> http://example.com/screaming.ogg]`
+    expect(Up.ast(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode('Do not pour the spiders into your milk.')
+        ]),
+        new AudioNode('six seconds of screaming', 'http://example.com/screaming.ogg'),
+      ]))
   })
 })
