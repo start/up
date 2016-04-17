a806776256641d1b3e8b5ea36d3a17bad5694125
Daniel Miller <daniel@wants.email>
2016-04-17 12:18:02 -0500

    Add passing media test

--- src/Test/Ast/EdgeCases/Media.ts
+++ src/Test/Ast/EdgeCases/Media.ts
@@ -8,6 +8,8 @@ import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 import { AudioNode } from '../../../SyntaxNodes/AudioNode'
 import { VideoNode } from '../../../SyntaxNodes/VideoNode'
+import { LineBlockNode } from '../../../SyntaxNodes/LineBlockNode'
+import { Line } from '../../../SyntaxNodes/Line'
 import { ImageNode } from '../../../SyntaxNodes/ImageNode'
 import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 
@@ -17,7 +19,7 @@ describe('A line consisting solely of media conventions', () => {
   it('produces a node for each convention and includes each directly into the outline, rather than squeezing them all into a paragraph', () => {
     const text =
       '[-_-: ghostly howling -> http://example.com/ghosts.ogg][o_o: haunted house -> http://example.com/hauntedhouse.svg][o_-: ghosts eating luggage -> http://example.com/poltergeists.webm]'
-    
+
     expect(Up.ast(text)).to.be.eql(
       new DocumentNode([
         new AudioNode('ghostly howling', 'http://example.com/ghosts.ogg'),
@@ -33,7 +35,7 @@ describe('A paragraph followed by a line consisting solely of media conventions'
     const text = `
 You'll never believe this fake evidence!
 [-_-: ghostly howling -> http://example.com/ghosts.ogg][o_o: haunted house -> http://example.com/hauntedhouse.svg][o_-: ghosts eating luggage -> http://example.com/poltergeists.webm]`
-    
+
     expect(Up.ast(text)).to.be.eql(
       new DocumentNode([
         new ParagraphNode([
@@ -42,6 +44,49 @@ You'll never believe this fake evidence!
         new AudioNode('ghostly howling', 'http://example.com/ghosts.ogg'),
         new ImageNode('haunted house', 'http://example.com/hauntedhouse.svg'),
         new VideoNode('ghosts eating luggage', 'http://example.com/poltergeists.webm')
+      ]))
+  })
+})
+
+
+describe('A line solely consisting solely of media conventions inserted in the middle of a line block', () => {
+  it('splits the line block in two', () => {
+    const text = `
+1234 Spooky Street
+Pepe, PA 17101
+[-_-: ghostly howling -> http://example.com/ghosts.ogg][o_o: haunted house -> http://example.com/hauntedhouse.svg][o_-: ghosts eating luggage -> http://example.com/poltergeists.webm]
+Roses are red
+Skeltals are white
+If you stay here
+You're in for a fright`
+
+    expect(Up.ast(text)).to.be.eql(
+      new DocumentNode([
+        new LineBlockNode([
+          new Line([
+            new PlainTextNode('1234 Spooky Street')
+          ]),
+          new Line([
+            new PlainTextNode('Pepe, PA 17101')
+          ])
+        ]),
+        new AudioNode('ghostly howling', 'http://example.com/ghosts.ogg'),
+        new ImageNode('haunted house', 'http://example.com/hauntedhouse.svg'),
+        new VideoNode('ghosts eating luggage', 'http://example.com/poltergeists.webm'),
+        new LineBlockNode([
+          new Line([
+            new PlainTextNode('Roses are red')
+          ]),
+          new Line([
+            new PlainTextNode('Skeltals are white')
+          ]),
+          new Line([
+            new PlainTextNode('If you stay here')
+          ]),
+          new Line([
+            new PlainTextNode("You're in for a fright")
+          ]),
+        ])
       ]))
   })
 })
