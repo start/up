37b9049486fadb348c18f8c74af64622cae10d11
Daniel Miller <daniel@wants.email>
2016-04-23 09:47:14 -0500

    Remove unnecessary I18n interface declarations

--- src/Writer/WriterConfig.ts
+++ src/Writer/WriterConfig.ts
@@ -1,32 +1,30 @@
-interface I18nArgs {
-  idDelimiter?: string,
-  terms?: I18nTerms,
-}
-
-interface I18nTerms {
-  footnote?: string,
-  footnoteReference?: string
-}
-
 export interface WriterConfigArgs {
   documentId?: string,
-  i18n?: I18nArgs
+  
+  i18n?: {    
+    idDelimiter?: string,
+    
+    terms?: {
+      footnote?: string,
+      footnoteReference?: string
+    },
+  }
 }
 
 export class WriterConfig {
   private config: WriterConfigArgs
 
   constructor(args: WriterConfigArgs) {
-    args = args || { }
-    const i18n = args.i18n || { }
-    const i18nTerms = i18n.terms || { }
-    
+    args = args || {}
+    const i18n = args.i18n || {}
+    const i18nTerms = i18n.terms || {}
+
     this.config = {
       documentId: args.documentId || '',
-      
+
       i18n: {
         idDelimiter: i18n.idDelimiter || '-',
-        
+
         terms: {
           footnote: i18nTerms.footnote || 'footnote',
           footnoteReference: i18nTerms.footnoteReference || 'footnote-reference',
