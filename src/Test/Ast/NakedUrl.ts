ba7ab040983968ac7e585f2bf65bbbf53307dc0c
Daniel Miller <daniel@wants.email>
2016-04-25 09:13:59 -0500

    Add 2 failing naked URL tests

---
+++ src/Test/Ast/NakedUrl.ts
@@ -0,0 +1,28 @@
+
+import { expect } from 'chai'
+import * as Up from '../../index'
+import { insideDocumentAndParagraph } from './Helpers'
+import { LinkNode } from '../../SyntaxNodes/LinkNode'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../SyntaxNodes/StressNode'
+import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
+import { RevisionInsertionNode } from '../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../SyntaxNodes/RevisionDeletionNode'
+import { SpoilerNode } from '../../SyntaxNodes/SpoilerNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
+
+
+describe('A naked URL', () => {
+  it('produces a link node. The content of the link is the URL minus its protocol', () => {
+    expect(Up.toAst('https://archive.org')).to.be.eql(
+      insideDocumentAndParagraph([
+        new LinkNode([
+          new PlainTextNode('archive.org')
+        ], 'https://archive.org')
+      ]))
+  })
+})
+
