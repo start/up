2d6bd63345371050f8ea9512a8ead071835c91b8
Daniel Miller <daniel@wants.email>
2016-04-28 12:49:27 -0500

    Remove unnecessary imports

--- src/Test/Ast/EdgeCases/Link.ts
+++ src/Test/Ast/EdgeCases/Link.ts
@@ -8,11 +8,6 @@ import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 import { StressNode } from '../../../SyntaxNodes/StressNode'
 import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
-import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
-import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
 
 
 describe('An opening bracket followed by " -> " followed by a closing bracket', () => {
