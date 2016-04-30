62e2e731991a1f6bd530d1d48607a65bfffd5757
Daniel Miller <daniel@wants.email>
2016-04-30 11:34:00 -0500

    [Broken] Update media cons. to use new token types

--- src/Parsing/Inline/MediaConventions.ts
+++ src/Parsing/Inline/MediaConventions.ts
@@ -1,14 +1,14 @@
-import { Convention } from './Convention'
-import { TokenMeaning } from './Token'
-import { MediaSyntaxNodeType } from '../../SyntaxNodes/MediaSyntaxNode'
 import { MediaConvention } from './MediaConvention'
+import { AudioToken } from './Tokens/AudioToken'
+import { ImageToken } from './Tokens/ImageToken'
+import { VideoToken } from './Tokens/VideoToken'
 import { AudioNode } from '../../SyntaxNodes/AudioNode'
 import { ImageNode } from '../../SyntaxNodes/ImageNode'
 import { VideoNode } from '../../SyntaxNodes/VideoNode'
 
-const AUDIO = new MediaConvention('audio', AudioNode, TokenMeaning.AudioStartAndAudioDescription, TokenMeaning.AudioUrlAndAudioEnd)
-const IMAGE = new MediaConvention('image', ImageNode, TokenMeaning.ImageStartAndAudioDescription, TokenMeaning.ImageUrlAndAudioEnd)
-const VIDEO = new MediaConvention('video', VideoNode, TokenMeaning.VideoStartAndAudioDescription, TokenMeaning.VideoUrlAndAudioEnd)
+const AUDIO = new MediaConvention('audio', AudioNode, AudioToken)
+const IMAGE = new MediaConvention('image', ImageNode, ImageToken)
+const VIDEO = new MediaConvention('video', VideoNode, VideoToken)
 
 export {
   AUDIO,
