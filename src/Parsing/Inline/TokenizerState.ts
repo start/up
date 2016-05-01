e4da1eea2f37ac562a2133197fa511a44cbe362f
Daniel Miller <daniel@wants.email>
2016-04-30 23:30:00 -0500

    Add tokenizer state class

---
+++ src/Parsing/Inline/TokenizerState.ts
@@ -0,0 +1,19 @@
+// For now, emphasis and stress aren't determined until after tokenization, so we don't
+// need to worry about keeping track of them here.
+export class TokenizerState {
+  public isLinkOpen = false
+  public isRevisionDeletionOpen = false
+  public isRevisionInsertionOpen = false
+  public countSpoilersOpen = 0
+  public countFootnotesOpen = 0
+  
+  failed(): boolean {
+    return (
+      this.isLinkOpen
+      || this.isRevisionDeletionOpen
+      || this.isRevisionInsertionOpen
+      || this.countSpoilersOpen > 0
+      || this.countFootnotesOpen > 0
+    )
+  }
+}
