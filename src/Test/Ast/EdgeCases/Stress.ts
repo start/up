dfc093bcca1d9dc3d17f1b9a36f0117e6c9ea206
Daniel Miller <daniel@wants.email>
2016-05-04 15:07:58 -0500

    Remove extra imports from EdgeCases/Stress.ts

--- src/Test/Ast/EdgeCases/Stress.ts
+++ src/Test/Ast/EdgeCases/Stress.ts
@@ -1,20 +1,8 @@
-
 import { expect } from 'chai'
 import * as Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
-import { LinkNode } from '../../../SyntaxNodes/LinkNode'
-import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
-import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 import { StressNode } from '../../../SyntaxNodes/StressNode'
-import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
-import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
-import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
-
-
 
 
 describe('Double asterisks followed by whitespace with matching double asterisks touching the end of a word', () => {
