32a22c98ebdd66286472a2c1b045e7fc44862c34
Daniel Miller <daniel@wants.email>
2016-05-04 15:30:56 -0500

    Remove extra imports from EdgeCases/Footnote.ts

--- src/Test/Ast/EdgeCases/Footnote.ts
+++ src/Test/Ast/EdgeCases/Footnote.ts
@@ -1,23 +1,14 @@
-
 import { expect } from 'chai'
 import * as Up from '../../../index'
-import { insideDocumentAndParagraph } from '../Helpers'
-import { LinkNode } from '../../../SyntaxNodes/LinkNode'
 import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
-import { StressNode } from '../../../SyntaxNodes/StressNode'
-import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
 import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 import { BlockquoteNode } from '../../../SyntaxNodes/BlockquoteNode'
 import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 import { UnorderedListNode } from '../../../SyntaxNodes/UnorderedListNode'
 import { UnorderedListItem } from '../../../SyntaxNodes/UnorderedListItem'
-import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
 
 
 describe('A footnote reference at the end of a paragraph', () => {
