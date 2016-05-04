7661c0ddd5d2fb194de45eb2e74942452c25b1e7
Daniel Miller <daniel@wants.email>
2016-05-04 15:04:50 -0500

    Remove extra imports from Ast/Audio.ts

--- src/Test/Ast/Audio.ts
+++ src/Test/Ast/Audio.ts
@@ -1,20 +1,10 @@
-
 import { expect } from 'chai'
 import * as Up from '../../index'
 import { insideDocumentAndParagraph } from './Helpers'
-import { LinkNode } from '../../SyntaxNodes/LinkNode'
-import { ImageNode } from '../../SyntaxNodes/ImageNode'
 import { AudioNode } from '../../SyntaxNodes/AudioNode'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
-import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
-import { StressNode } from '../../SyntaxNodes/StressNode'
-import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../SyntaxNodes/SpoilerNode'
 import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
-import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
 
 
 describe('Bracketed text starting with "audio:" with a description pointing to a URL', () => {
