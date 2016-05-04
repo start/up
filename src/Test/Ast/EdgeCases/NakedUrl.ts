20e4820bc4a9c1ba8e06ad6c73510cc671da3b6a
Daniel Miller <daniel@wants.email>
2016-05-04 15:13:31 -0500

    Remove extra imports from EdgeCases/NakedUrl.ts

--- src/Test/Ast/EdgeCases/NakedUrl.ts
+++ src/Test/Ast/EdgeCases/NakedUrl.ts
@@ -1,18 +1,9 @@
-
 import { expect } from 'chai'
 import * as Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
 import { LinkNode } from '../../../SyntaxNodes/LinkNode'
-import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
-import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
-import { StressNode } from '../../../SyntaxNodes/StressNode'
-import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
 import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
-import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
 
 
 describe('A naked URL containing another URL', () => {
@@ -26,6 +17,7 @@ describe('A naked URL containing another URL', () => {
   })
 })
 
+
 describe('A naked URL following an open parenthesis', () => {
   it("can contain an escaped closing parenthesis", () => {
     expect(Up.toAst('(https://nintendo.com\)')).to.be.eql(
@@ -38,6 +30,7 @@ describe('A naked URL following an open parenthesis', () => {
       ]))
   })
 })
+
 
 describe('A naked URL following an open square bracket', () => {
   it("can contain an escaped closing square bracket", () => {
