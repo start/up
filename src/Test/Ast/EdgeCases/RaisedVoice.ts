a426d475e8410c052d0e36b04cc217ec34c03608
Daniel Miller <daniel@wants.email>
2016-05-04 15:11:07 -0500

    Remove extra improts from EdgeCases/RaisedVoice.ts

--- src/Test/Ast/EdgeCases/RaisedVoice.ts
+++ src/Test/Ast/EdgeCases/RaisedVoice.ts
@@ -2,15 +2,10 @@
 import { expect } from 'chai'
 import * as Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
-import { LinkNode } from '../../../SyntaxNodes/LinkNode'
 import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 import { StressNode } from '../../../SyntaxNodes/StressNode'
-import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
 import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 
 
