a4b630804635e918c53b1751ca4afb69d7739074
Daniel Miller <daniel@wants.email>
2016-05-04 15:07:23 -0500

    Remove imports from EdgeCases/UnorderedList.ts

--- src/Test/Ast/EdgeCases/UnorderedList.ts
+++ src/Test/Ast/EdgeCases/UnorderedList.ts
@@ -1,25 +1,15 @@
-
 import { expect } from 'chai'
 import * as Up from '../../../index'
-import { LinkNode } from '../../../SyntaxNodes/LinkNode'
 import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
-import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
-import { StressNode } from '../../../SyntaxNodes/StressNode'
-import { InlineCodeNode } from '../../../SyntaxNodes/InlineCodeNode'
-import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
-import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
-import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
 import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 import { UnorderedListNode } from '../../../SyntaxNodes/UnorderedListNode'
 import { UnorderedListItem } from '../../../SyntaxNodes/UnorderedListItem'
-import { OrderedListNode } from '../../../SyntaxNodes/OrderedListNode'
-import { OrderedListItem } from '../../../SyntaxNodes/OrderedListItem'
 import { SectionSeparatorNode } from '../../../SyntaxNodes/SectionSeparatorNode'
 import { HeadingNode } from '../../../SyntaxNodes/HeadingNode'
 import { LineBlockNode } from '../../../SyntaxNodes/LineBlockNode'
-import { CodeBlockNode } from '../../../SyntaxNodes/CodeBlockNode'
 import { Line } from '../../../SyntaxNodes/Line'
+import { CodeBlockNode } from '../../../SyntaxNodes/CodeBlockNode'
 
 
 describe('An unordered list with a single item', () => {
