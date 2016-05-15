1b553a7bdb1032687a43f49e989996ffffc96f2a
Daniel Miller <daniel@wants.email>
2016-05-15 03:00:45 -0500

    Add passing html config test

--- src/Test/Html/Config/FootnoteBlock.ts
+++ src/Test/Html/Config/FootnoteBlock.ts
@@ -59,3 +59,33 @@ describe("The words in footnote's ID (as well as the ID of the footnote referenc
     expect(up.toHtml(node)).to.be.eql(html)
   })
 })
+
+
+describe("Separate words in the provided document name", () => {
+  it("are delimited by specified the ID word delimiter", () => {
+    const up = new Up({
+      documentName: 'reply 11',
+      i18n: {
+        idWordDelimiter: '::'
+      }
+    })
+    
+    const node =
+      new FootnoteBlockNode([
+        new FootnoteNode([
+          new PlainTextNode("Arwings"),
+        ], 2),
+        new FootnoteNode([
+          new PlainTextNode("Killer Bees"),
+        ], 3),
+      ])
+
+    const html =
+      '<dl data-footnotes>'
+      + '<dt id="reply::11::footnote::2" data-footnote><a href="#reply::11::footnote::reference::2">2</a></dt><dd>Arwings</dd>'
+      + '<dt id="reply::11::footnote::3" data-footnote><a href="#reply::11::footnote::reference::3">3</a></dt><dd>Killer Bees</dd>'
+      + '</dl>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
