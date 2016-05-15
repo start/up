3f3e370f423c75a6aa261e5e8d7da516a7795320
Daniel Miller <daniel@wants.email>
2016-05-15 02:40:19 -0500

    Add 2 passing config tests

--- src/Test/Html/Config/Footnote.ts
+++ src/Test/Html/Config/Footnote.ts
@@ -4,15 +4,48 @@ import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 
 
-describe("A footnote reference's ID, as well as the ID of the referenced footnote", () => {
-  const up = new Up({
-    documentName: 'reply-11'
-  }) 
-  
+describe("A footnote reference's ID (as well as the ID of the footnote it points to)", () => {
   it("are prefixed with the document name, if one was provided", () => {
     const node = new FootnoteNode([], 3)
-    
+
+    const up = new Up({
+      documentName: 'reply-11'
+    })
+
     expect(up.toHtml(node)).to.be.eql(
       '<sup id="reply-11-footnote-reference-3" data-footnote-reference><a href="#reply-11-footnote-3">3</a></sup>')
+  })
+})
+
+
+describe("The words in footnote reference's ID (as well as the ID of the footnote it points to)", () => {
+it("are delimited by specified the ID word delimiter", () => {
+    const node = new FootnoteNode([], 3)
+
+    const up = new Up({
+      i18n: {
+        idWordDelimiter: '::'
+      }
+    })
+
+    expect(up.toHtml(node)).to.be.eql(
+      '<sup id="footnote::reference::3" data-footnote-reference><a href="#footnote::3">3</a></sup>')
+  })
+})
+
+
+describe("Separate words in the provided document name", () => {
+it("are delimited by specified the ID word delimiter", () => {
+    const node = new FootnoteNode([], 3)
+
+    const up = new Up({
+      documentName: 'reply 11',
+      i18n: {
+        idWordDelimiter: '::'
+      }
+    })
+
+    expect(up.toHtml(node)).to.be.eql(
+      '<sup id="reply::11::footnote::reference::3" data-footnote-reference><a href="#reply::11::footnote::3">3</a></sup>')
   })
 })
