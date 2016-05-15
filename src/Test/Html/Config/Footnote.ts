8d8ec3ffbcec11a0b12069fc38f6fb9685e7d649
Daniel Miller <daniel@wants.email>
2016-05-15 02:26:33 -0500

    Add passing config test

--- src/Test/Html/Config/Footnote.ts
+++ src/Test/Html/Config/Footnote.ts
@@ -2,3 +2,17 @@ import { expect } from 'chai'
 import { Up } from '../../../index'
 import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
+
+
+describe("A footnote reference's ID, as well as the ID of the referenced footnote", () => {
+  const up = new Up({
+    documentName: 'reply-11'
+  }) 
+  
+  it("are prefixed with the document name, if one was provided", () => {
+    const node = new FootnoteNode([], 3)
+    
+    expect(up.toHtml(node)).to.be.eql(
+      '<sup id="reply-11-footnote-reference-3" data-footnote-reference><a href="#reply-11-footnote-3">3</a></sup>')
+  })
+})
