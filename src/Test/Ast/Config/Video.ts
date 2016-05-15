7229192eaeee97f043626a75f88774dee6819d2f
Daniel Miller <daniel@wants.email>
2016-05-14 23:41:45 -0500

    Add 2 config tests, 1 passing and 1 failing

---
+++ src/Test/Ast/Config/Video.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai'
+import { Up } from '../../../index'
+import { insideDocumentAndParagraph } from '../Helpers'
+import { VideoNode } from '../../../SyntaxNodes/VideoNode'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+
+
+describe('The term that represents video conventions', () => {
+    const up = new Up({
+      i18n: {
+        terms: { video: 'watch' }
+      }
+    })
+    
+  it('comes from the "video" config term', () => {
+    const text = '[watch: Nevada caucus footage -> https://example.com/video.webm]'
+
+    expect(up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new VideoNode('Nevada caucus footage', 'https://example.com/video.webm')
+      ])
+    )
+  })
+    
+  it('is always case insensitive', () => {
+    const text = '[WATCH: Nevada caucus footage -> https://example.com/video.webm]'
+
+    expect(up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new VideoNode('Nevada caucus footage', 'https://example.com/video.webm')
+      ])
+    )
+  })
+})
