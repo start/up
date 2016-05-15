e2b8a8bb01b2c1b95202e03107db125c89e8332f
Daniel Miller <daniel@wants.email>
2016-05-15 02:11:32 -0500

    Add empty file for footnote html config tests

---
+++ src/Test/Html/Config/Footnote.ts
@@ -0,0 +1,4 @@
+import { expect } from 'chai'
+import { Up } from '../../../index'
+import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
