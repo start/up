14f0d33f791af59891a7a71a02ed7c014dfd19f2
Daniel Miller <daniel@wants.email>
2016-05-15 00:10:19 -0500

    Add 2 passing config tests

---
+++ src/Test/Ast/Config/Image.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai'
+import { Up } from '../../../index'
+import { insideDocumentAndParagraph } from '../Helpers'
+import { ImageNode } from '../../../SyntaxNodes/ImageNode'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+
+
+describe('The term that represents image conventions', () => {
+    const up = new Up({
+      i18n: {
+        terms: { image: 'see' }
+      }
+    })
+    
+  it('comes from the "audio" config term', () => {
+    const text = '[see: Chrono Cross logo -> https://example.com/cc.png]'
+
+    expect(up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ImageNode('Chrono Cross logo', 'https://example.com/cc.png')
+      ])
+    )
+  })
+    
+  it('is always case insensitive', () => {
+    const lowercase = '[see: Chrono Cross logo -> https://example.com/cc.png]'
+    const misedCase = '[SeE: Chrono Cross logo -> https://example.com/cc.png]'
+
+    expect(up.toAst(misedCase)).to.be.eql(up.toAst(lowercase))
+  })
+})
