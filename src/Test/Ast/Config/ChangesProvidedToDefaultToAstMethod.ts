37d2c582a4ae31970d77f433e903ae4469aa5752
Daniel Miller <daniel@wants.email>
2016-05-15 16:25:32 -0500

    Refactor some config tests

--- src/Test/Ast/Config/ChangesProvidedToDefaultToAstMethod.ts
+++ src/Test/Ast/Config/ChangesProvidedToDefaultToAstMethod.ts
@@ -7,81 +7,65 @@ import { VideoNode } from '../../../SyntaxNodes/VideoNode'
 import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 
 
-describe('Providing the "audio" config term when creating the Up object', () => {
-  const configChanges: UpConfigArgs = {
-    i18n: {
-      terms: { audio: 'listen' }
-    }
-  }
-
+function expectConfigChangesToHaveSameEffectWhenProvidedToDefaultToAstMethod(text: string, configChanges: UpConfigArgs): void {
   const up = new Up(configChanges)
 
+  const whenProvidingConfigAtCreation =
+    up.toAst(text)
+
+  const whenProvidingChangesWhenCallingMethod =
+    Up.toAst(text, configChanges)
+
+  expect(whenProvidingConfigAtCreation).to.be.eql(whenProvidingChangesWhenCallingMethod)
+}
+
+describe('Providing the "audio" config term when creating the Up object', () => {
   it('has the same result as providing the term when calling the (default) static toAst method', () => {
     const text = '[listen: chanting at Nevada caucus -> https://example.com/audio.ogg]'
 
-    const astFromOriginalConfig = up.toAst(text)
-    const astFromChangesToDefault = Up.toAst(text, configChanges)
-
-    expect(astFromOriginalConfig).to.be.eql(astFromChangesToDefault)
+    expectConfigChangesToHaveSameEffectWhenProvidedToDefaultToAstMethod(text, {
+      i18n: {
+        terms: { audio: 'listen' }
+      }
+    })
   })
 })
 
 
 describe('Providing the "image" config term when creating the Up object', () => {
-  const configChanges: UpConfigArgs = {
-    i18n: {
-      terms: { image: 'see' }
-    }
-  }
-
-  const up = new Up(configChanges)
-
   it('has the same result as providing the term when calling the (default) static toAst method', () => {
     const text = '[see: Chrono Cross logo -> https://example.com/cc.png]'
 
-    const astFromOriginalConfig = up.toAst(text)
-    const astFromChangesToDefault = Up.toAst(text, configChanges)
-
-    expect(astFromOriginalConfig).to.be.eql(astFromChangesToDefault)
+    expectConfigChangesToHaveSameEffectWhenProvidedToDefaultToAstMethod(text, {
+      i18n: {
+        terms: { image: 'see' }
+      }
+    })
   })
 })
 
 
 describe('Providing the "video" config term when creating the Up object', () => {
-  const configChanges: UpConfigArgs = {
-    i18n: {
-      terms: { video: 'watch' }
-    }
-  }
-
-  const up = new Up(configChanges)
-
   it('has the same result as providing the term when calling the (default) static toAst method', () => {
     const text = '[watch: Nevada caucus footage -> https://example.com/video.webm]'
 
-    const astFromOriginalConfig = up.toAst(text)
-    const astFromChangesToDefault = Up.toAst(text, configChanges)
-
-    expect(astFromOriginalConfig).to.be.eql(astFromChangesToDefault)
+    expectConfigChangesToHaveSameEffectWhenProvidedToDefaultToAstMethod(text, {
+      i18n: {
+        terms: { video: 'watch' }
+      }
+    })
   })
 })
 
 
 describe('Providing the "spoiler" config term when creating the Up object', () => {
-  const configChanges: UpConfigArgs = {
-    i18n: {
-      terms: { spoiler: 'ruins ending' }
-    }
-  }
-
-  const up = new Up(configChanges)
-
   it('has the same result as providing the term when calling the (default) static toAst method', () => {
     const text = '[RUINS ENDING: Ash fights Gary]'
 
-    const astFromOriginalConfig = up.toAst(text)
-    const astFromChangesToDefault = Up.toAst(text, configChanges)
-
-    expect(astFromOriginalConfig).to.be.eql(astFromChangesToDefault)
+    expectConfigChangesToHaveSameEffectWhenProvidedToDefaultToAstMethod(text, {
+      i18n: {
+        terms: { spoiler: 'ruins ending' }
+      }
+    })
   })
 })
