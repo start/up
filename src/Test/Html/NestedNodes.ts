82c478c4f4ec770b9104af43475a416e04763b16
Daniel Miller <daniel@wants.email>
2016-05-17 10:32:00 -0500

    Add failing html test

---
+++ src/Test/Html/NestedNodes.ts
@@ -0,0 +1,44 @@
+import { expect } from 'chai'
+import Up from '../../index'
+
+import { LinkNode } from '../../SyntaxNodes/LinkNode'
+import { ImageNode } from '../../SyntaxNodes/ImageNode'
+import { AudioNode } from '../../SyntaxNodes/AudioNode'
+import { VideoNode } from '../../SyntaxNodes/VideoNode'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../SyntaxNodes/StressNode'
+import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
+import { RevisionInsertionNode } from '../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../SyntaxNodes/RevisionDeletionNode'
+import { SpoilerNode } from '../../SyntaxNodes/SpoilerNode'
+import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../SyntaxNodes/FootnoteBlockNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { BlockquoteNode } from '../../SyntaxNodes/BlockquoteNode'
+import { UnorderedListNode } from '../../SyntaxNodes/UnorderedListNode'
+import { UnorderedListItem } from '../../SyntaxNodes/UnorderedListItem'
+import { OrderedListNode } from '../../SyntaxNodes/OrderedListNode'
+import { OrderedListItem } from '../../SyntaxNodes/OrderedListItem'
+import { DescriptionListNode } from '../../SyntaxNodes/DescriptionListNode'
+import { DescriptionListItem } from '../../SyntaxNodes/DescriptionListItem'
+import { DescriptionTerm } from '../../SyntaxNodes/DescriptionTerm'
+import { Description } from '../../SyntaxNodes/Description'
+import { LineBlockNode } from '../../SyntaxNodes/LineBlockNode'
+import { Line } from '../../SyntaxNodes/Line'
+import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+import { CodeBlockNode } from '../../SyntaxNodes/CodeBlockNode'
+import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
+import { HtmlWriter } from '../../Writer/HtmlWriter'
+
+
+describe('Inside a link, a footnote', () => {
+  it("'does not produce an anchor element. The footnote's sup element directly includes the would-be anchor's contents", () => {
+    const node = new LinkNode([
+      new PlainTextNode('Google'),
+      new FootnoteNode([new PlainTextNode('An older search engine')], 2)
+    ], 'https://google.com')
+    expect(Up.toHtml(node)).to.be.eql('<a href="https://google.com">Google<sup id="footnote-reference-2" data-footnote-reference>2</sup></a>')
+  })
+})
