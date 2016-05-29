5aee05b5ed59fae752387894a328dba21dd32178
Daniel Miller <daniel@wants.email>
2016-05-28 19:47:41 -0500

    Add failing spoiler link tests

--- src/Test/Ast/SpoilerLink.ts
+++ src/Test/Ast/SpoilerLink.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import Up from '../../index'
-import { insideDocumentAndParagraph } from './Helpers'
+import { insideDocumentAndParagraph, expectEveryCombinationOf } from './Helpers'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { LinkNode } from '../../SyntaxNodes/LinkNode'
 import { SpoilerNode } from '../../SyntaxNodes/SpoilerNode'
@@ -18,5 +18,30 @@ describe('A spoiler followed immediately by a parenthesized/bracketd URL"', () =
         ]),
         new PlainTextNode('.')
       ]))
+  })
+})
+
+
+describe('A spoiler produced by parenthesized, square bracketed, or curly bracketed text followed immediately by a parenthesized, a square bracketed, or a curly bracketed URL', () => {
+  it('produces a spoiler node whose contents are put inside a link pointing to that URL. The type of bracket surrounding the spoiler can be different from the type of bracket surrounding the URL', () => {
+    expectEveryCombinationOf({
+      firstHalves: [
+        '[SPOILER: you fight Gary]',
+        '(SPOILER: you fight Gary)',
+        '{SPOILER: you fight Gary}'
+      ],
+      secondHalves: [
+        '[http://example.com/finalbattle]',
+        '(http://example.com/finalbattle)',
+        '{http://example.com/finalbattle}'
+      ],
+      toProduce: insideDocumentAndParagraph([
+        new SpoilerNode([
+          new LinkNode([
+            new PlainTextNode('you fight Gary')  
+          ], 'http://example.com/finalbattle')
+        ]),
+      ])
+    })
   })
 })
