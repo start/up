e40309991b98c371777278a3083fd377d9037b75
Daniel Miller <daniel@wants.email>
2016-05-28 20:04:25 -0500

    Add failing footnote link tests

--- src/Test/Ast/FootnoteLink.ts
+++ src/Test/Ast/FootnoteLink.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import Up from '../../index'
+import { expectEveryCombinationOf } from './Helpers'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
 import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 import { LinkNode } from '../../SyntaxNodes/LinkNode'
@@ -10,12 +11,12 @@ import { FootnoteBlockNode } from '../../SyntaxNodes/FootnoteBlockNode'
 
 describe('A footnote directly followed by a bracketed/parenthesized URL', () => {
   it("produces a footnote whose entire contents is put inside a link pointing to that URL", () => {
-    const text = "I don't eat cereal. ((Well, I do, but I pretend not to.))[http://example.com] Never have."
+    const text = "I don't eat cereal. ((Well, I do, but I pretend not to.))[http://example.com/luckycharms] Never have."
 
     const footnote = new FootnoteNode([
       new LinkNode([
         new PlainTextNode('Well, I do, but I pretend not to.')
-      ], 'http://example.com')
+      ], 'http://example.com/luckycharms')
     ], 1)
 
     expect(Up.toAst(text)).to.be.eql(
@@ -27,5 +28,36 @@ describe('A footnote directly followed by a bracketed/parenthesized URL', () => 
         ]),
         new FootnoteBlockNode([footnote])
       ]))
+  })
+})
+
+
+describe('A footnote produced by doubly parenthesized, square bracketed, or curly bracketed text followed immediately by a parenthesized, a square bracketed, or a curly bracketed URL', () => {
+  it('produces a footnote node whose contents are put inside a link pointing to that URL. The type of bracket surrounding the footnote can be different from the type of bracket surrounding the URL', () => {
+    
+    const footnote = new FootnoteNode([
+      new LinkNode([
+        new PlainTextNode('Well, I do, but I pretend not to.')
+      ], 'http://example.com/luckycharms')
+    ], 1)
+    
+    expectEveryCombinationOf({
+      firstHalves: [
+        '((Well, I do, but I pretend not to.))',
+        '[[Well, I do, but I pretend not to.]]',
+        '{{Well, I do, but I pretend not to.}}'
+      ],
+      secondHalves: [
+        '[http://example.com/luckycharms]',
+        '(http://example.com/luckycharms)',
+        '{http://example.com/luckycharms}'
+      ],
+      toProduce: new DocumentNode([
+        new ParagraphNode([
+          footnote
+        ]),
+        new FootnoteBlockNode([footnote])
+      ])
+    })
   })
 })
