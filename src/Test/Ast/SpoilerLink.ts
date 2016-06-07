df70068d5e1071ea3a2348178102e71ef6c92319
Daniel Miller <daniel@wants.email>
2016-06-06 19:36:59 -0500

    Fix spoiler test description

--- src/Test/Ast/SpoilerLink.ts
+++ src/Test/Ast/SpoilerLink.ts
@@ -6,7 +6,7 @@ import { LinkNode } from '../../SyntaxNodes/LinkNode'
 import { SpoilerNode } from '../../SyntaxNodes/SpoilerNode'
 
 
-describe('A spoiler followed immediately by a parenthesized/bracketd URL"', () => {
+describe('A spoiler followed immediately by a parenthesized/bracketd URL', () => {
   it('produces a spoiler node whose contents are put inside a link pointing to that URL', () => {
     expect(Up.toAst('After you beat the Elite Four, [SPOILER: you fight Gary](http://example.com/finalbattle).')).to.be.eql(
       insideDocumentAndParagraph([
