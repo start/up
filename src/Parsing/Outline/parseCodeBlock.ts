7215d2cc57868d12ab7d73141255c6cbe5ef0046
Daniel Miller <daniel@wants.email>
2016-06-09 14:47:20 -0500

    [Broken] Fix three more files

--- src/Parsing/Outline/parseCodeBlock.ts
+++ src/Parsing/Outline/parseCodeBlock.ts
@@ -1,6 +1,6 @@
 import { LineConsumer } from './LineConsumer'
 import { CodeBlockNode } from '../../SyntaxNodes/CodeBlockNode'
-import { streakOf } from '../../Patterns'
+import { streakOf } from '../../PatternHelpers'
 import { OutlineParser } from './OutlineParser'
 import { OutlineParserArgs } from './OutlineParserArgs'
 
