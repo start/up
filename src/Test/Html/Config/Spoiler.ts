202e9b51ea0889604e322a422aee7060304de138
Daniel Miller <daniel@wants.email>
2016-06-10 03:22:29 -0500

    Add passing spoiler test

--- src/Test/Html/Config/Spoiler.ts
+++ src/Test/Html/Config/Spoiler.ts
@@ -1,9 +1,12 @@
 import { expect } from 'chai'
 import Up from '../../../index'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
+import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 
 
 describe("The ID of a spoiler's checkbox (on both the checkbox and the label)", () => {
@@ -24,5 +27,62 @@ describe("The ID of a spoiler's checkbox (on both the checkbox and the label)", 
       + '</span>'
 
     expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("Multiple spoilers in a document", () => {
+  it("have have sequential IDs", () => {
+    const node =
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode('Hello, '),
+          new SpoilerNode([
+            new PlainTextNode('Red')
+          ]),
+          new PlainTextNode('. We meet for the '),
+          new SpoilerNode([
+            new EmphasisNode([
+              new PlainTextNode('eighth')
+            ])
+          ]),
+          new PlainTextNode(' time.')
+        ]),
+        new ParagraphNode([
+          new PlainTextNode('Hello, '),
+          new SpoilerNode([
+            new PlainTextNode('Blue')
+          ]),
+          new PlainTextNode('.'),
+        ])
+      ])
+
+    const html =
+      '<p>'
+      + 'Hello, '
+      + '<span class="up-spoiler up-revealable">'
+      + '<label for="up-spoiler-1">toggle spoiler</label>'
+      + '<input id="up-spoiler-1" type="checkbox">'
+      + '<span>Red</span>'
+      + '</span>'
+      + '. We meet for the '
+      + '<span class="up-spoiler up-revealable">'
+      + '<label for="up-spoiler-2">toggle spoiler</label>'
+      + '<input id="up-spoiler-2" type="checkbox">'
+      + '<span><em>eighth</em></span>'
+      + '</span>'
+      + ' time.'
+      + '</p>'
+      + '<p>'
+      + 'Hello, '
+      + '<span class="up-spoiler up-revealable">'
+      + '<label for="up-spoiler-3">toggle spoiler</label>'
+      + '<input id="up-spoiler-3" type="checkbox">'
+      + '<span>Blue</span>'
+      + '</span>'
+      + '.'
+      + '</p>'
+
+    expect(Up.toHtml(node)).to.be.eql(html)
   })
 })
