deed9779ad16cf020b6c77eaf1593d14483f9d15
Daniel Miller <daniel@wants.email>
2016-06-10 13:23:21 -0500

    Add nsfw and nsfl syntax nodes

---
+++ src/SyntaxNodes/NotSafeForLifeNode.ts
@@ -0,0 +1,6 @@
+import { RichInlineSyntaxNode } from './RichInlineSyntaxNode'
+
+
+export class NotSafeForLifeNode extends RichInlineSyntaxNode {
+  private NOT_SAFE_FOR_LIFE: any = null
+}
---
+++ src/SyntaxNodes/NotSafeForWorkNode.ts
@@ -0,0 +1,6 @@
+import { RichInlineSyntaxNode } from './RichInlineSyntaxNode'
+
+
+export class NotSafeForWorkNode extends RichInlineSyntaxNode {
+  private NOT_SAFE_FOR_WORK: any = null
+}
