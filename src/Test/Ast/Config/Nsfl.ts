1144ae810f82954c0878ed0c7e2d2e8ef7f6f111
Daniel Miller <daniel@wants.email>
2016-06-10 14:46:22 -0500

    Add 2 failing nsfl tests

--- src/Test/Ast/Config/Nsfl.ts
+++ src/Test/Ast/Config/Nsfl.ts
@@ -2,28 +2,28 @@ import { expect } from 'chai'
 import Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
-import { NotSafeForWorkNode } from '../../../SyntaxNodes/NotSafeForWorkNode'
+import { NotSafeForLifeNode } from '../../../SyntaxNodes/NotSafeForLifeNode'
 
 
-describe('The term that represents NSFW conventions', () => {
+describe('The term that represents NSFL conventions', () => {
     const up = new Up({
       i18n: {
-        terms: { nsfw: 'explicit' }
+        terms: { nsfl: 'ruins life' }
       }
     })
     
   it('comes from the "nsfw" config term ', () => {
-    expect(up.toAst('[explicit: Ash wrestles naked Gary]')).to.be.eql(
+    expect(up.toAst('[ruins life: Ash eats rotten Gary]')).to.be.eql(
       insideDocumentAndParagraph([
-        new NotSafeForWorkNode([
-          new PlainTextNode('Ash wrestles naked Gary')
+        new NotSafeForLifeNode([
+          new PlainTextNode('Ash eats rotten Gary')
         ])
       ]))
   })
   
   it('is case-insensitive even when custom', () => {
-    const uppercase = '[EXPLICIT: Ash wrestles naked Gary]'
-    const mixedCase = '[eXplIciT: Ash wrestles naked Gary]'
+    const uppercase = '[RUINS LIFE: Ash eats rotten Gary]'
+    const mixedCase = '[RuINs LiFe: Ash eats rotten Gary]'
     
     expect(up.toAst(uppercase)).to.be.eql(up.toAst(mixedCase))
   })
