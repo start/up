715b20c457583922fa945ad19283b0d9c820a23d
Daniel Miller <daniel@wants.email>
2016-06-10 14:29:50 -0500

    Add 3 passing nsfw html config tests

---
+++ src/Test/Html/Config/NotSafeForWork.ts
@@ -0,0 +1,121 @@
+import { expect } from 'chai'
+import Up from '../../../index'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
+import { NotSafeForWorkNode } from '../../../SyntaxNodes/NotSafeForWorkNode'
+import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
+
+
+describe("The ID of a NSFW convention's checkbox (on both the checkbox and the label)", () => {
+  it("uses the provided term for 'nsfw'", () => {
+    const up = new Up({
+      i18n: {
+        terms: { nsfw: 'explicit' }
+      }
+    })
+
+    const node = new NotSafeForWorkNode([])
+
+    const html =
+      '<span class="up-nsfw up-revealable">'
+      + '<label for="up-explicit-1">toggle nsfw</label>'
+      + '<input id="up-explicit-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("Multiple NSFW conventions in a document", () => {
+  it("have have sequential IDs", () => {
+    const node =
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode('Hello, '),
+          new NotSafeForWorkNode([
+            new PlainTextNode('naked Red')
+          ]),
+          new PlainTextNode('. We meet for the eighth'),
+          new NotSafeForWorkNode([
+            new EmphasisNode([
+              new PlainTextNode('naked')
+            ])
+          ]),
+          new PlainTextNode(' time.')
+        ]),
+        new ParagraphNode([
+          new PlainTextNode('Hello, '),
+          new NotSafeForWorkNode([
+            new PlainTextNode('naked Blue')
+          ]),
+          new PlainTextNode('.'),
+        ])
+      ])
+
+    const html =
+      '<p>'
+      + 'Hello, '
+      + '<span class="up-nsfw up-revealable">'
+      + '<label for="up-nsfw-1">toggle nsfw</label>'
+      + '<input id="up-nsfw-1" type="checkbox">'
+      + '<span>naked Red</span>'
+      + '</span>'
+      + '. We meet for the eighth'
+      + '<span class="up-nsfw up-revealable">'
+      + '<label for="up-nsfw-2">toggle nsfw</label>'
+      + '<input id="up-nsfw-2" type="checkbox">'
+      + '<span><em>naked</em></span>'
+      + '</span>'
+      + ' time.'
+      + '</p>'
+      + '<p>'
+      + 'Hello, '
+      + '<span class="up-nsfw up-revealable">'
+      + '<label for="up-nsfw-3">toggle nsfw</label>'
+      + '<input id="up-nsfw-3" type="checkbox">'
+      + '<span>naked Blue</span>'
+      + '</span>'
+      + '.'
+      + '</p>'
+
+    expect(Up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("The ID of a NSFL convention's checkbox (on both the checkbox and the label)", () => {
+  it("reset each time a new document is written", () => {
+    const up = new Up()
+    
+    const node =
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode('Hello, '),
+          new NotSafeForWorkNode([
+            new PlainTextNode('naked Red')
+          ]),
+          new PlainTextNode('. We meet for the eighth'),
+          new NotSafeForWorkNode([
+            new EmphasisNode([
+              new PlainTextNode('naked')
+            ])
+          ]),
+          new PlainTextNode(' time.')
+        ]),
+        new ParagraphNode([
+          new PlainTextNode('Hello, '),
+          new NotSafeForWorkNode([
+            new PlainTextNode('naked Blue')
+          ]),
+          new PlainTextNode('.'),
+        ])
+      ])
+
+    expect(up.toHtml(node)).to.be.eql(up.toHtml(node))
+  })
+})
