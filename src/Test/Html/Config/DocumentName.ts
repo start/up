0bf163734696c4416688dc75d95fa4f609163062
Daniel Miller <daniel@wants.email>
2016-06-10 14:38:04 -0500

    Add 6 passing html config tests

--- src/Test/Html/Config/DocumentName.ts
+++ src/Test/Html/Config/DocumentName.ts
@@ -4,6 +4,8 @@ import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
+import { NotSafeForWorkNode } from '../../../SyntaxNodes/NotSafeForWorkNode'
+import { NotSafeForLifeNode } from '../../../SyntaxNodes/NotSafeForLifeNode'
 
 
 describe("A footnote reference's ID (as well as the ID of the footnote it points to)", () => {
@@ -152,6 +154,108 @@ describe("The ID of a spoiler's checkbox (on both the checkbox and the label)", 
       '<span class="up-spoiler up-revealable">'
       + '<label for="spoiler-1">toggle spoiler</label>'
       + '<input id="spoiler-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("The ID of a NSFW conventions's checkbox (on both the checkbox and the label)", () => {
+  it("are prefixed with the default document name 'up' if one wasn't provided", () => {
+    const node = new NotSafeForWorkNode([])
+    
+    const html =
+      '<span class="up-nsfw up-revealable">'
+      + '<label for="up-nsfw-1">toggle nsfw</label>'
+      + '<input id="up-nsfw-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+
+    expect(Up.toHtml(node)).to.be.eql(html)
+  })
+
+
+  it("are prefixed with the document name, if one was provided", () => {
+    const up = new Up({
+      documentName: 'reply-11'
+    })
+
+    const node = new NotSafeForWorkNode([])
+    
+    const html =
+      '<span class="up-nsfw up-revealable">'
+      + '<label for="reply-11-nsfw-1">toggle nsfw</label>'
+      + '<input id="reply-11-nsfw-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+
+  it("are not prefixed with a document name if a blank name was provided", () => {
+    const up = new Up({
+      documentName: ' \t'
+    })
+
+    const node = new NotSafeForWorkNode([])
+    
+    const html =
+      '<span class="up-nsfw up-revealable">'
+      + '<label for="nsfw-1">toggle nsfw</label>'
+      + '<input id="nsfw-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("The ID of a NSFL conventions's checkbox (on both the checkbox and the label)", () => {
+  it("are prefixed with the default document name 'up' if one wasn't provided", () => {
+    const node = new NotSafeForLifeNode([])
+    
+    const html =
+      '<span class="up-nsfl up-revealable">'
+      + '<label for="up-nsfl-1">toggle nsfl</label>'
+      + '<input id="up-nsfl-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+
+    expect(Up.toHtml(node)).to.be.eql(html)
+  })
+
+
+  it("are prefixed with the document name, if one was provided", () => {
+    const up = new Up({
+      documentName: 'reply-11'
+    })
+
+    const node = new NotSafeForLifeNode([])
+    
+    const html =
+      '<span class="up-nsfl up-revealable">'
+      + '<label for="reply-11-nsfl-1">toggle nsfl</label>'
+      + '<input id="reply-11-nsfl-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+
+  it("are not prefixed with a document name if a blank name was provided", () => {
+    const up = new Up({
+      documentName: ' \t'
+    })
+
+    const node = new NotSafeForLifeNode([])
+    
+    const html =
+      '<span class="up-nsfl up-revealable">'
+      + '<label for="nsfl-1">toggle nsfl</label>'
+      + '<input id="nsfl-1" type="checkbox">'
       + '<span></span>'
       + '</span>'
 
