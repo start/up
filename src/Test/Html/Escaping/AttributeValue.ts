6144121a79a93be7c8b9e2c05441dd37b3879075
Daniel Miller <daniel@wants.email>
2016-06-11 04:27:25 -0500

    Add 3 passing tests

--- src/Test/Html/Escaping/AttributeValue.ts
+++ src/Test/Html/Escaping/AttributeValue.ts
@@ -37,6 +37,26 @@ describe("Within an audio convention's src attribute, all instances of \" and &"
 })
 
 
+describe("Within a video's src attribute, all instances of \" and &", () => {
+  it("are escaped (and they're escaped within the fallback link src attribute)", () => {
+    const node = new VideoNode('', 'https://example.com/?x&y&z="hi"')
+
+    expect(Up.toHtml(node)).to.be.eql(
+      '<video src="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;" title=""><a href="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;"></a></video>')
+  })
+})
+
+
+describe("Within an image's src attribute, all instances of \" and &", () => {
+  it("are escaped", () => {
+    const node = new ImageNode('', 'https://example.com/?x&y&z="hi"')
+
+    expect(Up.toHtml(node)).to.be.eql(
+      '<img src="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;" alt="" title="">')
+  })
+})
+
+
 describe("Within a video's title attribute, all instances of \" and &", () => {
   it("are escaped (but they're not escaped within the fallback link's contents)", () => {
     const node = new VideoNode('John said, "1 and 2 > 0. I can\'t believe it."', '')
@@ -47,11 +67,21 @@ describe("Within a video's title attribute, all instances of \" and &", () => {
 })
 
 
-describe("Within an audio convention's detitle attribute, all instances of \" and &", () => {
+describe("Within an audio convention's title attribute, all instances of \" and &", () => {
   it("are escaped (but they're not escaped within the fallback link's contents)", () => {
     const node = new AudioNode('John said, "1 and 2 > 0. I can\'t believe it."', '')
 
     expect(Up.toHtml(node)).to.be.eql(
       '<audio src="" title="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;"><a href="">John said, "1 and 2 > 0. I can\'t believe it."</a></audio>')
+  })
+})
+
+
+describe("Within an image's title and alt attributes, all instances of \" and &", () => {
+  it("are escaped", () => {
+    const node = new ImageNode('John said, "1 and 2 > 0. I can\'t believe it."', '')
+
+    expect(Up.toHtml(node)).to.be.eql(
+      '<img src="" alt="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;" title="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;">')
   })
 })
