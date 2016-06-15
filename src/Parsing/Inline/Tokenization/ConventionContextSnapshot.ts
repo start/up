7d750acfd0d203f140b4b282fc313a9fa0193586
Daniel Miller <daniel@wants.email>
2016-06-15 16:57:05 -0500

    Add ConventionContextSnapshot

---
+++ src/Parsing/Inline/Tokenization/ConventionContextSnapshot.ts
@@ -0,0 +1,6 @@
+import { TokenizableConvention } from './TokenizableConvention'
+import { TokenizerSnapshot } from './TokenizerSnapshot'
+
+export class ConventionContextSnapshot {
+  constructor(public startTokenIndex: number) { }
+}
