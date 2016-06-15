80fa29008f9c4a7f42fc030dad279842d40c8c37
Daniel Miller <daniel@wants.email>
2016-06-15 15:35:16 -0500

    Add 2 passing tests

---
+++ src/Test/Html/Config/BaseForUrlsStartingWithFragmentIdentifier.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai'
+import Up from '../../../index'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
+import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
+import { NsfwNode } from '../../../SyntaxNodes/NsfwNode'
+import { NsflNode } from '../../../SyntaxNodes/NsflNode'
+
+
+
+describe('The "baseForUrlsStartingWithFragmentIdentifier" setting', () => {
+  const up = new Up({
+    baseForUrlsStartingWithFragmentIdentifier: 'https://example.com/page'
+  })
+
+  it("does not affect a footnote reference's link to its footnote", () => {
+    const node = new FootnoteNode([], 3)
+
+    expect(up.toHtml(node)).to.be.eql(
+      '<sup id="up-footnote-reference-3" class="up-footnote-reference"><a href="#up-footnote-3">3</a></sup>')
+  })
+
+  it("does not affect a footnote's link back to its reference", () => {
+    const node =
+      new FootnoteBlockNode([
+        new FootnoteNode([
+          new PlainTextNode("Arwings"),
+        ], 2),
+        new FootnoteNode([
+          new PlainTextNode("Killer Bees"),
+        ], 3),
+      ])
+
+    const html =
+      '<dl class="up-footnotes">'
+      + '<dt id="up-footnote-2"><a href="#up-footnote-reference-2">2</a></dt><dd>Arwings</dd>'
+      + '<dt id="up-footnote-3"><a href="#up-footnote-reference-3">3</a></dt><dd>Killer Bees</dd>'
+      + '</dl>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
