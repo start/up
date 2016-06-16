febe881adcd52b026c4920f655306bde393c1c13
Daniel Miller <daniel@wants.email>
2016-06-16 15:22:37 -0500

    Remove 2 now-duplicate tests

--- src/Test/Ast/BaseForUrlsStartingWithSlash.ts
+++ src/Test/Ast/BaseForUrlsStartingWithSlash.ts
@@ -9,19 +9,6 @@ import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
 
 
-describe('An audio URL starting with a slash', () => {
-  it('has no added prefix by default, because the default "baseForUrlsStartingWithSlash" config setting is blank', () => {
-    const text = '[audio: Chrono Cross ending theme](/wiki/Chrono_Chross.mp3)'
-
-    expect(Up.toAst(text)).to.be.eql(
-      new DocumentNode([
-        new AudioNode('Chrono Cross ending theme', '/wiki/Chrono_Chross.mp3')
-      ])
-    )
-  })
-})
-
-
 describe('An image URL starting with a slash', () => {
   it('has no added prefix by default, because the default "baseForUrlsStartingWithSlash" config setting is blank', () => {
     const text = '[image: Chrono Cross title screen](/wiki/Chrono_Chross.png)'
