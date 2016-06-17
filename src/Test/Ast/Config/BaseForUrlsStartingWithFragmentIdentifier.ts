25113eb7132b4d87afde08860d7913d03097094d
Daniel Miller <daniel@wants.email>
2016-06-17 10:41:08 -0500

    Add 4 passing config tests

--- src/Test/Ast/Config/BaseForUrlsStartingWithFragmentIdentifier.ts
+++ src/Test/Ast/Config/BaseForUrlsStartingWithFragmentIdentifier.ts
@@ -32,7 +32,19 @@ describe('The "baseForUrlsStartingWithFragmentIdentifier" config setting', () =>
     )
   })
 
-  it('is prefixed to image URLs that start with a slash', () => {
+  it('is prefixed to link URLs that start with a hash mark when the link content and URL are separated by whitespace', () => {
+    const text = '[Chrono Cross] (#wiki/Chrono_Chross)'
+
+    expect(up.toAst(text)).to.be.eql(
+      insideDocumentAndParagraph([
+        new LinkNode([
+          new PlainTextNode('Chrono Cross')
+        ], 'https://example.com/page#wiki/Chrono_Chross')
+      ])
+    )
+  })
+
+  it('is prefixed to image URLs that start with a hash mark', () => {
     const text = '[image: Chrono Cross logo](#cc-logo.png)'
 
     expect(up.toAst(text)).to.be.eql(
@@ -42,7 +54,7 @@ describe('The "baseForUrlsStartingWithFragmentIdentifier" config setting', () =>
     )
   })
 
-  it('is prefixed to audio URLs that start with a slash', () => {
+  it('is prefixed to audio URLs that start with a hash mark', () => {
     const text = '[audio: Chrono Cross ending theme](#radical dreamers.mp3)'
 
     expect(up.toAst(text)).to.be.eql(
@@ -52,7 +64,7 @@ describe('The "baseForUrlsStartingWithFragmentIdentifier" config setting', () =>
     )
   })
 
-  it('is prefixed to video URLs that start with a slash', () => {
+  it('is prefixed to video URLs that start with a hash mark', () => {
     const text = '[video: Chrono Cross ending cinematic][#radical dreamers.webm]'
 
     expect(up.toAst(text)).to.be.eql(
@@ -109,6 +121,71 @@ describe('The "baseForUrlsStartingWithFragmentIdentifier" config setting', () =>
 
   it("is prefixed to linkified footnote URLs that start with a hash mark", () => {
     const text = "I don't eat cereal. ((Well, I eat one.))[#cereals/lucky-charms?show=nutrition] Never have."
+
+    const footnote = new FootnoteNode([
+      new LinkNode([
+        new PlainTextNode('Well, I eat one.')
+      ], 'https://example.com/page#cereals/lucky-charms?show=nutrition')
+    ], 1)
+
+    expect(up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode("I don't eat cereal."),
+          footnote,
+          new PlainTextNode(" Never have."),
+        ]),
+        new FootnoteBlockNode([footnote])
+      ]))
+  })
+
+  it('is prefixed to linkified spoiler URLs that start with a hash mark when the spoiler part and the URL are separated by whitespace', () => {
+    const text = 'Walter White produces [SPOILER: Blue Sky meth] (#wiki/Blue_Sky)'
+
+    expect(up.toAst(text)).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('Walter White produces '),
+        new SpoilerNode([
+          new LinkNode([
+            new PlainTextNode('Blue Sky meth')
+          ], 'https://example.com/page#wiki/Blue_Sky')
+        ])
+      ])
+    )
+  })
+
+  it('is prefixed to linkified NSFW URLs that start with a hash mark when the NSFW part and the URL are separated by whitespace', () => {
+    const text = 'Walter White produces [NSFW: Blue Sky meth] (#wiki/Blue_Sky)'
+
+    expect(up.toAst(text)).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('Walter White produces '),
+        new NsfwNode([
+          new LinkNode([
+            new PlainTextNode('Blue Sky meth')
+          ], 'https://example.com/page#wiki/Blue_Sky')
+        ])
+      ])
+    )
+  })
+
+  it('is prefixed to linkified NSFL URLs that start with a hash mark when the NSFL part and the URL are separated by whitespace', () => {
+    const text = 'Walter White produces [NSFL: Blue Sky meth] (#wiki/Blue_Sky)'
+
+    expect(up.toAst(text)).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('Walter White produces '),
+        new NsflNode([
+          new LinkNode([
+            new PlainTextNode('Blue Sky meth')
+          ], 'https://example.com/page#wiki/Blue_Sky')
+        ])
+      ])
+    )
+  })
+
+  it("is prefixed to linkified footnote URLs that start with a hash mark when the footnote part and the URL are separated by whitespace", () => {
+    const text = "I don't eat cereal. ((Well, I eat one.)) [#cereals/lucky-charms?show=nutrition] Never have."
 
     const footnote = new FootnoteNode([
       new LinkNode([
