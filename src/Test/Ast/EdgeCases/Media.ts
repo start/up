af0aecd09326671d794a74e2337a508c98f753e1
Daniel Miller <daniel@wants.email>
2016-06-20 04:57:00 -0500

    Add edge case media test

--- src/Test/Ast/EdgeCases/Media.ts
+++ src/Test/Ast/EdgeCases/Media.ts
@@ -10,10 +10,25 @@ import { LineBlockNode } from '../../../SyntaxNodes/LineBlockNode'
 import { Line } from '../../../SyntaxNodes/Line'
 
 
-describe('A line consisting solely of media conventions (and optional whitespace)', () => {
+describe('A line consisting solely of media conventions', () => {
   it('produces a node for each convention and includes each directly into the outline, rather than squeezing them all into a paragraph', () => {
     const text =
-      '[audio: ghostly howling][http://example.com/ghosts.ogg] [image: haunted house][http://example.com/hauntedhouse.svg] [video: ghosts eating luggage][http://example.com/poltergeists.webm] '
+      '[audio: ghostly howling][http://example.com/ghosts.ogg][image: haunted house][http://example.com/hauntedhouse.svg][video: ghosts eating luggage][http://example.com/poltergeists.webm] '
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new AudioNode('ghostly howling', 'http://example.com/ghosts.ogg'),
+        new ImageNode('haunted house', 'http://example.com/hauntedhouse.svg'),
+        new VideoNode('ghosts eating luggage', 'http://example.com/poltergeists.webm')
+      ]))
+  })
+})
+
+
+describe('A line consisting solely of media conventions and optional whitespace', () => {
+  it('produces a node for each convention and includes each directly into the outline, rather than squeezing them all into a paragraph', () => {
+    const text =
+      '[audio: ghostly howling][http://example.com/ghosts.ogg] \t [image: haunted house][http://example.com/hauntedhouse.svg] \t [video: ghosts eating luggage][http://example.com/poltergeists.webm] '
 
     expect(Up.toAst(text)).to.be.eql(
       new DocumentNode([
