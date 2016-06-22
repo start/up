5cabf2ad65b545438a64985ed727733f139c2833
Daniel Miller <daniel@wants.email>
2016-06-22 04:47:04 -0500

    Reorganize attribute value escaping tests

--- src/Test/Html/Escaping/AttributeValue.ts
+++ src/Test/Html/Escaping/AttributeValue.ts
@@ -10,88 +10,58 @@ import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 
 
-describe("Within a link's src attribute, all instances of \" and &", () => {
-  it("are escaped by replacing them with &quot; and &amp;", () => {
+context('Within any attribute value, all instances of " and & are escaped. Specifically:', () => {
+
+  specify("src attribute of links", () => {
     const node = new LinkNode([], 'https://example.com/?x&y&z="hi"')
 
     expect(Up.toHtml(node)).to.be.eql(
       '<a href="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;"></a>')
   })
-})
 
-
-describe("Within a link's href attribute, <, ', and >", () => {
-  it("are not escaped", () => {
-    const node = new LinkNode([], "https://example.com/?z='<span>'")
-
-    expect(Up.toHtml(node)).to.be.eql(
-      '<a href="https://example.com/?z=\'<span>\'"></a>')
-  })
-})
-
-
-describe("Within an audio convention's src attribute, all instances of \" and &", () => {
-  it("are escaped (and they're escaped within the fallback link src attribute)", () => {
+  specify("src attribute of audio elements (and of their fallback links)", () => {
     const node = new AudioNode('', 'https://example.com/?x&y&z="hi"')
 
     expect(Up.toHtml(node)).to.be.eql(
       '<audio src="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;" title=""><a href="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;"></a></audio>')
   })
-})
 
-
-describe("Within a video's src attribute, all instances of \" and &", () => {
-  it("are escaped (and they're escaped within the fallback link src attribute)", () => {
+  specify("src attribute of videos (and of their fallback links)", () => {
     const node = new VideoNode('', 'https://example.com/?x&y&z="hi"')
 
     expect(Up.toHtml(node)).to.be.eql(
       '<video src="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;" title=""><a href="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;"></a></video>')
   })
-})
 
-
-describe("Within an image's src attribute, all instances of \" and &", () => {
-  it("are escaped", () => {
+  specify("src attribute of images", () => {
     const node = new ImageNode('', 'https://example.com/?x&y&z="hi"')
 
     expect(Up.toHtml(node)).to.be.eql(
       '<img src="https://example.com/?x&amp;y&amp;z=&quot;hi&quot;" alt="" title="">')
   })
-})
 
-
-describe("Within a video's title attribute, all instances of \" and &", () => {
-  it("are escaped (but they're not escaped within the fallback link's contents)", () => {
-    const node = new VideoNode('John said, "1 and 2 > 0. I can\'t believe it."', '')
-
-    expect(Up.toHtml(node)).to.be.eql(
-      '<video src="" title="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;"><a href="">John said, "1 and 2 > 0. I can\'t believe it."</a></video>')
-  })
-})
-
-
-describe("Within an audio convention's title attribute, all instances of \" and &", () => {
-  it("are escaped (but they're not escaped within the fallback link's contents)", () => {
+  specify("title attribute of audio elements", () => {
     const node = new AudioNode('John said, "1 and 2 > 0. I can\'t believe it."', '')
 
     expect(Up.toHtml(node)).to.be.eql(
       '<audio src="" title="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;"><a href="">John said, "1 and 2 > 0. I can\'t believe it."</a></audio>')
   })
-})
 
+  specify("title attribute of videos", () => {
+    const node = new VideoNode('John said, "1 and 2 > 0. I can\'t believe it."', '')
 
-describe("Within an image's title and alt attributes, all instances of \" and &", () => {
-  it("are escaped", () => {
+    expect(Up.toHtml(node)).to.be.eql(
+      '<video src="" title="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;"><a href="">John said, "1 and 2 > 0. I can\'t believe it."</a></video>')
+  })
+
+  specify("alt and title attributes of images", () => {
     const node = new ImageNode('John said, "1 and 2 > 0. I can\'t believe it."', '')
 
     expect(Up.toHtml(node)).to.be.eql(
       '<img src="" alt="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;" title="John said, &quot;1 and 2 > 0. I can\'t believe it.&quot;">')
   })
-})
 
-
-describe("Within the href attribute of a backlink in a footnote block, all instances of \" and &", () => {
-  it('are escaped', () => {
+  it('href attribute of backlinks in footnote blocks', () => {
     const up = new Up({
       i18n: {
         idWordDelimiter: '"&&"',
@@ -113,11 +83,8 @@ describe("Within the href attribute of a backlink in a footnote block, all insta
 
     expect(up.toHtml(node)).to.be.eql(html)
   })
-})
 
-
-describe("Within the id attribute of a footnote in a footnote block, all instances of \" and &", () => {
-  it('are escaped', () => {
+  it('id attribute of footntoes in a footnote block', () => {
     const up = new Up({
       i18n: {
         idWordDelimiter: '"&&"',
@@ -139,11 +106,8 @@ describe("Within the id attribute of a footnote in a footnote block, all instanc
 
     expect(up.toHtml(node)).to.be.eql(html)
   })
-})
 
-
-describe("Within the href attribute of a footnote reference's link, all instances of \" and &", () => {
-  it('are escaped', () => {
+  it("href attribute of a footnote reference's link", () => {
     const up = new Up({
       i18n: {
         idWordDelimiter: '"&&"',
@@ -158,11 +122,8 @@ describe("Within the href attribute of a footnote reference's link, all instance
       + '<a href="#up&quot;&amp;&amp;&quot;look&quot;&amp;&amp;&quot;&quot;down&quot;&quot;&amp;&amp;&quot;&amp;&quot;&amp;&amp;&quot;read&quot;&amp;&amp;&quot;&amp;&quot;&amp;&amp;&quot;learn&quot;&amp;&amp;&quot;3">3</a>'
       + '</sup>')
   })
-})
 
-
-describe("Within the id attribute of a footnote reference, all instances of \" and &", () => {
-  it('are escaped', () => {
+  it('id attribute of footnote references', () => {
     const up = new Up({
       i18n: {
         idWordDelimiter: '"&&"',
@@ -178,5 +139,15 @@ describe("Within the id attribute of a footnote reference, all instances of \" a
       + '</sup>'
 
     expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("Within a link's href attribute, <, ', and >", () => {
+  it("are not escaped", () => {
+    const node = new LinkNode([], "https://example.com/?z='<span>'")
+
+    expect(Up.toHtml(node)).to.be.eql(
+      '<a href="https://example.com/?z=\'<span>\'"></a>')
   })
 })
