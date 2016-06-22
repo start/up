5d1ec6231e33c1d56825718a79969c95326f1826
Daniel Miller <daniel@wants.email>
2016-06-21 19:29:39 -0500

    Add 6 passing tests

--- src/Test/Ast/EdgeCases/Emptiness/Inline.ts
+++ src/Test/Ast/EdgeCases/Emptiness/Inline.ts
@@ -3,6 +3,9 @@ import Up from '../../../../index'
 import { insideDocumentAndParagraph } from '../../Helpers'
 import { DocumentNode } from '../../../../SyntaxNodes/DocumentNode'
 import { ParagraphNode } from '../../../../SyntaxNodes/ParagraphNode'
+import { RevisionInsertionNode } from '../../../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../../../SyntaxNodes/RevisionDeletionNode'
+import { InlineCodeNode } from '../../../../SyntaxNodes/InlineCodeNode'
 import { PlainTextNode } from '../../../../SyntaxNodes/PlainTextNode'
 import { LinkNode } from '../../../../SyntaxNodes/LinkNode'
 import { EmphasisNode } from '../../../../SyntaxNodes/EmphasisNode'
@@ -79,43 +82,103 @@ describe('An empty revision insertion containing an empty revision deletion', ()
 })
 
 
-context('Most inline conventions produce no syntax nodes if they have no content. These conventions are:', () => {
+context('Most inline conventions produce no syntax nodes if they have no content.', () => {
+  context('Specifically:', () => {
 
-  specify('Spoilers', () => {
-    expect(Up.toAst('[SPOILER:]')).to.eql(new DocumentNode())
-  })
+    specify('Spoilers', () => {
+      expect(Up.toAst('[SPOILER:]')).to.eql(new DocumentNode())
+    })
 
-  specify('NSFW', () => {
-    expect(Up.toAst('[NSFW:]')).to.eql(new DocumentNode())
-  })
+    specify('NSFW', () => {
+      expect(Up.toAst('[NSFW:]')).to.eql(new DocumentNode())
+    })
 
-  specify('NSFL', () => {
-    expect(Up.toAst('[NSFL:]')).to.eql(new DocumentNode())
-  })
+    specify('NSFL', () => {
+      expect(Up.toAst('[NSFL:]')).to.eql(new DocumentNode())
+    })
 
-  specify('Inline code', () => {
-    expect(Up.toAst('``')).to.eql(new DocumentNode())
-  })
+    specify('Inline code', () => {
+      expect(Up.toAst('``')).to.eql(new DocumentNode())
+    })
 
-  specify('Revision insertion', () => {
-    // If the revision insertion delimiters were alone on a line, they would be interpreted as a section separator streak. 
-    expect(Up.toAst('Spiders.++++')).to.eql(
-      new DocumentNode([
-        new ParagraphNode([
-          new PlainTextNode('Spiders.')
+    specify('Revision insertion', () => {
+      // If the revision insertion delimiters were alone on a line, they would be interpreted as a section separator streak. 
+      expect(Up.toAst('Spiders.++++')).to.eql(
+        new DocumentNode([
+          new ParagraphNode([
+            new PlainTextNode('Spiders.')
+          ])
         ])
-      ])
-    )
+      )
+    })
+
+    specify('Revision insertion', () => {
+      // If the revision deletion delimiters were alone on a line, they would be interpreted as a section separator streak.
+      expect(Up.toAst('Spiders.~~~~')).to.eql(
+        new DocumentNode([
+          new ParagraphNode([
+            new PlainTextNode('Spiders.')
+          ])
+        ])
+      )
+    })
   })
 
-  specify('Revision insertion', () => {
-    // If the revision deletion delimiters were alone on a line, they would be interpreted as a section separator streak.
-    expect(Up.toAst('Spiders.~~~~')).to.eql(
-      new DocumentNode([
-        new ParagraphNode([
-          new PlainTextNode('Spiders.')
-        ])
-      ])
-    )
+  context('Of those conventions, only a few produce syntax nodes when they contain only unescaped whitespace.', () => {
+    context('Specifically:', () => {
+
+      specify('Inline code', () => {
+        expect(Up.toAst('` `')).to.eql(
+          new DocumentNode([
+            new ParagraphNode([
+              new InlineCodeNode(' ')
+            ])
+          ])
+        )
+      })
+
+      specify('Revision insertion', () => {
+        expect(Up.toAst('no++ ++one')).to.eql(
+          new DocumentNode([
+            new ParagraphNode([
+              new PlainTextNode('no'),
+              new RevisionInsertionNode([
+                new PlainTextNode(' ')
+              ]),
+              new PlainTextNode('one')
+            ])
+          ])
+        )
+      })
+
+      specify('Revision insertion', () => {
+        expect(Up.toAst('e~~ ~~mail')).to.eql(
+          new DocumentNode([
+            new ParagraphNode([
+              new PlainTextNode('e'),
+              new RevisionDeletionNode([
+                new PlainTextNode(' ')
+              ]),
+              new PlainTextNode('mail')
+            ])
+          ])
+        )
+      })
+    })
+
+    context("The rest don't:", () => {
+
+      specify('Spoilers', () => {
+        expect(Up.toAst('[SPOILER:  \t  \t ]')).to.eql(new DocumentNode())
+      })
+
+      specify('NSFW', () => {
+        expect(Up.toAst('[NSFW:  \t  \t ]')).to.eql(new DocumentNode())
+      })
+
+      specify('NSFL', () => {
+        expect(Up.toAst('[NSFL:  \t  \t ]')).to.eql(new DocumentNode())
+      })
+    })
   })
 })
