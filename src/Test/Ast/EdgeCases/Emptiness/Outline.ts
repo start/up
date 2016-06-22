e22c1211b35b26b276e9e07cc20757ec7ac0aeec
Daniel Miller <daniel@wants.email>
2016-06-21 19:02:19 -0500

    Add passing outlint emptiness test

--- src/Test/Ast/EdgeCases/Emptiness/Outline.ts
+++ src/Test/Ast/EdgeCases/Emptiness/Outline.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import Up from '../../../../index'
 import { insideDocumentAndParagraph } from '../../Helpers'
 import { DocumentNode } from '../../../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../../../SyntaxNodes/ParagraphNode'
 import { PlainTextNode } from '../../../../SyntaxNodes/PlainTextNode'
 import { LinkNode } from '../../../../SyntaxNodes/LinkNode'
 import { EmphasisNode } from '../../../../SyntaxNodes/EmphasisNode'
@@ -23,6 +24,28 @@ describe('A document with only blank lines', () => {
       
 `
     expect(Up.toAst(text)).to.eql(new DocumentNode())
+  })
+})
+
+
+describe('A paragraph consisting only of blank lines and lines consisting of empty inline conventions', () => {
+  it('produces an empty document node', () => {
+    const text = `     
+I had too much cereal.
+
+[NSFW: \t][NSFL:]
+
+I'm never eating again.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode('I had too much cereal.')
+        ]),
+        new ParagraphNode([
+          new PlainTextNode("I'm never eating again."),
+        ])
+      ])
+      )
   })
 })
 
