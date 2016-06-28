a691d0e3b1bcd4ce168719ad146b6ee6b269e0a2
Daniel Miller <daniel@wants.email>
2016-06-28 04:29:26 -0500

    Remove blockquote test helper

--- src/Test/Ast/Blockquote.ts
+++ src/Test/Ast/Blockquote.ts
@@ -8,70 +8,87 @@ import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
 import { BlockquoteNode } from '../../SyntaxNodes/BlockquoteNode'
 
-function expectBlockquoteContentsToEqualDocumentContents(blockquotedText: string, text: string): void {
-  expect(Up.toAst(blockquotedText)).to.be.eql(
-    new DocumentNode([
-      new BlockquoteNode(Up.toAst(text).children)
-    ]))
-}
-
 
 describe('Consecutive lines starting with "> "', () => {
   it('are parsed like a document and then placed in a blockquote node', () => {
-    const blockquotedText = `
+    const text = `
 > Hello, world!
 >
 > Goodbye, world!`
 
-    const text = `
-Hello, world!
-
-Goodbye, world!`
-
-    expectBlockquoteContentsToEqualDocumentContents(blockquotedText, text)
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new BlockquoteNode([
+          new ParagraphNode([
+            new PlainTextNode('Hello, world!')
+          ]),
+          new ParagraphNode([
+            new PlainTextNode('Goodbye, world!')
+          ])
+        ])
+      ]))
   })
 })
 
 
 describe('A blockquote', () => {
   it('can contain inline conventions', () => {
-    const blockquotedText = `
+    const text = `
 > Hello, world!
 >
 > Goodbye, *world*!`
 
-    const text = `
-Hello, world!
-
-Goodbye, *world*!`
-
-    expectBlockquoteContentsToEqualDocumentContents(blockquotedText, text)
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new BlockquoteNode([
+          new ParagraphNode([
+            new PlainTextNode('Hello, world!')
+          ]),
+          new ParagraphNode([
+            new PlainTextNode('Goodbye, '),
+            new EmphasisNode([
+              new PlainTextNode('world')
+            ]),
+            new PlainTextNode('!')
+          ])
+        ])
+      ]))
   })
 
   it('can contain headings', () => {
-    const blockquotedText = `
+    const text = `
 > Hello, world!
 > ===========`
 
-    const text = `
-Hello, world!
-===========`
-
-    expectBlockquoteContentsToEqualDocumentContents(blockquotedText, text)
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new BlockquoteNode([
+          new HeadingNode([
+            new PlainTextNode('Hello, world!')
+          ], 1)
+        ])
+      ]))
   })
 
   it('can contain nested blockquotes', () => {
-    const blockquotedText = `
+    const text = `
 > Hello, world!
 >
 > > Hello, mantle!`
 
-    const text = `
-Hello, world!
-
-> Hello, mantle!`
-
-    expectBlockquoteContentsToEqualDocumentContents(blockquotedText, text)
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new BlockquoteNode([
+          new ParagraphNode([
+            new PlainTextNode('Hello, world!')
+          ]),
+          new BlockquoteNode([
+            new ParagraphNode([
+              new PlainTextNode('Hello, mantle!')
+            ])
+          ])
+        ])
+      ]))
   })
 })
 
@@ -106,15 +123,32 @@ describe('Multiple blockquote delimiters each missing their trailing space, foll
 
 describe('A single line blockquote', () => {
   it('can contain nested blockquotes', () => {
-    const blockquotedText = '> > > Hello, *world*!!'
-    const text = '> > Hello, *world*!!'
-    expectBlockquoteContentsToEqualDocumentContents(blockquotedText, text)
+    expect(Up.toAst('> > > Hello, *world*!!')).to.be.eql(
+      new DocumentNode([
+        new BlockquoteNode([
+          new BlockquoteNode([
+            new BlockquoteNode([
+              new ParagraphNode([
+                new PlainTextNode('Hello, '),
+                new EmphasisNode([
+                  new PlainTextNode('world')
+                ]),
+                new PlainTextNode('!!')
+              ])
+            ])
+          ])
+        ])
+      ]))
   })
 })
 
 
 describe('A blank blockquoted line', () => {
   it('does not require a trailing space after the blockquote delimiter', () => {
-    expectBlockquoteContentsToEqualDocumentContents('>', '')
+
+    expect(Up.toAst('>')).to.be.eql(
+      new DocumentNode([
+        new BlockquoteNode([])
+      ]))
   })
 })
