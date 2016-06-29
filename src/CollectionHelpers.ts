46af4f3471201587aa75625ff378f8d25deb5a58
Daniel Miller <daniel@wants.email>
2016-06-29 16:47:41 -0500

    Clean up and explain collection helpers

--- src/CollectionHelpers.ts
+++ src/CollectionHelpers.ts
@@ -1,17 +1,22 @@
+// Returns the last item from `items`.
 export function last<T>(items: T[]): T {
   return items[items.length - 1]
 }
 
+// Returns a single flattened array containing every item from every array in `collections`.
+// The items' order is preserved.
 export function concat<T>(collections: T[][]): T[] {
-  return [].concat([], ...collections)
+  return [].concat(...collections)
 }
 
+// Returns a reversed shallow copy of `items`. 
 export function reversed<T>(items: T[]): T[] {
   return items.slice().reverse()
 }
 
+// Removes the last instance of `itemToRemove` from `items`.
 export function remove<T>(items: T[], itemToRemove: T): void {
-  for (let i = 0; i < items.length; i++) {
+  for (let i = items.length - 1; i >= 0; i--) {
     if (items[i] === itemToRemove) {
       items.splice(i, 1)
       return
