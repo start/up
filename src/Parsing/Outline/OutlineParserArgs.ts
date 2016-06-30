dab981de5a27e1fc81946ec1cdfa5f042763f40c
Daniel Miller <daniel@wants.email>
2016-06-30 09:10:53 -0500

    [Broken] Make outline parsers accept LineConsumers

--- src/Parsing/Outline/OutlineParserArgs.ts
+++ src/Parsing/Outline/OutlineParserArgs.ts
@@ -1,10 +1,11 @@
 import { OutlineSyntaxNode } from '../../SyntaxNodes/OutlineSyntaxNode'
+import { LineConsumer } from './LineConsumer'
 import { HeadingLeveler } from './HeadingLeveler'
 import { UpConfig } from '../../UpConfig'
 
 
 export interface OutlineParserArgs {
-  text: string
+  consumer: LineConsumer
   headingLeveler: HeadingLeveler
   config: UpConfig
   then: (resultNodes: OutlineSyntaxNode[], lengthParsed: number) => void
