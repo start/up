d2e5fa6ef338376f84112b815a104112a3ee91cb
Daniel Miller <daniel@wants.email>
2016-07-01 08:52:39 -0500

    Remove unused imports

--- src/Parsing/Inline/Tokenization/tryToTokenizeInlineCodeOrDelimiter.ts
+++ src/Parsing/Inline/Tokenization/tryToTokenizeInlineCodeOrDelimiter.ts
@@ -1,27 +1,7 @@
-import { EMPHASIS_CONVENTION, STRESS_CONVENTION, REVISION_DELETION_CONVENTION, REVISION_INSERTION_CONVENTION, SPOILER_CONVENTION, NSFW_CONVENTION, NSFL_CONVENTION, FOOTNOTE_CONVENTION, LINK_CONVENTION, PARENTHESIZED_CONVENTION, SQUARE_BRACKETED_CONVENTION, ACTION_CONVENTION } from '../RichConventions'
-import { escapeForRegex, regExpStartingWith, solely, everyOptional, either, optional, atLeast, anyCharBut, exactly, followedBy, notFollowedBy, anyCharMatching, anyCharNotMatching, capture } from '../../PatternHelpers'
-import { SOME_WHITESPACE, ANY_WHITESPACE, WHITESPACE_CHAR, DIGIT, ANY_CHAR } from '../../PatternPieces'
-import { NON_BLANK_PATTERN } from '../../Patterns'
-import { ESCAPER_CHAR } from '../../Strings'
-import { AUDIO_CONVENTION, IMAGE_CONVENTION, VIDEO_CONVENTION } from '../MediaConventions'
-import { UpConfig } from '../../../UpConfig'
-import { RichConvention } from '../RichConvention'
-import { MediaConvention } from '../MediaConvention'
-import { nestOverlappingConventions } from './nestOverlappingConventions'
-import { insertBracketsInsideBracketedConventions } from './insertBracketsInsideBracketedConventions'
-import { last, concat, reversed } from '../../../CollectionHelpers'
-import { Bracket } from './Bracket'
-import { BRACKETS } from './Brackets'
-import { FailedConventionTracker } from './FailedConventionTracker'
-import { ConventionContext } from './ConventionContext'
-import { TokenizerSnapshot } from './TokenizerSnapshot'
+import { regExpStartingWith, atLeast, anyCharBut } from '../../PatternHelpers'
 import { InlineTextConsumer } from './InlineTextConsumer'
 import { TokenKind } from './TokenKind'
 import { Token } from './Token'
-import { EncloseWithinRichConventionArgs } from './EncloseWithinRichConventionArgs'
-import { TokenizableConvention, OnConventionEvent } from './TokenizableConvention'
-import { RaisedVoiceHandler } from './RaisedVoiceHandler'
-
 
 export function tryToTokenizeInlineCodeOrDelimiter(
   args: {
