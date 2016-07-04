85a87a37e316a3eef8f654efd35e5350c58338d8
Daniel Miller <daniel@wants.email>
2016-07-04 07:17:35 -0500

    Clean up tryToPromoteToOutline

--- src/Parsing/Outline/tryToPromoteToOutline.ts
+++ src/Parsing/Outline/tryToPromoteToOutline.ts
@@ -14,49 +14,56 @@ import { NON_BLANK_PATTERN } from '../Patterns'
 import { OutlineParserArgs } from './OutlineParserArgs'
 
 
+
 // If a line consists solely of media conventions (and/or whitespace), those media conventions are
 // placed directly into the outline rather into a paragraph.
 //
-// If an image is "linkified", or if a link otherwise contains only images (and whitespace), the link
-// counts as an image for the purpose of the rule above. In that situation, the link itself is placed
-// directly into the outline.
+// If a media convention is "linkified", or if a link otherwise contains only media conventions (and
+// whitespace), the link counts as media. In that situation, the link itself is placed directly into
+// the outline.
+export type InlineSyntaxNodePromotableToOutline =
+  MediaSyntaxNode | LinkNode
+
+
 export function tryToPromoteToOutline(
   args: {
     inlineNodes: InlineSyntaxNode[]
-    then: (outlineNodes: OutlineSyntaxNode[]) => void
+    then: (promotedNodes: InlineSyntaxNodePromotableToOutline[]) => void
   }
 ): boolean {
   const { inlineNodes, then } = args
+  const promotedNodes: InlineSyntaxNodePromotableToOutline[] = []
 
-  const doesLineConsistSolelyOfMediaConventions =
-    inlineNodes.every(node => isMediaSyntaxNode(node) || isWhitespace(node))
-    && inlineNodes.some(isMediaSyntaxNode)
+  for (const inlineNode of inlineNodes) {
+    if (inlineNode instanceof MediaSyntaxNode) {
+      promotedNodes.push(inlineNode)
+      continue
+    }
 
-  if (doesLineConsistSolelyOfMediaConventions) {
-    then(<OutlineSyntaxNode[]><any>withoutWhitespace(inlineNodes))
+    if (inlineNode instanceof LinkNode) {
+      const canPromoteLinkChildrenToOutline = tryToPromoteToOutline({
+        inlineNodes: inlineNode.children,
+        then: promotableChildren => {
+          promotedNodes.push(new LinkNode(promotableChildren, inlineNode.url))
+        }
+      })
+
+      if (!canPromoteLinkChildrenToOutline) {
+        return false
+      }
+      
+      continue
+    }
+
+    if (!isWhitespace(inlineNode)) {
+      return false
+    }
+  }
+
+  if (promotedNodes.length) {
+    then(promotedNodes)
     return true
   }
 
   return false
-}
-
-
-function isMediaSyntaxNode(node: InlineSyntaxNode): boolean {
-  return (
-    node instanceof MediaSyntaxNode
-    || (
-      node instanceof LinkNode
-      // A link node cannot consist solely of whitespace, so if `every` returns true, we know there's
-      // at least one image node within the link.
-      && node.children.every(child => (child instanceof ImageNode) || isWhitespace(child))))
-}
-
-// This function assumes any plain text nodes are whitespace.
-function withoutWhitespace(nodes: InlineSyntaxNode[]): InlineSyntaxNode[] {
-  return (
-    nodes
-      .filter(node =>
-        !(node instanceof PlainTextNode))
-      .map(node =>
-        (node instanceof LinkNode) ? new LinkNode(withoutWhitespace(node.children), node.url) : node))
 }
