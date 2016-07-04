4476f76aa31b0dda522bfa70c681c8fde8b26245
Daniel Miller <daniel@wants.email>
2016-07-04 07:18:36 -0500

    Update comment

--- src/SyntaxNodes/LinkNode.ts
+++ src/SyntaxNodes/LinkNode.ts
@@ -3,13 +3,13 @@ import { InlineSyntaxNode } from './InlineSyntaxNode'
 
 
 export class LinkNode extends RichInlineSyntaxNode {
-  // If a line consists solely of media conventions (and optional whitespace), those media conventions
-  // are placed directly into the outline rather than into a paragraph.
+  // If a line consists solely of media conventions (and/or whitespace), those media conventions are
+  // placed directly into the outline rather into a paragraph.
   //
-  // If an image is "linkified", or if a link otherwise contains only images (and optional whitespace),
-  // the link counts as an image for the purpose of the rule above. In that situation, the link itself
-  // is placed directly into the outline.
-  OUTLINE_SYNTAX_NODE(): void { } 
+  // If a media convention is "linkified", or if a link otherwise contains only media conventions (and
+  // whitespace), the link counts as media. In that situation, the link itself is placed directly into
+  // the outline.
+  OUTLINE_SYNTAX_NODE(): void { }
   private LINK: any = null
 
   constructor(public children: InlineSyntaxNode[] = [], public url: string = '') {
