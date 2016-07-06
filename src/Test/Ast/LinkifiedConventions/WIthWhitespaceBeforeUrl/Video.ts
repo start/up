4fc35aa43a6cc3a3cf1810d1a9cce1fa6f4d1ec0
Daniel Miller <daniel@wants.email>
2016-07-06 09:09:10 -0500

    Add many linkifying video tests

---
+++ src/Test/Ast/LinkifiedConventions/WIthWhitespaceBeforeUrl/Video.ts
@@ -0,0 +1,546 @@
+import { expect } from 'chai'
+import Up from '../../../../index'
+import { insideDocumentAndParagraph, expectEveryPermutationOfBrackets } from '../../Helpers'
+import { DocumentNode } from '../../../../SyntaxNodes/DocumentNode'
+import { LinkNode } from '../../../../SyntaxNodes/LinkNode'
+import { PlainTextNode } from '../../../../SyntaxNodes/PlainTextNode'
+import { ParenthesizedNode } from '../../../../SyntaxNodes/ParenthesizedNode'
+import { VideoNode } from '../../../../SyntaxNodes/VideoNode'
+
+
+context('A linkified video convention can have whitespace between its video URL and its linkifying URL (regardless of whether there is whitespace between its content and its video URL), but the linkifying URL must satisfy one of the following conditions:', () => {
+  specify('It has a scheme', () => {
+    expectEveryPermutationOfBrackets({
+      contentToWrapInBrackets: 'video: something terrible',
+      urlSegments: [
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          urlToWrapInBrackets: 'app:wiki/terrible-thing'
+        }],
+      toProduce: new DocumentNode([
+        new LinkNode([
+          new VideoNode('something terrible', 'https://example.com/sounds.ogg')
+        ], 'app:wiki/terrible-thing')
+      ])
+    })
+  })
+
+
+  describe('When the URL has a scheme', () => {
+    specify('the top-level domain may be followed by a slash and a resource path', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: Advance Wars',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: 'http://advancewars.wikia.com/wiki/Advance_Wars_(game)'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('Advance Wars', 'https://example.com/sounds.ogg')
+          ], 'http://advancewars.wikia.com/wiki/Advance_Wars_(game)')
+        ])
+      })
+    })
+
+    specify('the URL must not contain any spaces', () => {
+      expect(Up.toAst('[video: something terrible](https://example.com/sounds.ogg) (https://stackoverflow.com is nice)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('something terrible', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('('),
+            new LinkNode([
+              new PlainTextNode('stackoverflow.com')
+            ], 'https://stackoverflow.com'),
+            new PlainTextNode(' is nice)')
+          ]),
+        ])
+      )
+    })
+
+    specify('there must be somethng after the scheme', () => {
+      expect(Up.toAst('[video: email sending] (https://example.com/sounds.ogg) (mailto:)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('email sending', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(mailto:)')
+          ]),
+        ])
+      )
+    })
+
+    specify('there must be somethng after the scheme beyond only slashes', () => {
+      expect(Up.toAst('[video: local files rustling](https://example.com/sounds.ogg) (file:///)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('local files rustling', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(file:///)')
+          ]),
+        ])
+      )
+    })
+
+    specify('the rest of the URL can consist solely of digits', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: spooky phone call',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: 'tel:5555555555'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('spooky phone call', 'https://example.com/sounds.ogg')
+          ], 'tel:5555555555')
+        ])
+      })
+    })
+
+    specify('the scheme must not be escaped', () => {
+      expect(Up.toAst('[video: email sending] (https://example.com/sounds.ogg) (\\mailto:daniel@wants.email)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('email sending', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(mailto:daniel@wants.email)')
+          ]),
+        ])
+      )
+    })
+  })
+
+
+  specify('It starts with a slash', () => {
+    expectEveryPermutationOfBrackets({
+      contentToWrapInBrackets: 'video: something terrible',
+      urlSegments: [
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          urlToWrapInBrackets: '/wiki/something-terrible'
+        }],
+      toProduce: new DocumentNode([
+        new LinkNode([
+          new VideoNode('something terrible', 'https://example.com/sounds.ogg')
+        ], '/wiki/something-terrible')
+      ])
+    })
+  })
+
+
+  describe('When the URL starts with a slash, the URL', () => {
+    it('must not contain any spaces', () => {
+      expect(Up.toAst('[video: something terrible](https://example.com/sounds.ogg) (/r9k/ created it)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('something terrible', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(/r9k/ created it)')
+          ]),
+        ])
+      )
+    })
+
+    it('must have something after the slash', () => {
+      expect(Up.toAst('[video: slash] (https://example.com/sounds.ogg) (/)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('slash', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(/)')
+          ]),
+        ])
+      )
+    })
+
+    it('can consist solely of digits after the slash', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: Model 3 theft',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: '/3'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('Model 3 theft', 'https://example.com/sounds.ogg')
+          ], '/3')
+        ])
+      })
+    })
+
+    specify('must not have its slash escaped', () => {
+      expect(Up.toAst('[video: robots](https://example.com/sounds.ogg) (\\/r9k/)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('robots', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(/r9k/)')
+          ]),
+        ])
+      )
+    })
+  })
+
+
+  specify('It starts with a hash mark ("#")', () => {
+    expectEveryPermutationOfBrackets({
+      contentToWrapInBrackets: 'video: something terrible',
+      urlSegments: [
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          urlToWrapInBrackets: '#wiki/something-terrible'
+        }],
+      toProduce: new DocumentNode([
+        new LinkNode([
+          new VideoNode('something terrible', 'https://example.com/sounds.ogg')
+        ], '#wiki/something-terrible')
+      ])
+    })
+  })
+
+
+  describe('When the URL starts with a hash mark ("#"), the URL', () => {
+    it('may consist solely of digits after the hask mark', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: Model 3 theft',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: '#3'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('Model 3 theft', 'https://example.com/sounds.ogg')
+          ], '#3')
+        ])
+      })
+    })
+
+    it('must have something after the hash mark', () => {
+      expect(Up.toAst('[video: hash marking](https://example.com/sounds.ogg) (#)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('hash marking', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(#)')
+          ]),
+        ])
+      )
+    })
+
+    it('must not contain any spaces', () => {
+      expect(Up.toAst('[video: something terrible] (https://example.com/sounds.ogg) (#starcraft2 was never trending)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('something terrible', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(#starcraft2 was never trending)')
+          ]),
+        ])
+      )
+    })
+
+    it('must not have its hashmark escaped', () => {
+      expect(Up.toAst('[video: hash marking](https://example.com/sounds.ogg) (\\#starcraft2)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('hash marking', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(#starcraft2)')
+          ]),
+        ])
+      )
+    })
+  })
+
+
+  specify('It has a top-level domain', () => {
+    expectEveryPermutationOfBrackets({
+      contentToWrapInBrackets: 'video: Chrono Trigger',
+      urlSegments: [
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          urlToWrapInBrackets: 'https://chrono-trigger.wiki'
+        }],
+      toProduce: new DocumentNode([
+        new LinkNode([
+          new VideoNode('Chrono Trigger', 'https://example.com/sounds.ogg')
+        ], 'https://chrono-trigger.wiki')
+      ])
+    })
+  })
+
+
+  describe('When the URL merely has a top-level domain', () => {
+    specify('the top-level domain may be followed by a slash and a resource path', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: Advance Wars',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: 'advancewars.wikia.com/wiki/Advance_Wars_(game)'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('Advance Wars', 'https://example.com/sounds.ogg')
+          ], 'https://advancewars.wikia.com/wiki/Advance_Wars_(game)')
+        ])
+      })
+    })
+
+    specify('the top-level domain may be followed by a slash and no resource path', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: Advance Wars',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: 'advancewars.wikia.com/'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('Advance Wars', 'https://example.com/sounds.ogg')
+          ], 'https://advancewars.wikia.com/')
+        ])
+      })
+    })
+
+    specify('the top-level domain may not be followed by any character other than a forward slash', () => {
+      expect(Up.toAst('[video: 4chan] (https://example.com/sounds.ogg) (4chan.org--terrifying)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('4chan', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(4chan.org--terrifying)')
+          ]),
+        ])
+      )
+    })
+
+    specify('all domains before the top-level domain may consist solely of digits', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: good luck',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: '88.8888.cn'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('good luck', 'https://example.com/sounds.ogg')
+          ], 'https://88.8888.cn')
+        ])
+      })
+    })
+
+    context('The top-level domain must contain only letters ', () => {
+      specify('No numbers', () => {
+        expect(Up.toAst('[video: usernaming](https://example.com/sounds.ogg) (john.e.smith5)')).to.be.eql(
+          insideDocumentAndParagraph([
+            new VideoNode('usernaming', 'https://example.com/sounds.ogg'),
+            new PlainTextNode(' '),
+            new ParenthesizedNode([
+              new PlainTextNode('(john.e.smith5)')
+            ]),
+          ])
+        )
+      })
+
+      specify('No hyphens', () => {
+        expect(Up.toAst('[video: usernaming] (https://example.com/sounds.ogg) (john.e.smith-kline)')).to.be.eql(
+          insideDocumentAndParagraph([
+            new VideoNode('usernaming', 'https://example.com/sounds.ogg'),
+            new PlainTextNode(' '),
+            new ParenthesizedNode([
+              new PlainTextNode('(john.e.smith-kline)')
+            ]),
+          ])
+        )
+      })
+    })
+
+    specify('the URL must start with a letter or a number, not a period', () => {
+      expect(Up.toAst('[video: being British](https://example.com/sounds.ogg) (.co.uk)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('being British', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(.co.uk)')
+          ]),
+        ])
+      )
+    })
+
+    specify('the URL must not have consecutive periods before the top-level domain', () => {
+      expect(Up.toAst('[video: Ash claiming to be his own father] (https://example.com/sounds.ogg) (um..uh)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('Ash claiming to be his own father', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(um..uh)')
+          ]),
+        ])
+      )
+    })
+
+    specify('the URL must not have consecutive periods directly after the top-level domain before the slash that indicates the start of the resource path', () => {
+      expect(Up.toAst('[video: debilitating sadness](https://example.com/sounds.ogg) (4chan.org../r9k/)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('debilitating sadness', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(4chan.org../r9k/)')
+          ]),
+        ])
+      )
+    })
+
+    specify('the URL may have consecutive periods after the slash that indicates the start of the resource path', () => {
+      expectEveryPermutationOfBrackets({
+        contentToWrapInBrackets: 'video: rocket ship',
+        urlSegments: [
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            urlToWrapInBrackets: 'example.com/321...blastoff/1'
+          }],
+        toProduce: new DocumentNode([
+          new LinkNode([
+            new VideoNode('rocket ship', 'https://example.com/sounds.ogg')
+          ], 'https://example.com/321...blastoff/1')
+        ])
+      })
+    })
+
+    specify('the URL must not contain any spaces', () => {
+      expect(Up.toAst('[video: yeah] (https://example.com/sounds.ogg) (ign.com had some hilarious forums)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('yeah', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(ign.com had some hilarious forums)')
+          ]),
+        ])
+      )
+    })
+
+    specify('the domain part must not be escaped', () => {
+      expect(Up.toAst('[video: yeah](https://example.com/sounds.ogg) (\\ign.com)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new VideoNode('yeah', 'https://example.com/sounds.ogg'),
+          new PlainTextNode(' '),
+          new ParenthesizedNode([
+            new PlainTextNode('(ign.com)')
+          ]),
+        ])
+      )
+    })
+  })
+
+
+  specify('If none of the conditions are satisfied, the spoiler is not linkified', () => {
+    expect(Up.toAst('[video: something terrible] (https://example.com/sounds.ogg) (really)')).to.be.eql(
+      insideDocumentAndParagraph([
+        new VideoNode('something terrible', 'https://example.com/sounds.ogg'),
+        new PlainTextNode(' '),
+        new ParenthesizedNode([
+          new PlainTextNode('(really)')
+        ]),
+      ]))
+  })
+})
+
+
+describe("If there is whitespace between an otherwise-valid linkified video convention's video URL and its linkifying URL", () => {
+  it('the video convention is not linkified', () => {
+    expect(Up.toAst('[video: something terrible](https://example.com/sounds.ogg)  \\  (https://example.com)')).to.be.eql(
+      insideDocumentAndParagraph([
+        new VideoNode('something terrible', 'https://example.com/sounds.ogg'),
+        new PlainTextNode('    '),
+        new ParenthesizedNode([
+          new PlainTextNode('('),
+          new LinkNode([
+            new PlainTextNode('example.com')
+          ], 'https://example.com'),
+          new PlainTextNode(')')
+        ])
+      ]))
+  })
+})
+
+
+describe("A video convention's linkifying URL, when separated from its video URL by whitespace,", () => {
+  it('can itself contain whitespace if each whitespace character is escaped', () => {
+    expectEveryPermutationOfBrackets({
+      contentToWrapInBrackets: 'video: something terrible',
+      urlSegments: [
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          urlToWrapInBrackets: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          urlToWrapInBrackets: 'stackoverflow.com/search=something\\ very\\ terrible'
+        }],
+      toProduce: new DocumentNode([
+        new LinkNode([
+          new VideoNode('something terrible', 'https://example.com/sounds.ogg')
+        ], 'https://stackoverflow.com/search=something very terrible')
+      ])
+    })
+  })
+})
+
+
+const WHTIESPACE = ' \t\t '
+const WITH_AND_WITHOUT_WHITESPACE = ['', WHTIESPACE]
+const WITH_WHITESPACE = [WHTIESPACE]
