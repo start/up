9a1e367b7333a2c907208e4e9171fd0c72ca6e98
Daniel Miller <daniel@wants.email>
2016-07-06 07:17:02 -0500

    Explain new inline code logic

--- src/Parsing/Inline/Tokenization/tryToTokenizeInlineCodeOrUnmatchedDelimiter.ts
+++ src/Parsing/Inline/Tokenization/tryToTokenizeInlineCodeOrUnmatchedDelimiter.ts
@@ -5,6 +5,35 @@ import { Token } from './Token'
 
 
 // Text surrounded on either side by an equal number of backticks is treated as inline code.
+//
+// Inline code can contain streaks of backticks that aren't exactly as long as the delimiters.
+//
+// In this example, the delimiters are 1 backtick long, so the inline code can contain streaks of
+// 2 backticks:
+//
+// `let display = ``score:`` + 5`
+//
+// In this example, the delimiters are 2 backticks long, so the inline code can contain "streaks"
+// of 1 backtick:
+//
+// ``let display = `score:` + 5``
+//
+// Delimiters can be any length.
+//
+// Like (outline) code blocks, backslashes are treated as regular characters within inline code,
+// which means backticks within inline code cannot be escaped (hence the fancy delimiter syntax).
+//
+// If inline code needs to start or end with backticks, those backticks can be separated from the
+// delimiters with a single space. This single space is trimmed away:
+//
+// ` ``inline_code`` `
+//
+// Anything beyond that single space is preserved. If there are two spaces between the delimiter
+// and the starting/ending backticks, only one is trimmed away.
+//
+// Furthermore, that single space is only trimmed when it's needed to separate a delimiter from
+// backticks. If a given "side" of inline code doesn't have any backticks that require separation
+// from the delimiter, nothing gets trimmed from that side.
 export function tryToTokenizeInlineCodeOrUnmatchedDelimiter(
   args: {
     text: string
