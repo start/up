a34ed2395045e6af5cf77745886e6b90837edf89
Daniel Miller <daniel@wants.email>
2016-07-07 10:38:35 -0500

    Add failing test

--- src/Test/Ast/Nsfl.ts
+++ src/Test/Ast/Nsfl.ts
@@ -164,3 +164,17 @@ describe('Any whitespace between "NSFL:" and the start of the NSFL content', () 
       ]))
   })
 })
+
+
+describe("A NSFL convention's closing bracket, if preceded by whitespace,", () => {
+  it('does not close the convention', () => {
+    expect(Up.toAst('After you beat the Elite Four, {NSFL: you wrestle }c8 a naked Gary}.')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new NsflNode([
+          new PlainTextNode('you wrestle }c8 a naked Gary')
+        ]),
+        new PlainTextNode('.')
+      ]))
+  })
+})
