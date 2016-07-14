2ecb36bce071543886f98d4d40eb96fcbd195b4c
Daniel Miller <daniel@wants.email>
2016-07-14 06:58:52 -0500

    Add passing image test

--- src/Test/Ast/LinkifiedConventions/Image.ts
+++ src/Test/Ast/LinkifiedConventions/Image.ts
@@ -144,3 +144,25 @@ describe('An otherwise valid linkified audio convention with its linkifying URL 
       ]))
   })
 })
+
+
+describe('An already-linkified image', () => {
+  it('cannot be linkified again', () => {
+    expect(Up.toAst('After you beat the Elite Four, [image: you fight Gary] (example.com/fight.ogg) (example.com/finalbattle) (https://example.com).')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new LinkNode([
+          new ImageNode('you fight Gary', 'https://example.com/fight.ogg')
+        ], 'https://example.com/finalbattle'),
+        new PlainTextNode(' '),
+        new ParenthesizedNode([
+          new PlainTextNode('('),
+          new LinkNode([
+            new PlainTextNode('example.com')
+          ], 'https://example.com'),
+          new PlainTextNode(')'),
+        ]),
+        new PlainTextNode('.')
+      ]))
+  })
+})
