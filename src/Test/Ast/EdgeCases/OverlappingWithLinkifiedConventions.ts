1fdffcd609cefa17ca6498a28af147c24ab9cdcb
Daniel Miller <daniel@wants.email>
2016-07-14 10:37:33 -0500

    Add passing overlapping test

--- src/Test/Ast/EdgeCases/OverlappingWithLinkifiedConventions.ts
+++ src/Test/Ast/EdgeCases/OverlappingWithLinkifiedConventions.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai'
 import Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 import { StressNode } from '../../../SyntaxNodes/StressNode'
@@ -11,6 +13,8 @@ import { NsflNode } from '../../../SyntaxNodes/NsflNode'
 import { LinkNode } from '../../../SyntaxNodes/LinkNode'
 import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
 import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
+import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 
 
 describe('Emphasis overlapping a linkified spoiler', () => {
@@ -52,6 +56,38 @@ describe('A linkified spoiler overlapping revision deletion', () => {
           new PlainTextNode(' and then the credits roll')
         ]),
         new PlainTextNode('.')
+      ]))
+  })
+})
+
+
+describe('A footnote that overlaps a linkified NSFL convention', () => {
+  it("splits the NSFL convention node and its inner link, not the footnote node", () => {
+    const text = 'Eventually, I will think of one (^reasonable [NSFL: and realistic) example of a] {example.com} footnote that overlaps a NSFL convention.'
+
+    const footnote =
+      new FootnoteNode([
+        new PlainTextNode('reasonable '),
+        new NsflNode([
+          new LinkNode([
+            new PlainTextNode('and realistic')
+          ], 'https://example.com')
+        ])
+      ], 1)
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode('Eventually, I will think of one'),
+          footnote,
+          new NsflNode([
+            new LinkNode([
+              new PlainTextNode(' example of a')
+            ], 'https://example.com')
+          ]),
+          new PlainTextNode(' footnote that overlaps a NSFL convention.')
+        ]),
+        new FootnoteBlockNode([footnote])
       ]))
   })
 })
