ec35a10a3deaad7211434e8f71b566e0f2f2f025
Daniel Miller <daniel@wants.email>
2016-07-14 09:17:53 -0500

    Add passing overlapping test

---
+++ src/Test/Ast/EdgeCases/OverlappingWithLinkifiedConventions.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+import Up from '../../../index'
+import { insideDocumentAndParagraph } from '../Helpers'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../../SyntaxNodes/StressNode'
+import { ActionNode } from '../../../SyntaxNodes/ActionNode'
+import { SpoilerNode } from '../../../SyntaxNodes/SpoilerNode'
+import { NsfwNode } from '../../../SyntaxNodes/NsfwNode'
+import { NsflNode } from '../../../SyntaxNodes/NsflNode'
+import { LinkNode } from '../../../SyntaxNodes/LinkNode'
+import { RevisionDeletionNode } from '../../../SyntaxNodes/RevisionDeletionNode'
+import { RevisionInsertionNode } from '../../../SyntaxNodes/RevisionInsertionNode'
+
+
+describe('Emphasis overlapping a linkified spoiler', () => {
+  it('splits the emphasis node', () => {
+    expect(Up.toAst('After you beat the Elite Four, *only [SPOILER: you* fight Gary] (http://example.com/finalbattle).')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new EmphasisNode([
+          new PlainTextNode('only ')
+        ]),
+        new SpoilerNode([
+          new LinkNode([
+            new EmphasisNode([
+              new PlainTextNode('you')
+            ]),
+            new PlainTextNode(' fight Gary')
+          ], 'http://example.com/finalbattle')
+        ]),
+        new PlainTextNode('.')
+      ]))
+  })
+})
