8244f8d3bbb3d2039d6bd37352124cbae056c5f9
Daniel Miller <daniel@wants.email>
2016-07-16 04:06:49 -0500

    Unbreak build

--- src/SyntaxNodes/OrderedListItem.ts
+++ src/SyntaxNodes/OrderedListItem.ts
@@ -6,5 +6,5 @@ export class OrderedListItem {
 
   // During parsing, `ordinal` can be either `null` or a number. Defaulting `ordinal` to `null`
   // rather than `undefined` allows our unit tests to be cleaner.
-  constructor(public children?: OutlineSyntaxNode[], public ordinal: number = null) { }
+  constructor(public children: OutlineSyntaxNode[], public ordinal: number = null) { }
 }
