6ebcc6e15111dbecea9dce9d1f3f462917136bf9
Daniel Miller <daniel@wants.email>
2016-07-17 07:27:45 -0500

    Add failing spoiler block test

---
+++ src/Test/Ast/SpoilerBlock.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai'
+import Up from '../../index'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
+import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+import { BlockquoteNode } from '../../SyntaxNodes/BlockquoteNode'
+import { SpoilerBlockNode } from '../../SyntaxNodes/SpoilerBlockNode'
+import { DescriptionListNode } from '../../SyntaxNodes/DescriptionListNode'
+import { DescriptionListItem } from '../../SyntaxNodes/DescriptionListItem'
+import { DescriptionTerm } from '../../SyntaxNodes/DescriptionTerm'
+import { Description } from '../../SyntaxNodes/Description'
+
+
+describe('A line consisting solely of "SPOILER:", followed by an indented block of text,', () => {
+  it('produces a spoiler block node', () => {
+    const text = `
+SPOILER:
+  Ash said goodbye to Pikachu.
+  
+  Luckily, Pikachu ultimately decided to stay with Ash.`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new SpoilerBlockNode([
+          new ParagraphNode([
+            new PlainTextNode('Hello, world!')
+          ]),
+          new ParagraphNode([
+            new PlainTextNode('Goodbye, world!')
+          ])
+        ])
+      ]))
+  })
+})
