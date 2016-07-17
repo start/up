bebc90a2ab2c483c1dca1019f0773c86609f118d
Daniel Miller <daniel@wants.email>
2016-07-17 06:24:37 -0500

    Add syntax node for spoiler blocks

---
+++ src/SyntaxNodes/SpoilerBlockNode.ts
@@ -0,0 +1,9 @@
+import { OutlineSyntaxNode } from './OutlineSyntaxNode'
+
+
+export class SpoilerBlockNode {
+  OUTLINE_SYNTAX_NODE(): void { }
+  private SPOILER_BLOCK_NODE: any = null
+  
+  constructor(public children: OutlineSyntaxNode[] = []) { }
+}
