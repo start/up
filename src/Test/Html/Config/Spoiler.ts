5b9f197bf399ab57df160568819c93224801643f
Daniel Miller <daniel@wants.email>
2016-07-19 12:37:02 -0500

    Add passing spoiler HTML test

--- src/Test/Html/Config/Spoiler.ts
+++ src/Test/Html/Config/Spoiler.ts
@@ -6,6 +6,7 @@ import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 import { InlineSpoilerNode } from '../../../SyntaxNodes/InlineSpoilerNode'
+import { SpoilerBlockNode } from '../../../SyntaxNodes/SpoilerBlockNode'
 import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 
 
@@ -25,6 +26,28 @@ describe("The ID of an inline spoiler's checkbox (on both the checkbox and the l
       + '<input id="up-ruins-ending-1" type="checkbox">'
       + '<span></span>'
       + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("The ID of an spoiler block's checkbox (on both the checkbox and the label)", () => {
+  it("uses the provided term for 'spoiler'", () => {
+    const up = new Up({
+      i18n: {
+        terms: { spoiler: 'ruins ending' }
+      }
+    })
+
+    const node = new SpoilerBlockNode([])
+
+    const html =
+      '<div class="up-spoiler up-revealable">'
+      + '<label for="up-ruins-ending-1">toggle spoiler</label>'
+      + '<input id="up-ruins-ending-1" type="checkbox">'
+      + '<div></div>'
+      + '</div>'
 
     expect(up.toHtml(node)).to.be.eql(html)
   })
