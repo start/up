4afeedb61f4965148b92a21acdb904f572cbfb44
Daniel Miller <daniel@wants.email>
2016-07-20 07:39:05 -0500

    Specify type for media conventions

--- src/Parsing/Inline/MediaConventions.ts
+++ src/Parsing/Inline/MediaConventions.ts
@@ -1,22 +1,23 @@
-import { TokenKind } from './Tokenization/TokenKind'
+import { MediaConvention } from './MediaConvention'
 import { AudioNode } from '../../SyntaxNodes/AudioNode'
 import { ImageNode } from '../../SyntaxNodes/ImageNode'
 import { VideoNode } from '../../SyntaxNodes/VideoNode'
+import { TokenKind } from './Tokenization/TokenKind'
 
 
-export const AUDIO_CONVENTION = {
+export const AUDIO_CONVENTION: MediaConvention = {
   nonLocalizedTerm: 'audio',
   NodeType: AudioNode,
   descriptionAndStartTokenKind: TokenKind.AudioDescriptionAndStart
 }
 
-export const IMAGE_CONVENTION = {
+export const IMAGE_CONVENTION: MediaConvention = {
   nonLocalizedTerm: 'image',
   NodeType: ImageNode,
   descriptionAndStartTokenKind: TokenKind.ImageDescriptionAndStart
 }
 
-export const VIDEO_CONVENTION = {
+export const VIDEO_CONVENTION: MediaConvention = {
   nonLocalizedTerm: 'video',
   NodeType: VideoNode,
   descriptionAndStartTokenKind: TokenKind.VideoDescriptionAndStart
