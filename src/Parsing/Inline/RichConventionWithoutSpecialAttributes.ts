e7b0de00b63b13439d7cce188a1d53cc08c67ff6
Daniel Miller <daniel@wants.email>
2016-07-20 08:10:32 -0500

    Fix formatting

--- src/Parsing/Inline/RichConventionWithoutSpecialAttributes.ts
+++ src/Parsing/Inline/RichConventionWithoutSpecialAttributes.ts
@@ -1,11 +1,11 @@
-import { RichInlineSyntaxNode } from '../../SyntaxNodes/RichInlineSyntaxNode'
 import { InlineSyntaxNode } from '../../SyntaxNodes/InlineSyntaxNode'
+import { RichInlineSyntaxNode } from '../../SyntaxNodes/RichInlineSyntaxNode'
 import { TokenKind } from './Tokenization/TokenKind'
 
 
 // A rich inline convention is one that can contain other inline conventions.
 export interface RichConventionWithoutSpecialAttributes {
-   NodeType: new(children: InlineSyntaxNode[]) => RichInlineSyntaxNode
-   startTokenKind: TokenKind
-   endTokenKind: TokenKind
+  NodeType: new (children: InlineSyntaxNode[]) => RichInlineSyntaxNode
+  startTokenKind: TokenKind
+  endTokenKind: TokenKind
 }
