57e40eff47bd4f77e92b2232d189326dbc6f0682
Daniel Miller <daniel@wants.email>
2016-07-19 20:42:36 -0500

    Add classes for NSFW and NSFL blocks

---
+++ src/SyntaxNodes/NsflBlockNode.ts
@@ -0,0 +1,9 @@
+import { OutlineSyntaxNode } from './OutlineSyntaxNode'
+
+
+export class NsflBlockNode {
+  OUTLINE_SYNTAX_NODE(): void { }
+  private NSFL_BLOCK: any = null
+  
+  constructor(public children: OutlineSyntaxNode[] = []) { }
+}
---
+++ src/SyntaxNodes/NsfwBlockNode.ts
@@ -0,0 +1,9 @@
+import { OutlineSyntaxNode } from './OutlineSyntaxNode'
+
+
+export class NsfwBlockNode {
+  OUTLINE_SYNTAX_NODE(): void { }
+  private NSFW_BLOCK: any = null
+  
+  constructor(public children: OutlineSyntaxNode[] = []) { }
+}
