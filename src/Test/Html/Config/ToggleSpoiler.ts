ed9d904ef4398cf0994ff3de616e698dcbc756b6
Daniel Miller <daniel@wants.email>
2016-07-19 20:07:46 -0500

    Add passing spoiler block HTML test

--- src/Test/Html/Config/ToggleSpoiler.ts
+++ src/Test/Html/Config/ToggleSpoiler.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import Up from '../../../index'
 import { InlineSpoilerNode } from '../../../SyntaxNodes/InlineSpoilerNode'
+import { SpoilerBlockNode } from '../../../SyntaxNodes/SpoilerBlockNode'
 
 
 describe("The text in an inline spoiler's label", () => {
@@ -23,3 +24,26 @@ describe("The text in an inline spoiler's label", () => {
     expect(up.toHtml(node)).to.be.eql(html)
   })
 })
+
+
+describe("The text in a spoiler block's label", () => {
+  it("uses the provided term for 'toggleSpoiler'", () => {
+    const up = new Up({
+      i18n: {
+        terms: { toggleSpoiler: 'show/hide' }
+      }
+    })
+
+    const node = new SpoilerBlockNode([])
+
+    const html =
+      '<div class="up-spoiler up-revealable">'
+      + '<label for="up-spoiler-1">show/hide</label>'
+      + '<input id="up-spoiler-1" type="checkbox">'
+      + '<div></div>'
+      + '</div>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
