2027d64a41d35805d931c257f479dcd12a93b544
Daniel Miller <daniel@wants.email>
2016-07-20 13:55:27 -0500

    Add passing NSFW/NSFL tests

--- src/Test/Html/Config/Nsfl.ts
+++ src/Test/Html/Config/Nsfl.ts
@@ -6,6 +6,7 @@ import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
 import { InlineNsflNode } from '../../../SyntaxNodes/InlineNsflNode'
+import { NsflBlockNode } from '../../../SyntaxNodes/NsflBlockNode'
 import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 
 
@@ -25,6 +26,28 @@ describe("The ID of an inline NSFL convention's checkbox (on both the checkbox a
       + '<input id="up-life-ruining-1" type="checkbox">'
       + '<span></span>'
       + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("The ID of a NSFL block's checkbox (on both the checkbox and the label)", () => {
+  it("uses the provided term for 'nsfl'", () => {
+    const up = new Up({
+      i18n: {
+        terms: { nsfl: 'life ruining' }
+      }
+    })
+
+    const node = new NsflBlockNode([])
+
+    const html =
+      '<div class="up-nsfl up-revealable">'
+      + '<label for="up-life-ruining-1">toggle nsfl</label>'
+      + '<input id="up-life-ruining-1" type="checkbox">'
+      + '<div></div>'
+      + '</div>'
 
     expect(up.toHtml(node)).to.be.eql(html)
   })
--- src/Test/Html/Config/ToggleNsfw.ts
+++ src/Test/Html/Config/ToggleNsfw.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import Up from '../../../index'
 import { InlineNsfwNode } from '../../../SyntaxNodes/InlineNsfwNode'
+import { NsfwBlockNode } from '../../../SyntaxNodes/NsfwBlockNode'
 
 
 describe("The text in an inline NSFW convention's label", () => {
@@ -19,6 +20,28 @@ describe("The text in an inline NSFW convention's label", () => {
       + '<input id="up-nsfw-1" type="checkbox">'
       + '<span></span>'
       + '</span>'
+
+    expect(up.toHtml(node)).to.be.eql(html)
+  })
+})
+
+
+describe("The text in an inline NSFW convention's label", () => {
+  it("uses the provided term for 'toggleNsfw'", () => {
+    const up = new Up({
+      i18n: {
+        terms: { toggleNsfw: 'show/hide' }
+      }
+    })
+
+    const node = new NsfwBlockNode([])
+
+    const html =
+      '<div class="up-nsfw up-revealable">'
+      + '<label for="up-nsfw-1">show/hide</label>'
+      + '<input id="up-nsfw-1" type="checkbox">'
+      + '<div></div>'
+      + '</div>'
 
     expect(up.toHtml(node)).to.be.eql(html)
   })
