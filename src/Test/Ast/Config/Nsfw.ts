5e092c8bbd5ecd241a3e64cf0a0e51e2fe2ad2cb
Daniel Miller <daniel@wants.email>
2016-07-20 16:27:25 -0500

    Add 2 passing NSFW block tests

--- src/Test/Ast/Config/Nsfw.ts
+++ src/Test/Ast/Config/Nsfw.ts
@@ -1,30 +1,92 @@
 import { expect } from 'chai'
 import Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
+import { NsfwBlockNode } from '../../../SyntaxNodes/NsfwBlockNode'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
 import { InlineNsfwNode } from '../../../SyntaxNodes/InlineNsfwNode'
 
 
-describe('The term that represents NSFW conventions', () => {
-  const up = new Up({
-    i18n: {
-      terms: { nsfw: 'explicit' }
-    }
+context('The "nsfw" config term is used by both inline NSFW conventions and NSFW blocks.', () => {
+  context('For inline NSFW conventions:', () => {
+    const up = new Up({
+      i18n: {
+        terms: { nsfw: 'gross' }
+      }
+    })
+
+    specify('The term is used', () => {
+      expect(up.toAst('[gross: Ash fights Gary]')).to.be.eql(
+        insideDocumentAndParagraph([
+          new InlineNsfwNode([
+            new PlainTextNode('Ash fights Gary')
+          ])
+        ]))
+    })
+
+    it('The term is case-insensitive, even when custom', () => {
+      const lowercase = '[gross: Ash fights Gary]'
+      const mixedCase = '[gRoSs: Ash fights Gary]'
+
+      expect(up.toAst(lowercase)).to.be.eql(up.toAst(mixedCase))
+    })
   })
 
-  it('comes from the "nsfw" config term ', () => {
-    expect(up.toAst('[explicit: Ash wrestles naked Gary]')).to.be.eql(
-      insideDocumentAndParagraph([
-        new InlineNsfwNode([
-          new PlainTextNode('Ash wrestles naked Gary')
-        ])
-      ]))
-  })
 
-  it('is case-insensitive even when custom', () => {
-    const uppercase = '[EXPLICIT: Ash wrestles naked Gary]'
-    const mixedCase = '[eXplIciT: Ash wrestles naked Gary]'
+  context('For NSFW blocks:', () => {
+    const up = new Up({
+      i18n: {
+        terms: { nsfw: 'gross' }
+      }
+    })
 
-    expect(up.toAst(uppercase)).to.be.eql(up.toAst(mixedCase))
+    specify('The term is used', () => {
+      it('produces an inline spoiler block node', () => {
+        const text = `
+gross:
+
+  With a very sad song playing in the background, Ash said goodbye to Pikachu.
+  
+  Luckily, Pikachu ultimately decided to stay.`
+
+        expect(Up.toAst(text)).to.be.eql(
+          new DocumentNode([
+            new NsfwBlockNode([
+              new ParagraphNode([
+                new PlainTextNode('With a very sad song playing in the background, Ash said goodbye to Pikachu.')
+              ]),
+              new ParagraphNode([
+                new PlainTextNode('Luckily, Pikachu ultimately decided to stay.')
+              ])
+            ])
+          ]))
+      })
+
+      expect(up.toAst('[gross: Ash fights Gary]')).to.be.eql(
+        insideDocumentAndParagraph([
+          new InlineNsfwNode([
+            new PlainTextNode('Ash fights Gary')
+          ])
+        ]))
+    })
+
+    it('The term is case-insensitive, even when custom', () => {
+        const lowercase = `
+gross:
+
+  With a very sad song playing in the background, Ash said goodbye to Pikachu.
+  
+  Luckily, Pikachu ultimately decided to stay.`
+
+        const mixedCase = `
+gRosS:
+
+  With a very sad song playing in the background, Ash said goodbye to Pikachu.
+  
+  Luckily, Pikachu ultimately decided to stay.`
+
+      expect(up.toAst(lowercase)).to.be.eql(up.toAst(mixedCase))
+    })
   })
 })
