ea446a3f55fac7e350c7d59ceae844afc705b24d
Daniel Miller <daniel@wants.email>
2016-07-19 20:02:52 -0500

    Update test to include spoiler blocks

--- src/Test/Html/SequentialIds/Spoiler.ts
+++ src/Test/Html/SequentialIds/Spoiler.ts
@@ -9,57 +9,65 @@ import { InlineSpoilerNode } from '../../../SyntaxNodes/InlineSpoilerNode'
 import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
 
 
-describe("Multiple spoilers in a document", () => {
-  it("have have sequential IDs", () => {
+describe("Inline spoilers and spoiler blocks", () => {
+  it("have sequential IDs", () => {
     const node =
       new DocumentNode([
-        new ParagraphNode([
-          new PlainTextNode('Hello, '),
-          new InlineSpoilerNode([
-            new PlainTextNode('Red')
+        new SpoilerBlockNode([
+          new ParagraphNode([
+            new PlainTextNode('The main character defeats the bad guy.')
           ]),
-          new PlainTextNode('. We meet for the '),
-          new InlineSpoilerNode([
-            new EmphasisNode([
-              new PlainTextNode('eighth')
+          new SpoilerBlockNode([
+            new ParagraphNode([
+              new PlainTextNode('The hero was unambiguously '),
+              new InlineSpoilerNode([
+                new PlainTextNode('good '),
+                new InlineSpoilerNode([
+                  new PlainTextNode('and righteous.')
+                ])
+              ])
             ])
-          ]),
-          new PlainTextNode(' time.')
+          ])
         ]),
-        new ParagraphNode([
-          new PlainTextNode('Hello, '),
-          new InlineSpoilerNode([
-            new PlainTextNode('Blue')
-          ]),
-          new PlainTextNode('.'),
+        new SpoilerBlockNode([
+          new ParagraphNode([
+            new PlainTextNode('Everyone lived happily ever after, except for the bad men.')
+          ])
         ])
       ])
 
     const html =
-      '<p>'
-      + 'Hello, '
-      + '<span class="up-spoiler up-revealable">'
-      + '<label for="up-spoiler-1">toggle spoiler</label>'
-      + '<input id="up-spoiler-1" type="checkbox">'
-      + '<span>Red</span>'
-      + '</span>'
-      + '. We meet for the '
-      + '<span class="up-spoiler up-revealable">'
-      + '<label for="up-spoiler-2">toggle spoiler</label>'
-      + '<input id="up-spoiler-2" type="checkbox">'
-      + '<span><em>eighth</em></span>'
-      + '</span>'
-      + ' time.'
-      + '</p>'
+      '<div class="up-spoiler up-revealable"><label for="up-spoiler-1">toggle spoiler</label><input id="up-spoiler-1" type="checkbox">'
+      + '<div>'
+      + '<p>The main character defeats the bad guy.</p>'
+      + '<div class="up-spoiler up-revealable"><label for="up-spoiler-2">toggle spoiler</label><input id="up-spoiler-2" type="checkbox">'
+      + '<div>'
       + '<p>'
-      + 'Hello, '
+      + 'The hero was unambiguously '
       + '<span class="up-spoiler up-revealable">'
       + '<label for="up-spoiler-3">toggle spoiler</label>'
       + '<input id="up-spoiler-3" type="checkbox">'
-      + '<span>Blue</span>'
+      + '<span>'
+      + 'good '
+      + '<span class="up-spoiler up-revealable">'
+      + '<label for="up-spoiler-4">toggle spoiler</label>'
+      + '<input id="up-spoiler-4" type="checkbox">'
+      + '<span>'
+      + 'and righteous.'
       + '</span>'
-      + '.'
+      + '</span>'
+      + '</span>'
+      + '</span>'
       + '</p>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '<div class="up-spoiler up-revealable"><label for="up-spoiler-5">toggle spoiler</label><input id="up-spoiler-5" type="checkbox">'
+      + '<div>'
+      + '<p>Everyone lived happily ever after, except for the bad men.</p>'
+      + '</div>'
+      + '</div>'
 
     expect(Up.toHtml(node)).to.be.eql(html)
   })
@@ -69,7 +77,7 @@ describe("Multiple spoilers in a document", () => {
 describe("The ID of an inline spoiler's checkbox (on both the checkbox and the label)", () => {
   it("reset each time a new document is written", () => {
     const up = new Up()
-    
+
     const node =
       new DocumentNode([
         new ParagraphNode([
@@ -103,7 +111,7 @@ describe("The ID of an inline spoiler's checkbox (on both the checkbox and the l
 describe("The ID of a spoiler block's checkbox (on both the checkbox and the label)", () => {
   it("reset each time a new document is written", () => {
     const up = new Up()
-    
+
     const node =
       new DocumentNode([
         new SpoilerBlockNode([
