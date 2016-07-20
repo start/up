b8361527527f535ea9d64a0ed23b67e84d8ad48f
Daniel Miller <daniel@wants.email>
2016-07-20 08:09:44 -0500

    Add getLabeledBlockParser

---
+++ src/Parsing/Outline/getLabeledBlockParser.ts
@@ -0,0 +1,47 @@
+import { LineConsumer } from './LineConsumer'
+import { OutlineSyntaxNode } from '../../SyntaxNodes/OutlineSyntaxNode'
+import { getOutlineNodes } from './getOutlineNodes'
+import { getIndentedBlock } from './getIndentedBlock'
+import { solelyAndIgnoringCapitalization, escapeForRegex } from '../PatternHelpers'
+import { OutlineParserArgs } from './OutlineParserArgs'
+
+
+// A labaled block consists of a "label line" (a line consisting solely of a term followed by a colon)
+// followed by an indented block.
+//
+// A labeled block can contain any outline convention, and its label's term is case-insensitive. 
+export function getLabeledBlockParser(
+  labelTerm: string,
+  NodeType: new (children: OutlineSyntaxNode[]) => OutlineSyntaxNode
+) {
+  return function tryToParseLabeledBlock(args: OutlineParserArgs): boolean {
+    const lineConsumer = new LineConsumer(args.lines)
+
+    const labelLinePattern =
+      solelyAndIgnoringCapitalization(
+        escapeForRegex(labelTerm) + ':')
+
+    if (!lineConsumer.consume({ linePattern: labelLinePattern })) {
+      return false
+    }
+
+    const contentLines: string[] = []
+
+    getIndentedBlock({
+      lines: lineConsumer.getRemainingLines(),
+      then: (lines, countLinesConsumed) => {
+        contentLines.push(...lines)
+        lineConsumer.skipLines(countLinesConsumed)
+      }
+    })
+
+    if (!contentLines.length) {
+      return false
+    }
+
+    const children = getOutlineNodes(contentLines, args.headingLeveler, args.config)
+
+    args.then([new NodeType(children)], lineConsumer.countLinesConsumed)
+    return true
+  }
+}
