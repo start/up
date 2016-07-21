e2b00e6e63277b89a42069fa336e0f674ba7a851
Daniel Miller <daniel@wants.email>
2016-07-21 09:25:15 -0500

    Restructure indented code block test; add 1

--- src/Test/Ast/IndentedCodeBlock.ts
+++ src/Test/Ast/IndentedCodeBlock.ts
@@ -7,8 +7,8 @@ import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { SpoilerBlockNode } from '../../SyntaxNodes/SpoilerBlockNode'
 
 
-describe('A code block', () => {
-  it('preserves all indentation', () => {
+context('A code block preserves all indentation when it is', () => {
+  specify('a top-level convention', () => {
     const text = `
 \`\`\`
   if (x < 0) {
@@ -23,5 +23,28 @@ describe('A code block', () => {
 \t\treturn false
   }`),
       ]))
+  })
+
+
+  context('within a spoiler block', () => {
+    specify('using 2 spaces for indentation', () => {
+      const text = `
+SPOILER:
+  \`\`\`
+    if (x < 0) {
+  \t\treturn false
+    }
+  \`\`\``
+
+      expect(Up.toAst(text)).to.be.eql(
+        new DocumentNode([
+          new SpoilerBlockNode([
+            new CodeBlockNode(
+              `  if (x < 0) {
+\t\treturn false
+  }`),
+          ])
+        ]))
+    })
   })
 })
