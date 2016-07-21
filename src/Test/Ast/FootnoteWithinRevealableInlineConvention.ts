a825ab800fed38e630fd1d57b7e9b4d7092ff332
Daniel Miller <daniel@wants.email>
2016-07-21 14:37:09 -0500

    Add 2 passing footnote tests

--- src/Test/Ast/FootnoteWithinRevealableInlineConvention.ts
+++ src/Test/Ast/FootnoteWithinRevealableInlineConvention.ts
@@ -5,6 +5,8 @@ import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
 import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 import { InlineSpoilerNode } from '../../SyntaxNodes/InlineSpoilerNode'
+import { InlineNsfwNode } from '../../SyntaxNodes/InlineNsfwNode'
+import { InlineNsflNode } from '../../SyntaxNodes/InlineNsflNode'
 import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
 import { FootnoteBlockNode } from '../../SyntaxNodes/FootnoteBlockNode'
 
@@ -29,6 +31,66 @@ context("When a footnote is inside a revealable inline convention, the footnote'
             footnoteOutsideHiddenConvention,
             new PlainTextNode(" "),
             new InlineSpoilerNode([
+              new PlainTextNode("I don't eat cereal."),
+              footnoteInsideHiddenConvention,
+              new PlainTextNode(" Never have."),
+            ])
+          ]),
+          new FootnoteBlockNode([
+            footnoteOutsideHiddenConvention,
+            footnoteInsideHiddenConvention
+          ])
+        ]))
+    })
+
+    specify("NSFW conventions", () => {
+      const footnoteOutsideHiddenConvention = new FootnoteNode([
+        new PlainTextNode('Really.')
+      ], 1)
+
+      const footnoteInsideHiddenConvention = new FootnoteNode([
+        new InlineNsfwNode([
+          new PlainTextNode('Well, I do, but I pretend not to.')
+        ])
+      ], 2)
+
+      expect(Up.toAst("I'm normal. (^Really.) [NSFW: I don't eat cereal. (^ Well, I do, but I pretend not to.) Never have.]")).to.be.eql(
+        new DocumentNode([
+          new ParagraphNode([
+            new PlainTextNode("I'm normal."),
+            footnoteOutsideHiddenConvention,
+            new PlainTextNode(" "),
+            new InlineNsfwNode([
+              new PlainTextNode("I don't eat cereal."),
+              footnoteInsideHiddenConvention,
+              new PlainTextNode(" Never have."),
+            ])
+          ]),
+          new FootnoteBlockNode([
+            footnoteOutsideHiddenConvention,
+            footnoteInsideHiddenConvention
+          ])
+        ]))
+    })
+
+    specify("NSFL conventions", () => {
+      const footnoteOutsideHiddenConvention = new FootnoteNode([
+        new PlainTextNode('Really.')
+      ], 1)
+
+      const footnoteInsideHiddenConvention = new FootnoteNode([
+        new InlineNsflNode([
+          new PlainTextNode('Well, I do, but I pretend not to.')
+        ])
+      ], 2)
+
+      expect(Up.toAst("I'm normal. (^Really.) [NSFL: I don't eat cereal. (^ Well, I do, but I pretend not to.) Never have.]")).to.be.eql(
+        new DocumentNode([
+          new ParagraphNode([
+            new PlainTextNode("I'm normal."),
+            footnoteOutsideHiddenConvention,
+            new PlainTextNode(" "),
+            new InlineNsflNode([
               new PlainTextNode("I don't eat cereal."),
               footnoteInsideHiddenConvention,
               new PlainTextNode(" Never have."),
