d9d608b1b748b97e257798435f49805e0cc71533
Daniel Miller <daniel@wants.email>
2016-07-21 16:53:31 -0500

    Clarify some config test descriptions

--- src/Test/Ast/Config/TermsStartingWithAnotherStartDelimiter.ts
+++ src/Test/Ast/Config/TermsStartingWithAnotherStartDelimiter.ts
@@ -7,76 +7,78 @@ import { InlineNsfwNode } from '../../../SyntaxNodes/InlineNsfwNode'
 import { InlineNsflNode } from '../../../SyntaxNodes/InlineNsflNode'
 
 
-context("When the custom term for 'spoiler' starts with the a caret, its start delimiter starts with the footnote's start delimiter,", () => {
-  const up = new Up({
-    i18n: {
-      terms: { spoiler: '^lookaway^' }
-    }
+context("When the custom term for an inline convention starts with a caret, the fact that it happens to start with the start delimiter for footnotes doesn't affect anything.", () => {
+  context("When the custom term for 'spoiler' starts with the a caret", () => {
+    const up = new Up({
+      i18n: {
+        terms: { spoiler: '^lookaway^' }
+      }
+    })
+
+    specify('inline spoilers can be produced using the term', () => {
+      expect(up.toAst('[^lookaway^: Ash fights Gary]')).to.be.eql(
+        insideDocumentAndParagraph([
+          new InlineSpoilerNode([
+            new PlainTextNode('Ash fights Gary')
+          ])
+        ]))
+    })
+
+    specify('an unmatched inline spoiler start delimiter is treated as plain text', () => {
+      expect(up.toAst('[^lookaway^: Not finished typi')).to.be.eql(
+        insideDocumentAndParagraph([
+          new PlainTextNode('[^lookaway^: Not finished typi')
+        ]))
+    })
   })
 
-  specify('When there is a matching closing bracket, an inline spoiler is produced.', () => {
-    expect(up.toAst('[^lookaway^: Ash fights Gary]')).to.be.eql(
-      insideDocumentAndParagraph([
-        new InlineSpoilerNode([
-          new PlainTextNode('Ash fights Gary')
-        ])
-      ]))
+
+  context("When the custom term for 'nsfw' starts with the a caret", () => {
+    const up = new Up({
+      i18n: {
+        terms: { nsfw: '^lookaway^' }
+      }
+    })
+
+    specify('inline NSFW conventions can be produced using the term', () => {
+      expect(up.toAst('[^lookaway^: Ash fights Gary]')).to.be.eql(
+        insideDocumentAndParagraph([
+          new InlineNsfwNode([
+            new PlainTextNode('Ash fights Gary')
+          ])
+        ]))
+    })
+
+    specify('an unmatched inline NSFW start delimiter is treated as plain text', () => {
+      expect(up.toAst('[^lookaway^: Not finished typi')).to.be.eql(
+        insideDocumentAndParagraph([
+          new PlainTextNode('[^lookaway^: Not finished typi')
+        ]))
+    })
   })
 
-  specify('When there is not a matching closing bracket, the start delimiter is preserved as plain text', () => {
-    expect(up.toAst('[^lookaway^: Not finished typi')).to.be.eql(
-      insideDocumentAndParagraph([
-        new PlainTextNode('[^lookaway^: Not finished typi')
-      ]))
-  })
-})
 
+  context("When the custom term for 'nsfl' starts with the a caret", () => {
+    const up = new Up({
+      i18n: {
+        terms: { nsfl: '^lookaway^' }
+      }
+    })
 
-context("When the custom term for 'nsfw' starts with the a caret, its start delimiter starts with the footnote's start delimiter,", () => {
-  const up = new Up({
-    i18n: {
-      terms: { nsfw: '^lookaway^' }
-    }
-  })
+    specify('inline NSFL conventions can be produced using the term', () => {
+      expect(up.toAst('[^lookaway^: Ash fights Gary]')).to.be.eql(
+        insideDocumentAndParagraph([
+          new InlineNsflNode([
+            new PlainTextNode('Ash fights Gary')
+          ])
+        ]))
+    })
 
-  specify('When there is a matching closing bracket, an inline spoiler is produced.', () => {
-    expect(up.toAst('[^lookaway^: Ash fights Gary]')).to.be.eql(
-      insideDocumentAndParagraph([
-        new InlineNsfwNode([
-          new PlainTextNode('Ash fights Gary')
-        ])
-      ]))
-  })
-
-  specify('When there is not a matching closing bracket, the start delimiter is preserved as plain text', () => {
-    expect(up.toAst('[^lookaway^: Not finished typi')).to.be.eql(
-      insideDocumentAndParagraph([
-        new PlainTextNode('[^lookaway^: Not finished typi')
-      ]))
-  })
-})
-
-
-context("When the custom term for 'nsfl' starts with the a caret, its start delimiter starts with the footnote's start delimiter,", () => {
-  const up = new Up({
-    i18n: {
-      terms: { nsfl: '^lookaway^' }
-    }
-  })
-
-  specify('When there is a matching closing bracket, an inline spoiler is produced.', () => {
-    expect(up.toAst('[^lookaway^: Ash fights Gary]')).to.be.eql(
-      insideDocumentAndParagraph([
-        new InlineNsflNode([
-          new PlainTextNode('Ash fights Gary')
-        ])
-      ]))
-  })
-
-  specify('When there is not a matching closing bracket, the start delimiter is preserved as plain text', () => {
-    expect(up.toAst('[^lookaway^: Not finished typi')).to.be.eql(
-      insideDocumentAndParagraph([
-        new PlainTextNode('[^lookaway^: Not finished typi')
-      ]))
+    specify('an unmatched inline NSFL start delimiter is treated as plain text', () => {
+      expect(up.toAst('[^lookaway^: Not finished typi')).to.be.eql(
+        insideDocumentAndParagraph([
+          new PlainTextNode('[^lookaway^: Not finished typi')
+        ]))
+    })
   })
 })
