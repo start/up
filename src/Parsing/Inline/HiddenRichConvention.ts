1ed431c57bc9573daa45407689d818e887e4babc
Daniel Miller <daniel@wants.email>
2016-07-21 13:16:01 -0500

    Add HiddenRichConvention class

--- src/Parsing/Inline/HiddenRichConvention.ts
+++ src/Parsing/Inline/HiddenRichConvention.ts
@@ -1,16 +1,11 @@
 import { InlineSyntaxNode } from '../../SyntaxNodes/InlineSyntaxNode'
 import { RichInlineSyntaxNode } from '../../SyntaxNodes/RichInlineSyntaxNode'
 import { TokenKind } from './Tokenization/TokenKind'
-
+import { RichConventionWithoutExtraFields, RichSyntaxNodeWithoutExtraFields } from './RichConventionWithoutExtraFields'
 
 // A rich inline convention is one that can contain other inline conventions.
-export interface RichConventionWithoutExtraFields {
+export class HiddenRichConvention {
   NodeType: RichSyntaxNodeWithoutExtraFields
   startTokenKind: TokenKind
   endTokenKind: TokenKind
-  isInitiallyHidden?: boolean
-}
-
-export interface RichSyntaxNodeWithoutExtraFields {
-  new (children: InlineSyntaxNode[]): RichInlineSyntaxNode
 }
