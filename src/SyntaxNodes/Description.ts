542c62bd9a2235f982976b177504e0b29d606e42
Daniel Miller <daniel@wants.email>
2016-07-22 10:29:59 -0500

    Remove extraneous files

--- src/SyntaxNodes/Description.ts
+++
@@ -1,8 +0,0 @@
-import { OutlineSyntaxNode } from './OutlineSyntaxNode'
-
-
-export class Description {
-  constructor(public children: OutlineSyntaxNode[]) { }
-  
-  protected DESCRIPTION: any = null
-}
--- src/SyntaxNodes/DescriptionTerm.ts
+++
@@ -1,8 +0,0 @@
-import { InlineSyntaxNode } from './InlineSyntaxNode'
-
-
-export class DescriptionTerm {
-  protected DESCRIPTION_TERM: any = null
-
-  constructor(public children: InlineSyntaxNode[]) { }
-}
