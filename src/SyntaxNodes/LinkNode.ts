dba4ee328696b4113b9538dd6d7ffb0035499ff1
Daniel Miller <daniel@wants.email>
2016-07-23 12:42:43 -0500

    Explicitly indicate links are inline and outline

--- src/SyntaxNodes/LinkNode.ts
+++ src/SyntaxNodes/LinkNode.ts
@@ -1,19 +1,19 @@
 import { RichInlineSyntaxNode } from './RichInlineSyntaxNode'
 import { InlineSyntaxNode } from './InlineSyntaxNode'
+import { OutlineSyntaxNode } from './OutlineSyntaxNode'
 
 
-export class LinkNode extends RichInlineSyntaxNode {
-  // If a line consists solely of media conventions (and/or whitespace), those media conventions are
-  // placed directly into the outline rather into a paragraph.
-  //
-  // If a media convention is "linkified", or if a link otherwise contains only media conventions (and
-  // whitespace), the link counts as media. In that situation, the link itself is placed directly into
-  // the outline.
-  OUTLINE_SYNTAX_NODE(): void { }
-
+// If a line consists solely of media conventions (and/or whitespace), those media conventions are
+// placed directly into the outline rather into a paragraph.
+//
+// If a media convention is "linkified", or if a link otherwise contains only media conventions (and
+// whitespace), the link counts as media. In that situation, the link itself is placed directly into
+// the outline.
+export class LinkNode extends RichInlineSyntaxNode implements InlineSyntaxNode, OutlineSyntaxNode {
   constructor(public children: InlineSyntaxNode[], public url: string) {
     super(children)
   }
 
+  OUTLINE_SYNTAX_NODE(): void { }
   protected LINK_NODE(): void { }
 }
