2e3ab4d97da0b6025535ac8ab34428a091bf7fae
Daniel Miller <daniel@wants.email>
2016-07-22 19:22:17 -0500

    Inline the interface for Convention's constructor

--- src/Parsing/Inline/Tokenization/Convention.ts
+++ src/Parsing/Inline/Tokenization/Convention.ts
@@ -4,64 +4,49 @@ import { TokenKind } from './TokenKind'
 import { patternStartingWith, patternIgnoringCapitalizationAndStartingWith } from '../../PatternHelpers'
 
 
-export interface ConventionArgs {
-  onlyOpenIfDirectlyFollowing?: TokenKind[]
-
-  startsWith: string
-  startPatternContainsATerm?: boolean
-  endsWith?: string
-
-  isCutShortByWhitespace?: boolean
-  canConsistSolelyOfWhitespace?: boolean
-
-  beforeOpeningItFlushesNonEmptyBufferToPlainTextToken?: boolean
-  whenOpening?: OnTextMatch
-
-  insteadOfClosingOuterConventionsWhileOpen?: OnConventionEvent
-  insteadOfOpeningRegularConventionsWhileOpen?: OnConventionEvent
-
-  failsIfWhitespaceIsEnounteredBeforeClosing?: boolean
-
-  beforeClosingItAlwaysFlushesBufferTo?: TokenKind
-  beforeClosingItFlushesNonEmptyBufferTo?: TokenKind
-  whenClosingItAlsoClosesInnerConventions?: boolean
-  mustBeDirectlyFollowedBy?: Convention[]
-
-  whenClosing?: OnConventionEvent
-  insteadOfFailingWhenLeftUnclosed?: OnConventionEvent
-}
-
-
 export class Convention {
   onlyOpenIfDirectlyFollowing: TokenKind[]
-
   startsWith: RegExp
   endsWith: RegExp
-
   isCutShortByWhitespace: boolean
   canConsistSolelyOfWhitespace: boolean
-
   flushesBufferToPlainTextTokenBeforeOpening: boolean
   whenOpening: OnTextMatch
-
   insteadOfClosingOuterConventionsWhileOpen: OnConventionEvent
   insteadOfOpeningRegularConventionsWhileOpen: OnConventionEvent
-
   failsIfWhitespaceIsEnounteredBeforeClosing: boolean
-
   beforeClosingItFlushesNonEmptyBufferTo: TokenKind
   beforeClosingItAlwaysFlushesBufferTo: TokenKind
   whenClosingItAlsoClosesInnerConventions: boolean
   mustBeDirectlyFollowedBy: Convention[]
-
   whenClosing: OnConventionEvent
   insteadOfFailingWhenLeftUnclosed: OnConventionEvent
 
-  constructor(args: ConventionArgs) {
+  constructor(
+    args: {
+      onlyOpenIfDirectlyFollowing?: TokenKind[]
+      startsWith: string
+      startPatternContainsATerm?: boolean
+      endsWith?: string
+      isCutShortByWhitespace?: boolean
+      canConsistSolelyOfWhitespace?: boolean
+      beforeOpeningItFlushesNonEmptyBufferToPlainTextToken?: boolean
+      whenOpening?: OnTextMatch
+      insteadOfClosingOuterConventionsWhileOpen?: OnConventionEvent
+      insteadOfOpeningRegularConventionsWhileOpen?: OnConventionEvent
+      failsIfWhitespaceIsEnounteredBeforeClosing?: boolean
+      beforeClosingItAlwaysFlushesBufferTo?: TokenKind
+      beforeClosingItFlushesNonEmptyBufferTo?: TokenKind
+      whenClosingItAlsoClosesInnerConventions?: boolean
+      mustBeDirectlyFollowedBy?: Convention[]
+      whenClosing?: OnConventionEvent
+      insteadOfFailingWhenLeftUnclosed?: OnConventionEvent
+    }
+  ) {
     this.onlyOpenIfDirectlyFollowing = args.onlyOpenIfDirectlyFollowing
 
     const { startsWith, endsWith } = args
-    
+
     this.startsWith =
       args.startPatternContainsATerm
         ? patternIgnoringCapitalizationAndStartingWith(startsWith)
@@ -73,20 +58,15 @@ export class Convention {
 
     this.isCutShortByWhitespace = args.isCutShortByWhitespace
     this.canConsistSolelyOfWhitespace = args.canConsistSolelyOfWhitespace
-
     this.flushesBufferToPlainTextTokenBeforeOpening = args.beforeOpeningItFlushesNonEmptyBufferToPlainTextToken
     this.whenOpening = args.whenOpening
-
     this.insteadOfClosingOuterConventionsWhileOpen = args.insteadOfClosingOuterConventionsWhileOpen
     this.insteadOfOpeningRegularConventionsWhileOpen = args.insteadOfOpeningRegularConventionsWhileOpen
-
     this.failsIfWhitespaceIsEnounteredBeforeClosing = args.failsIfWhitespaceIsEnounteredBeforeClosing
-
     this.beforeClosingItFlushesNonEmptyBufferTo = args.beforeClosingItFlushesNonEmptyBufferTo
     this.beforeClosingItAlwaysFlushesBufferTo = args.beforeClosingItAlwaysFlushesBufferTo
     this.whenClosingItAlsoClosesInnerConventions = args.whenClosingItAlsoClosesInnerConventions
     this.mustBeDirectlyFollowedBy = args.mustBeDirectlyFollowedBy
-
     this.whenClosing = args.whenClosing
     this.insteadOfFailingWhenLeftUnclosed = args.insteadOfFailingWhenLeftUnclosed
   }
