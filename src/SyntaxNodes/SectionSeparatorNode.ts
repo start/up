98a6d27366ea736a638fdc49319e14cfeb96c493
Daniel Miller <daniel@wants.email>
2016-07-23 13:04:08 -0500

    Explicitly indicate section separators are outline

--- src/SyntaxNodes/SectionSeparatorNode.ts
+++ src/SyntaxNodes/SectionSeparatorNode.ts
@@ -1,4 +1,7 @@
-export class SectionSeparatorNode {
+import { OutlineSyntaxNode } from './OutlineSyntaxNode'
+
+
+export class SectionSeparatorNode implements OutlineSyntaxNode {
   OUTLINE_SYNTAX_NODE(): void { }
   protected SECTION_SEPARATOR_NODE(): void { }
 }
