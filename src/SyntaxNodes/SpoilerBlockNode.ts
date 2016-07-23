dd0cbbe6d485104416076e27f0fa170dfe6d2b32
Daniel Miller <daniel@wants.email>
2016-07-23 12:52:46 -0500

    Use RichOutlineSyntaxNode for spoiler blocks

--- src/SyntaxNodes/SpoilerBlockNode.ts
+++ src/SyntaxNodes/SpoilerBlockNode.ts
@@ -1,10 +1,6 @@
-import { OutlineSyntaxNode } from './OutlineSyntaxNode'
+import { RichOutlineSyntaxNode } from './RichOutlineSyntaxNode'
 
 
-export class SpoilerBlockNode {
-  OUTLINE_SYNTAX_NODE(): void { }
-
-  constructor(public children: OutlineSyntaxNode[] = []) { }
-
+export class SpoilerBlockNode extends RichOutlineSyntaxNode {
   protected SPOILER_BLOCK_NODE(): void { }
 }
