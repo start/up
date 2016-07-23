f9172f15c83e7c581376cac3ce400a0aad17b8e1
Daniel Miller <daniel@wants.email>
2016-07-23 18:51:24 -0500

    Make RichOutlineSyntaxNode extend container

--- src/SyntaxNodes/RichOutlineSyntaxNode.ts
+++ src/SyntaxNodes/RichOutlineSyntaxNode.ts
@@ -1,9 +1,8 @@
 import { OutlineSyntaxNode } from './OutlineSyntaxNode'
+import { OutlineSyntaxNodeContainer } from './OutlineSyntaxNodeContainer'
 
 
-export abstract class RichOutlineSyntaxNode implements OutlineSyntaxNode {
-  constructor(public children: OutlineSyntaxNode[]) { }
-
+export abstract class RichOutlineSyntaxNode extends OutlineSyntaxNodeContainer implements OutlineSyntaxNode {
   OUTLINE_SYNTAX_NODE(): void { }
   protected RICH_OUTLINE_SYNTAX_NODE(): void { }
 }
