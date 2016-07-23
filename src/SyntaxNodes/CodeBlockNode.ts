87f14e04b1e2aa634548e51356edb68c201a3d7c
Daniel Miller <daniel@wants.email>
2016-07-23 13:03:18 -0500

    Explicitly indicate code blocks are outline

--- src/SyntaxNodes/CodeBlockNode.ts
+++ src/SyntaxNodes/CodeBlockNode.ts
@@ -1,7 +1,9 @@
-export class CodeBlockNode {
-  OUTLINE_SYNTAX_NODE(): void { }
+import { OutlineSyntaxNode } from './OutlineSyntaxNode'
 
+
+export class CodeBlockNode implements OutlineSyntaxNode {
   constructor(public text: string) { }
 
+  OUTLINE_SYNTAX_NODE(): void { }
   protected CODE_BLOCK_NODE(): void { }
 }
