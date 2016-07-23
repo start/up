aa5ac4df89319a62990d5f1b2392cf599b3ad06f
Daniel Miller <daniel@wants.email>
2016-07-23 12:40:39 -0500

    Explicitly mark media as inline and outline

--- src/SyntaxNodes/MediaSyntaxNode.ts
+++ src/SyntaxNodes/MediaSyntaxNode.ts
@@ -1,11 +1,14 @@
-export abstract class MediaSyntaxNode {
-  // If a line consists solely of media conventions, those media conventions are placed directly
-  // into the outline. Otherwise, media conventions are placed within a paragraph (or line block). 
-  OUTLINE_SYNTAX_NODE(): void { }
-  INLINE_SYNTAX_NODE(): void { }
+import { InlineSyntaxNode } from './InlineSyntaxNode'
+import { OutlineSyntaxNode } from './OutlineSyntaxNode'
 
+
+// If a line consists solely of media conventions, those media conventions are placed directly
+// into the outline. Otherwise, media conventions are placed within a paragraph (or line block).
+export abstract class MediaSyntaxNode implements InlineSyntaxNode, OutlineSyntaxNode {
   constructor(public description: string, public url: string) { }
 
+  OUTLINE_SYNTAX_NODE(): void { }
+  INLINE_SYNTAX_NODE(): void { }
   protected MEDIA_SYNTAX_NODE(): void { }
 }
 
