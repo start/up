6268ba3ecc003016d0ad9524a8f99f813fe2db2d
Daniel Miller <daniel@wants.email>
2016-07-23 17:37:00 -0500

    Make rich inline node explicitly impl. container

--- src/SyntaxNodes/RichInlineSyntaxNode.ts
+++ src/SyntaxNodes/RichInlineSyntaxNode.ts
@@ -1,7 +1,8 @@
 import { InlineSyntaxNode } from './InlineSyntaxNode'
+import { InlineSyntaxNodeContainer } from './InlineSyntaxNodeContainer'
 
 
-export abstract class RichInlineSyntaxNode implements InlineSyntaxNode {
+export abstract class RichInlineSyntaxNode implements InlineSyntaxNode, InlineSyntaxNodeContainer {
   constructor(public children: InlineSyntaxNode[]) { }
 
   INLINE_SYNTAX_NODE(): void { }
