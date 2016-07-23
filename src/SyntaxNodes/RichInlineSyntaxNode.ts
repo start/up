776bde03ce6706df1463827e0b70607d32849e6d
Daniel Miller <daniel@wants.email>
2016-07-23 12:38:56 -0500

    Explictly mark RichInlineSyntaxNode as inline

--- src/SyntaxNodes/RichInlineSyntaxNode.ts
+++ src/SyntaxNodes/RichInlineSyntaxNode.ts
@@ -1,10 +1,9 @@
 import { InlineSyntaxNode } from './InlineSyntaxNode'
 
 
-export abstract class RichInlineSyntaxNode {
-  INLINE_SYNTAX_NODE(): void { }
-
+export abstract class RichInlineSyntaxNode implements InlineSyntaxNode {
   constructor(public children: InlineSyntaxNode[]) { }
 
+  INLINE_SYNTAX_NODE(): void { }
   protected RICH_INLINE_SYNTAX_NODE(): void { }
 }
