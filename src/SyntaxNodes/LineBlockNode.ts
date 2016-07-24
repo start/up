5e3ee6e0f8b6d68a1aba954f26e589c95fff7c22
Daniel Miller <daniel@wants.email>
2016-07-24 11:38:59 -0500

    Make line blocks process their own footnotes

--- src/SyntaxNodes/LineBlockNode.ts
+++ src/SyntaxNodes/LineBlockNode.ts
@@ -1,9 +1,17 @@
 import { OutlineSyntaxNode } from './OutlineSyntaxNode'
 import { InlineSyntaxNodeContainer } from './InlineSyntaxNodeContainer'
+import { FootnoteNode } from './FootnoteNode'
+import { Sequence } from '../Sequence'
+import { getTopLevelFootnotesFromInlineNodeContainersAndAssignTheirReferenceNumbers } from '../Parsing/handleFootnotes'
 
 
 export class LineBlockNode implements OutlineSyntaxNode {
   constructor(public lines: LineBlockNode.Line[]) { }
+
+  processFootnotesAndGetThoseThatAreStillBlockless(referenceNumberSequence: Sequence): FootnoteNode[] {
+    return (
+      getTopLevelFootnotesFromInlineNodeContainersAndAssignTheirReferenceNumbers(this.lines, referenceNumberSequence))
+  }
 
   OUTLINE_SYNTAX_NODE(): void { }
 }
