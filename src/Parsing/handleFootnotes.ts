2182fa9a516ca0f578c0803be2f8c8cd2ea9e9c5
Daniel Miller <daniel@wants.email>
2016-07-24 13:26:55 -0500

    Move footnote process initiation to DocumentNode

--- src/Parsing/handleFootnotes.ts
+++ src/Parsing/handleFootnotes.ts
@@ -2,83 +2,16 @@ import { concat } from '../CollectionHelpers'
 import { Sequence } from'../Sequence'
 import { OutlineSyntaxNodeContainer } from '../SyntaxNodes/OutlineSyntaxNodeContainer'
 import { InlineSyntaxNodeContainer } from '../SyntaxNodes/InlineSyntaxNodeContainer'
-import { DocumentNode } from '../SyntaxNodes/DocumentNode'
 import { FootnoteNode } from '../SyntaxNodes/FootnoteNode'
 import { InlineSyntaxNode } from '../SyntaxNodes/InlineSyntaxNode'
 import { RichInlineSyntaxNode } from '../SyntaxNodes/RichInlineSyntaxNode'
 import { OutlineSyntaxNode } from '../SyntaxNodes/OutlineSyntaxNode'
 
 
-// Footnotes are written inline, but they aren't meant to appear inline in the final document. That would
-// defeat the purpose of footnotes! Instead, footnotes are extracted and placed in footnote blocks.
-// 
-// Right now, Up only supports one output format: HTML. In the HTML output format, the original inline
-// footnote is replaced by its reference number, which links to the content of the footnote in the
-// appropriate footnote block. If you're ever seen a Wikipedia article, you're familiar with this setup.
-// Future output formats might handle footnotes slightly differently, but they should all use a similar
-// strategy.
-//
-// This function is responsible for assigning footnote reference numbers and producing footnote blocks.
-//
-// The specific rules are below:
-//
-//
-// 1. Any footnotes within a top-level outline convention are placed into a footnote block directly following
-//    that top-level outline convention. Even if a footnote is inside a paragraph inside an ordered list
-//    inside a description list, it's placed into a block after the description list, because the description
-//    list is the outermost, top-level outline convention.
-//
-// 2. Rule 1 applies to all outline conventions except:
-//
-//     * Blockquotes
-//     * Spoiler blocks
-//     * NSFW blocks
-//     * NSFL blocks
-//
-//    Blockquotes are considered mini-documents! Therefore, footnotes inside a blockquote are placed into a
-//    footnote block inside the blockquote, and rule 1 is applied to all top-level outline conventions within
-//    the blockquote. In other words, a footnote inside a paragraph inside an ordered list inside a blockquote
-//    is placed into a footnote block after the ordered list, but still inside the blockquote, because the
-//    ordered list is the outermost, top-level convention within the blockquote.
-//
-//    Spoiler blocks, NSFW blocks, and NSFL blocks shouldn't "leak" any of their content, so they have the
-//    same footnote block rules as blockquotes.
-//
-//    TODO: Better handle footnotes within the inline spoilers, inline NSFW conventions, and inline NSFL
-//    conventions.
-//
-// 3. It's contrived, but footnotes can reference other footnotes. For example:
-//
-//    I'm normal. [^That said, I don't eat cereal. (^Well, I do, but I pretend not to.) Never have.] Really.
-//
-//    The nesting can be arbitrarily deep.
-//
-//    Any nested footnotes are added to end of the footnote block containing the outer footnote, after any
-//    other non-nested footnotes. Then, any (doubly) nested footnotes inside the nested footnotes are added to
-//    the end of that same footnote block, and the process repeats until no more nested footnotes are found.
-//
-// 4. Footnotes are assigned reference numbers based on the order those footnotes are referenced in the final
-//    document.
-//
-//    Due to rule 3, a nested footnote (one that is referenced by another footnote) isn't actually referenced
-//    in the final document until its footnote block. As a result, that nested footnote is assigned a
-//    reference number after any non-nested footnotes appearing in the same top-level outline convention,
-//    because those footnotes are referenced inside the outline convention itself (and the footnote block
-//    comes after the outline convention).
-//
-//
-// Oh, one last thing! We'll use the term "blockless footnote" to describe a FootnoteNode that hasn't yet been
-// placed in a footnote block.
-export function handleFootnotes(documentNode: DocumentNode): void {
-  const referenceNumberSequence = new Sequence({ startingAt: 1 })
-  documentNode.insertFootnoteBlocksAndAssignFootnoteReferenceNumbers(referenceNumberSequence)
-}
-
 // Here, "outermost footnote" refers to any footnote that isn't nested within another footnote. It does not
 // exclude footntoes nested within other inline conventions (e.g. emphasis or stress).
 //
-// Because of rule 4 (described above), the reference numbers of nested footnotes aren't assigned until we
-// produce their containing footnote blocks.
+// The reference numbers of nested footnotes aren't assigned until we produce their containing footnote blocks.
 export function getOutermostFootnotesAndAssignTheirReferenceNumbers(nodes: InlineSyntaxNode[], referenceNumberSequence: Sequence): FootnoteNode[] {
   const footnotes: FootnoteNode[] = []
 
--- src/Parsing/parseDocument.ts
+++ src/Parsing/parseDocument.ts
@@ -1,4 +1,3 @@
-import { handleFootnotes } from './handleFootnotes'
 import { DocumentNode } from '../SyntaxNodes/DocumentNode'
 import { HeadingLeveler } from './Outline/HeadingLeveler'
 import { getOutlineNodes } from './Outline/getOutlineNodes'
@@ -8,10 +7,7 @@ import { UpConfig } from '../UpConfig'
 
 export function parseDocument(text: string, config: UpConfig): DocumentNode {
   const lines = text.split(INPUT_LINE_BREAK)
-  const outlineNodes = getOutlineNodes(lines, new HeadingLeveler(), config)
+  const documentChildren = getOutlineNodes(lines, new HeadingLeveler(), config)
 
-  const documentNode = new DocumentNode(outlineNodes)
-  handleFootnotes(documentNode)
-
-  return documentNode
+  return DocumentNode.withFootnotesPlacedInBlocks(documentChildren)
 }
