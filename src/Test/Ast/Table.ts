830772c0edc381ed5ad67a29092cda89459648d7
Daniel Miller <daniel@wants.email>
2016-07-26 13:41:17 -0500

    Add failing table test

--- src/Test/Ast/Table.ts
+++ src/Test/Ast/Table.ts
@@ -34,8 +34,8 @@ Chrono Cross;1999`
 })
 
 
-describe('Within a table', () => {
-  it('single blank lines are allowed', () => {
+context('Within a table', () => {
+  specify('single blank lines are allowed', () => {
     const text = `
 Table:
 
@@ -73,4 +73,44 @@ Chrono Cross;1999`
           ])
       ]))
   })
+
+  specify('outer whitespace is trimmed from each header and row cell', () => {
+    const text = `
+Table:
+
+ \t Game\t ; \t Release Date
+
+ \t Final Fantasy\t ;\t 1987
+ \t Final Fantasy II\t ;\t 1988
+
+ \t Chrono Trigger\t ;\t 1995
+ \t Chrono Cross\t ;\t 1999`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy')]),
+              new TableNode.Row.Cell([new PlainTextNode('1987')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy II')]),
+              new TableNode.Row.Cell([new PlainTextNode('1988')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Cross')]),
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ]),
+          ])
+      ]))
+  })
+
 })
