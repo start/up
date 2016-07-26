1d7cff2649f02e39b0e9a0255462d93c089ae182
Daniel Miller <daniel@wants.email>
2016-07-26 10:16:37 -0500

    Add unimplemented, commented-out table parser

---
+++ src/Parsing/Outline/tryToParseTable.ts
@@ -0,0 +1,12 @@
+/*
+import { LineConsumer } from './LineConsumer'
+import { TableNode } from '../../SyntaxNodes/TableNode'
+import { streakOf } from '../PatternHelpers'
+import { OutlineParserArgs } from './OutlineParserArgs'
+import { outlineLable } from '../PatternHelpers'
+
+// Tables consist of a header and a collection of rows. 
+export function tryToParseTable(args: OutlineParserArgs): boolean {
+  return false
+}
+*/
