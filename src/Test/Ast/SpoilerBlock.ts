9653753cfb7dcd2934666cf78b90fafec65e7233
Daniel Miller <daniel@wants.email>
2016-07-25 22:11:51 -0500

    Move new spoiler block test to bottom of context

--- src/Test/Ast/SpoilerBlock.ts
+++ src/Test/Ast/SpoilerBlock.ts
@@ -52,27 +52,6 @@ sPoiLeR:
       ]))
   })
 
-  it('can have whitespace after the colon', () => {
-    const text = `
-SPOILER:  \t  \t  
-
-  With a very sad song playing in the background, Ash said goodbye to Pikachu.
-  
-  Luckily, Pikachu ultimately decided to stay.`
-
-    expect(Up.toAst(text)).to.be.eql(
-      new DocumentNode([
-        new SpoilerBlockNode([
-          new ParagraphNode([
-            new PlainTextNode('With a very sad song playing in the background, Ash said goodbye to Pikachu.')
-          ]),
-          new ParagraphNode([
-            new PlainTextNode('Luckily, Pikachu ultimately decided to stay.')
-          ])
-        ])
-      ]))
-  })
-
   it('can be followed by a blank line', () => {
     const text = `
 SPOILER:
@@ -122,6 +101,27 @@ SPOILER:
 
 
 
+
+  With a very sad song playing in the background, Ash said goodbye to Pikachu.
+  
+  Luckily, Pikachu ultimately decided to stay.`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new SpoilerBlockNode([
+          new ParagraphNode([
+            new PlainTextNode('With a very sad song playing in the background, Ash said goodbye to Pikachu.')
+          ]),
+          new ParagraphNode([
+            new PlainTextNode('Luckily, Pikachu ultimately decided to stay.')
+          ])
+        ])
+      ]))
+  })
+
+  it('can have whitespace after the colon', () => {
+    const text = `
+SPOILER:  \t  \t  
 
   With a very sad song playing in the background, Ash said goodbye to Pikachu.
   
