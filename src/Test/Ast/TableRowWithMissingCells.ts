298df7552d5bf6d8d52a1c77fc9437f2db4dace4
Daniel Miller <daniel@wants.email>
2016-07-27 00:10:41 -0500

    Add failing table test

--- src/Test/Ast/TableRowWithMissingCells.ts
+++ src/Test/Ast/TableRowWithMissingCells.ts
@@ -6,7 +6,7 @@ import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 
 
 context('When a table row has fewer cells than the header', () => {
-  specify('a corresponding number of empty cells are added to the row', () => {
+  specify('a corresponding number of empty cells are appended to the row', () => {
     const text = `
 Table:
 
@@ -32,6 +32,58 @@ Starcraft;          Blizzard;             PC;               March 31, 1998`
               new TableNode.Row.Cell([new PlainTextNode('Square')]),
               new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
               new TableNode.Row.Cell([new PlainTextNode('March 11, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Terranigma')]),
+              new TableNode.Row.Cell([new PlainTextNode('Quintet')]),
+              new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('October 20, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Command & Conquer')]),
+              new TableNode.Row.Cell([]),
+              new TableNode.Row.Cell([]),
+              new TableNode.Row.Cell([])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Starcraft')]),
+              new TableNode.Row.Cell([new PlainTextNode('Blizzard')]),
+              new TableNode.Row.Cell([new PlainTextNode('PC')]),
+              new TableNode.Row.Cell([new PlainTextNode('March 31, 1998')])
+            ])
+          ])
+      ]))
+  })
+})
+
+
+context('When multiple table rows have fewer cells than the header', () => {
+  specify('a corresponding number of empty cells are appended to each row', () => {
+    const text = `
+Table:
+
+Game;               Developer;            Platform;         Release Date
+
+Chrono Trigger;     Square
+Terranigma;         Quintet;              Super Nintendo;   October 20, 1995
+
+Command & Conquer
+Starcraft;          Blizzard;             PC;               March 31, 1998`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Developer')]),
+            new TableNode.Header.Cell([new PlainTextNode('Platform')]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('Square')]),
+              new TableNode.Row.Cell([]),
+              new TableNode.Row.Cell([])
             ]),
             new TableNode.Row([
               new TableNode.Row.Cell([new PlainTextNode('Terranigma')]),
