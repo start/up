7605ff040fb09e9d652496759895e78c84ed0ee9
Daniel Miller <daniel@wants.email>
2016-07-27 00:08:54 -0500

    Improve failing table test

--- src/Test/Ast/TableRowWithMissingCells.ts
+++ src/Test/Ast/TableRowWithMissingCells.ts
@@ -5,42 +5,52 @@ import { TableNode } from '../../SyntaxNodes/TableNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 
 
-context('When a cell in a table row ommitted', () => {
-  specify('an empty cell is appended to the row', () => {
+context('When a table row has fewer cells than the header', () => {
+  specify('a corresponding number of empty cells are added to the row', () => {
     const text = `
 Table:
 
-Game;               Release Date
+Game;               Developer;            Platform;         Release Date
 
-Final Fantasy;      1987
-Final Fantasy II
+Chrono Trigger;     Square;               Super Nintendo;   March 11, 1995
+Terranigma;         Quintet;              Super Nintendo;   October 20, 1995
 
-Chrono Trigger;     1995
-Chrono Cross;       1999`
+Command & Conquer
+Starcraft;          Blizzard;             PC;               March 31, 1998`
 
     expect(Up.toAst(text)).to.be.eql(
       new DocumentNode([
         new TableNode(
           new TableNode.Header([
             new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Developer')]),
+            new TableNode.Header.Cell([new PlainTextNode('Platform')]),
             new TableNode.Header.Cell([new PlainTextNode('Release Date')])
           ]), [
             new TableNode.Row([
-              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy')]),
-              new TableNode.Row.Cell([new PlainTextNode('1987')])
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('Square')]),
+              new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('March 11, 1995')])
             ]),
             new TableNode.Row([
-              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy II')]),
+              new TableNode.Row.Cell([new PlainTextNode('Terranigma')]),
+              new TableNode.Row.Cell([new PlainTextNode('Quintet')]),
+              new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('October 20, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Command & Conquer')]),
+              new TableNode.Row.Cell([]),
+              new TableNode.Row.Cell([]),
               new TableNode.Row.Cell([])
             ]),
             new TableNode.Row([
-              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
-              new TableNode.Row.Cell([new PlainTextNode('1995')])
-            ]),
-            new TableNode.Row([
-              new TableNode.Row.Cell([new PlainTextNode('Chrono Cross')]),
-              new TableNode.Row.Cell([new PlainTextNode('1999')])
-            ]),
+              new TableNode.Row.Cell([new PlainTextNode('Starcraft')]),
+              new TableNode.Row.Cell([new PlainTextNode('Blizzard')]),
+              new TableNode.Row.Cell([new PlainTextNode('PC')]),
+              new TableNode.Row.Cell([new PlainTextNode('March 31, 1998')])
+            ])
           ])
       ]))
   })
