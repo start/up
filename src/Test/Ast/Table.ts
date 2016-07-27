5b98c0a2e9f86ea707774b92f08ed34f2fef83fe
Daniel Miller <daniel@wants.email>
2016-07-26 22:49:30 -0500

    Add passing table test

--- src/Test/Ast/Table.ts
+++ src/Test/Ast/Table.ts
@@ -335,3 +335,52 @@ Chrono Cross;       1999`
       ]))
   })
 })
+
+
+context('Table row cells', () => {
+  specify('can contain inline conventions', () => {
+    const text = `
+Table:
+
+Game;               Release Date
+
+Final Fantasy;      1987
+Final Fantasy II;   1988 (almost 1989)
+
+Chrono Trigger;     1995
+Chrono Cross;       1999`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([
+              new PlainTextNode('Release Date')
+            ])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy')]),
+              new TableNode.Row.Cell([new PlainTextNode('1987')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy II')]),
+              new TableNode.Row.Cell([
+                new PlainTextNode('1988 '),
+                new ParenthesizedNode([
+                  new PlainTextNode('(almost 1989)')
+                ])
+              ])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Cross')]),
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ]),
+          ])
+      ]))
+  })
+})
