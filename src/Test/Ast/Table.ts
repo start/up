e1e59ca9289d92e4bb924125d0332604de6e8bd7
Daniel Miller <daniel@wants.email>
2016-07-27 13:19:20 -0500

    Add failing table test

--- src/Test/Ast/Table.ts
+++ src/Test/Ast/Table.ts
@@ -485,3 +485,45 @@ Starcraft;          Blizzard;             PC;               March 31, 1998`
       ]))
   })
 })
+
+
+describe('A table row ending with an unescaped semicolon', () => {
+  it('ends with an empty cell', () => {
+    const text = `
+Table:
+
+Game;               Release Date
+
+Final Fantasy;      1987
+Final Fantasy II;
+
+Chrono Trigger;     1995
+Chrono Cross;       1999`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy')]),
+              new TableNode.Row.Cell([new PlainTextNode('1987')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Final Fantasy II')]),
+              new TableNode.Row.Cell([])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Cross')]),
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ]),
+          ])
+      ]))
+  })
+})
