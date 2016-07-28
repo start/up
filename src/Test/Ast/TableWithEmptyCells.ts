a9e9b589946debaef78fcc52d2c7f9114c8cb705
Daniel Miller <daniel@wants.email>
2016-07-28 14:06:04 -0500

    Add passing table test

--- src/Test/Ast/TableWithEmptyCells.ts
+++ src/Test/Ast/TableWithEmptyCells.ts
@@ -175,3 +175,55 @@ Chrono Cross;       1999`
       ]))
   })
 })
+
+
+describe('A (non-empty) table row consisting only of whitespace', () => {
+  it('is considered empty', () => {
+    const text = `
+Table:
+
+Game;               Developer;            Platform;         Release Date
+
+Chrono Trigger;     ;                     Super Nintendo;   March 11, 1995
+Terranigma;         Quintet;              Super Nintendo;   October 20, 1995
+
+Command & Conquer;  Westwood Studios;     ;                 August 31, 1995
+Starcraft;          Blizzard;             PC;               March 31, 1998`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Developer')]),
+            new TableNode.Header.Cell([new PlainTextNode('Platform')]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([]),
+              new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('March 11, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Terranigma')]),
+              new TableNode.Row.Cell([new PlainTextNode('Quintet')]),
+              new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('October 20, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Command & Conquer')]),
+              new TableNode.Row.Cell([new PlainTextNode('Westwood Studios')]),
+              new TableNode.Row.Cell([]),
+              new TableNode.Row.Cell([new PlainTextNode('August 31, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Starcraft')]),
+              new TableNode.Row.Cell([new PlainTextNode('Blizzard')]),
+              new TableNode.Row.Cell([new PlainTextNode('PC')]),
+              new TableNode.Row.Cell([new PlainTextNode('March 31, 1998')])
+            ])
+          ])
+      ]))
+  })
+})
