c859f2da5324e54227356b9e740681e90bb76a15
Daniel Miller <daniel@wants.email>
2016-07-28 14:07:31 -0500

    Add passing table test

--- src/Test/Ast/TableWithEmptyCells.ts
+++ src/Test/Ast/TableWithEmptyCells.ts
@@ -177,7 +177,61 @@ Chrono Cross;       1999`
 })
 
 
-describe('A (non-empty) table row consisting only of whitespace', () => {
+describe('A (non-empty) table header cell consisting only of whitespace', () => {
+  it('is considered empty', () => {
+    const text = `
+Table:
+
+Game;               Developer;            ;                 Release Date
+
+Chrono Trigger;     Square;               Super Nintendo;   March 11, 1995
+Terranigma;         Quintet;              Super Nintendo;   October 20, 1995
+
+Command & Conquer;  Westwood Studios;     PC;               August 31, 1995
+Starcraft;          Blizzard;             PC;               March 31, 1998`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Developer')]),
+            new TableNode.Header.Cell([]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('Square')]),
+              new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('March 11, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Terranigma')]),
+              new TableNode.Row.Cell([new PlainTextNode('Quintet')]),
+              new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('October 20, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Command & Conquer')]),
+              new TableNode.Row.Cell([new PlainTextNode('Westwood Studios')]),
+              new TableNode.Row.Cell([new PlainTextNode('PC')]),
+              new TableNode.Row.Cell([new PlainTextNode('August 31, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Starcraft')]),
+              new TableNode.Row.Cell([new PlainTextNode('Blizzard')]),
+              new TableNode.Row.Cell([new PlainTextNode('PC')]),
+              new TableNode.Row.Cell([new PlainTextNode('March 31, 1998')])
+            ])
+          ])
+      ]))
+  })
+})
+
+
+
+
+describe('A (non-empty) table row cell consisting only of whitespace', () => {
   it('is considered empty', () => {
     const text = `
 Table:
@@ -227,3 +281,4 @@ Starcraft;          Blizzard;             PC;               March 31, 1998`
       ]))
   })
 })
+
