04acd447c20e8721d9b6219e0a1e0a453826119e
Daniel Miller <daniel@wants.email>
2016-07-27 21:51:51 -0500

    Add failing table test

--- src/Test/Ast/TableWithRowsSpanningMultipleColumns.ts
+++ src/Test/Ast/TableWithRowsSpanningMultipleColumns.ts
@@ -47,6 +47,55 @@ Chrono Cross;       1999`
 })
 
 
+describe('A table header terminated by two semicolons', () => {
+  it('spans two columns', () => {
+    const text = `
+Table:
+
+Game;               Publisher;;                       Release Date
+
+Chrono Trigger;     Square;;                          March 11, 1995
+Terranigma;         Nintendo;             Enix;       October 20, 1995
+
+Command & Conquer;  Westwood Studios;;                August 31, 1995
+Starcraft;          Blizzard;;                        March 31, 1998`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Publisher')], 2),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('Square')], 2),
+              new TableNode.Row.Cell([new PlainTextNode('March 11, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Terranigma')]),
+              new TableNode.Row.Cell([new PlainTextNode('Nintendo')]),
+              new TableNode.Row.Cell([new PlainTextNode('Enix')]),
+              new TableNode.Row.Cell([new PlainTextNode('October 20, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Command & Conquer')]),
+              new TableNode.Row.Cell([new PlainTextNode('Westwood Studios')], 2),
+              new TableNode.Row.Cell([new PlainTextNode('August 31, 1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Starcraft')]),
+              new TableNode.Row.Cell([new PlainTextNode('Blizzard')], 2),
+              new TableNode.Row.Cell([new PlainTextNode('March 31, 1998')])
+            ])
+          ])
+      ]))
+  })
+})
+
+
+
 describe('A table row cell terminated by two semicolons', () => {
   it('spans two columns', () => {
     const text = `
