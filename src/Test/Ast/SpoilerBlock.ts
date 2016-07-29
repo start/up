d1879f278eba4f77e42597e594fe2b1d8f5eaf18
Daniel Miller <daniel@wants.email>
2016-07-29 13:07:32 -0500

    Add passing spoiler block test

--- src/Test/Ast/SpoilerBlock.ts
+++ src/Test/Ast/SpoilerBlock.ts
@@ -74,6 +74,27 @@ SPOILER:
       ]))
   })
 
+  it('does require a colon at the end', () => {
+    const text = `
+SPOILER
+
+  With a very sad song playing in the background, Ash said goodbye to Pikachu.
+  
+  Luckily, Pikachu ultimately decided to stay.`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new SpoilerBlockNode([
+          new ParagraphNode([
+            new PlainTextNode('With a very sad song playing in the background, Ash said goodbye to Pikachu.')
+          ]),
+          new ParagraphNode([
+            new PlainTextNode('Luckily, Pikachu ultimately decided to stay.')
+          ])
+        ])
+      ]))
+  })
+
   it('can be followed by 2 blank lines', () => {
     const text = `
 SPOILER:
