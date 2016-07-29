6b2cb74a913045efe6ef83f9ccafc5ce792b1f8e
Daniel Miller <daniel@wants.email>
2016-07-29 12:42:24 -0500

    Pass all but 4 table tests

--- src/Parsing/Outline/tryToParseTable.ts
+++ src/Parsing/Outline/tryToParseTable.ts
@@ -86,28 +86,29 @@ function getCells<TCell extends TableNode.Cell>(
   config: UpConfig
 ): TCell[] {
   // We trim the contents of each cell, which means trimming the whole row isn't strictly
-  // necessary. However, doing so makes it a bit simpler to tell when a row ends with cell
-  // delimiters (a scenario with some specific rules).
+  // necessary. However, doing so (or at least trimming the end of the strimg) makes it a
+  // bit easier for us to tell when a row ends with a single unescaped semicolon.
   //
-  // TODO: Explain rules
+  // As a rule, if the last cell in the table isn't blank, and if it spans just a single
+  // column, we add an extra empty cell to the end of the row.
   row = row.trim()
 
   const cells: TCell[] = []
-  let charIndexOfNextCell = 0
+  let charIndexOfStartOfNextCell = 0
   let charIndex = 0
 
-  function collectCell(countColumnsSpanned: number): void {
-    const rawCellValue = row.slice(charIndexOfNextCell, charIndex)
-    const cellChildren = getInlineNodes(rawCellValue, config)
-    
-    cells.push(new CellType(cellChildren, countColumnsSpanned))
+  function collectCell(args: { countColumnsSpanned: number }): void {
+    const rawCellValue = row.slice(charIndexOfStartOfNextCell, charIndex)
+    const cellChildren = getInlineNodes(rawCellValue.trim(), config)
+
+    cells.push(new CellType(cellChildren, args.countColumnsSpanned))
   }
 
   for (; charIndex < row.length; charIndex++) {
     const char = row[charIndex]
 
     if (char === ESCAPER_CHAR) {
-      // Escaped delimiters don't delimit cells, so we can safely skip over the next character.
+      // Escaped delimiters don't delimit cells, so we can safely skip the next character.
       charIndex++
       continue
     }
@@ -120,30 +121,46 @@ function getCells<TCell extends TableNode.Cell>(
     }
 
     const [delimiter] = result
-     
-    collectCell(delimiter.length)
-    charIndexOfNextCell += delimiter.length
+
+    collectCell({ countColumnsSpanned: delimiter.length })
+    charIndexOfStartOfNextCell = charIndex + delimiter.length
 
     // `i` is going to be incremented again at the start of the next iteration
     charIndex += delimiter.length - 1
   }
 
-  // In the loop above, cells are only collected after each delimiter.  We still need to collect
-  // the row's final cell (the one after the last delimiter).
+  // In the loop above, cells are only collected after each delimiter.  We still need to
+  // collect the row's final cell (the one after the last delimiter).
   //
-  // Furthermore, as a rule, if the last cell in the table isn't blank, and if it ends in with a
-  // single semicolon, we ad an extra empty cell to the end of the row.
-  // 
-  //
-  // 1. This cel
-  //
-  // In fact, that's the rule! Ending a row with a non-escaped semicolon indicates that one last
-  // blank cell should be added to the row.
+  // Furthermore, even if the last delimiter was on the end of the row, we have to satisfy
+  // the rule described at the to of the mthod: If the last cell in the table isn't blank,
+  // and if it spans just a single column, we have to add an extra empty cell to the end
+  // of the row.
 
   const lastCell = last(cells)
-  
-  if (charIndexOfNextCell != row.length || (lastCell)) {
-    collectCell(1)
+
+  const shouldCollectAnotherCell =
+    // If the last delimiter on the row wasn't at the end of the row...
+    charIndexOfStartOfNextCell < row.length
+    || (
+      // ... or if the last cell isnt blank...
+      //
+      // Two notes:
+      //
+      // 1. We know `row` will never be empty, and we know the first cell always starts at
+      //    the `0` index of `row`. Therefore, if `charIndexOfStartOfNextCell` is the row's
+      //    length, we must have already found at least one cell (and thus `lastCell` won't
+      //    be null).
+      //
+      // 2. Before we parse a cell for inline nodes, its content is trimmed. Therefore, to
+      //    check whether a cell is blank, we can simply check whether it has no children.
+      lastCell.children.length !== 0
+      // ... and if the last cell spans just one column, then we should collect another
+      // cell.
+      && lastCell.countColumnsSpanned === 1)
+
+  if (shouldCollectAnotherCell) {
+    collectCell({ countColumnsSpanned: 1 })
   }
 
   return cells
