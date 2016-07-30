b5073b19e00fe89d41176bd4100da62ad06719f0
Daniel Miller <daniel@wants.email>
2016-07-30 17:23:24 -0500

    Add passing table caption test

--- src/Test/Ast/TableCaption.ts
+++ src/Test/Ast/TableCaption.ts
@@ -76,4 +76,34 @@ Chrono Cross;   1999`
           ]))
       ]))
   })
+
+  specify('does not need to have a blank line between it and the header row', () => {
+    const text = `
+Table: Games in the Chrono series
+Game;           Release Date
+
+Chrono Trigger; 1995
+Chrono Cross;   1999`
+
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Cross')]),
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ])
+          ],
+          new TableNode.Caption([
+            new PlainTextNode('Games in the Chrono series')
+          ]))
+      ]))
+  })
 })
