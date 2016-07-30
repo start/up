d016ecdcb979d736c230ea5b5e371a3b6a2c59f6
Daniel Miller <daniel@wants.email>
2016-07-30 18:32:33 -0500

    Add 2 passing section separation tests

--- src/Test/Ast/OutlineSeparation.ts
+++ src/Test/Ast/OutlineSeparation.ts
@@ -5,6 +5,37 @@ import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 
 
+describe('1 empty line between paragraphs', () => {
+  it('simply provides separation, producing no syntax node itself', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
+      ]))
+  })
+})
+
+
+describe('2 empty lines between paragraphs', () => {
+  it('simply provides separation, producing no syntax node itself', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+
+
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
+      ]))
+  })
+})
+
+
 describe('1 blank line between paragraphs', () => {
   it('simply provides separation, producing no syntax node itself', () => {
     const text = `
@@ -18,6 +49,7 @@ Pokemon Sun is a truck.`
       ]))
   })
 })
+
 
 describe('2 blank lines between paragraphs', () => {
   it('simply provides separation, producing no syntax node itself', () => {
