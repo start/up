6e29c6aded64a3b7af65e41128ca1ec01e301130
Daniel Miller <daniel@wants.email>
2016-07-30 09:52:30 -0500

    Improve numeric cell helpers

--- src/Test/Ast/NumericTableCell.ts
+++ src/Test/Ast/NumericTableCell.ts
@@ -3,16 +3,21 @@ import Up from '../../index'
 import { TableNode } from '../../SyntaxNodes/TableNode'
 
 
-function expectFirstTableCellToBeNumeric(textProducingTable: string): void {
-  expect(isFirstTableCellNumeric(textProducingTable)).to.be.true
+function expectTableCellToBeNumeric(rawCellValue: string): void {
+  expect(isTableCellNumeric(rawCellValue)).to.be.true
 }
 
-function expectFirstTableCellNotToBeNumeric(textProducingTable: string): void {
-  expect(isFirstTableCellNumeric(textProducingTable)).to.be.false
+function expectTableCellNotToBeNumeric(rawCellValue: string): void {
+  expect(isTableCellNumeric(rawCellValue)).to.be.false
 }
 
-function isFirstTableCellNumeric(textProducingTable: string): boolean {
-  const documentNode = Up.toAst(textProducingTable)
+function isTableCellNumeric(rawCellValue: string): boolean {
+  const tableText = `
+Table:
+Header Cell 1; Header Cell 2
+${rawCellValue};`
+
+  const documentNode = Up.toAst(tableText)
   const table = documentNode.children[0] as TableNode
 
   return table.rows[0].cells[0].isNumeric()
@@ -22,186 +27,82 @@ function isFirstTableCellNumeric(textProducingTable: string): boolean {
 context('A tale row cell is numeric if its text content (ignoring footnotes) contains digits and no spaces.', () => {
   context('This includes when the cell contains', () => {
     specify('an integer', () => {
-      const text = `
-Table:
-Release Date
-1995`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('1995')
     })
 
     specify('an italicized and stressed integer', () => {
-      const text = `
-Table:
-Release Date
-***1995***`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('***1995***')
     })
 
     specify('a time of day', () => {
-      const text = `
-Table:
-Release Time
-10:10am`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('10:10am')
     })
 
     specify('an italicized and stressed time of day', () => {
-      const text = `
-Table:
-Release Time
-*10:10am*`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('***10:10am***')
     })
 
     specify('a time of day with the time period italicized and stressed', () => {
-      const text = `
-Table:
-Release Time
-10:10***am***`
-
-      expectFirstTableCellToBeNumeric(text)
-    })
-
-    specify('a time of day', () => {
-      const text = `
-Table:
-Release Time
-10:10am`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('10:10***am***')
     })
 
     specify('a price', () => {
-      const text = `
-Table:
-Price
-$13.01`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('$13.01')
     })
 
     specify('an italicized and stressed price', () => {
-      const text = `
-Table:
-Price
-$13.01`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('***$13.01***')
     })
 
     specify('inline code containing a numeric value', () => {
-      const text = `
-Table:
-Notation
-\`10.0f\``
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('`10.0f`')
     })
 
     specify('italicized and stressed inline code containing a numeric value', () => {
-      const text = `
-Table:
-Notation
-***\`10.0f\`***`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('***`10.0f`***')
     })
 
     specify('a link to a non-numeric URL whose content is numeric', () => {
-      const text = `
-Table:
-Price
-[$15.40] (https://example.com/price)`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('[$15.40] (https://example.com/price)')
     })
 
     specify('a linkified spoiler to a non-numeric URL whose content is numeric', () => {
-      const text = `
-Table:
-Defense Value
-[SPOILER: 44.7] (https://example.com/defense)`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('[SPOILER: 44.7] (https://example.com/defense)')
     })
 
     specify('an integer followed by a non-numeric footnote separated from the integer by whitespace', () => {
-      const text = `
-Table:
-Release Date
-2018 [^ It had been delayed since 2016]`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('2018 [^ It had been delayed since 2016]')
     })
 
     specify('an italicized and stressed integer followed by a non-numeric footnote separated from the integer by whitespace', () => {
-      const text = `
-Table:
-Release Date
-****2018**** [^ It had been delayed since 2016]`
-
-      expectFirstTableCellToBeNumeric(text)
+      expectTableCellToBeNumeric('****2018**** [^ It had been delayed since 2016]')
     })
   })
 
 
   context('This excludes when the cell contains', () => {
     specify('a single non-integer word', () => {
-      const text = `
-Table:
-Game
-StarCraft`
-
-      expectFirstTableCellNotToBeNumeric(text)
+      expectTableCellNotToBeNumeric('StarCraft')
     })
 
     specify('a movie sequel', () => {
-      const text = `
-Table:
-Movie
-Lion King 2`
-
-      expectFirstTableCellNotToBeNumeric(text)
+      expectTableCellNotToBeNumeric('Lion King 2')
     })
 
     specify('inline code with a non-numeric value', () => {
-      const text = `
-Table:
-Element
-\`<div>\``
-
-      expectFirstTableCellNotToBeNumeric(text)
+      expectTableCellNotToBeNumeric('`<div>`')
     })
 
-    specify('a link to a numeric URL whose content is not numeric', () => {
-      const text = `
-Table:
-Contact
-[John] (tel:5555555555)`
-
-      expectFirstTableCellNotToBeNumeric(text)
+    specify('a link to a numeric URL whose content is a non-numeric word', () => {
+      expectTableCellNotToBeNumeric('[John] (tel:5555555555)')
     })
 
-    specify('a linkified spoiler to a numeric URL whose content is not numeric', () => {
-      const text = `
-Table:
-Secret Santa
-[SPOILER: John] (tel:5555555555)`
-
-      expectFirstTableCellNotToBeNumeric(text)
+    specify('a linkified spoiler to a numeric URL whose content is a non-numeric word', () => {
+      expectTableCellNotToBeNumeric('[SPOILER: John] (tel:5555555555)')
     })
 
     specify('nothing', () => {
-      const text = `
-Table:
-Element
-;`
-
-      expectFirstTableCellNotToBeNumeric(text)
+      expectTableCellNotToBeNumeric('')
     })
   })
 })
