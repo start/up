82dc842112dd55ba9cb25281f00487971fae1619
Daniel Miller <daniel@wants.email>
2016-07-30 18:40:24 -0500

    Move outline separation tests to paragraph file

--- src/Test/Ast/Paragraph.ts
+++ src/Test/Ast/Paragraph.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai'
 import Up from '../../index'
 import { insideDocumentAndParagraph } from './Helpers'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
 
@@ -24,6 +26,85 @@ describe('Paragraphs', () => {
           new PlainTextNode('everyone')
         ]),
         new PlainTextNode(" like that?")        
+      ]))
+  })
+})
+
+
+context('Between paragraphs, 1 or 2 empty or blank lines provide separation without producing any syntax nodes of their own. Specifically:', () => {
+  specify('1 empty line', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
+      ]))
+  })
+
+  specify('2 empty lines', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+
+
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
+      ]))
+  })
+
+  specify('1 blank line', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+ \t \t 
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
+      ]))
+  })
+
+  specify('2 blank lines', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+  \t \t 
+\t \t 
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
+      ]))
+  })
+
+  specify('1 empty and 1 blank line', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+
+\t \t 
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
+      ]))
+  })
+
+  specify('1 blank and 1 empty line', () => {
+    const text = `
+Pokemon Moon has a Mew under a truck.
+\t \t 
+
+Pokemon Sun is a truck.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Pokemon Moon has a Mew under a truck.')]),
+        new ParagraphNode([new PlainTextNode('Pokemon Sun is a truck.')]),
       ]))
   })
 })
