c07365f06b9d8ba93cc78a3359aa635dc390170d
Daniel Miller <daniel@wants.email>
2016-07-30 10:29:51 -0500

    Update numeric rules for table cells

--- src/Test/Ast/NumericTableCell.ts
+++ src/Test/Ast/NumericTableCell.ts
@@ -24,7 +24,7 @@ ${rawCellValue};`
 }
 
 
-context('A tale row cell is numeric if its text content (ignoring footnotes) contains digits and no spaces.', () => {
+context('A tale row cell is numeric if its text content (ignoring footnotes) contains digits, no word characters, and no spaces.', () => {
   context('This includes when the cell contains', () => {
     specify('an integer', () => {
       expectTableCellToBeNumeric('1995')
@@ -34,16 +34,28 @@ context('A tale row cell is numeric if its text content (ignoring footnotes) con
       expectTableCellToBeNumeric('***1995***')
     })
 
-    specify('a time of day', () => {
-      expectTableCellToBeNumeric('10:10am')
+    specify('a decimal', () => {
+      expectTableCellToBeNumeric('.5')
     })
 
-    specify('an italicized and stressed time of day', () => {
-      expectTableCellToBeNumeric('***10:10am***')
+    specify('an italicized and stressed integer', () => {
+      expectTableCellToBeNumeric('***.5***')
     })
 
-    specify('a time of day with the time period italicized and stressed', () => {
-      expectTableCellToBeNumeric('10:10***am***')
+    specify('an italicized and stressed integer surrounded by whitespace', () => {
+      expectTableCellToBeNumeric('  \t \t \t ***.5***  \t \t \t ')
+    })
+
+    specify('a percentage', () => {
+      expectTableCellToBeNumeric('55%')
+    })
+
+    specify('an italicized and stressed percentage', () => {
+      expectTableCellToBeNumeric('***55%***')
+    })
+
+    specify('a percentage with the percent sign italicized and stressed', () => {
+      expectTableCellToBeNumeric('99***%***')
     })
 
     specify('a price', () => {
@@ -54,12 +66,28 @@ context('A tale row cell is numeric if its text content (ignoring footnotes) con
       expectTableCellToBeNumeric('***$13.01***')
     })
 
+    specify('a price whose dollar sign is italicized and stressed', () => {
+      expectTableCellToBeNumeric('***$***13.01')
+    })
+
+    specify('a parenthesized price', () => {
+      expectTableCellToBeNumeric('($13.01)')
+    })
+
+    specify('a big number with commas grouping digits', () => {
+      expectTableCellToBeNumeric('***$2,000,000***')
+    })
+
+    specify('a price whose dollar sign is italicized and stressed', () => {
+      expectTableCellToBeNumeric('***$***13.01')
+    })
+
     specify('inline code containing a numeric value', () => {
-      expectTableCellToBeNumeric('`10.0f`')
+      expectTableCellToBeNumeric('`10.0`')
     })
 
     specify('italicized and stressed inline code containing a numeric value', () => {
-      expectTableCellToBeNumeric('***`10.0f`***')
+      expectTableCellToBeNumeric('***`10.0`***')
     })
 
     specify('a link to a non-numeric URL whose content is numeric', () => {
@@ -70,26 +98,30 @@ context('A tale row cell is numeric if its text content (ignoring footnotes) con
       expectTableCellToBeNumeric('[SPOILER: 44.7] (https://example.com/defense)')
     })
 
-    specify('an integer followed by a non-numeric footnote separated from the integer by whitespace', () => {
+    specify('an integer followed by a non-numeric footnote', () => {
       expectTableCellToBeNumeric('2018 [^ It had been delayed since 2016]')
     })
 
-    specify('an italicized and stressed integer followed by a non-numeric footnote separated from the integer by whitespace', () => {
+    specify('an italicized and stressed integer followed by a non-numeric footnote', () => {
       expectTableCellToBeNumeric('****2018**** [^ It had been delayed since 2016]')
     })
   })
 
 
   context('This excludes when the cell contains', () => {
-    specify('a single non-integer word', () => {
+    specify('a single without digits', () => {
       expectTableCellNotToBeNumeric('StarCraft')
     })
 
-    specify('a movie sequel', () => {
-      expectTableCellNotToBeNumeric('Lion King 2')
+    specify('a word conaining digits and regular word characters', () => {
+      expectTableCellNotToBeNumeric('3D')
     })
 
-    specify('inline code with a non-numeric value', () => {
+    specify('a list of numbers separated by whitespace', () => {
+      expectTableCellNotToBeNumeric('1 2 3')
+    })
+
+    specify('inline code containing a non-numeric word', () => {
       expectTableCellNotToBeNumeric('`<div>`')
     })
 
@@ -99,6 +131,10 @@ context('A tale row cell is numeric if its text content (ignoring footnotes) con
 
     specify('a linkified spoiler to a numeric URL whose content is a non-numeric word', () => {
       expectTableCellNotToBeNumeric('[SPOILER: John] (tel:5555555555)')
+    })
+
+    specify('whitespace', () => {
+      expectTableCellNotToBeNumeric(' \t \t')
     })
 
     specify('nothing', () => {
