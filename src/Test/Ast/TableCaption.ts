e329cdc1ff3e94e215a700e9451c5b37b3d28ed6
Daniel Miller <daniel@wants.email>
2016-07-30 18:59:41 -0500

    Add passing table caption test

--- src/Test/Ast/TableCaption.ts
+++ src/Test/Ast/TableCaption.ts
@@ -1,13 +1,14 @@
 import { expect } from 'chai'
 import Up from '../../index'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 import { TableNode } from '../../SyntaxNodes/TableNode'
 import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 
 
 context("In a table's label line, when the term for 'table' is followed by a colon,", () => {
-  specify('the colon can be folowed by a table caption', () => {
+  specify('the colon can be folowed by a caption', () => {
     const text = `
 Table: Games in the Chrono series
 
@@ -126,6 +127,21 @@ Game;           Release Date`
           new TableNode.Caption([
             new PlainTextNode('Games in the Chrono series')
           ]))
+      ]))
+  })
+})
+
+
+context("When there isn't a colon after the term for 'table' in a table's label line", () => {
+  specify('the table cannot have a caption', () => {
+    const text = `
+Table the proposal.
+
+Do it now; I'm tired of waiting.`
+    expect(Up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Table the proposal.')]),
+        new ParagraphNode([new PlainTextNode("Do it now; I'm tired of waiting.")]),
       ]))
   })
 })
