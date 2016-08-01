39a03a39dab15f8c6e954da87f38a38685aa6a9c
Daniel Miller <daniel@wants.email>
2016-08-01 16:30:54 -0500

    Add 2 passing chart tests

---
+++ src/Test/Ast/Config/Chart.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai'
+import Up from '../../../index'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { TableNode } from '../../../SyntaxNodes/TableNode'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+
+
+describe('The term that represents chart conventions', () => {
+  const up = new Up({
+    i18n: {
+      terms: { chart: 'data' }
+    }
+  })
+
+  it('comes from the "chart" config term', () => {
+    const text = `
+Data:
+
+                  Release Date
+Chrono Trigger;   1995
+Chrono Cross;     1999`
+
+    expect(up.toAst(text)).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ], new TableNode.Header.Cell([new PlainTextNode('Chrono Trigger')])),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ], new TableNode.Header.Cell([new PlainTextNode('Chrono Cross')]))
+          ])
+      ]))
+  })
+
+  it('is case-insensitive even when custom', () => {
+    const uppercase = `
+Data:
+
+                  Release Date
+Chrono Trigger;   1995
+Chrono Cross;     1999`
+
+    const mixedCase = `
+dAtA:
+
+                  Release Date
+Chrono Trigger;   1995
+Chrono Cross;     1999`
+
+    expect(up.toAst(uppercase)).to.be.eql(up.toAst(mixedCase))
+  })
+})
