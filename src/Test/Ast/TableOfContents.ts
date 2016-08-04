1e0de9caa7f549f008b69403494a433a7801892e
Daniel Miller <daniel@wants.email>
2016-08-04 17:18:58 -0500

    Add failing table of contents test

--- src/Test/Ast/TableOfContents.ts
+++ src/Test/Ast/TableOfContents.ts
@@ -2,23 +2,72 @@ import { expect } from 'chai'
 import Up from '../../index'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
-/*import { StressNode } from '../../SyntaxNodes/StressNode'
 import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { TableNode } from '../../SyntaxNodes/TableNode'
+import { SpoilerBlockNode } from '../../SyntaxNodes/SpoilerBlockNode'
+/*import { StressNode } from '../../SyntaxNodes/StressNode'
 import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'*/
 import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
 
 
-context("When the 'createTableOfContents' config setting isn't specified as true", () => {
-  specify("the document isn't given a table of contents", () => {
+context("A document is not given a table of contents if:", () => {
+  const tableOfContents: DocumentNode.TableOfContents = undefined
+
+  specify("The 'createTableOfContents' config setting (which defaults to false) isn't set to true", () => {
     const markup = `
 I enjoy apples
 ==============`
 
-    const tableOfContents: DocumentNode.TableOfContents = undefined
-
     expect(Up.toAst(markup)).to.be.eql(
       new DocumentNode([
         new HeadingNode([new PlainTextNode('I enjoy apples')], 1),
+      ], tableOfContents))
+  })
+
+  specify("the 'createTableOfContents' config setting is set to true, but the document has no outline conventions that would be put into its table of contents", () => {
+    const markup = `
+Can you guess what this chart represents?
+
+Chart:
+
+        1;      0
+0;      true;   false
+1;      false;  false
+
+
+SPOILER:
+  The answer!
+  -----------
+  
+  The chart represents the logic for the \`AND\` operator.`
+
+    expect(Up.toAst(markup, { createTableOfContents: true })).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([new PlainTextNode('Can you guess what this chart represents?')]),
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([]),
+            new TableNode.Header.Cell([new PlainTextNode('1')]),
+            new TableNode.Header.Cell([new PlainTextNode('0')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('true')]),
+              new TableNode.Row.Cell([new PlainTextNode('false')]),
+            ], new TableNode.Header.Cell([new PlainTextNode('0')])),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('false')]),
+              new TableNode.Row.Cell([new PlainTextNode('false')])
+            ], new TableNode.Header.Cell([new PlainTextNode('1')]))
+          ]),
+        new SpoilerBlockNode([
+          new HeadingNode([new PlainTextNode('The answer!')], 1),
+          new ParagraphNode([
+            new PlainTextNode('The chart represents the logic for the '),
+            new InlineCodeNode('AND'),
+            new PlainTextNode(' operator.'),
+          ])
+        ])
       ], tableOfContents))
   })
 })
