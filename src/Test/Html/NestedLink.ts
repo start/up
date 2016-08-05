9b3723215fb1dcbe0fa353466d19585fa72ca58e
Daniel Miller <daniel@wants.email>
2016-08-05 15:05:32 -0500

    Reorganize nested link tests

--- src/Test/Html/NestedLink.ts
+++ src/Test/Html/NestedLink.ts
@@ -5,19 +5,16 @@ import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
 
 
-describe('Inside a link, a footnote', () => {
-  it("'does not produce an anchor element. The footnote's sup element directly contains the footnote's reference number", () => {
+context('Inside a link', () => {
+  specify("a footnote does not produce another link element. The footnote's <sup> directly contains the footnote's reference number", () => {
     const node = new LinkNode([
       new PlainTextNode('Google'),
       new FootnoteNode([new PlainTextNode('A really old search engine.')], 2)
     ], 'https://google.com')
     expect(Up.toHtml(node)).to.be.eql('<a href="https://google.com">Google<sup id="up-footnote-reference-2" class="up-footnote-reference">2</sup></a>')
   })
-})
 
-
-describe('Inside a link, another link', () => {
-  it("'does not produce an anchor element. The would-be anchor's contents are included directly inside the outer link", () => {
+  specify("a nested link does not produce another link element. The nested link's contents are included directly inside the outer link", () => {
     const node = new LinkNode([
       new PlainTextNode('Google is probably not '),
       new LinkNode([new PlainTextNode('Bing')], 'https://bing.com')
