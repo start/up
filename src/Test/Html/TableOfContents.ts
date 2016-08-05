288c8e68562fe6633d868cccf178d7e5dfcc09fd
Daniel Miller <daniel@wants.email>
2016-08-05 18:14:23 -0500

    Add file for table of contents HTML tests

---
+++ src/Test/Html/TableOfContents.ts
@@ -0,0 +1,45 @@
+/*import { expect } from 'chai'
+import Up from '../../index'
+import { LinkNode } from '../../SyntaxNodes/LinkNode'
+import { ImageNode } from '../../SyntaxNodes/ImageNode'
+import { AudioNode } from '../../SyntaxNodes/AudioNode'
+import { VideoNode } from '../../SyntaxNodes/VideoNode'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
+import { StressNode } from '../../SyntaxNodes/StressNode'
+import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
+import { RevisionInsertionNode } from '../../SyntaxNodes/RevisionInsertionNode'
+import { RevisionDeletionNode } from '../../SyntaxNodes/RevisionDeletionNode'
+import { ParenthesizedNode } from '../../SyntaxNodes/ParenthesizedNode'
+import { SquareBracketedNode } from '../../SyntaxNodes/SquareBracketedNode'
+import { ActionNode } from '../../SyntaxNodes/ActionNode'
+import { InlineSpoilerNode } from '../../SyntaxNodes/InlineSpoilerNode'
+import { InlineNsfwNode } from '../../SyntaxNodes/InlineNsfwNode'
+import { InlineNsflNode } from '../../SyntaxNodes/InlineNsflNode'
+import { SpoilerBlockNode } from '../../SyntaxNodes/SpoilerBlockNode'
+import { NsfwBlockNode } from '../../SyntaxNodes/NsfwBlockNode'
+import { NsflBlockNode } from '../../SyntaxNodes/NsflBlockNode'
+import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../SyntaxNodes/FootnoteBlockNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { BlockquoteNode } from '../../SyntaxNodes/BlockquoteNode'
+import { UnorderedListNode } from '../../SyntaxNodes/UnorderedListNode'
+import { OrderedListNode } from '../../SyntaxNodes/OrderedListNode'
+import { DescriptionListNode } from '../../SyntaxNodes/DescriptionListNode'
+import { TableNode } from '../../SyntaxNodes/TableNode'
+import { LineBlockNode } from '../../SyntaxNodes/LineBlockNode'
+import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+import { CodeBlockNode } from '../../SyntaxNodes/CodeBlockNode'
+import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
+
+
+context('A table of contents produces a <nav class="up-table-of-contents"> as the first element of the HTML document.', () => {
+  context('The <nav> contains elements corresponding to its entries.', () => {
+    context("Heading entries produce heading elements corresponding to their level. The heading's content is wrapped in a link pointing to that same heading in the document.", () => {
+      specify('Level 1 headings produce <h1>', () => {
+
+      })
+    })
+  })
+})*/
