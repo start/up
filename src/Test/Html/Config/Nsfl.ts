2188e22a08c25463674e5416aa645c2837fe9e08
Daniel Miller <daniel@wants.email>
2016-08-06 00:34:03 -0500

    [Broken] Fix more compilation errors

--- src/Test/Html/Config/Nsfl.ts
+++ src/Test/Html/Config/Nsfl.ts
@@ -1,5 +1,7 @@
 import { expect } from 'chai'
 import Up from '../../../index'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
 import { InlineNsflNode } from '../../../SyntaxNodes/InlineNsflNode'
 import { NsflBlockNode } from '../../../SyntaxNodes/NsflBlockNode'
 
@@ -12,16 +14,22 @@ describe("The ID of an inline NSFL convention's checkbox (on both the checkbox a
       }
     })
 
-    const node = new InlineNsflNode([])
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new InlineNsflNode([])
+      ])
+    ])
 
     const html =
-      '<span class="up-nsfl up-revealable">'
+      '<p>'
+    '<span class="up-nsfl up-revealable">'
       + '<label for="up-life-ruining-1">toggle NSFL</label>'
       + '<input id="up-life-ruining-1" type="checkbox">'
       + '<span></span>'
       + '</span>'
+      + '</p>'
 
-    expect(up.toHtml(node)).to.be.eql(html)
+    expect(up.toHtml(documentNode)).to.be.eql(html)
   })
 })
 
@@ -34,7 +42,9 @@ describe("The ID of a NSFL block's checkbox (on both the checkbox and the label)
       }
     })
 
-    const node = new NsflBlockNode([])
+    const documentNode = new DocumentNode([
+      new NsflBlockNode([])
+    ])
 
     const html =
       '<div class="up-nsfl up-revealable">'
@@ -43,6 +53,6 @@ describe("The ID of a NSFL block's checkbox (on both the checkbox and the label)
       + '<div></div>'
       + '</div>'
 
-    expect(up.toHtml(node)).to.be.eql(html)
+    expect(up.toHtml(documentNode)).to.be.eql(html)
   })
 })
