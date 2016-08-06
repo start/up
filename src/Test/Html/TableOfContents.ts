80c908038689c445de1652f1ddf360844465fe2b
Daniel Miller <daniel@wants.email>
2016-08-06 13:49:14 -0500

    Add 2 failing table of contents HTML tests

--- src/Test/Html/TableOfContents.ts
+++ src/Test/Html/TableOfContents.ts
@@ -3,6 +3,7 @@ import Up from '../../index'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
 import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { TableNode } from '../../SyntaxNodes/TableNode'
 
 /*import { LinkNode } from '../../SyntaxNodes/LinkNode'
 import { ImageNode } from '../../SyntaxNodes/ImageNode'
@@ -29,13 +30,12 @@ import { BlockquoteNode } from '../../SyntaxNodes/BlockquoteNode'
 import { UnorderedListNode } from '../../SyntaxNodes/UnorderedListNode'
 import { OrderedListNode } from '../../SyntaxNodes/OrderedListNode'
 import { DescriptionListNode } from '../../SyntaxNodes/DescriptionListNode'
-import { TableNode } from '../../SyntaxNodes/TableNode'
 import { LineBlockNode } from '../../SyntaxNodes/LineBlockNode'
 import { CodeBlockNode } from '../../SyntaxNodes/CodeBlockNode'
 import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'*/
 
 
-context('A table of contents produces a <nav class="up-table-of-contents"> as the first element of the HTML. The <nav> starts with an <h1> containing the term for "Table of Contents"', () => {
+context('When a document has table of contents, it produces a <nav class="up-table-of-contents"> as the first element of the HTML. The <nav> starts with an <h1> containing the term for "Table of Contents"', () => {
   context("After that <h1>, each table of contents entry gets its own HTML element.", () => {
     context("A heading entry produces a heading element with a level one higher than its own. The heading's content is wrapped in a link pointing to the actual heading in the document.", () => {
       specify('A level 1 heading produces an <h2>', () => {
@@ -143,6 +143,79 @@ context('A table of contents produces a <nav class="up-table-of-contents"> as th
             + '</nav>'
             + '<h6 id="up-outline-1">I enjoy apples</h6>')
         })
+      })
+    })
+
+    context('Charts and tables (both of which must have a caption to be included in the table of contents) each produce a link element containing their caption.', () => {
+      specify('A table produces a link containing its caption', () => {
+        const table =
+          new TableNode(
+            new TableNode.Header([
+              new TableNode.Header.Cell([new PlainTextNode('Game')]),
+              new TableNode.Header.Cell([new PlainTextNode('Developer')])
+            ]), [
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('Final Fantasy')]),
+                new TableNode.Row.Cell([new PlainTextNode('Square')])
+              ]),
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('Super Mario Kart')]),
+                new TableNode.Row.Cell([new PlainTextNode('Nintendo')])
+              ])
+            ],
+            new TableNode.Caption([
+              new PlainTextNode('Influential games')
+            ]))
+
+        const documentNode =
+          new DocumentNode([table], new DocumentNode.TableOfContents([table]))
+
+        expect(Up.toHtml(documentNode)).to.be.eql(
+          '<nav class="up-table-of-contents">'
+          + '<h1>Table of Contents</h1>'
+          + '<a href="#up-outline-1">I enjoy apples</a>'
+          + '</nav>'
+          + '<table>'
+          + '<caption>Influential games</caption>'
+          + '<thead><tr><th scope="col">Game</th><th scope="col">Developer</th></tr></thead>'
+          + '<tr><td>Final Fantasy</td><td>Square</td></tr>'
+          + '<tr><td>Super Mario Kart</td><td>Nintendo</td></tr>'
+          + '</table>')
+      })
+
+      specify('A chart produces a link containing its caption', () => {
+        const table =
+          new TableNode(
+            new TableNode.Header([
+              new TableNode.Header.Cell([]),
+              new TableNode.Header.Cell([new PlainTextNode('1')]),
+              new TableNode.Header.Cell([new PlainTextNode('0')])
+            ]), [
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('true')]),
+                new TableNode.Row.Cell([new PlainTextNode('false')]),
+              ], new TableNode.Header.Cell([new PlainTextNode('1')])),
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('false')]),
+                new TableNode.Row.Cell([new PlainTextNode('false')])
+              ], new TableNode.Header.Cell([new PlainTextNode('0')]))
+            ],
+            new TableNode.Caption([new PlainTextNode('AND operator logic')]))
+
+        const documentNode =
+          new DocumentNode([table], new DocumentNode.TableOfContents([table]))
+
+        expect(Up.toHtml(documentNode)).to.be.eql(
+          '<nav class="up-table-of-contents">'
+          + '<h1>Table of Contents</h1>'
+          + '<a href="#up-outline-1">AND operator logic</a>'
+          + '</nav>'
+          + '<table>'
+          + '<caption>AND operator logic</caption>'
+          + '<thead><tr><th scope="col"></th><th scope="col">1</th><th scope="col">0</th></tr></thead>'
+          + '<tr><th scope="row">1</th><td>true</td><td>false</td></tr>'
+          + '<tr><th scope="row">0</th><td>false</td><td>false</td></tr>'
+          + '</table>')
       })
     })
   })
