059f3904cbdd5f851a95ebb20d2b31228da5f7c9
Daniel Miller <daniel@wants.email>
2016-08-05 23:36:58 -0500

    [Broken] Fix more compilation errors

--- src/Test/Html/NestedLink.ts
+++ src/Test/Html/NestedLink.ts
@@ -3,22 +3,34 @@ import Up from '../../index'
 import { LinkNode } from '../../SyntaxNodes/LinkNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 
 
 context('Inside a link', () => {
   specify("a footnote does not produce another link element. The footnote's <sup> directly contains the footnote's reference number", () => {
-    const node = new LinkNode([
-      new PlainTextNode('Google'),
-      new FootnoteNode([new PlainTextNode('A really old search engine.')], 2)
-    ], 'https://google.com')
-    expect(Up.toHtml(node)).to.be.eql('<a href="https://google.com">Google<sup id="up-footnote-reference-2" class="up-footnote-reference">2</sup></a>')
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new LinkNode([
+          new PlainTextNode('Google'),
+          new FootnoteNode([new PlainTextNode('A really old search engine.')], 2)
+        ], 'https://google.com')
+      ])
+    ])
+
+    expect(Up.toHtml(documentNode)).to.be.eql('<a href="https://google.com">Google<sup id="up-footnote-reference-2" class="up-footnote-reference">2</sup></a>')
   })
 
   specify("a nested link does not produce another link element. The nested link's contents are included directly inside the outer link", () => {
-    const node = new LinkNode([
-      new PlainTextNode('Google is probably not '),
-      new LinkNode([new PlainTextNode('Bing')], 'https://bing.com')
-    ], 'https://google.com')
-    expect(Up.toHtml(node)).to.be.eql('<a href="https://google.com">Google is probably not Bing</a>')
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new LinkNode([
+          new PlainTextNode('Google is probably not '),
+          new LinkNode([new PlainTextNode('Bing')], 'https://bing.com')
+        ], 'https://google.com')
+      ])
+    ])
+
+    expect(Up.toHtml(documentNode)).to.be.eql('<a href="https://google.com">Google is probably not Bing</a>')
   })
 })
