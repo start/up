65d9a4ca80292eb8f4272ea1da7de44e150d9525
Daniel Miller <daniel@wants.email>
2016-08-05 19:34:30 -0500

    Add failing table of contents HTML test

--- src/Test/Html/TableOfContents.ts
+++ src/Test/Html/TableOfContents.ts
@@ -1,11 +1,13 @@
-/*import { expect } from 'chai'
+import { expect } from 'chai'
 import Up from '../../index'
-import { LinkNode } from '../../SyntaxNodes/LinkNode'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+
+/*import { LinkNode } from '../../SyntaxNodes/LinkNode'
 import { ImageNode } from '../../SyntaxNodes/ImageNode'
 import { AudioNode } from '../../SyntaxNodes/AudioNode'
 import { VideoNode } from '../../SyntaxNodes/VideoNode'
-import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
-import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from '../../SyntaxNodes/EmphasisNode'
 import { StressNode } from '../../SyntaxNodes/StressNode'
 import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
@@ -29,17 +31,28 @@ import { OrderedListNode } from '../../SyntaxNodes/OrderedListNode'
 import { DescriptionListNode } from '../../SyntaxNodes/DescriptionListNode'
 import { TableNode } from '../../SyntaxNodes/TableNode'
 import { LineBlockNode } from '../../SyntaxNodes/LineBlockNode'
-import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
 import { CodeBlockNode } from '../../SyntaxNodes/CodeBlockNode'
-import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'
+import { SectionSeparatorNode } from '../../SyntaxNodes/SectionSeparatorNode'*/
 
 
-context('A table of contents produces a <nav class="up-table-of-contents"> as the first element of the HTML document.', () => {
-  context('The <nav> contains elements corresponding to its entries.', () => {
-    context("Heading entries produce heading elements corresponding to their level. The heading's content is wrapped in a link pointing to that same heading in the document.", () => {
-      specify('Level 1 headings produce <h1>', () => {
+context('A table of contents produces a <nav class="up-table-of-contents"> as the first element of the HTML. The <nav> starts with <h1>Table of Contents</h1>.', () => {
+  context("After the <h1>, each table of contents entry gets its own heading element.", () => {
+    context("A heading entry produces a heading element 1 level lower than its own. The heading's content is wrapped in a link pointing to the heading in the document.", () => {
+      specify('A level 1 heading produces an <h2>', () => {
+        const heading =
+          new HeadingNode([new PlainTextNode('I enjoy apples')], 1)
 
+        const documentNode =
+          new DocumentNode([heading], new DocumentNode.TableOfContents([heading]))
+
+        expect(Up.toHtml(documentNode)).to.be.eql(
+          '<nav class="up-table-ofcontents">'
+          + '<h1>Table of Contents<h1>'
+          + '<h2><a href="#up-outline-1">I enjoy apples</a><h2>'
+          + '</nav>'
+          + '<h1 id="up-outline-1">I enjoy apples</h1>'
+        )
       })
     })
   })
-})*/
+})
