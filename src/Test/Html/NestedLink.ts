2197492b520855c1a03db08574426437be764d43
Daniel Miller <daniel@wants.email>
2016-08-06 22:38:08 -0500

    Add passing table of contents HTML test

--- src/Test/Html/NestedLink.ts
+++ src/Test/Html/NestedLink.ts
@@ -5,6 +5,7 @@ import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
 import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
 import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
 
 
 context('Inside a link', () => {
@@ -32,5 +33,25 @@ context('Inside a link', () => {
     ])
 
     expect(Up.toHtml(documentNode)).to.be.eql('<p><a href="https://google.com">Google is probably not Bing</a></p>')
+  })
+})
+
+
+context('Links nested within table of contents entries do not produce anchor elements. This applies to links within:', () => {
+  specify('Headings', () => {
+    const heading =
+      new HeadingNode([new LinkNode([new PlainTextNode('I enjoy apples')], 'https://google.com')], 1)
+
+    const documentNode =
+      new DocumentNode([heading], new DocumentNode.TableOfContents([heading]))
+
+    expect(Up.toHtml(documentNode)).to.be.eql(
+      '<nav class="up-table-of-contents">'
+      + '<h1>Table of Contents</h1>'
+      + '<ul>'
+      + '<li><h2><a href="#up-part-1">I enjoy apples</a></h2></li>'
+      + '</ul>'
+      + '</nav>'
+      + '<h1 id="up-part-1"><a href="https://google.com">I enjoy apples</a></h1>')
   })
 })
