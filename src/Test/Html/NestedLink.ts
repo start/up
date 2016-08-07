5eb8a20d6979d1f1458f37da922b428b28ba2efa
Daniel Miller <daniel@wants.email>
2016-08-06 23:37:57 -0500

    Use less robust version of HTML tests, too

--- src/Test/Html/NestedLink.ts
+++ src/Test/Html/NestedLink.ts
@@ -11,7 +11,135 @@ import { TableNode } from '../../SyntaxNodes/TableNode'
 
 
 context('Inside a link', () => {
-  specify("a footnote does not produce another <a> element. The footnote's <sup> directly contains the footnote's reference number", () => {
+  specify("a footnote does not produce another link element. The footnote's <sup> directly contains the footnote's reference number", () => {
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new LinkNode([
+          new PlainTextNode('Google'),
+          new FootnoteNode([new PlainTextNode('A really old search engine.')], 2)
+        ], 'https://google.com')
+      ])
+    ])
+
+    expect(Up.toHtml(documentNode)).to.be.eql('<p><a href="https://google.com">Google<sup id="up-footnote-reference-2" class="up-footnote-reference">2</sup></a></p>')
+  })
+
+  specify("a nested link does not produce another link element. The nested link's contents are included directly inside the outer link", () => {
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new LinkNode([
+          new PlainTextNode('Google is probably not '),
+          new LinkNode([new PlainTextNode('Bing')], 'https://bing.com')
+        ], 'https://google.com')
+      ])
+    ])
+
+    expect(Up.toHtml(documentNode)).to.be.eql('<p><a href="https://google.com">Google is probably not Bing</a></p>')
+  })
+})
+
+
+context('Links nested within table of contents entries do not produce <a> elements. This applies to links within:', () => {
+  specify('Headings', () => {
+    const heading =
+      new HeadingNode([
+        new LinkNode([new PlainTextNode('I enjoy apples')], 'https://google.com')
+      ], 1)
+
+    const documentNode =
+      new DocumentNode([heading], new DocumentNode.TableOfContents([heading]))
+
+    expect(Up.toHtml(documentNode)).to.be.eql(
+      '<nav class="up-table-of-contents">'
+      + '<h1>Table of Contents</h1>'
+      + '<ul>'
+      + '<li><h2><a href="#up-part-1">I enjoy apples</a></h2></li>'
+      + '</ul>'
+      + '</nav>'
+      + '<h1 id="up-part-1"><a href="https://google.com">I enjoy apples</a></h1>')
+  })
+
+  specify('Tables', () => {
+    const table =
+      new TableNode(
+        new TableNode.Header([
+          new TableNode.Header.Cell([new PlainTextNode('Game')]),
+          new TableNode.Header.Cell([new PlainTextNode('Developer')])
+        ]), [
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('Final Fantasy')]),
+            new TableNode.Row.Cell([new PlainTextNode('Square')])
+          ]),
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('Super Mario Kart')]),
+            new TableNode.Row.Cell([new PlainTextNode('Nintendo')])
+          ])
+        ],
+        new TableNode.Caption([
+          new LinkNode([new PlainTextNode('Influential games')], 'https://google.com')
+        ]))
+
+    const documentNode =
+      new DocumentNode([table], new DocumentNode.TableOfContents([table]))
+
+    expect(Up.toHtml(documentNode)).to.be.eql(
+      '<nav class="up-table-of-contents">'
+      + '<h1>Table of Contents</h1>'
+      + '<ul>'
+      + '<li><a href="#up-part-1">Influential games</a></li>'
+      + '</ul>'
+      + '</nav>'
+      + '<table id="up-part-1">'
+      + '<caption><a href="https://google.com">Influential games</a></caption>'
+      + '<thead><tr><th scope="col">Game</th><th scope="col">Developer</th></tr></thead>'
+      + '<tr><td>Final Fantasy</td><td>Square</td></tr>'
+      + '<tr><td>Super Mario Kart</td><td>Nintendo</td></tr>'
+      + '</table>')
+  })
+
+  specify('Charts', () => {
+    const chart =
+      new TableNode(
+        new TableNode.Header([
+          new TableNode.Header.Cell([]),
+          new TableNode.Header.Cell([new PlainTextNode('1')]),
+          new TableNode.Header.Cell([new PlainTextNode('0')])
+        ]), [
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('true')]),
+            new TableNode.Row.Cell([new PlainTextNode('false')]),
+          ], new TableNode.Header.Cell([new PlainTextNode('1')])),
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('false')]),
+            new TableNode.Row.Cell([new PlainTextNode('false')])
+          ], new TableNode.Header.Cell([new PlainTextNode('0')]))
+        ],
+        new TableNode.Caption([
+          new LinkNode([new PlainTextNode('AND operator logic')], 'https://google.com')
+        ]))
+
+    const documentNode =
+      new DocumentNode([chart], new DocumentNode.TableOfContents([chart]))
+
+    expect(Up.toHtml(documentNode)).to.be.eql(
+      '<nav class="up-table-of-contents">'
+      + '<h1>Table of Contents</h1>'
+      + '<ul>'
+      + '<li><a href="#up-part-1">AND operator logic</a></li>'
+      + '</ul>'
+      + '</nav>'
+      + '<table id="up-part-1">'
+      + '<caption><a href="https://google.com">AND operator logic</a></caption>'
+      + '<thead><tr><th scope="col"></th><th scope="col">1</th><th scope="col">0</th></tr></thead>'
+      + '<tr><th scope="row">1</th><td>true</td><td>false</td></tr>'
+      + '<tr><th scope="row">0</th><td>false</td><td>false</td></tr>'
+      + '</table>')
+  })
+})
+
+
+context("Even when a link is nested deep within another link, it doesn't produce an <a> element. This is true for", () => {
+  specify("regular links inside another link", () => {
     const documentNode = new DocumentNode([
       new ParagraphNode([
         new LinkNode([
@@ -29,7 +157,7 @@ context('Inside a link', () => {
       + '</p>')
   })
 
-  specify("a nested link does not produce another <a> element. The nested link's contents are included directly inside the outer link", () => {
+  specify("footnotes inside a link", () => {
     const documentNode = new DocumentNode([
       new ParagraphNode([
         new LinkNode([
@@ -46,11 +174,8 @@ context('Inside a link', () => {
       + '<a href="https://google.com"><em>Google is probably not Bing</em></a>'
       + '</p>')
   })
-})
 
-
-context('Links nested within table of contents entries do not produce <a> elements. This applies to links within:', () => {
-  specify('Headings', () => {
+  specify('the table of contents entry for a heading', () => {
     const heading =
       new HeadingNode([
         new EmphasisNode([
@@ -71,7 +196,7 @@ context('Links nested within table of contents entries do not produce <a> elemen
       + '<h1 id="up-part-1"><em><a href="https://google.com">I enjoy apples</a></em></h1>')
   })
 
-  specify('Tables', () => {
+  specify('the table of contents entry for a table', () => {
     const table =
       new TableNode(
         new TableNode.Header([
@@ -111,7 +236,7 @@ context('Links nested within table of contents entries do not produce <a> elemen
       + '</table>')
   })
 
-  specify('Charts', () => {
+  specify('the table of contents entry for a chart', () => {
     const chart =
       new TableNode(
         new TableNode.Header([
