193abf405642896fa4be352b754de91421126bcc
Daniel Miller <daniel@wants.email>
2016-08-07 16:10:37 -0500

    Add several passing HTML tests

--- src/Test/Html/Config/DocumentName.ts
+++ src/Test/Html/Config/DocumentName.ts
@@ -40,6 +40,21 @@ describe("A footnote reference's ID (as well as the ID of the footnote it points
       '<p><sup id="reply-11-footnote-reference-3" class="up-footnote-reference"><a href="#reply-11-footnote-3">3</a></sup></p>')
   })
 
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new FootnoteNode([], 3)
+      ])
+    ])
+
+    expect(up.toHtml(documentNode)).to.be.eql(
+      '<p><sup id="footnote-reference-3" class="up-footnote-reference"><a href="#footnote-3">3</a></sup></p>')
+  })
+
   it("is not prefixed with a document name if a blank name was provided", () => {
     const up = new Up({
       documentName: ' \t'
@@ -101,6 +116,32 @@ describe("A footnote's ID (as well as the ID of the footnote reference pointing 
       '<dl class="up-footnotes">'
       + '<dt id="reply-11-footnote-2"><a href="#reply-11-footnote-reference-2">2</a></dt><dd>Arwings</dd>'
       + '<dt id="reply-11-footnote-3"><a href="#reply-11-footnote-reference-3">3</a></dt><dd>Killer Bees</dd>'
+      + '</dl>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode =
+      new DocumentNode([
+        new FootnoteBlockNode([
+          new FootnoteNode([
+            new PlainTextNode("Arwings")
+          ], 2),
+          new FootnoteNode([
+            new PlainTextNode("Killer Bees")
+          ], 3)
+        ])
+      ])
+
+    const html =
+      '<dl class="up-footnotes">'
+      + '<dt id="footnote-2"><a href="#footnote-reference-2">2</a></dt><dd>Arwings</dd>'
+      + '<dt id="footnote-3"><a href="#footnote-reference-3">3</a></dt><dd>Killer Bees</dd>'
       + '</dl>'
 
     expect(up.toHtml(documentNode)).to.be.eql(html)
@@ -178,6 +219,29 @@ describe("The ID of an inline spoiler's checkbox (on both the checkbox and the l
     expect(up.toHtml(documentNode)).to.be.eql(html)
   })
 
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new InlineSpoilerNode([])
+      ])
+    ])
+
+    const html =
+      '<p>'
+      + '<span class="up-spoiler up-revealable">'
+      + '<label for="spoiler-1">toggle spoiler</label>'
+      + '<input id="spoiler-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+      + '</p>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
   it("is not prefixed with a document name if a blank name was provided", () => {
     const up = new Up({
       documentName: ' \t'
@@ -240,6 +304,29 @@ describe("The ID of an inline NSFW conventions's checkbox (on both the checkbox 
       + '<span class="up-nsfw up-revealable">'
       + '<label for="reply-11-nsfw-1">toggle NSFW</label>'
       + '<input id="reply-11-nsfw-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+      + '</p>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new InlineNsfwNode([])
+      ])
+    ])
+
+    const html =
+      '<p>'
+      + '<span class="up-nsfw up-revealable">'
+      + '<label for="nsfw-1">toggle NSFW</label>'
+      + '<input id="nsfw-1" type="checkbox">'
       + '<span></span>'
       + '</span>'
       + '</p>'
@@ -316,6 +403,29 @@ describe("The ID of an inline NSFL conventions's checkbox (on both the checkbox 
     expect(up.toHtml(documentNode)).to.be.eql(html)
   })
 
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode = new DocumentNode([
+      new ParagraphNode([
+        new InlineNsflNode([])
+      ])
+    ])
+
+    const html =
+      '<p>'
+      + '<span class="up-nsfl up-revealable">'
+      + '<label for="nsfl-1">toggle NSFL</label>'
+      + '<input id="nsfl-1" type="checkbox">'
+      + '<span></span>'
+      + '</span>'
+      + '</p>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
   it("is not prefixed with a document name if a blank name was provided", () => {
     const up = new Up({
       documentName: ' \t'
@@ -379,6 +489,25 @@ describe("The ID of a spoiler block's checkbox (on both the checkbox and the lab
     expect(up.toHtml(documentNode)).to.be.eql(html)
   })
 
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode = new DocumentNode([
+      new SpoilerBlockNode([])
+    ])
+
+    const html =
+      '<div class="up-spoiler up-revealable">'
+      + '<label for="spoiler-1">toggle spoiler</label>'
+      + '<input id="spoiler-1" type="checkbox">'
+      + '<div></div>'
+      + '</div>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
   it("is not prefixed with a document name if a blank name was provided", () => {
     const up = new Up({
       documentName: ' \t'
@@ -436,6 +565,25 @@ describe("The ID of an NSFW block's checkbox (on both the checkbox and the label
     expect(up.toHtml(documentNode)).to.be.eql(html)
   })
 
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode = new DocumentNode([
+      new NsfwBlockNode([])
+    ])
+
+    const html =
+      '<div class="up-nsfw up-revealable">'
+      + '<label for="nsfw-1">toggle NSFW</label>'
+      + '<input id="nsfw-1" type="checkbox">'
+      + '<div></div>'
+      + '</div>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
   it("is not prefixed with a document name if a blank name was provided", () => {
     const up = new Up({
       documentName: ' \t'
@@ -487,6 +635,25 @@ describe("The ID of an NSFL block's checkbox (on both the checkbox and the label
       '<div class="up-nsfl up-revealable">'
       + '<label for="reply-11-nsfl-1">toggle NSFL</label>'
       + '<input id="reply-11-nsfl-1" type="checkbox">'
+      + '<div></div>'
+      + '</div>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const documentNode = new DocumentNode([
+      new NsflBlockNode([])
+    ])
+
+    const html =
+      '<div class="up-nsfl up-revealable">'
+      + '<label for="nsfl-1">toggle NSFL</label>'
+      + '<input id="nsfl-1" type="checkbox">'
       + '<div></div>'
       + '</div>'
 
@@ -553,6 +720,28 @@ describe("The ID of an element referenced by the table of contents", () => {
       + '</ul>'
       + '</nav>'
       + '<h1 id="reply-11-part-1"></h1>'
+
+    expect(up.toHtml(documentNode)).to.be.eql(html)
+  })
+
+  it("is not prefixed with a document name if an empty name was provided", () => {
+    const up = new Up({
+      documentName: ''
+    })
+
+    const heading = new HeadingNode([], 1)
+
+    const documentNode =
+      new DocumentNode([heading], new DocumentNode.TableOfContents([heading]))
+
+    const html =
+      '<nav class="up-table-of-contents">'
+      + '<h1>Table of Contents</h1>'
+      + '<ul>'
+      + '<li><h2><a href="#part-1"></a></h2></li>'
+      + '</ul>'
+      + '</nav>'
+      + '<h1 id="part-1"></h1>'
 
     expect(up.toHtml(documentNode)).to.be.eql(html)
   })
