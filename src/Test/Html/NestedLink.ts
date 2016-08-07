5f048c9c591676c9e2362ebf434bf269c2a1e394
Daniel Miller <daniel@wants.email>
2016-08-06 22:44:36 -0500

    Add 2 passing table of contents HTML tests

--- src/Test/Html/NestedLink.ts
+++ src/Test/Html/NestedLink.ts
@@ -6,6 +6,7 @@ import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
 import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
 import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
 import { HeadingNode } from '../../SyntaxNodes/HeadingNode'
+import { TableNode } from '../../SyntaxNodes/TableNode'
 
 
 context('Inside a link', () => {
@@ -40,7 +41,9 @@ context('Inside a link', () => {
 context('Links nested within table of contents entries do not produce anchor elements. This applies to links within:', () => {
   specify('Headings', () => {
     const heading =
-      new HeadingNode([new LinkNode([new PlainTextNode('I enjoy apples')], 'https://google.com')], 1)
+      new HeadingNode([
+        new LinkNode([new PlainTextNode('I enjoy apples')], 'https://google.com')
+      ], 1)
 
     const documentNode =
       new DocumentNode([heading], new DocumentNode.TableOfContents([heading]))
@@ -53,5 +56,82 @@ context('Links nested within table of contents entries do not produce anchor ele
       + '</ul>'
       + '</nav>'
       + '<h1 id="up-part-1"><a href="https://google.com">I enjoy apples</a></h1>')
+  })
+
+  specify('Tables', () => {
+    const table =
+      new TableNode(
+        new TableNode.Header([
+          new TableNode.Header.Cell([new PlainTextNode('Game')]),
+          new TableNode.Header.Cell([new PlainTextNode('Developer')])
+        ]), [
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('Final Fantasy')]),
+            new TableNode.Row.Cell([new PlainTextNode('Square')])
+          ]),
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('Super Mario Kart')]),
+            new TableNode.Row.Cell([new PlainTextNode('Nintendo')])
+          ])
+        ],
+        new TableNode.Caption([
+          new LinkNode([new PlainTextNode('Influential games')], 'https://google.com')
+        ]))
+
+    const documentNode =
+      new DocumentNode([table], new DocumentNode.TableOfContents([table]))
+
+    expect(Up.toHtml(documentNode)).to.be.eql(
+      '<nav class="up-table-of-contents">'
+      + '<h1>Table of Contents</h1>'
+      + '<ul>'
+      + '<li><a href="#up-part-1">Influential games</a></li>'
+      + '</ul>'
+      + '</nav>'
+      + '<table id="up-part-1">'
+      + '<caption><a href="https://google.com">Influential games</a></caption>'
+      + '<thead><tr><th scope="col">Game</th><th scope="col">Developer</th></tr></thead>'
+      + '<tr><td>Final Fantasy</td><td>Square</td></tr>'
+      + '<tr><td>Super Mario Kart</td><td>Nintendo</td></tr>'
+      + '</table>')
+  })
+
+  specify('Charts', () => {
+    const chart =
+      new TableNode(
+        new TableNode.Header([
+          new TableNode.Header.Cell([]),
+          new TableNode.Header.Cell([new PlainTextNode('1')]),
+          new TableNode.Header.Cell([new PlainTextNode('0')])
+        ]), [
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('true')]),
+            new TableNode.Row.Cell([new PlainTextNode('false')]),
+          ], new TableNode.Header.Cell([new PlainTextNode('1')])),
+          new TableNode.Row([
+            new TableNode.Row.Cell([new PlainTextNode('false')]),
+            new TableNode.Row.Cell([new PlainTextNode('false')])
+          ], new TableNode.Header.Cell([new PlainTextNode('0')]))
+        ],
+        new TableNode.Caption([
+          new LinkNode([new PlainTextNode('AND operator logic')], 'https://google.com')
+        ]))
+
+    const documentNode =
+      new DocumentNode([chart], new DocumentNode.TableOfContents([chart]))
+
+    expect(Up.toHtml(documentNode)).to.be.eql(
+      '<nav class="up-table-of-contents">'
+      + '<h1>Table of Contents</h1>'
+      + '<ul>'
+      + '<li><a href="#up-part-1">AND operator logic</a></li>'
+      + '</ul>'
+      + '</nav>'
+      + '<table id="up-part-1">'
+      + '<caption><a href="https://google.com">AND operator logic</a></caption>'
+      + '<thead><tr><th scope="col"></th><th scope="col">1</th><th scope="col">0</th></tr></thead>'
+      + '<tr><th scope="row">1</th><td>true</td><td>false</td></tr>'
+      + '<tr><th scope="row">0</th><td>false</td><td>false</td></tr>'
+      + '</table>')
   })
 })
