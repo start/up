f2eed9b53f9638b1d4b845c6b5ecccf48a8e6e7c
Daniel Miller <daniel@wants.email>
2016-08-08 03:39:30 -0500

    Add 12 passing HTML tests

---
+++ src/Test/Html/Config/WriteUnsafeContent.ts
@@ -0,0 +1,143 @@
+import { expect } from 'chai'
+import Up from '../../../index'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { ImageNode } from '../../../SyntaxNodes/ImageNode'
+import { AudioNode } from '../../../SyntaxNodes/AudioNode'
+import { VideoNode } from '../../../SyntaxNodes/VideoNode'
+
+
+context('When the "writeUnsafeContent" config setting is set to true, links/media with dangerous URL schemes produce their regular HTML elements.', () => {
+  const up = new Up({
+    writeUnsafeContent: true
+  })
+
+  context('Images produce HTML even if their scheme is:', () => {
+    specify('javascript', () => {
+      const documentNode = new DocumentNode([
+        new ImageNode('Uh-oh!', 'javascript:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<img src="javascript:malicious" alt="Uh-oh!" title="Uh-oh!">')
+    })
+
+    specify('data', () => {
+      const documentNode = new DocumentNode([
+        new ImageNode('Uh-oh!', 'data:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<img src="data:malicious" alt="Uh-oh!" title="Uh-oh!">')
+    })
+
+    specify('file', () => {
+      const documentNode = new DocumentNode([
+        new ImageNode('Uh-oh!', 'file:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<img src="file:malicious" alt="Uh-oh!" title="Uh-oh!">')
+    })
+
+    specify('vbscript', () => {
+      const documentNode = new DocumentNode([
+        new ImageNode('Uh-oh!', 'vbscript:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<img src="vbscript:malicious" alt="Uh-oh!" title="Uh-oh!">')
+    })
+  })
+
+  context('Audio conventions produce HTML even if their scheme is:', () => {
+    specify('javascript', () => {
+      const documentNode = new DocumentNode([
+        new AudioNode('Uh-oh!', 'javascript:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<audio src="javascript:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="javascript:malicious">Uh-oh!</a>'
+        + '</audio>')
+    })
+
+    specify('data', () => {
+      const documentNode = new DocumentNode([
+        new AudioNode('Uh-oh!', 'data:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<audio src="data:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="data:malicious">Uh-oh!</a>'
+        + '</audio>')
+    })
+
+    specify('file', () => {
+      const documentNode = new DocumentNode([
+        new AudioNode('Uh-oh!', 'file:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<audio src="file:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="file:malicious">Uh-oh!</a>'
+        + '</audio>')
+    })
+
+    specify('vbscript', () => {
+      const documentNode = new DocumentNode([
+        new AudioNode('Uh-oh!', 'vbscript:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<audio src="vbscript:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="vbscript:malicious">Uh-oh!</a>'
+        + '</audio>')
+    })
+  })
+
+  context('Video conventions produce HTML even if their scheme is:', () => {
+    specify('javascript', () => {
+      const documentNode = new DocumentNode([
+        new VideoNode('Uh-oh!', 'javascript:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<video src="javascript:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="javascript:malicious">Uh-oh!</a>'
+        + '</video>')
+    })
+
+    specify('data', () => {
+      const documentNode = new DocumentNode([
+        new VideoNode('Uh-oh!', 'data:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<video src="data:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="data:malicious">Uh-oh!</a>'
+        + '</video>')
+    })
+
+    specify('file', () => {
+      const documentNode = new DocumentNode([
+        new VideoNode('Uh-oh!', 'file:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<video src="file:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="file:malicious">Uh-oh!</a>'
+        + '</video>')
+    })
+
+    specify('vbscript', () => {
+      const documentNode = new DocumentNode([
+        new VideoNode('Uh-oh!', 'vbscript:malicious')
+      ])
+
+      expect(up.toHtml(documentNode)).to.be.eql(
+        '<video src="vbscript:malicious" title="Uh-oh!" controls loop>'
+        + '<a href="vbscript:malicious">Uh-oh!</a>'
+        + '</video>')
+    })
+  })
+})
