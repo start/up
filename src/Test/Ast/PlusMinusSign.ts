70ceb5a4f0da64ed706bd32dd19bafd0fa1a92ac
Daniel Miller <daniel@wants.email>
2016-08-09 14:13:23 -0500

    Add more plus-minus tests

--- src/Test/Ast/PlusMinusSign.ts
+++ src/Test/Ast/PlusMinusSign.ts
@@ -1,7 +1,14 @@
 import { expect } from 'chai'
 import Up from '../../index'
 import { insideDocumentAndParagraph } from './Helpers'
+import { DocumentNode } from '../../SyntaxNodes/DocumentNode'
+import { ImageNode } from '../../SyntaxNodes/ImageNode'
+import { VideoNode } from '../../SyntaxNodes/VideoNode'
+import { CodeBlockNode } from '../../SyntaxNodes/CodeBlockNode'
 import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { LinkNode } from '../../SyntaxNodes/LinkNode'
+import { InlineSpoilerNode } from '../../SyntaxNodes/InlineSpoilerNode'
+import { InlineCodeNode } from '../../SyntaxNodes/InlineCodeNode'
 
 
 context('A plus symbol followed by a hyphen normally produces a plus-minus sign', () => {
@@ -33,5 +40,81 @@ context('A plus symbol followed by a hyphen normally produces a plus-minus sign'
           new PlainTextNode("Well, ± a million.")
         ]))
     })
+  })
+
+
+  context('This does not apply within:', () => {
+    specify('Link URLs', () => {
+      expect(Up.toAst("[American flag emoji] (https://example.com/empojis/US+-flag?info)")).to.be.eql(
+        insideDocumentAndParagraph([
+          new LinkNode([
+            new PlainTextNode("American flag emoji")
+          ], 'https://example.com/empojis/US+-flag?info')
+        ]))
+    })
+
+    specify('Media URLs', () => {
+      expect(Up.toAst('[video: ghosts eating luggage] (http://example.com/polter+-geists.webm)')).to.be.eql(
+        new DocumentNode([
+          new VideoNode('ghosts eating luggage', 'http://example.com/polter+-geists.webm')
+        ]))
+    })
+
+    specify('Linkified media URLs', () => {
+      expect(Up.toAst('[image: you fight Gary] (https://example.com/fight.svg) (http://example.com/final+-battle)')).to.be.eql(
+        new DocumentNode([
+          new LinkNode([
+            new ImageNode('you fight Gary', 'https://example.com/fight.svg')
+          ], 'http://example.com/final+-battle')
+        ]))
+    })
+
+    specify('Linkified URLs for non-media conventions', () => {
+      expect(Up.toAst('[SPOILER: you fight Gary] (http://example.com/final+-battle)')).to.be.eql(
+        insideDocumentAndParagraph([
+          new InlineSpoilerNode([
+            new LinkNode([
+              new PlainTextNode('you fight Gary')
+            ], 'http://example.com/final+-battle')
+          ])
+        ]))
+    })
+
+    specify('Inline code', () => {
+      expect(Up.toAst("`x+-y`")).to.be.eql(
+        insideDocumentAndParagraph([
+          new InlineCodeNode('x+-y')
+        ]))
+    })
+
+    specify('Code blocks', () => {
+        const markup = `
+\`\`\`
+for (let i = items.length - 1; i >= 0; i = i+-1) { }
+\`\`\``
+
+      expect(Up.toAst(markup)).to.be.eql(
+        new DocumentNode([
+          new CodeBlockNode(
+            `for (let i = items.length - 1; i >= 0; i = i+-1) { }`)
+        ]))
+    })
+  })
+})
+
+
+describe('When either of the hyphens are escaped, no en dash is produced:', () => {
+  specify('First dash:', () => {
+    expect(Up.toAst("Okay\\--I'll eat the tarantula.")).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode("Okay--I'll eat the tarantula.")
+      ]))
+  })
+
+  specify('Second hyphen:', () => {
+    expect(Up.toAst("Okay-\\-I'll eat the tarantula.")).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode("Okay--I'll eat the tarantula.")
+      ]))
   })
 })
