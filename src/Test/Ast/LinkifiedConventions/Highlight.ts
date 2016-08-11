956de74e9ee3180e3ba7022ee41fb11c0dac1421
Daniel Miller <daniel@wants.email>
2016-08-11 03:14:20 -0500

    Add several highlight tests, 3 failing

---
+++ src/Test/Ast/LinkifiedConventions/Highlight.ts
@@ -0,0 +1,182 @@
+import { expect } from 'chai'
+import Up from '../../../index'
+import { insideDocumentAndParagraph, expectEveryPermutationOfBracketsAroundContentAndUrl } from '../Helpers'
+import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
+import { ParagraphNode } from '../../../SyntaxNodes/ParagraphNode'
+import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
+import { LinkNode } from '../../../SyntaxNodes/LinkNode'
+import { HighlightNode } from '../../../SyntaxNodes/HighlightNode'
+import { InlineNsfwNode } from '../../../SyntaxNodes/InlineNsfwNode'
+import { InlineNsflNode } from '../../../SyntaxNodes/InlineNsflNode'
+import { AudioNode } from '../../../SyntaxNodes/AudioNode'
+import { ParenthesizedNode } from '../../../SyntaxNodes/ParenthesizedNode'
+import { FootnoteNode } from '../../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../../SyntaxNodes/FootnoteBlockNode'
+
+
+describe('A highlight followed immediately by a parenthesized/bracketd URL', () => {
+  it('produces a highlight node whose contents are put inside a link pointing to that URL', () => {
+    expect(Up.toAst('After you beat the Elite Four, [highlight: you fight Gary](http://example.com/finalbattle).')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new HighlightNode([
+          new LinkNode([
+            new PlainTextNode('you fight Gary')
+          ], 'http://example.com/finalbattle')
+        ]),
+        new PlainTextNode('.')
+      ]))
+  })
+})
+
+
+describe('Any highlight followed immediately by a parenthesized/bracketed URL', () => {
+  it('produces a highlight node whose contents are put inside a link pointing to that URL. The type of bracket surrounding the highlight can be different from the type of bracket surrounding the URL', () => {
+    expectEveryPermutationOfBracketsAroundContentAndUrl({
+      content: 'highlight: you fight Gary',
+      url: 'http://example.com/finalbattle',
+      toProduce: insideDocumentAndParagraph([
+        new HighlightNode([
+          new LinkNode([
+            new PlainTextNode('you fight Gary')
+          ], 'http://example.com/finalbattle')
+        ]),
+      ])
+    })
+  })
+})
+
+
+describe('A highlight directly followed by another highlight', () => {
+  it('is not linkified', () => {
+    expect(Up.toAst('After you beat the Elite Four, [highlight: you fight Gary][highlight: and win].')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new HighlightNode([
+          new PlainTextNode('you fight Gary')
+        ]),
+        new HighlightNode([
+          new PlainTextNode('and win')
+        ]),
+        new PlainTextNode('.')
+      ]))
+  })
+})
+
+
+describe('A highlight directly followed by an inline NSFW convention', () => {
+  it('is not linkified', () => {
+    expect(Up.toAst('After you beat the Elite Four, [highlight: you fight Gary][NSFW: and win].')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new HighlightNode([
+          new PlainTextNode('you fight Gary')
+        ]),
+        new InlineNsfwNode([
+          new PlainTextNode('and win')
+        ]),
+        new PlainTextNode('.')
+      ]))
+  })
+})
+
+
+describe('A highlight directly followed by an inline NSFL convention', () => {
+  it('is not linkified', () => {
+    expect(Up.toAst('After you beat the Elite Four, [highlight: you fight Gary][NSFL: and win].')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new HighlightNode([
+          new PlainTextNode('you fight Gary')
+        ]),
+        new InlineNsflNode([
+          new PlainTextNode('and win')
+        ]),
+        new PlainTextNode('.')
+      ]))
+  })
+})
+
+
+describe('A highlight directly followed by a media convention', () => {
+  it('is not linkified', () => {
+    expect(Up.toAst('After you beat the Elite Four, [highlight: you fight Gary][audio: final battle theme](https://example.com/songs/123.ogg)')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('After you beat the Elite Four, '),
+        new HighlightNode([
+          new PlainTextNode('you fight Gary')
+        ]),
+        new AudioNode('final battle theme', 'https://example.com/songs/123.ogg'),
+      ]))
+  })
+})
+
+
+describe('A highlight directly followed by a footnote', () => {
+  it("is not linkified", () => {
+    const markup = "After you beat the Elite Four, [highlight: you fight Gary](^Or whatever you name him.)"
+
+    const footnotes = [
+      new FootnoteNode([
+        new PlainTextNode('Or whatever you name him.')
+      ], 1)
+    ]
+
+    expect(Up.toAst(markup)).to.be.eql(
+      new DocumentNode([
+        new ParagraphNode([
+          new PlainTextNode("After you beat the Elite Four, "),
+          new HighlightNode([
+            new PlainTextNode('you fight Gary')
+          ]),
+          footnotes[0],
+        ]),
+        new FootnoteBlockNode(footnotes)
+      ]))
+  })
+})
+
+
+describe('An otherwise-valid linkified highlight with its URL escaped', () => {
+  it('is not linkified', () => {
+    expect(Up.toAst('[highlight: he called her](\\tel:5555555555)')).to.be.eql(
+      insideDocumentAndParagraph([
+        new HighlightNode([
+          new PlainTextNode('he called her')
+        ]),
+        new ParenthesizedNode([
+          new PlainTextNode('(tel:5555555555)')
+        ]),
+      ]))
+  })
+})
+
+
+context("When an otherwise-valid linkified highlight's URL starts with whitespace, and the first character in the actual URL is escaped,", () => {
+  specify('the highlight convention is not linkified', () => {
+    expect(Up.toAst('[highlight: he called her]( \t \\tel:5555555555)')).to.be.eql(
+      insideDocumentAndParagraph([
+        new HighlightNode([
+          new PlainTextNode('he called her')
+        ]),
+        new PlainTextNode('( \t tel:5555555555)')
+      ]))
+  })
+})
+
+
+context("If there's no whitespace between a highlight and its bracketed URL", () => {
+  specify("the URL can start with whitespace", () => {
+    expectEveryPermutationOfBracketsAroundContentAndUrl({
+      content: 'highlight: you fight Gary',
+      url: ' \t \thttp://example.com/finalbattle',
+      toProduce: insideDocumentAndParagraph([
+        new HighlightNode([
+          new LinkNode([
+            new PlainTextNode('you fight Gary')
+          ], 'http://example.com/finalbattle')
+        ]),
+      ])
+    })
+  })
+})
