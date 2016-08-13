a63cbc1bf447c145acf96a780c6fc7b078576af7
Daniel Miller <daniel@wants.email>
2016-08-13 17:03:00 -0500

    Make underscores indicate italic/bold; fail tests

--- src/Parsing/Inline/Tokenization/tokenize.ts
+++ src/Parsing/Inline/Tokenization/tokenize.ts
@@ -1,4 +1,4 @@
-import { EMPHASIS_CONVENTION, STRESS_CONVENTION, REVISION_DELETION_CONVENTION, REVISION_INSERTION_CONVENTION, HIGHLIGHT_CONVENTION, SPOILER_CONVENTION, NSFW_CONVENTION, NSFL_CONVENTION, FOOTNOTE_CONVENTION, LINK_CONVENTION, PARENTHESIZED_CONVENTION, SQUARE_BRACKETED_CONVENTION, ACTION_CONVENTION } from '../RichConventions'
+import { EMPHASIS_CONVENTION, STRESS_CONVENTION, ITALIC_CONVENTION, BOLD_CONVENTION, REVISION_DELETION_CONVENTION, REVISION_INSERTION_CONVENTION, HIGHLIGHT_CONVENTION, SPOILER_CONVENTION, NSFW_CONVENTION, NSFL_CONVENTION, FOOTNOTE_CONVENTION, LINK_CONVENTION, PARENTHESIZED_CONVENTION, SQUARE_BRACKETED_CONVENTION, ACTION_CONVENTION } from '../RichConventions'
 import { escapeForRegex, patternStartingWith, solely, everyOptional, either, optional, atLeastOne, atLeast, followedBy, notFollowedBy, anyCharMatching, anyCharNotMatching, capture } from '../../PatternHelpers'
 import { SOME_WHITESPACE, ANY_WHITESPACE, WHITESPACE_CHAR, LETTER_CLASS, DIGIT } from '../../PatternPieces'
 import { NON_BLANK_PATTERN } from '../../Patterns'
@@ -112,8 +112,8 @@ class Tokenizer {
       conventionForMajorInflection: STRESS_CONVENTION
     }, {
       delimiterChar: '_',
-      conventionForMinorInflection: EMPHASIS_CONVENTION,
-      conventionForMajorInflection: STRESS_CONVENTION
+      conventionForMinorInflection: ITALIC_CONVENTION,
+      conventionForMajorInflection: BOLD_CONVENTION
     }
   ].map(args => this.getInflectionHandler(args))
 
