45576745beb14f6b1342decfe204d8d3256247b9
Daniel Miller <daniel@wants.email>
2016-08-13 00:30:57 -0500

    Add failing test

--- src/Test/Ast/Config/Table.ts
+++ src/Test/Ast/Config/Table.ts
@@ -37,6 +37,54 @@ Chrono Cross;     1999`
       ]))
   })
 
+  it('can contain multiple variations', () => {
+    const markup = `
+Info:
+
+Game;             Release Date
+Chrono Trigger;   1995
+Chrono Cross;     1999
+
+
+Data:
+
+Game;             Release Date
+Chrono Trigger;   1995
+Chrono Cross;     1999`
+
+    expect(up.toAst(markup, { terms: { table: ['data', 'info'] } })).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Cross')]),
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ])
+          ]),
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([new PlainTextNode('Game')]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Trigger')]),
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ]),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('Chrono Cross')]),
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ])
+          ])
+      ]))
+  })
+
   it('is case-insensitive even when custom', () => {
     const uppercase = `
 Data:
