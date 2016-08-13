5a93d56a321317e77fcde6199035b41bb960e465
Daniel Miller <daniel@wants.email>
2016-08-13 17:51:07 -0500

    Rename/update test files to reflect new rules

--- src/Test/Ast/EdgeCases/Italics.ts
+++ src/Test/Ast/EdgeCases/Italics.ts
@@ -2,21 +2,21 @@ import { expect } from 'chai'
 import Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
 import { PlainTextNode } from '../../../SyntaxNodes/PlainTextNode'
-import { EmphasisNode } from '../../../SyntaxNodes/EmphasisNode'
+import { ItalicNode } from '../../../SyntaxNodes/ItalicNode'
 
 
 describe('An unmatched underscore', () => {
-  it('does not create an emphasis node', () => {
+  it('does not create an italic node', () => {
     expect(Up.toAst('Hello, _world!')).to.be.eql(
       insideDocumentAndParagraph([
         new PlainTextNode('Hello, _world!')
       ]))
   })
 
-  it('does not create an emphasis node, even when following 2 matching underscores', () => {
+  it('does not create an italic node, even when following 2 matching underscores', () => {
     expect(Up.toAst('_Hello_, _world!')).to.be.eql(
       insideDocumentAndParagraph([
-        new EmphasisNode([
+        new ItalicNode([
           new PlainTextNode('Hello'),
         ]),
         new PlainTextNode(', _world!')
@@ -36,7 +36,7 @@ describe('Matching single underscores each surrounded by whitespace', () => {
 
 
 describe('An underscore followed by whitespace with a matching underscore touching the end of a word', () => {
-  it('does not produce an emphasis node and is preserved as plain text', () => {
+  it('does not produce an italic node and is preserved as plain text', () => {
     expect(Up.toAst('I believe_ my spelling_ was wrong.')).to.be.eql(
       insideDocumentAndParagraph([
         new PlainTextNode('I believe_ my spelling_ was wrong.')
@@ -46,7 +46,7 @@ describe('An underscore followed by whitespace with a matching underscore touchi
 
 
 describe('An underscore touching the beginning of a word with a matching underscore preceded by whitespace', () => {
-  it('does not produce an emphasis node and is preserved as plain text', () => {
+  it('does not produce an italic node and is preserved as plain text', () => {
     expect(Up.toAst('I _believe my _spelling was wrong.')).to.be.eql(
       insideDocumentAndParagraph([
         new PlainTextNode('I _believe my _spelling was wrong.')
