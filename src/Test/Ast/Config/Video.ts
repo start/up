b26fd42cb1137dd8e87e53880defd5f1cb418d97
Daniel Miller <daniel@wants.email>
2016-08-12 23:48:09 -0500

    Add passing config test

--- src/Test/Ast/Config/Video.ts
+++ src/Test/Ast/Config/Video.ts
@@ -5,20 +5,30 @@ import { DocumentNode } from '../../../SyntaxNodes/DocumentNode'
 
 
 describe('The term that represents video conventions', () => {
-  const up = new Up({
-    terms: { video: 'watch' }
-  })
-
   it('comes from the "video" config term', () => {
     const markup = '[watch: Nevada caucus footage][https://example.com/video.webm]'
 
-    expect(up.toAst(markup)).to.be.eql(
+    expect(Up.toAst(markup, { terms: { video: 'watch' } })).to.be.eql(
       new DocumentNode([
         new VideoNode('Nevada caucus footage', 'https://example.com/video.webm')
       ]))
   })
 
+  it('can contain multiple variations', () => {
+    const markup = '[watch: Nevada caucus footage](https://example.com/video.webm) [view: Nevada caucus footage](https://example.com/video.webm)'
+
+    expect(Up.toAst(markup, { terms: { video: ['watch', 'view'] } })).to.be.eql(
+      new DocumentNode([
+        new VideoNode('Nevada caucus footage', 'https://example.com/video.webm'),
+        new VideoNode('Nevada caucus footage', 'https://example.com/video.webm')
+      ]))
+  })
+
   it('is case-insensitive even when custom', () => {
+    const up = new Up({
+      terms: { video: 'watch' }
+    })
+
     const lowercase = '[watch: Nevada caucus footage][https://example.com/video.webm]'
     const mixedCase = '[WaTCH: Nevada caucus footage][https://example.com/video.webm]'
 
