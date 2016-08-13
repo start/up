8063aa89ee6721589a23362d6357a6406f27cc5c
Daniel Miller <daniel@wants.email>
2016-08-13 02:20:12 -0500

    Add 2 passing tests

--- src/Test/Ast/Config/Chart.ts
+++ src/Test/Ast/Config/Chart.ts
@@ -52,4 +52,54 @@ Chrono Cross;     1999`
 
     expect(up.toAst(uppercase)).to.be.eql(up.toAst(mixedCase))
   })
+
+  it('is trimmed', () => {
+    const markup = `
+Data:
+
+                  Release Date
+Chrono Trigger;   1995
+Chrono Cross;     1999`
+
+    expect(Up.toAst(markup, { terms: { chart: ' \t data \t ' } })).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ], new TableNode.Header.Cell([new PlainTextNode('Chrono Trigger')])),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ], new TableNode.Header.Cell([new PlainTextNode('Chrono Cross')]))
+          ])
+      ]))
+  })
+
+  it('ignores inline conventions and regular expression rules', () => {
+    const markup = `
+*Data*:
+
+                  Release Date
+Chrono Trigger;   1995
+Chrono Cross;     1999`
+
+    expect(Up.toAst(markup, { terms: { chart: '*data*' } })).to.be.eql(
+      new DocumentNode([
+        new TableNode(
+          new TableNode.Header([
+            new TableNode.Header.Cell([]),
+            new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+          ]), [
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('1995')])
+            ], new TableNode.Header.Cell([new PlainTextNode('Chrono Trigger')])),
+            new TableNode.Row([
+              new TableNode.Row.Cell([new PlainTextNode('1999')])
+            ], new TableNode.Header.Cell([new PlainTextNode('Chrono Cross')]))
+          ])
+      ]))
+  })
 })
