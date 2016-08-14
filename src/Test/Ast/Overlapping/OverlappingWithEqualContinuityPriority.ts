693aaa715af8489baa86e175b3a591149c5f1afe
Daniel Miller <daniel@wants.email>
2016-08-13 20:43:50 -0500

    Add 4 passing tests

--- src/Test/Ast/Overlapping/OverlappingWithEqualContinuityPriority.ts
+++ src/Test/Ast/Overlapping/OverlappingWithEqualContinuityPriority.ts
@@ -4,6 +4,8 @@ import { insideDocumentAndParagraph } from'.././Helpers'
 import { PlainTextNode } from'../../../SyntaxNodes/PlainTextNode'
 import { EmphasisNode } from'../../../SyntaxNodes/EmphasisNode'
 import { StressNode } from'../../../SyntaxNodes/StressNode'
+import { ItalicNode } from'../../../SyntaxNodes/ItalicNode'
+import { BoldNode } from'../../../SyntaxNodes/BoldNode'
 import { RevisionDeletionNode } from'../../../SyntaxNodes/RevisionDeletionNode'
 import { RevisionInsertionNode } from'../../../SyntaxNodes/RevisionInsertionNode'
 import { ParenthesizedNode } from'../../../SyntaxNodes/ParenthesizedNode'
@@ -85,6 +87,86 @@ describe('Overlapped stressed and emphasized text', () => {
           ])
         ]),
         new EmphasisNode([
+          new PlainTextNode(' whole')
+        ]),
+        new PlainTextNode(' milk.')
+      ]))
+  })
+})
+
+
+describe('Overlapped italicized and emphasized text', () => {
+  it('split the emphasis node because it opened second', () => {
+    expect(Up.toAst('I _love *drinking_ whole* milk.')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('I '),
+        new ItalicNode([
+          new PlainTextNode('love '),
+          new EmphasisNode([
+            new PlainTextNode('drinking')
+          ])
+        ]),
+        new EmphasisNode([
+          new PlainTextNode(' whole')
+        ]),
+        new PlainTextNode(' milk.')
+      ]))
+  })
+})
+
+
+describe('Overlapped emphasized and italicized text', () => {
+  it('split the italic node because it opened second', () => {
+    expect(Up.toAst('I *love _drinking* whole_ milk.')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('I '),
+        new EmphasisNode([
+          new PlainTextNode('love '),
+          new ItalicNode([
+            new PlainTextNode('drinking')
+          ])
+        ]),
+        new ItalicNode([
+          new PlainTextNode(' whole')
+        ]),
+        new PlainTextNode(' milk.')
+      ]))
+  })
+})
+
+
+describe('Overlapped bold and stressed text', () => {
+  it('split the stress node because it opened second', () => {
+    expect(Up.toAst('I __love **drinking__ whole** milk.')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('I '),
+        new BoldNode([
+          new PlainTextNode('love '),
+          new StressNode([
+            new PlainTextNode('drinking')
+          ])
+        ]),
+        new StressNode([
+          new PlainTextNode(' whole')
+        ]),
+        new PlainTextNode(' milk.')
+      ]))
+  })
+})
+
+
+describe('Overlapped stressed and bold text', () => {
+  it('split the bold node because it opened second', () => {
+    expect(Up.toAst('I **love __drinking** whole__ milk.')).to.be.eql(
+      insideDocumentAndParagraph([
+        new PlainTextNode('I '),
+        new StressNode([
+          new PlainTextNode('love '),
+          new BoldNode([
+            new PlainTextNode('drinking')
+          ])
+        ]),
+        new BoldNode([
           new PlainTextNode(' whole')
         ]),
         new PlainTextNode(' milk.')
