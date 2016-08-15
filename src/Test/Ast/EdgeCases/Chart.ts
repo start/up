e21be88191e3157c195493064363acd272663d39
Daniel Miller <daniel@wants.email>
2016-08-14 20:25:10 -0500

    Add 3 passing chart tests

--- src/Test/Ast/EdgeCases/Chart.ts
+++ src/Test/Ast/EdgeCases/Chart.ts
@@ -251,4 +251,91 @@ Square\s Chrono Cross;        Playstation;      1990\`s`
         ]))
     })
   })
+
+
+  context('Delimiters for example input do not interfere with', () => {
+    specify('Header cells', () => {
+      const markup = `
+Chart:
+
+                  {: Platform;          Release Date :}
+Chrono Trigger;   Super Nintendo;       1995
+Chrono Cross;     Playstation;          1999`
+
+      expect(Up.toAst(markup)).to.be.eql(
+        new DocumentNode([
+          new TableNode(
+            new TableNode.Header([
+              new TableNode.Header.Cell([]),
+              new TableNode.Header.Cell([new PlainTextNode('{: Platform')]),
+              new TableNode.Header.Cell([new PlainTextNode('Release Date :}')])
+            ]), [
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+                new TableNode.Row.Cell([new PlainTextNode('1995')])
+              ], new TableNode.Header.Cell([new PlainTextNode('Chrono Trigger')])),
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('Playstation')]),
+                new TableNode.Row.Cell([new PlainTextNode('1999')])
+              ], new TableNode.Header.Cell([new PlainTextNode('Chrono Cross')]))
+            ])
+        ]))
+    })
+
+    specify('Row cells', () => {
+      const markup = `
+Chart:
+
+                  Platform;           Release Date
+Chrono Trigger;   Super Nintendo;     1995
+Chrono Cross;     {: Playstation;     1999 :}`
+
+      expect(Up.toAst(markup)).to.be.eql(
+        new DocumentNode([
+          new TableNode(
+            new TableNode.Header([
+              new TableNode.Header.Cell([]),
+              new TableNode.Header.Cell([new PlainTextNode('Platform')]),
+              new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+            ]), [
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('Super Nintendo')]),
+                new TableNode.Row.Cell([new PlainTextNode('1995')])
+              ], new TableNode.Header.Cell([new PlainTextNode('Chrono Trigger')])),
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('{: Playstation')]),
+                new TableNode.Row.Cell([new PlainTextNode('1999 :}')])
+              ], new TableNode.Header.Cell([new PlainTextNode('Chrono Cross')]))
+            ])
+        ]))
+    })
+
+    specify('Row header cells', () => {
+      const markup = `
+Chart:
+
+                      Platform;            Release Date
+{: Chrono Trigger;    Super :} Nintendo;   1995
+Chrono Cross;         Playstation;         1999`
+
+      expect(Up.toAst(markup)).to.be.eql(
+        new DocumentNode([
+          new TableNode(
+            new TableNode.Header([
+              new TableNode.Header.Cell([]),
+              new TableNode.Header.Cell([new PlainTextNode('Platform')]),
+              new TableNode.Header.Cell([new PlainTextNode('Release Date')])
+            ]), [
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('Super :} Nintendo')]),
+                new TableNode.Row.Cell([new PlainTextNode('1995')])
+              ], new TableNode.Header.Cell([new PlainTextNode('{: Chrono Trigger')])),
+              new TableNode.Row([
+                new TableNode.Row.Cell([new PlainTextNode('Playstation')]),
+                new TableNode.Row.Cell([new PlainTextNode('1999')])
+              ], new TableNode.Header.Cell([new PlainTextNode('Chrono Cross')]))
+            ])
+        ]))
+    })
+  })
 })
