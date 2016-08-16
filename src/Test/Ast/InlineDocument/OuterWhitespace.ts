b1668b6b074c78955adf2f1161c188e089724254
Daniel Miller <daniel@wants.email>
2016-08-16 07:11:26 -0500

    Add 4 passing tests

---
+++ src/Test/Ast/InlineDocument/OuterWhitespace.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai'
+import Up from'../../../index'
+import { InlineUpDocument } from'../../../SyntaxNodes/InlineUpDocument'
+import { PlainTextNode } from'../../../SyntaxNodes/PlainTextNode'
+
+
+context("In inline documents, all outer whitespace is considered meaningless, even when it's escaped. This includes:", () => {
+  context("Trailing whitespace:", () => {
+    specify('Not escaped', () => {
+      expect(Up.toInlineDocument("I'm just a normal guy who only eats when it's raining. Isn't everyone like that?  \t  \t ")).to.be.eql(
+        new InlineUpDocument([
+          new PlainTextNode("I'm just a normal guy who only eats when it's raining. Isn't everyone like that?")
+        ]))
+    })
+
+    specify('Escaped', () => {
+      expect(Up.toInlineDocument("I'm just a normal guy who only eats when it's raining. Isn't everyone like that?\\ \t  ")).to.be.eql(
+        new InlineUpDocument([
+          new PlainTextNode("I'm just a normal guy who only eats when it's raining. Isn't everyone like that?")
+        ]))
+    })
+
+    specify('Both escaped and not escaped', () => {
+      expect(Up.toInlineDocument("I'm just a normal guy who only eats when it's raining. Isn't everyone like that? \t \\ \\\t  \t ")).to.be.eql(
+        new InlineUpDocument([
+          new PlainTextNode("I'm just a normal guy who only eats when it's raining. Isn't everyone like that?")
+        ]))
+    })
+
+    specify('Both escaped and not escaped, all following a backslash itself following an escaped backslash', () => {
+      expect(Up.toInlineDocument("I'm just a normal guy who only eats when it's raining. Isn't everyone like that?\\\\\\  \t \\ \\\t  \t ")).to.be.eql(
+        new InlineUpDocument([
+          new PlainTextNode("I'm just a normal guy who only eats when it's raining. Isn't everyone like that?\\")
+        ]))
+    })
+  })
+})
