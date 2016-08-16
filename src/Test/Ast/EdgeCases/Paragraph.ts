4abdbe9e459c107de60e80e17175e401080484ea
Daniel Miller <daniel@wants.email>
2016-08-16 05:53:43 -0500

    Organize new tests into context

--- src/Test/Ast/EdgeCases/Paragraph.ts
+++ src/Test/Ast/EdgeCases/Paragraph.ts
@@ -69,8 +69,8 @@ You'll never believe this fake evidence!
 })
 
 
-describe('A paragraph directly followed by several (non-indented) lines consisting solely of escaped whitespace', () => {
-  it('produces a paragraph node', () => {
+context('Normally, consecutive non-blank lines produce a line block. However, if all but one of the lines consist solely of escaped whitespace, a paragraph is produced instead. This includes when:', () => {
+  specify('The blank lines are all trailing, and none of them are indented', () => {
     const markup = `
 You'll never believe this fake evidence!
 \\   \\  \t \\\t 
@@ -84,11 +84,8 @@ You'll never believe this fake evidence!
         ])
       ]))
   })
-})
 
-
-describe('A paragraph directly following several (non-indented) lines consisting solely of escaped whitespace', () => {
-  it('produces a paragraph node', () => {
+  specify('The blank lines are all leading, and none but the first are indented', () => {
     const markup = `
     \\   \t\\   \\  \t \\\t 
 \\   \\  \t \\\t 
@@ -102,11 +99,8 @@ You'll never believe this fake evidence!`
         ])
       ]))
   })
-})
 
-
-describe('A paragraph sandwiched by several (non-indented) lines consisting solely of escaped whitespace', () => {
-  it('produces a paragraph node', () => {
+  specify('The blank lines surround the paragraph, and none but the first are indented', () => {
     const markup = `
   \\   \t\\   \\  \t \\\t 
 \\   \\  \t \\\t 
