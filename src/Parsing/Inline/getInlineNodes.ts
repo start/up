3752ab0d4d651f2d88bfd4e20ecf5f96d0e8aa39
Daniel Miller <daniel@wants.email>
2016-08-16 17:08:17 -0500

    Remove extraneous comment

--- src/Parsing/Inline/getInlineNodes.ts
+++ src/Parsing/Inline/getInlineNodes.ts
@@ -8,13 +8,6 @@ export function getInlineNodes(markup: string, config: Config): InlineSyntaxNode
   return parse(tokenize(markup, config))
 }
 
-// This function is identical to the `getInlineNodes` function, except footnotes are treated as
-// parentheticals of the appropriate bracket type.
-//
-// In other words:
-//
-// 1. Footnotes produced by square brackets [^ like this] are treated as square-bracket parentheticals.
-// 2. Footnotes produced by parentheses (^ like this) are treated as normal parentheticals.
 export function getInlineNodesForInlineDocument(markup: string, config: Config): InlineSyntaxNode[] {
   return parse(tokenizeForInlineDocument(markup, config))
 }
