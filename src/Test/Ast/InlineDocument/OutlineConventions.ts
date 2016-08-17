4f6b5b1cda03d11ec130440da1edff6385fe4c18
Daniel Miller <daniel@wants.email>
2016-08-16 20:19:24 -0500

    Add 5 passing inline document tests

---
+++ src/Test/Ast/InlineDocument/OutlineConventions.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai'
+import Up from'../../../index'
+import { InlineUpDocument } from'../../../SyntaxNodes/InlineUpDocument'
+import { PlainTextNode } from'../../../SyntaxNodes/PlainTextNode'
+
+
+context('Inline documents completely ignore outline conventions. This includes:', () => {
+  specify('Outline separation streaks', () => {
+    expect(Up.toInlineDocument('#~#~#~#~#')).to.be.eql(
+      new InlineUpDocument([
+        new PlainTextNode('#~#~#~#~#')
+      ]))
+  })
+
+  specify('Ordered lists', () => {
+    expect(Up.toInlineDocument('1) I agree.')).to.be.eql(
+      new InlineUpDocument([
+        new PlainTextNode('1) I agree.')
+      ]))
+  })
+
+  specify('Unordered lists', () => {
+    expect(Up.toInlineDocument('* Prices and participation may vary')).to.be.eql(
+      new InlineUpDocument([
+        new PlainTextNode('* Prices and participation may vary')
+      ]))
+  })
+
+  specify('Blockquotes', () => {
+    expect(Up.toInlineDocument('> o_o <')).to.be.eql(
+      new InlineUpDocument([
+        new PlainTextNode('> o_o <')
+      ]))
+  })
+
+  specify('Code blocks', () => {
+    expect(Up.toInlineDocument('```')).to.be.eql(
+      new InlineUpDocument([
+        new PlainTextNode('```')
+      ]))
+  })
+})
