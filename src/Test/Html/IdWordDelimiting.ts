80e4ca127220cfc713fda692e4c5fba2ed9152cb
Daniel Miller <daniel@wants.email>
2016-08-17 21:09:56 -0500

    Add empty tests for ID word delimiting

---
+++ src/Test/Html/IdWordDelimiting.ts
@@ -0,0 +1,48 @@
+/*import { expect } from 'chai'
+import Up from '../../index'
+import { UpDocument } from '../../SyntaxNodes/UpDocument'
+import { ParagraphNode } from '../../SyntaxNodes/ParagraphNode'
+import { PlainTextNode } from '../../SyntaxNodes/PlainTextNode'
+import { FootnoteNode } from '../../SyntaxNodes/FootnoteNode'
+import { FootnoteBlockNode } from '../../SyntaxNodes/FootnoteBlockNode'
+import { InlineSpoilerNode } from '../../SyntaxNodes/InlineSpoilerNode'
+import { InlineNsfwNode } from '../../SyntaxNodes/InlineNsfwNode'
+import { InlineNsflNode } from '../../SyntaxNodes/InlineNsflNode'
+import { SpoilerBlockNode } from '../../SyntaxNodes/SpoilerBlockNode'
+import { NsfwBlockNode } from '../../SyntaxNodes/NsfwBlockNode'
+import { NsflBlockNode } from '../../SyntaxNodes/NsflBlockNode'*/
+
+
+context('Words within HTML IDs are delimited by hyphens.', () => {
+  context('This applies to terms appearing in IDs:', () => {
+    specify('The "footnote" term', () => {
+    })
+
+    specify('The "footnoteReference" term', () => {
+    })
+  })
+
+
+  context('This applies to the "documentName" configuration setting, which is prefixed to every ID:', () => {
+    specify('The ID of the checkboxes for inline spoilers', () => {
+    })
+
+    specify('The ID of the checkboxes for inline NSFW conventions', () => {
+    })
+
+    specify('The ID of the checkboxes for inline NSFL conventions', () => {
+    })
+
+    specify('The ID of the checkboxes for spoiler blocks', () => {
+    })
+
+    specify('The ID of the checkboxes for NSFW blocks', () => {
+    })
+
+    specify('The ID of the checkboxes for NSFL blocks', () => {
+    })
+
+    specify('The ID of elements referenced by the table of contents', () => {
+    })
+  })
+})
