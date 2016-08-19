bbc642f419e915df8d234bb95b3234201a6b18c3
Daniel Miller <daniel@wants.email>
2016-08-19 00:56:53 -0500

    Remove extraneous file

--- src/SyntaxNodes/EmphasisNode.1.ts
+++
@@ -1,9 +0,0 @@
-import { RichInlineSyntaxNode } from './RichInlineSyntaxNode'
-
-
-// Equivalent to the `<em>` HTML element.
-//
-// Not to be confused with `ItalicNode`! 
-export class Emphasis extends RichInlineSyntaxNode {
-  protected EMPHASIS(): void { }
-}
