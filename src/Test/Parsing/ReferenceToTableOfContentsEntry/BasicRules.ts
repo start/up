ee9ec0d9dc7ae70b87e99066ae286ea50dafcd45
Daniel Miller <daniel@wants.email>
2016-08-22 02:29:49 -0500

    Add passing test

--- src/Test/Parsing/ReferenceToTableOfContentsEntry/BasicRules.ts
+++ src/Test/Parsing/ReferenceToTableOfContentsEntry/BasicRules.ts
@@ -7,8 +7,8 @@ import { PlainText } from '../../../SyntaxNodes/PlainText'
 import { ReferenceToTableOfContentsEntry } from '../../../SyntaxNodes/ReferenceToTableOfContentsEntry'
 
 
-describe('Bracketed text starting with "section:"', () => {
-  specify('produces a reference to a table of contents entry', () => {
+context('Bracketed text starting with "section:" produces a reference to a table of contents entry. Either type of brackets can be used:', () => {
+  specify('Square brackets', () => {
     const markup = `
 I drink soda
 ============
@@ -19,6 +19,39 @@ I never lie
 ===========
 
 Not quite true. For example, see [section: soda].`
+
+    const sodaHeading =
+      new Heading([new PlainText('I drink soda')], { level: 1, ordinalInTableOfContents: 1 })
+
+    const neverLieHeading =
+      new Heading([new PlainText('I never lie')], { level: 1, ordinalInTableOfContents: 2 })
+
+    expect(Up.toDocument(markup)).to.be.eql(
+      new UpDocument([
+        sodaHeading,
+        new Paragraph([
+          new PlainText('Actually, I only drink milk.')
+        ]),
+        neverLieHeading,
+        new Paragraph([
+          new PlainText('Not quite true. For example, see '),
+          new ReferenceToTableOfContentsEntry('soda', sodaHeading),
+          new PlainText('.')
+        ])
+      ], new UpDocument.TableOfContents([sodaHeading, neverLieHeading])))
+  })
+
+  specify('Parenthesis', () => {
+    const markup = `
+I drink soda
+============
+
+Actually, I only drink milk.
+
+I never lie
+===========
+
+Not quite true. For example, see (section: soda).`
 
     const sodaHeading =
       new Heading([new PlainText('I drink soda')], { level: 1, ordinalInTableOfContents: 1 })
