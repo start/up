0e0721f412c2fd4e63ab11c54f35bfe81a7ab526
Daniel Miller <daniel@wants.email>
2016-08-29 01:28:07 -0500

    Add passing tests

--- src/Test/Parsing/EdgeCases/Italics.ts
+++ src/Test/Parsing/EdgeCases/Italics.ts
@@ -3,9 +3,176 @@ import Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
 import { PlainText } from '../../../SyntaxNodes/PlainText'
 import { Italic } from '../../../SyntaxNodes/Italic'
+import { NormalParenthetical } from '../../../SyntaxNodes/NormalParenthetical'
+import { Emphasis } from '../../../SyntaxNodes/Emphasis'
+import { RevisionInsertion } from '../../../SyntaxNodes/RevisionInsertion'
+import { Highlight } from '../../../SyntaxNodes/Highlight'
 
 
-describe('An unmatched underscore', () => {
+context('Within italics, (inner) italics can be the first convention within any other inner convention.', () => {
+  context('The other convention can be (but is not limited to):', () => {
+    specify('Normal parentheticals', () => {
+      expect(Up.toDocument('Luigi stood up. _Hello, my (_leetle_) Mario!_')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Italic([
+            new PlainText('Hello, my '),
+            new NormalParenthetical([
+              new PlainText('('),
+              new Italic([
+                new PlainText('leetle')
+              ]),
+              new PlainText(')')
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+
+    specify('Emphasis', () => {
+      expect(Up.toDocument('Luigi stood up. _Hello, my *_leetle_* Mario!_')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Italic([
+            new PlainText('Hello, my '),
+            new Emphasis([
+              new Italic([
+                new PlainText('leetle')
+              ]),
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+
+    specify('Revision insertion', () => {
+      expect(Up.toDocument('Luigi stood up. _Hello, my ++_leetle_++ Mario!_')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Italic([
+            new PlainText('Hello, my '),
+            new RevisionInsertion([
+              new Italic([
+                new PlainText('leetle')
+              ]),
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+
+    specify('Highlights (even when there is no space after the colon)', () => {
+      expect(Up.toDocument('Luigi stood up. _Hello, my [highlight:_leetle_] Mario!_')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Italic([
+            new PlainText('Hello, my '),
+            new Highlight([
+              new Italic([
+                new PlainText('leetle')
+              ])
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+  })
+
+
+  specify('The inner italics can open immediately after several conventions have just opened', () => {
+    expect(Up.toDocument('Luigi stood up. _Hello, my ++(*_leetle_*)++ Mario!_')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new PlainText('Luigi stood up. '),
+        new Italic([
+          new PlainText('Hello, my '),
+          new RevisionInsertion([
+            new NormalParenthetical([
+              new PlainText('('),
+              new Emphasis([
+                new Italic([
+                  new PlainText('leetle')
+                ])
+              ]),
+              new PlainText(')')
+            ])
+          ]),
+          new PlainText(' Mario!')
+        ])
+      ]))
+  })
+})
+
+
+context('Within italics, (inner) italics can close directly after a convention inside of it has closed.', () => {
+  context('The innermost convention can be (but is not limited to):', () => {
+    specify('Normal parentheticals', () => {
+      expect(Up.toDocument('_Luigi stood up. _Help me find brother (Mario)_, I heard Luigi say._')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Italic([
+            new PlainText('Luigi stood up. '),
+            new Italic([
+              new PlainText('Help me find brother '),
+              new NormalParenthetical([
+                new PlainText('(Mario)'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+
+    specify('Emphasis', () => {
+      expect(Up.toDocument('_Luigi stood up. _Help me find brother *Mario*_, I heard Luigi say._')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Italic([
+            new PlainText('Luigi stood up. '),
+            new Italic([
+              new PlainText('Help me find brother '),
+              new Emphasis([
+                new PlainText('Mario'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+
+    specify('Revision insertion', () => {
+      expect(Up.toDocument('_Luigi stood up. _Help me find brother ++Mario++_, I heard Luigi say._')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Italic([
+            new PlainText('Luigi stood up. '),
+            new Italic([
+              new PlainText('Help me find brother '),
+              new RevisionInsertion([
+                new PlainText('Mario'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+
+    specify('Highlights (even when there is no space after the colon)', () => {
+      expect(Up.toDocument('_Luigi stood up. _Help me find brother [highlight:Mario]_, I heard Luigi say._')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Italic([
+            new PlainText('Luigi stood up. '),
+            new Italic([
+              new PlainText('Help me find brother '),
+              new Highlight([
+                new PlainText('Mario'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+  })
+})
+
+
+describe('An unmatched opening underscore', () => {
   it('does not create an italic node', () => {
     expect(Up.toDocument('Hello, _world!')).to.deep.equal(
       insideDocumentAndParagraph([
