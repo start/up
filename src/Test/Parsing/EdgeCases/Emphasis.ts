8607bd0a365771d6f137826ff07c33d4989c9205
Daniel Miller <daniel@wants.email>
2016-08-29 01:41:49 -0500

    Add passing tests

--- src/Test/Parsing/EdgeCases/Emphasis.ts
+++ src/Test/Parsing/EdgeCases/Emphasis.ts
@@ -1,11 +1,178 @@
 import { expect } from 'chai'
 import Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
-import { PlainText } from '../../../SyntaxNodes/PlainText'
 import { Emphasis } from '../../../SyntaxNodes/Emphasis'
+import { PlainText } from '../../../SyntaxNodes/PlainText'
+import { Italic } from '../../../SyntaxNodes/Italic'
+import { NormalParenthetical } from '../../../SyntaxNodes/NormalParenthetical'
+import { Highlight } from '../../../SyntaxNodes/Highlight'
+import { RevisionInsertion } from '../../../SyntaxNodes/RevisionInsertion'
 
 
-describe('An unmatched asterisk', () => {
+context('Within emphasis, (inner) emphasis can be the first convention within any other inner convention.', () => {
+  context('The other convention can be (but is not limited to):', () => {
+    specify('Normal parentheticals', () => {
+      expect(Up.toDocument('Luigi stood up. *Hello, my (*leetle*) Mario!*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Emphasis([
+            new PlainText('Hello, my '),
+            new NormalParenthetical([
+              new PlainText('('),
+              new Emphasis([
+                new PlainText('leetle')
+              ]),
+              new PlainText(')')
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+
+    specify('Italics', () => {
+      expect(Up.toDocument('Luigi stood up. *Hello, my _*leetle*_ Mario!*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Emphasis([
+            new PlainText('Hello, my '),
+            new Italic([
+              new Emphasis([
+                new PlainText('leetle')
+              ]),
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+
+    specify('Revision insertion', () => {
+      expect(Up.toDocument('Luigi stood up. *Hello, my ++*leetle*++ Mario!*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Emphasis([
+            new PlainText('Hello, my '),
+            new RevisionInsertion([
+              new Emphasis([
+                new PlainText('leetle')
+              ]),
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+
+    specify('Highlights (even when there is no space after the colon)', () => {
+      expect(Up.toDocument('Luigi stood up. *Hello, my [highlight:*leetle*] Mario!*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new PlainText('Luigi stood up. '),
+          new Emphasis([
+            new PlainText('Hello, my '),
+            new Highlight([
+              new Emphasis([
+                new PlainText('leetle')
+              ])
+            ]),
+            new PlainText(' Mario!')
+          ])
+        ]))
+    })
+  })
+
+
+  specify('The inner emphasis can open immediately after several conventions have just opened', () => {
+    expect(Up.toDocument('Luigi stood up. *Hello, my ++(_*leetle*_)++ Mario!*')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new PlainText('Luigi stood up. '),
+        new Emphasis([
+          new PlainText('Hello, my '),
+          new RevisionInsertion([
+            new NormalParenthetical([
+              new PlainText('('),
+              new Italic([
+                new Emphasis([
+                  new PlainText('leetle')
+                ])
+              ]),
+              new PlainText(')')
+            ])
+          ]),
+          new PlainText(' Mario!')
+        ])
+      ]))
+  })
+})
+
+
+context('Within emphasis, (inner) emphasis can close directly after a convention inside of it has closed.', () => {
+  context('The innermost convention can be (but is not limited to):', () => {
+    specify('Normal parentheticals', () => {
+      expect(Up.toDocument('*Luigi stood up. *Help me find brother (Mario)*, I heard Luigi say.*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Emphasis([
+            new PlainText('Luigi stood up. '),
+            new Emphasis([
+              new PlainText('Help me find brother '),
+              new NormalParenthetical([
+                new PlainText('(Mario)'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+
+    specify('Italics', () => {
+      expect(Up.toDocument('*Luigi stood up. *Help me find brother _Mario_*, I heard Luigi say.*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Emphasis([
+            new PlainText('Luigi stood up. '),
+            new Emphasis([
+              new PlainText('Help me find brother '),
+              new Italic([
+                new PlainText('Mario'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+
+    specify('Revision insertion', () => {
+      expect(Up.toDocument('*Luigi stood up. *Help me find brother ++Mario++*, I heard Luigi say.*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Emphasis([
+            new PlainText('Luigi stood up. '),
+            new Emphasis([
+              new PlainText('Help me find brother '),
+              new RevisionInsertion([
+                new PlainText('Mario'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+
+    specify('Highlights (even when there is no space after the colon)', () => {
+      expect(Up.toDocument('*Luigi stood up. *Help me find brother [highlight:Mario]*, I heard Luigi say.*')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new Emphasis([
+            new PlainText('Luigi stood up. '),
+            new Emphasis([
+              new PlainText('Help me find brother '),
+              new Highlight([
+                new PlainText('Mario'),
+              ]),
+            ]),
+            new PlainText(', I heard Luigi say.')
+          ])
+        ]))
+    })
+  })
+})
+
+
+describe('An unmatched opening asterisk', () => {
   it('does not create an emphasis node', () => {
     expect(Up.toDocument('Hello, *world!')).to.deep.equal(
       insideDocumentAndParagraph([
