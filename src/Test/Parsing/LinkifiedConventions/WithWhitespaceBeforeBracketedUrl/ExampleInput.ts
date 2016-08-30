07b05eafc0cdd674841e9445eab42675b235cec6
Daniel Miller <daniel@wants.email>
2016-08-30 14:13:41 -0500

    [Broken] Add more linkified example input tests

---
+++ src/Test/Parsing/LinkifiedConventions/WithWhitespaceBeforeBracketedUrl/ExampleInput.ts
@@ -0,0 +1,511 @@
+import { expect } from 'chai'
+import Up from '../../../../index'
+import { insideDocumentAndParagraph, expectEveryPermutationOfBrackets } from '../../Helpers'
+import { UpDocument } from '../../../../SyntaxNodes/UpDocument'
+import { Link } from '../../../../SyntaxNodes/Link'
+import { PlainText } from '../../../../SyntaxNodes/PlainText'
+import { NormalParenthetical } from '../../../../SyntaxNodes/NormalParenthetical'
+import { ExampleInput } from '../../../../SyntaxNodes/ExampleInput'
+
+
+const WITH_AND_WITHOUT_WHITESPACE = ['', ' \t\t ']
+
+
+context('A linkified example input convention can have whitespace between itself and its linkifying URL, but only if the linkifying URL satisfies one of the following conditions:', () => {
+  specify('It has a scheme', () => {
+    expectEveryPermutationOfBrackets({
+      precededBy: '{ something terrible }',
+      bracketedSegments: [{
+        prefixes: WITH_AND_WITHOUT_WHITESPACE,
+        text: 'app:wiki/terrible-thing'
+      }],
+      toProduce: new UpDocument([
+        new Link([
+          new ExampleInput('something terrible')
+        ], 'app:wiki/terrible-thing')
+      ])
+    })
+  })
+
+
+  context('When the URL has a scheme', () => {
+    specify('the top-level domain may be followed by a slash and a resource path', () => {
+      expectEveryPermutationOfBrackets({
+        precededBy: '{ Advance Wars }',
+        bracketedSegments: [{
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          text: 'http://advancewars.wikia.com/wiki/Advance_Wars_(game)'
+        }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('Advance Wars')
+          ], 'http://advancewars.wikia.com/wiki/Advance_Wars_(game)')
+        ])
+      })
+    })
+
+    specify('the URL must not contain any spaces', () => {
+      expect(Up.toDocument('{ Ask Question } (https://stackoverflow.com is nice)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('Ask Question'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('('),
+            new Link([
+              new PlainText('stackoverflow.com')
+            ], 'https://stackoverflow.com'),
+            new PlainText(' is nice)')
+          ]),
+        ]))
+    })
+
+    specify('there must be somethng after the scheme', () => {
+      expect(Up.toDocument('{ Send Email } (mailto:)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('Send Email'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(mailto:)')
+          ]),
+        ]))
+    })
+
+    specify('there must be somethng after the scheme beyond only slashes', () => {
+      expect(Up.toDocument('{ Rustle Files } (https://example.com/sounds.ogg) (file:///)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('Rustle Files'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(file:///)')
+          ]),
+        ]))
+    })
+
+    specify('the rest of the URL can consist solely of digits', () => {
+      expectEveryPermutationOfBrackets({
+        precededBy: '{ Place Call }',
+        bracketedSegments: [{
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          text: 'tel:5555555555'
+        }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('spooky phone call')
+          ], 'tel:5555555555')
+        ])
+      })
+    })
+
+    specify('the scheme must not be escaped', () => {
+      expect(Up.toDocument('[audio: email sending] (https://example.com/sounds.ogg) (\\mailto:daniel@wants.email)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('email sending'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(mailto:daniel@wants.email)')
+          ]),
+        ]))
+    })
+  })
+
+
+  specify('It starts with a slash', () => {
+    expectEveryPermutationOfBrackets({
+      bracketedSegments: [
+        { text: 'audio: something terrible' },
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          text: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          text: '/wiki/something-terrible'
+        }],
+      toProduce: new UpDocument([
+        new Link([
+          new ExampleInput('something terrible')
+        ], '/wiki/something-terrible')
+      ])
+    })
+  })
+
+
+  describe('When the URL starts with a slash, the URL', () => {
+    it('must not contain any spaces', () => {
+      expect(Up.toDocument('[audio: something terrible](https://example.com/sounds.ogg) (/r9k/ created it)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('something terrible'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(/r9k/ created it)')
+          ]),
+        ]))
+    })
+
+    it('must have something after the slash', () => {
+      expect(Up.toDocument('[audio: slash] (https://example.com/sounds.ogg) (/)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('slash'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(/)')
+          ]),
+        ]))
+    })
+
+    it('can consist solely of digits after the slash', () => {
+      expectEveryPermutationOfBrackets({
+        bracketedSegments: [
+          { text: 'audio: Model 3 theft' },
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            text: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            text: '/3'
+          }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('Model 3 theft')
+          ], '/3')
+        ])
+      })
+    })
+
+    it('must not have its slash escaped', () => {
+      expect(Up.toDocument('[audio: robots](https://example.com/sounds.ogg) (\\/r9k/)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('robots'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(/r9k/)')
+          ]),
+        ]))
+    })
+  })
+
+
+  specify('It starts with a hash mark ("#")', () => {
+    expectEveryPermutationOfBrackets({
+      bracketedSegments: [
+        { text: 'audio: something terrible' },
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          text: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          text: '#wiki/something-terrible'
+        }],
+      toProduce: new UpDocument([
+        new Link([
+          new ExampleInput('something terrible')
+        ], '#wiki/something-terrible')
+      ])
+    })
+  })
+
+
+  describe('When the URL starts with a hash mark ("#"), the URL', () => {
+    it('may consist solely of digits after the hask mark', () => {
+      expectEveryPermutationOfBrackets({
+        bracketedSegments: [
+          { text: 'audio: Model 3 theft' },
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            text: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            text: '#3'
+          }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('Model 3 theft')
+          ], '#3')
+        ])
+      })
+    })
+
+    it('must have something after the hash mark', () => {
+      expect(Up.toDocument('[audio: hash marking](https://example.com/sounds.ogg) (#)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('hash marking'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(#)')
+          ]),
+        ]))
+    })
+
+    it('must not contain any spaces', () => {
+      expect(Up.toDocument('[audio: something terrible] (https://example.com/sounds.ogg) (#starcraft2 was never trending)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('something terrible'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(#starcraft2 was never trending)')
+          ]),
+        ]))
+    })
+
+    it('must not have its hashmark escaped', () => {
+      expect(Up.toDocument('[audio: hash marking](https://example.com/sounds.ogg) (\\#starcraft2)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('hash marking'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(#starcraft2)')
+          ]),
+        ]))
+    })
+  })
+
+
+  specify('It has a subdomain and a top-level domain', () => {
+    expectEveryPermutationOfBrackets({
+      bracketedSegments: [
+        { text: 'audio: Chrono Trigger' },
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          text: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          text: 'https://chrono-trigger.wiki'
+        }],
+      toProduce: new UpDocument([
+        new Link([
+          new ExampleInput('Chrono Trigger')
+        ], 'https://chrono-trigger.wiki')
+      ])
+    })
+  })
+
+
+  describe('When the URL merely has a subdomain and a top-level domain', () => {
+    specify('the top-level domain may be followed by a slash and a resource path', () => {
+      expectEveryPermutationOfBrackets({
+        bracketedSegments: [
+          { text: 'audio: Advance Wars' },
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            text: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            text: 'advancewars.wikia.com/wiki/Advance_Wars_(game)'
+          }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('Advance Wars')
+          ], 'https://advancewars.wikia.com/wiki/Advance_Wars_(game)')
+        ])
+      })
+    })
+
+    specify('the top-level domain may be followed by a slash and no resource path', () => {
+      expectEveryPermutationOfBrackets({
+        bracketedSegments: [
+          { text: 'audio: Advance Wars' },
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            text: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            text: 'advancewars.wikia.com/'
+          }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('Advance Wars')
+          ], 'https://advancewars.wikia.com/')
+        ])
+      })
+    })
+
+    specify('the top-level domain may not be followed by any character other than a forward slash', () => {
+      expect(Up.toDocument('[audio: 4chan] (https://example.com/sounds.ogg) (4chan.org-terrifying)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('4chan'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(4chan.org-terrifying)')
+          ]),
+        ]))
+    })
+
+    specify('all domains before the top-level domain may consist solely of digits', () => {
+      expectEveryPermutationOfBrackets({
+        bracketedSegments: [
+          { text: 'audio: good luck' },
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            text: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            text: '88.8888.cn'
+          }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('good luck')
+          ], 'https://88.8888.cn')
+        ])
+      })
+    })
+
+    context('The top-level domain must contain only letters', () => {
+      specify('No numbers', () => {
+        expect(Up.toDocument('[audio: usernaming](https://example.com/sounds.ogg) (john.e.smith5)')).to.deep.equal(
+          insideDocumentAndParagraph([
+            new ExampleInput('usernaming'),
+            new PlainText(' '),
+            new NormalParenthetical([
+              new PlainText('(john.e.smith5)')
+            ]),
+          ]))
+      })
+
+      specify('No hyphens', () => {
+        expect(Up.toDocument('[audio: usernaming] (https://example.com/sounds.ogg) (john.e.smith-kline)')).to.deep.equal(
+          insideDocumentAndParagraph([
+            new ExampleInput('usernaming'),
+            new PlainText(' '),
+            new NormalParenthetical([
+              new PlainText('(john.e.smith-kline)')
+            ]),
+          ]))
+      })
+    })
+
+    specify('the URL must start with a letter or a number, not a period', () => {
+      expect(Up.toDocument('[audio: being British](https://example.com/sounds.ogg) (.co.uk)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('being British'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(.co.uk)')
+          ]),
+        ]))
+    })
+
+    specify('the URL must not have consecutive periods before the top-level domain', () => {
+      expect(Up.toDocument('[audio: Ash claiming to be his own father] (https://example.com/sounds.ogg) (um..uh)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('Ash claiming to be his own father'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(um…uh)')
+          ]),
+        ]))
+    })
+
+    specify('the URL must not have consecutive periods directly after the top-level domain before the slash that indicates the start of the resource path', () => {
+      expect(Up.toDocument('[audio: debilitating sadness](https://example.com/sounds.ogg) (4chan.org../r9k/)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('debilitating sadness'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(4chan.org…/r9k/)')
+          ]),
+        ]))
+    })
+
+    specify('the URL may have consecutive periods after the slash that indicates the start of the resource path', () => {
+      expectEveryPermutationOfBrackets({
+        bracketedSegments: [
+          { text: 'audio: rocket ship' },
+          {
+            prefixes: WITH_AND_WITHOUT_WHITESPACE,
+            text: 'https://example.com/sounds.ogg'
+          },
+          {
+            prefixes: WITH_WHITESPACE,
+            text: 'example.com/321...blastoff/1'
+          }],
+        toProduce: new UpDocument([
+          new Link([
+            new ExampleInput('rocket ship')
+          ], 'https://example.com/321...blastoff/1')
+        ])
+      })
+    })
+
+    specify('the URL must not contain any spaces', () => {
+      expect(Up.toDocument('[audio: yeah] (https://example.com/sounds.ogg) (ign.com had some hilarious forums)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('yeah'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(ign.com had some hilarious forums)')
+          ]),
+        ]))
+    })
+
+    specify('the domain part must not be escaped', () => {
+      expect(Up.toDocument('[audio: yeah](https://example.com/sounds.ogg) (\\ign.com)')).to.deep.equal(
+        insideDocumentAndParagraph([
+          new ExampleInput('yeah'),
+          new PlainText(' '),
+          new NormalParenthetical([
+            new PlainText('(ign.com)')
+          ]),
+        ]))
+    })
+  })
+
+
+  specify('If none of the conditions are satisfied, the audio convention is not linkified', () => {
+    expect(Up.toDocument('[audio: something terrible] (https://example.com/sounds.ogg) (really)')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new ExampleInput('something terrible'),
+        new PlainText(' '),
+        new NormalParenthetical([
+          new PlainText('(really)')
+        ]),
+      ]))
+  })
+})
+
+
+describe("If there is whitespace between an otherwise-valid linkified audio convention's audio URL and its linkifying URL", () => {
+  it('the audio convention is not linkified', () => {
+    expect(Up.toDocument('[audio: something terrible](https://example.com/sounds.ogg)  \\  (https://example.com)')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new ExampleInput('something terrible'),
+        new PlainText('    '),
+        new NormalParenthetical([
+          new PlainText('('),
+          new Link([
+            new PlainText('example.com')
+          ], 'https://example.com'),
+          new PlainText(')')
+        ])
+      ]))
+  })
+})
+
+
+describe("An audio convention's linkifying URL, when separated from its audio URL by whitespace,", () => {
+  it('can itself contain whitespace if each whitespace character is escaped', () => {
+    expectEveryPermutationOfBrackets({
+      bracketedSegments: [
+        { text: 'audio: something terrible' },
+        {
+          prefixes: WITH_AND_WITHOUT_WHITESPACE,
+          text: 'https://example.com/sounds.ogg'
+        },
+        {
+          prefixes: WITH_WHITESPACE,
+          text: 'stackoverflow.com/search=something\\ very\\ terrible'
+        }],
+      toProduce: new UpDocument([
+        new Link([
+          new ExampleInput('something terrible')
+        ], 'https://stackoverflow.com/search=something very terrible')
+      ])
+    })
+  })
+})
