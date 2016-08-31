9a461420d781825526b1c06a8ae4e357706ab136
Daniel Miller <daniel@wants.email>
2016-08-31 15:53:39 -0500

    Add passing test

--- src/Test/Parsing/ReferenceToTableOfContentsEntry/WithinOutlineConventions.ts
+++ src/Test/Parsing/ReferenceToTableOfContentsEntry/WithinOutlineConventions.ts
@@ -705,4 +705,41 @@ Not quite true.
         ])
       ], new UpDocument.TableOfContents([sodaHeading, neverLieHeading])))
   })
+
+
+  context('Headings:', () => {
+    specify('When a previous heading contains the snippet', () => {
+      const markup = `
+I drink soda
+============
+
+Actually, I only drink milk.
+
+I never lie. See [topic: soda]
+==============================
+
+Not quite true.`
+
+      const sodaHeading =
+        new Heading([new PlainText('I drink soda')], { level: 1, ordinalInTableOfContents: 1 })
+
+      const neverLieHeading =
+        new Heading([
+          new PlainText('I never lie. See '),
+          new ReferenceToTableOfContentsEntry('soda', sodaHeading),
+        ], { level: 1, ordinalInTableOfContents: 2 })
+
+      expect(Up.toDocument(markup)).to.deep.equal(
+        new UpDocument([
+          sodaHeading,
+          new Paragraph([
+            new PlainText('Actually, I only drink milk.')
+          ]),
+          neverLieHeading,
+          new Paragraph([
+            new PlainText('Not quite true.')
+          ])
+        ], new UpDocument.TableOfContents([sodaHeading, neverLieHeading])))
+    })
+  })
 })
