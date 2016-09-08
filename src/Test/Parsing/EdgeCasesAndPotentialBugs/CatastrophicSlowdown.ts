83c21488b36ea8fd65de0678ee02977a9b2dc811
Daniel Miller <daniel@wants.email>
2016-09-07 20:40:03 -0500

    Add passing test

--- src/Test/Parsing/EdgeCasesAndPotentialBugs/CatastrophicSlowdown.ts
+++ src/Test/Parsing/EdgeCasesAndPotentialBugs/CatastrophicSlowdown.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import Up from '../../../index'
 import { insideDocumentAndParagraph } from '../Helpers'
 import { repeat } from '../../../StringHelpers'
+import { Link } from '../../../SyntaxNodes/Link'
 import { PlainText } from '../../../SyntaxNodes/PlainText'
 
 
@@ -13,7 +14,16 @@ context('A long string of whitespace should never cause cause the parser to hang
   specify('Between words', () => {
     expect(Up.parseDocument('Hear' + lotsOfWhitespace + 'me?')).to.deep.equal(
       insideDocumentAndParagraph([
-          new PlainText('Hear' + lotsOfWhitespace + 'me?')
+        new PlainText('Hear' + lotsOfWhitespace + 'me?')
+      ]))
+  })
+
+  specify("Between a link's bracketed content and its bracketed URL", () => {
+    expect(Up.parseDocument('[Hear me?]' + lotsOfWhitespace + '(example.com)')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new Link([
+          new PlainText('Hear me?')
+        ], 'https://example.com')
       ]))
   })
 })
