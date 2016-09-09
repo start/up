d1d494f61233585301994a2ff86252f181a79cff
Daniel Miller <daniel@wants.email>
2016-09-09 02:35:10 -0500

    Clarify HTML escaping helpers

--- src/Rendering/Html/EscapingHelpers.ts
+++ src/Rendering/Html/EscapingHelpers.ts
@@ -2,15 +2,17 @@ export function escapeHtmlContent(content: string): string {
   return htmlEscape(content, /[&<]/g)
 }
 
-export function escapeHtmlAttrValue(attrValue: any): string {
+export function escapeHtmlAttrValue(attrValue: string | number): string {
   return htmlEscape(String(attrValue), /[&"]/g)
 }
 
 function htmlEscape(html: string, charsToEscape: RegExp): string {
-  return html.replace(charsToEscape, char => ESCAPED_HTML_ENTITIES_BY_CHAR[char])
+  return html.replace(
+    charsToEscape,
+    char => ESCAPED_HTML_ENTITIES_BY_CHAR[char])
 }
 
-const ESCAPED_HTML_ENTITIES_BY_CHAR: { [entity: string]: string } = {
+const ESCAPED_HTML_ENTITIES_BY_CHAR: { [char: string]: string } = {
   '&': '&amp;',
   '<': '&lt;',
   '"': '&quot;'
