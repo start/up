c799a1330987497227cb67613d7cfc96a5d78068
Daniel Miller <daniel@wants.email>
2016-09-10 14:42:24 -0500

    Add 2 passing tests

--- src/Test/Html/FromMarkup.ts
+++ src/Test/Html/FromMarkup.ts
@@ -3,8 +3,65 @@ import Up from '../../index'
 
 
 context("You can render HTML directly from markup.", () => {
-  describe('The parseAndRender method', () => {
-    specify('Can be used with settings', () => {
+  context('The parseAndRender method:', () => {
+    specify('Can be used with parsing settings', () => {
+      const markup = `
+Anyway, let us get to the point.
+
+I enjoy apples
+==============
+
+LOOK AWAY
+  After beating the Elite Four, Blue steals a Red Delicious from Red.`
+
+      const html = Up.parseAndRender(markup, {
+        parsing: {
+          createSourceMap: true,
+          terms: { spoiler: 'LOOK AWAY' }
+        }
+      })
+
+      expect(html).to.equal(
+        '<p data-up-source-line="2">Anyway, let us get to the point.</p>'
+        + '<h1 data-up-source-line="4" id="up-topic-1">I enjoy apples</h1>'
+        + '<div class="up-spoiler up-revealable" data-up-source-line="7">'
+        + '<label for="up-spoiler-1">toggle spoiler</label>'
+        + '<input id="up-spoiler-1" role="button" type="checkbox">'
+        + '<div role="alert">'
+        + '<p data-up-source-line="8">After beating the Elite Four, Blue steals a Red Delicious from Red.</p>'
+        + '</div>'
+        + '</div>')
+    })
+
+    specify('Can be used with rendering settings', () => {
+      const markup = `
+Anyway, let us get to the point.
+
+I enjoy apples
+==============
+
+SPOILER
+  After beating the Elite Four, Blue steals a Red Delicious from Red.`
+
+      const html = Up.parseAndRender(markup, {
+        rendering: {
+          idPrefix: 'article'
+        }
+      })
+
+      expect(html).to.equal(
+        '<p>Anyway, let us get to the point.</p>'
+        + '<h1 id="article-topic-1">I enjoy apples</h1>'
+        + '<div class="up-spoiler up-revealable">'
+        + '<label for="article-spoiler-1">toggle spoiler</label>'
+        + '<input id="article-spoiler-1" role="button" type="checkbox">'
+        + '<div role="alert">'
+        + '<p>After beating the Elite Four, Blue steals a Red Delicious from Red.</p>'
+        + '</div>'
+        + '</div>')
+    })
+
+    specify('Can be used with parsing and rendering settings together', () => {
       const markup = `
 Anyway, let us get to the point.
 
