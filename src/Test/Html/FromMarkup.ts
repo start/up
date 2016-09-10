573952e854b68dbd36cdcdd97f5941d796573ebe
Daniel Miller <daniel@wants.email>
2016-09-10 14:12:30 -0500

    Tweak test; add failing test

--- src/Test/Html/FromMarkup.ts
+++ src/Test/Html/FromMarkup.ts
@@ -2,9 +2,10 @@ import { expect } from 'chai'
 import Up from '../../index'
 
 
-context("You can produce HTML directly from markup!", () => {
-  specify('If you provide the render method with markup, it (internally) calls the parse method for you using configuration you provide', () => {
-    const markup = `
+context("You can render HTML directly from markup.", () => {
+  describe('The parseAndRenderMathod', () => {
+    specify('Can be used with settings', () => {
+      const markup = `
 Anyway, let us get to the point.
 
 I enjoy apples
@@ -13,26 +14,49 @@ I enjoy apples
 LOOK AWAY
   After beating the Elite Four, Blue steals a Red Delicious from Red.`
 
-    const config = {
-      parsing: {
-        createSourceMap: true,
-        terms: { spoiler: 'LOOK AWAY' }
-      },
-      rendering: {
-        terms: { tableOfContents: 'In This Article' }
+      const config = {
+        parsing: {
+          createSourceMap: true,
+          terms: { spoiler: 'LOOK AWAY' }
+        },
+        rendering: {
+          idPrefix: 'article'
+        }
       }
-    }
 
-    expect(Up.parseAndRender(markup, config)).to.equal(
-      '<p data-up-source-line="2">Anyway, let us get to the point.</p>'
-      + '<h1 data-up-source-line="4" id="up-topic-1">I enjoy apples</h1>'
-      + '<div class="up-spoiler up-revealable" data-up-source-line="7">'
-      + '<label for="up-spoiler-1">toggle spoiler</label>'
-      + '<input id="up-spoiler-1" role="button" type="checkbox">'
-      + '<div role="alert">'
-      + '<p data-up-source-line="8">After beating the Elite Four, Blue steals a Red Delicious from Red.</p>'
-      + '</div>'
-      + '</div>')
+      expect(Up.parseAndRender(markup, config)).to.equal(
+        '<p data-up-source-line="2">Anyway, let us get to the point.</p>'
+        + '<h1 data-up-source-line="4" id="article-topic-1">I enjoy apples</h1>'
+        + '<div class="up-spoiler up-revealable" data-up-source-line="7">'
+        + '<label for="article-spoiler-1">toggle spoiler</label>'
+        + '<input id="article-spoiler-1" role="button" type="checkbox">'
+        + '<div role="alert">'
+        + '<p data-up-source-line="8">After beating the Elite Four, Blue steals a Red Delicious from Red.</p>'
+        + '</div>'
+        + '</div>')
+    })
+
+    specify('Can be used without settings', () => {
+      const markup = `
+Anyway, let us get to the point.
+
+I enjoy apples
+==============
+
+SPOILER
+  After beating the Elite Four, Blue steals a Red Delicious from Red.`
+
+      expect(Up.parseAndRender(markup)).to.equal(
+        '<p>Anyway, let us get to the point.</p>'
+        + '<h1 id="up-topic-1">I enjoy apples</h1>'
+        + '<div class="up-spoiler up-revealable">'
+        + '<label for="up-spoiler-1">toggle spoiler</label>'
+        + '<input id="up-spoiler-1" role="button" type="checkbox">'
+        + '<div role="alert">'
+        + '<p>After beating the Elite Four, Blue steals a Red Delicious from Red.</p>'
+        + '</div>'
+        + '</div>')
+    })
   })
 
   specify('If you provide the renderDocumentAndTableOfContents method with markup, it (internally) calls the parse method for you using configuration you provide', () => {
