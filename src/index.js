7308c267d284aeb6c343a9afd1d7cdf54ea27392
Daniel Miller <daniel@wants.email>
2016-09-12 00:27:44 -0500

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,53 @@
 "use strict";
 
+const EXPORTED_SYNTAX_NODE_CLASS_NAMES = [
+  'Document',
+  'InlineDocument',
+  'Audio',
+  'Bold',
+  'Blockquote',
+  'CodeBlock',
+  'DescriptionList',
+  'Emphasis',
+  'ExampleInput',
+  'FootnoteBlock',
+  'Footnote',
+  'Heading',
+  'Highlight',
+  'Image',
+  'InlineCode',
+  'InlineNsfl',
+  'InlineNsfw',
+  'InlineSpoiler',
+  'InlineQuote',
+  'Italic',
+  'LineBlock',
+  'Link',
+  'NsflBlock',
+  'NsfwBlock',
+  'OrderedList',
+  'Paragraph',
+  'NormalParenthetical',
+  'PlainText',
+  'SectionLink',
+  'SpoilerBlock',
+  'SquareParenthetical',
+  'Stress',
+  'Table',
+  'ThematicBreak',
+  'UnorderedList',
+  'Video',
+  'InlineSyntaxNodeContainer',
+  'MediaSyntaxNode',
+  'OutlineSyntaxNodeContainer',
+  'RevealableInlineSyntaxNode',
+  'RevealableOutlineSyntaxNode',
+  'RichInlineSyntaxNode',
+  'RichOutlineSyntaxNode',
+]
+
 module.exports = require('./Up').Up
 
-module.exports.Document = require('./SyntaxNodes/Document').Document
-module.exports.InlineDocument = require('./SyntaxNodes/InlineDocument').InlineDocument
-
-module.exports.Audio = require('./SyntaxNodes/Audio').Audio
-module.exports.Bold = require('./SyntaxNodes/Bold').Bold
-module.exports.Blockquote = require('./SyntaxNodes/Blockquote').Blockquote
-module.exports.CodeBlock = require('./SyntaxNodes/CodeBlock').CodeBlock
-module.exports.DescriptionList = require('./SyntaxNodes/DescriptionList').DescriptionList
-module.exports.Emphasis = require('./SyntaxNodes/Emphasis').Emphasis
-module.exports.ExampleInput = require('./SyntaxNodes/ExampleInput').ExampleInput
-module.exports.FootnoteBlock = require('./SyntaxNodes/FootnoteBlock').FootnoteBlock
-module.exports.Footnote = require('./SyntaxNodes/Footnote').Footnote
-module.exports.Heading = require('./SyntaxNodes/Heading').Heading
-module.exports.Highlight = require('./SyntaxNodes/Highlight').Highlight
-module.exports.Image = require('./SyntaxNodes/Image').Image
-module.exports.InlineCode = require('./SyntaxNodes/InlineCode').InlineCode
-module.exports.InlineNsfl = require('./SyntaxNodes/InlineNsfl').InlineNsfl
-module.exports.InlineNsfw = require('./SyntaxNodes/InlineNsfw').InlineNsfw
-module.exports.InlineSpoiler = require('./SyntaxNodes/InlineSpoiler').InlineSpoiler
-module.exports.InlineQuote = require('./SyntaxNodes/InlineQuote').InlineQuote
-module.exports.Italic = require('./SyntaxNodes/Italic').Italic
-module.exports.LineBlock = require('./SyntaxNodes/LineBlock').LineBlock
-module.exports.Link = require('./SyntaxNodes/Link').Link
-module.exports.NsflBlock = require('./SyntaxNodes/NsflBlock').NsflBlock
-module.exports.NsfwBlock = require('./SyntaxNodes/NsfwBlock').NsfwBlock
-module.exports.OrderedList = require('./SyntaxNodes/OrderedList').OrderedList
-module.exports.Paragraph = require('./SyntaxNodes/Paragraph').Paragraph
-module.exports.NormalParenthetical = require('./SyntaxNodes/NormalParenthetical').NormalParenthetical
-module.exports.PlainText = require('./SyntaxNodes/PlainText').PlainText
-module.exports.SectionLink = require('./SyntaxNodes/SectionLink').SectionLink
-module.exports.SpoilerBlock = require('./SyntaxNodes/SpoilerBlock').SpoilerBlock
-module.exports.SquareParenthetical = require('./SyntaxNodes/SquareParenthetical').SquareParenthetical
-module.exports.Stress = require('./SyntaxNodes/Stress').Stress
-module.exports.Table = require('./SyntaxNodes/Table').Table
-module.exports.ThematicBreak = require('./SyntaxNodes/ThematicBreak').ThematicBreak
-module.exports.UnorderedList = require('./SyntaxNodes/UnorderedList').UnorderedList
-module.exports.Video = require('./SyntaxNodes/Video').Video
-
-module.exports.InlineSyntaxNodeContainer = require('./SyntaxNodes/InlineSyntaxNodeContainer').InlineSyntaxNodeContainer
-module.exports.MediaSyntaxNode = require('./SyntaxNodes/MediaSyntaxNode').MediaSyntaxNode
-module.exports.OutlineSyntaxNodeContainer = require('./SyntaxNodes/OutlineSyntaxNodeContainer').OutlineSyntaxNodeContainer
-module.exports.RevealableInlineSyntaxNode = require('./SyntaxNodes/RevealableInlineSyntaxNode').RevealableInlineSyntaxNode
-module.exports.RevealableOutlineSyntaxNode = require('./SyntaxNodes/RevealableOutlineSyntaxNode').RevealableOutlineSyntaxNode
-module.exports.RichInlineSyntaxNode = require('./SyntaxNodes/RichInlineSyntaxNode').RichInlineSyntaxNode
-module.exports.RichOutlineSyntaxNode = require('./SyntaxNodes/RichOutlineSyntaxNode').RichOutlineSyntaxNode
+for (const className of EXPORTED_SYNTAX_NODE_CLASS_NAMES) {
+  module.exports[className] = require('./SyntaxNodes/' + className)[className]
+}
