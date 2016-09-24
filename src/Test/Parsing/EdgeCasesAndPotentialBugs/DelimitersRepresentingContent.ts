ce0baf08dc514c88ac98e874707caa458349a464
Daniel Miller <daniel@wants.email>
2016-09-24 18:26:29 -0500

    Add 4 passing tests

--- src/Test/Parsing/EdgeCasesAndPotentialBugs/DelimitersRepresentingContent.ts
+++ src/Test/Parsing/EdgeCasesAndPotentialBugs/DelimitersRepresentingContent.ts
@@ -3,7 +3,7 @@ import { insideDocumentAndParagraph } from '../Helpers'
 import * as Up from '../../../index'
 
 
-context('An unmatched left parenthesis within quotation marks produces a quote:', () => {
+context('An unmatched left parenthesis within quotation marks produces an inline quote containing the parenthesis:', () => {
   specify('At the end of a paragraph', () => {
     expect(Up.parse('"("')).to.deep.equal(
       insideDocumentAndParagraph([
@@ -20,6 +20,50 @@ context('An unmatched left parenthesis within quotation marks produces a quote:'
           new Up.Text('"("')
         ]),
         new Up.Text(' is a left parenthesis.')
+      ]))
+  })
+})
+
+
+context('An unmatched left parenthesis within square brackets produces a square parenthetical containing the parenthesis:', () => {
+  specify('At the end of a paragraph', () => {
+    expect(Up.parse('[(]')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new Up.SquareParenthetical([
+          new Up.Text('[(]')
+        ])
+      ]))
+  })
+
+  specify('Before the end of a paragraph', () => {
+    expect(Up.parse('[(] is a left parenthesis.')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new Up.SquareParenthetical([
+          new Up.Text('[(]')
+        ]),
+        new Up.Text(' is a left parenthesis.')
+      ]))
+  })
+})
+
+
+context('An unmatched quotation mark within square brackets produces a square parenthetical containing the quotation mark:', () => {
+  specify('At the end of a paragraph', () => {
+    expect(Up.parse('["]')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new Up.SquareParenthetical([
+          new Up.Text('["]')
+        ])
+      ]))
+  })
+
+  specify('Before the end of a paragraph', () => {
+    expect(Up.parse('["] is a quotation mark.')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new Up.SquareParenthetical([
+          new Up.Text('["]')
+        ]),
+        new Up.Text(' is a quotation mark.')
       ]))
   })
 })
