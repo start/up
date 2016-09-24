a5983047bfec60d8e466fae424f8df9b278d6eb5
Daniel Miller <daniel@wants.email>
2016-09-23 23:50:59 -0500

    Add 2 failing tests

---
+++ src/Test/Parsing/EdgeCasesAndPotentialBugs/DelimitersRepresentingContent.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import { insideDocumentAndParagraph } from '../Helpers'
+import * as Up from '../../../index'
+
+
+
+context('An unmatched left parenthesis within quotation marks produces a quote:', () => {
+  specify('At the end of a paragraph', () => {
+    expect(Up.parse('"("')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new Up.InlineQuote([
+          new Up.Text('"("')
+        ])
+      ]))
+  })
+
+  specify('Before the end of a paragraph', () => {
+    expect(Up.parse('"(" is a left parenthesis.')).to.deep.equal(
+      insideDocumentAndParagraph([
+        new Up.InlineQuote([
+          new Up.Text('(')
+        ]),
+        new Up.Text(' is a left parenthesis')
+      ]))
+  })
+})
