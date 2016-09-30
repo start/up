5092855a3d5552264d2d6f5c52da470267d0dba3
Daniel Miller <daniel@wants.email>
2016-09-29 23:59:32 -0500

    Clarify a couple of comments

--- src/SyntaxNodes/InlineSyntaxNode.ts
+++ src/SyntaxNodes/InlineSyntaxNode.ts
@@ -5,13 +5,14 @@ export interface InlineSyntaxNode extends SyntaxNode {
   // Represents the text of the syntax node as it should appear inline. Some inline conventions
   // don't have any e.g. (footnotes, images).
   //
-  // This method is used help to determine whether table cells are numeric.
+  // This method is ultimately used to help determine whether table cells are numeric.
   textAppearingInline(): string
 
   // Represents the searchable text of the syntax node. In contrast to `textAppearingInline`,
   // footnotes and images do have searchable text (footnotes have content, and images have a
   // description).
   //
-  // This method is used to help section links match the most appropriate table of content entry.
+  // This method is used to help match section links to the most appropriate table of content
+  // entry.
   searchableText(): string
 }
