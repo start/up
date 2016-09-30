1a74019c3d04fdec6201234d58a093730db08f34
Daniel Miller <daniel@wants.email>
2016-09-29 23:28:59 -0500

    Replace block of imports in Renderer

--- src/Rendering/Renderer.ts
+++ src/Rendering/Renderer.ts
@@ -1,36 +1,4 @@
-import { Document } from '../SyntaxNodes/Document'
-import { InlineDocument } from '../SyntaxNodes/InlineDocument'
-import { Link } from '../SyntaxNodes/Link'
-import { Image } from '../SyntaxNodes/Image'
-import { Audio } from '../SyntaxNodes/Audio'
-import { Video } from '../SyntaxNodes/Video'
-import { Text } from '../SyntaxNodes/Text'
-import { Emphasis } from '../SyntaxNodes/Emphasis'
-import { ExampleInput } from '../SyntaxNodes/ExampleInput'
-import { Stress } from '../SyntaxNodes/Stress'
-import { Italics } from '../SyntaxNodes/Italics'
-import { Bold } from '../SyntaxNodes/Bold'
-import { InlineCode } from '../SyntaxNodes/InlineCode'
-import { SectionLink } from '../SyntaxNodes/SectionLink'
-import { NormalParenthetical } from '../SyntaxNodes/NormalParenthetical'
-import { SquareParenthetical } from '../SyntaxNodes/SquareParenthetical'
-import { Highlight } from '../SyntaxNodes/Highlight'
-import { InlineRevealable } from '../SyntaxNodes/InlineRevealable'
-import { RevealableBlock } from '../SyntaxNodes/RevealableBlock'
-import { InlineQuote } from '../SyntaxNodes/InlineQuote'
-import { Footnote } from '../SyntaxNodes/Footnote'
-import { FootnoteBlock } from '../SyntaxNodes/FootnoteBlock'
-import { Table } from '../SyntaxNodes/Table'
-import { Paragraph } from '../SyntaxNodes/Paragraph'
-import { Blockquote } from '../SyntaxNodes/Blockquote'
-import { UnorderedList } from '../SyntaxNodes/UnorderedList'
-import { OrderedList } from '../SyntaxNodes/OrderedList'
-import { DescriptionList } from '../SyntaxNodes/DescriptionList'
-import { LineBlock } from '../SyntaxNodes/LineBlock'
-import { Heading } from '../SyntaxNodes/Heading'
-import { CodeBlock } from '../SyntaxNodes/CodeBlock'
-import { ThematicBreak } from '../SyntaxNodes/ThematicBreak'
-import { SyntaxNode } from '../SyntaxNodes/SyntaxNode'
+import * as Up from '../index'
 import { Settings } from '../Settings'
 import { SOME_WHITESPACE } from '../PatternPieces'
 
@@ -38,49 +6,49 @@ import { SOME_WHITESPACE } from '../PatternPieces'
 export abstract class Renderer {
   constructor(protected settings: Settings.Rendering) { }
 
-  abstract renderDocument(document: Document): string
-  abstract renderInlineDocument(inlineDocument: InlineDocument): string
-  abstract renderTableOfContents(tableOfContents: Document.TableOfContents): string
+  abstract renderDocument(document: Up.Document): string
+  abstract renderInlineDocument(inlineDocument: Up.InlineDocument): string
+  abstract renderTableOfContents(tableOfContents: Up.Document.TableOfContents): string
 
   // Ideally, the following abstract methods wouldn't be public! But for the purpose of
   // double dispatch, they need to be exposed to our syntax node classes. 
 
-  abstract audio(audio: Audio): string
-  abstract bold(bold: Bold): string
-  abstract blockquote(blockquote: Blockquote): string
-  abstract codeBlock(codeBlock: CodeBlock): string
-  abstract descriptionList(list: DescriptionList): string
-  abstract emphasis(emphasis: Emphasis): string
-  abstract exampleInput(exampleInput: ExampleInput): string
-  abstract footnoteBlock(footnoteBlock: FootnoteBlock): string
-  abstract referenceToFootnote(footnote: Footnote): string
-  abstract heading(heading: Heading): string
-  abstract highlight(highlight: Highlight): string
-  abstract image(image: Image): string
-  abstract inlineCode(inlineCode: InlineCode): string
-  abstract inlineQuote(inlineQuote: InlineQuote): string
-  abstract italics(italics: Italics): string
-  abstract lineBlock(lineBlock: LineBlock): string
-  abstract link(link: Link): string
-  abstract orderedList(list: OrderedList): string
-  abstract thematicBreak(thematicBreak: ThematicBreak): string
-  abstract paragraph(paragraph: Paragraph): string
-  abstract normalParenthetical(normalParenthetical: NormalParenthetical): string
-  abstract text(text: Text): string
-  abstract inlineRevealable(inlineRevealable: InlineRevealable): string
-  abstract revealableBlock(revealableBlock: RevealableBlock): string
-  abstract sectionLink(sectionLink: SectionLink): string
-  abstract squareParenthetical(squareParenthetical: SquareParenthetical): string
-  abstract stress(stress: Stress): string
-  abstract table(table: Table): string
-  abstract unorderedList(list: UnorderedList): string
-  abstract video(video: Video): string
+  abstract audio(audio: Up.Audio): string
+  abstract bold(bold: Up.Bold): string
+  abstract blockquote(blockquote: Up.Blockquote): string
+  abstract codeBlock(codeBlock: Up.CodeBlock): string
+  abstract descriptionList(list: Up.DescriptionList): string
+  abstract emphasis(emphasis: Up.Emphasis): string
+  abstract exampleInput(exampleInput: Up.ExampleInput): string
+  abstract footnoteBlock(footnoteBlock: Up.FootnoteBlock): string
+  abstract referenceToFootnote(footnote: Up.Footnote): string
+  abstract heading(heading: Up.Heading): string
+  abstract highlight(highlight: Up.Highlight): string
+  abstract image(image: Up.Image): string
+  abstract inlineCode(inlineCode: Up.InlineCode): string
+  abstract inlineQuote(inlineQuote: Up.InlineQuote): string
+  abstract italics(italics: Up.Italics): string
+  abstract lineBlock(lineBlock: Up.LineBlock): string
+  abstract link(link: Up.Link): string
+  abstract orderedList(list: Up.OrderedList): string
+  abstract thematicBreak(thematicBreak: Up.ThematicBreak): string
+  abstract paragraph(paragraph: Up.Paragraph): string
+  abstract normalParenthetical(normalParenthetical: Up.NormalParenthetical): string
+  abstract text(text: Up.Text): string
+  abstract inlineRevealable(inlineRevealable: Up.InlineRevealable): string
+  abstract revealableBlock(revealableBlock: Up.RevealableBlock): string
+  abstract sectionLink(sectionLink: Up.SectionLink): string
+  abstract squareParenthetical(squareParenthetical: Up.SquareParenthetical): string
+  abstract stress(stress: Up.Stress): string
+  abstract table(table: Up.Table): string
+  abstract unorderedList(list: Up.UnorderedList): string
+  abstract video(video: Up.Video): string
 
-  protected renderEach(nodes: SyntaxNode[]): string[] {
+  protected renderEach(nodes: Up.SyntaxNode[]): string[] {
     return nodes.map(node => node.render(this))
   }
 
-  protected renderAll(nodes: SyntaxNode[]): string {
+  protected renderAll(nodes: Up.SyntaxNode[]): string {
     return this.renderEach(nodes).join('')
   }
 
