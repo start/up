62f599e37b2acdf90266a32eb208d2e7b2983818
Daniel Miller <daniel@wants.email>
2016-10-12 07:35:44 -0500

    Remove unhelpful comments

--- src/SyntaxNodes/InlineRevealable.ts
+++ src/SyntaxNodes/InlineRevealable.ts
@@ -2,7 +2,6 @@ import { RichInlineSyntaxNode } from './RichInlineSyntaxNode'
 import { Renderer } from '../Rendering/Renderer'
 
 
-// A "revealable" convention is one that requires deliberate action from the reader to reveal.
 export class InlineRevealable extends RichInlineSyntaxNode {
   render(renderer: Renderer): string {
     return renderer.inlineRevealable(this)
--- src/SyntaxNodes/RevealableBlock.ts
+++ src/SyntaxNodes/RevealableBlock.ts
@@ -3,12 +3,10 @@ import { Document } from './Document'
 import { Renderer } from '../Rendering/Renderer'
 
 
-// A "revealable" convention is one that requires deliberate action from the reader to reveal.
 export class RevealableBlock extends RichOutlineSyntaxNode {
   render(renderer: Renderer): string {
     return renderer.revealableBlock(this)
   }
-
 
   // As a rule, we don't want to include any revealable (i.e. initially hidden) headings in the
   // table of contents.
