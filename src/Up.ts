de5eefe265198050577ce3e18749f1473646f09f
Daniel Miller <daniel@wants.email>
2016-10-13 07:04:19 -0500

    Update version to 28.0.0

--- src/Up.ts
+++ src/Up.ts
@@ -114,4 +114,4 @@ export { OutlineSyntaxNode } from './SyntaxNodes/OutlineSyntaxNode'
 export { SyntaxNode } from './SyntaxNodes/SyntaxNode'
 
 // This should always match the `version` field in `package.json`.
-export const VERSION = '27.0.0'
+export const VERSION = '28.0.0'
