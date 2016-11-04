66cbd44355b8af5d969d3729b059f0ebb3907587
Daniel Miller <daniel@wants.email>
2016-11-04 15:46:19 -0500

    Tidy up package.json tests

--- verify-package-settings.js
+++ verify-package-settings.js
@@ -1,31 +1,25 @@
 "use strict";
 
-const path = require('path');
 const expect = require('chai').expect
-const packageSettings = require('./package.json')
+const path = require('path');
 
+const packageSettings = require('./package.json')
 const Up = require('./' + packageSettings.main)
 
 
-describe('The `main` field in package.json', () => {
-  it('points to the entry point of the library', () => {
+context('In package.json', () => {
+  specify('the `main` field points to the entry point of the library', () => {
     expect(Up.parseAndRender('It *actually* worked?')).to.equal('<p>It <em>actually</em> worked?</p>')
   })
-})
 
-
-describe('The `typings` field in package.json', () => {
-  it('points to the typings for the entry point of the library', () => {
+  specify('the `typings` field points to the typings for the entry point of the library', () => {
     const typingsBasename = path.basename(packageSettings.typings, '.d.ts')
-    const mainBasename =path.basename(packageSettings.main, '.js')
+    const entryPointBasename = path.basename(packageSettings.main, '.js')
 
-    expect(typingsBasename).to.equal(mainBasename)
+    expect(typingsBasename).to.equal(entryPointBasename)
   })
-})
 
-
-describe('The `version` field in package.json', () => {
-  it('matches the version of the library', () => {
+  specify('the `version` field matches the version of the library', () => {
     expect(packageSettings.version).to.equal(Up.VERSION)
   })
 })
