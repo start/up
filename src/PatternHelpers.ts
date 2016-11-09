dc824a38e5e462a73bb09c83d3f24b0dbeaa334e
Daniel Miller <daniel@wants.email>
2016-11-09 15:02:20 -0600

    Remove unused helper

--- src/PatternHelpers.ts
+++ src/PatternHelpers.ts
@@ -49,11 +49,6 @@ export function anyCharFrom(...chars: string[]): string {
   return anyCharMatching(...chars.map(escapeForRegex))
 }
 
-// Matches any character from the set of `chars`. Does not support patterns.
-export function anyCharBut(...chars: string[]): string {
-  return anyCharNotMatching(...chars.map(escapeForRegex))
-}
-
 export function followedBy(pattern: string): string {
   return `(?=${pattern})`
 }
